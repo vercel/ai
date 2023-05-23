fcca26793437c1d68c2deba719bc98f336e504af
Jared Palmer <jared@jaredpalmer.com>
2023-05-23 16:35:21 -0400

    Run prettier

--- src/HuggingFaceStream.ts
+++ src/HuggingFaceStream.ts
@@ -1,5 +1,5 @@
 // import { type TextGenerationStreamOutput } from "@huggingface/inference";
-import { AIStreamCallbacks } from './AIStream';
+import { AIStreamCallbacks } from "./AIStream";
 
 export function HuggingFaceStream(
   res: AsyncGenerator<any>,
@@ -11,7 +11,7 @@ export function HuggingFaceStream(
   const stream = new ReadableStream({
     async start(controller): Promise<void> {
       for await (const chunk of res) {
-        const text = chunk.token?.text ?? '';
+        const text = chunk.token?.text ?? "";
         // some HF models return generated_text instead of a real ending token
         if (chunk.generated_text != null && chunk.generated_text.length > 0) {
           controller.close();
@@ -20,12 +20,12 @@ export function HuggingFaceStream(
 
         // <|endoftext|> is for https://huggingface.co/OpenAssistant/oasst-sft-4-pythia-12b-epoch-3.5
         // </s> is also often last token in the stream depending on the model
-        if (text !== '</s>' && text !== '<|endoftext|>') {
+        if (text !== "</s>" && text !== "<|endoftext|>") {
           if (counter < 2 && (text.match(/\n/) || []).length) {
             return;
           }
 
-          const queue = encoder.encode(JSON.stringify(text) + '\n');
+          const queue = encoder.encode(JSON.stringify(text) + "\n");
           controller.enqueue(queue);
           counter++;
         } else {
@@ -36,7 +36,7 @@ export function HuggingFaceStream(
     },
   });
 
-  let fullResponse = '';
+  let fullResponse = "";
   const forkedStream = new TransformStream({
     start: async () => {
       if (callbacks?.onStart) {
@@ -46,7 +46,7 @@ export function HuggingFaceStream(
     transform: async (chunk, controller) => {
       controller.enqueue(chunk);
       const item = decoder.decode(chunk);
-      const value = JSON.parse(item.split('\n')[0]);
+      const value = JSON.parse(item.split("\n")[0]);
       if (callbacks?.onToken) {
         await callbacks?.onToken(value);
       }
--- tsup.config.js
+++ tsup.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from 'tsup';
+import { defineConfig } from "tsup";
 
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
-  entry: ['src/index.ts'],
-  format: ['esm', 'cjs'],
+  entry: ["src/index.ts"],
+  format: ["esm", "cjs"],
   splitting: true,
   sourcemap: false,
   minify: true,
   clean: true,
   skipNodeModulesBundle: true,
   dts: true,
-  external: ['node_modules'],
+  external: ["node_modules"],
 });
