e0312fbe6ed2fa20177f3e6e55466bb5f5ebc4d2
Jared Palmer <jared@jaredpalmer.com>
2023-05-24 08:57:29 -0400

    Fix import

--- src/useChat.ts
+++ src/useChat.ts
@@ -2,7 +2,7 @@ import { useCallback, useId, useRef, useEffect } from 'react'
 import useSWRMutation from 'swr/mutation'
 import useSWR from 'swr'
 
-import { AIStreamCallbacks } from './AIStream'
+import { AIStreamCallbacks } from './ai-stream'
 
 export type Message = {
   id: string
