28b739b12aa0ba0c177060716c2186cd70ba8b75
Jared Palmer <jared@jaredpalmer.com>
2023-05-25 10:12:11 -0400

    Implement example

--- example/app/chat.tsx
+++ example/app/chat.tsx
@@ -1,70 +1,38 @@
-'use client';
+'use client'
 
-import { useChat } from '@vercel/ai-utils';
-
+import { OpenAIStream, useChat } from '@vercel/ai-utils'
+import { nanoid } from 'nanoid'
+import { useState } from 'react'
 export function Chat() {
   const { messages, append } = useChat({
     initialMessages: [],
     api: '/api/generate',
-    parser: async (res, { onCompletion, onToken, onStart }) => {
-      // This data is a ReadableStream
-      const data = res.body;
-      if (!data) {
-        return new ReadableStream();
-      }
-      if (onStart) {
-        onStart();
-      }
-      const reader = data.getReader();
-      const decoder = new TextDecoder();
-      let done = false;
-      let accumulatedValue = ''; // Variable to accumulate chunks
-
-      while (!done) {
-        const { value, done: doneReading } = await reader.read();
-        done = doneReading;
-        const chunkValue = decoder.decode(value);
-        accumulatedValue += chunkValue; // Accumulate the chunk value
-
-        // Check if the accumulated value contains the delimiter
-        const delimiter = '\n';
-        const chunks = accumulatedValue.split(delimiter);
-
-        // Process all chunks except the last one (which may be incomplete)
-        while (chunks.length > 1) {
-          const chunkToDispatch = chunks.shift(); // Get the first chunk
-          if (chunkToDispatch && chunkToDispatch.length > 0) {
-            const chunk = JSON.parse(chunkToDispatch);
-            if (onToken) {
-              onToken(chunk);
-            }
-          }
-        }
-
-        // The last chunk may be incomplete, so keep it in the accumulated value
-        accumulatedValue = chunks[0];
-      }
-
-      // Process any remaining accumulated value after the loop is done
-      if (accumulatedValue.length > 0) {
-        if (onCompletion) {
-          onCompletion(accumulatedValue);
-        }
-      }
-
-      return res.body as ReadableStream<any>;
-    },
-  });
-
+    StreamProvider: OpenAIStream
+  })
+  const [input, setInput] = useState('')
   return (
-    <div>
+    <div className="mx-auto w-full max-w-md py-24">
       {messages && messages.length
-        ? messages.map((m) => <div>{m.content}</div>)
+        ? messages.map(m => <div key={m.id}>{m.content}</div>)
         : null}
-      
-      <form>
-        <textarea 
+
+      <form
+        onSubmit={e => {
+          e.preventDefault()
+          append({
+            id: nanoid(12),
+            content: input,
+            role: 'user'
+          })
+          setInput('')
+        }}
+      >
+        <input
+          className="border rounded w-full"
+          value={input}
+          onChange={e => setInput(e.target.value)}
+        />
       </form>
     </div>
-  );
+  )
 }
