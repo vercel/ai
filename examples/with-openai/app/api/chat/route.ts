7bec9d2b7bf2e172f4a5e3d1d5f97fb40ee046f4
Jared Palmer <jared@jaredpalmer.com>
2023-05-31 13:23:40 -0400

    Split examples

--- examples/with-openai/app/api/chat/route.ts
+++ examples/with-openai/app/api/chat/route.ts
@@ -1,32 +1,28 @@
-// app/api/generate/route.ts
+// ./app/api/chat/route.ts
 import { Configuration, OpenAIApi } from 'openai-edge'
 import { OpenAIStream, StreamingTextResponse } from '@vercel/ai-utils'
 
+// Create an OpenAI API client (that's edge friendly!)
 const config = new Configuration({
   apiKey: process.env.OPENAI_API_KEY
 })
 const openai = new OpenAIApi(config)
 
+// IMPORTANT! Set the runtime to edge
 export const runtime = 'edge'
 
 export async function POST(req: Request) {
+  // Extract the `prompt` from the body of the request
   const { messages } = await req.json()
+
+  // Ask OpenAI for a streaming chat completion given the prompt
   const response = await openai.createChatCompletion({
-    model: 'gpt-4',
+    model: 'gpt-3.5-turbo',
     stream: true,
-    messages: messages.map(({ content, role }: any) => ({ content, role }))
+    messages
   })
-  const stream = OpenAIStream(response, {
-    async onStart() {
-      console.log('streamin yo')
-    },
-    async onToken(token) {
-      console.log('token: ' + token)
-    },
-    async onCompletion(content) {
-      console.log('full text: ' + content)
-      // await prisma.messages.create({ content }) or something
-    }
-  })
+  // Convert the response into a friendly text-stream
+  const stream = OpenAIStream(response)
+  // Respond with the stream
   return new StreamingTextResponse(stream)
 }
