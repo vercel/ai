4f56e336a48022b2a1b5141167da40b749a906f5
Shu Ding <g@shud.in>
2023-06-07 15:11:30 +0200

    write header

--- packages/core/src/streaming-text-response.ts
+++ packages/core/src/streaming-text-response.ts
@@ -21,8 +21,14 @@ export class StreamingTextResponse extends Response {
  */
 export function streamToResponse(
   res: ReadableStream,
-  response: ServerResponse
+  response: ServerResponse,
+  init?: { headers?: Record<string, string>; status?: number }
 ) {
+  response.writeHead(init?.status || 200, {
+    'Content-Type': 'text/plain; charset=utf-8',
+    ...init?.headers
+  })
+
   const reader = res.getReader()
   function read() {
     reader.read().then(({ done, value }: { done: boolean; value?: any }) => {
