d70a9e7e4774a4af0ff54abbbbf8bd7af435b170
Shu Ding <g@shud.in>
2023-06-07 15:21:07 +0200

    Add `streamToResponse` (#24)

---
+++ .changeset/clever-crabs-train.md
@@ -0,0 +1,5 @@
+---
+'ai-connector': patch
+---
+
+Add streamToResponse
--- packages/core/src/streaming-text-response.ts
+++ packages/core/src/streaming-text-response.ts
@@ -1,3 +1,5 @@
+import type { ServerResponse } from 'node:http'
+
 /**
  * A utility class for streaming text responses.
  */
@@ -12,4 +14,31 @@ export class StreamingTextResponse extends Response {
       }
     })
   }
+}
+
+/**
+ * A utility function to stream a ReadableStream to a Node.js response-like object.
+ */
+export function streamToResponse(
+  res: ReadableStream,
+  response: ServerResponse,
+  init?: { headers?: Record<string, string>; status?: number }
+) {
+  response.writeHead(init?.status || 200, {
+    'Content-Type': 'text/plain; charset=utf-8',
+    ...init?.headers
+  })
+
+  const reader = res.getReader()
+  function read() {
+    reader.read().then(({ done, value }: { done: boolean; value?: any }) => {
+      if (done) {
+        response.end()
+        return
+      }
+      response.write(value)
+      return read()
+    })
+  }
+  read()
 }
