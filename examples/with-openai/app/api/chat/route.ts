2ea759182dc8555977434a3a7b5cf1efab030f99
Justin Ridgewell <justin@ridgewell.name>
2023-06-08 11:47:37 -0400

    Refactor LangChain (#29)
    Co-authored-by: Jared Palmer <jared@jaredpalmer.com>

--- examples/with-openai/app/api/chat/route.ts
+++ examples/with-openai/app/api/chat/route.ts
@@ -19,8 +19,12 @@ export async function POST(req: Request) {
   const response = await openai.createChatCompletion({
     model: 'gpt-3.5-turbo',
     stream: true,
-    messages
+    messages: messages.map((message: any) => ({
+      content: message.content,
+      role: message.role
+    }))
   })
+
   // Convert the response into a friendly text-stream
   const stream = OpenAIStream(response)
   // Respond with the stream
--- packages/core/src/langchain-stream.ts
+++ packages/core/src/langchain-stream.ts
@@ -1,39 +1,14 @@
-import { AIStreamCallbacks } from './ai-stream'
+import { type AIStreamCallbacks, createCallbacksTransformer } from './ai-stream'
 
 export function LangChainStream(callbacks?: AIStreamCallbacks) {
   const stream = new TransformStream()
-  const encoder = new TextEncoder()
   const writer = stream.writable.getWriter()
-  const decoder = new TextDecoder()
-  let fullResponse = ''
-  const forkedStream = new TransformStream({
-    start: async (): Promise<void> => {
-      if (callbacks?.onStart) {
-        await callbacks.onStart()
-      }
-    },
-    transform: async (chunk, controller): Promise<void> => {
-      controller.enqueue(chunk)
-      const item = decoder.decode(chunk)
-      const value = JSON.parse(item.split('\n')[0])
-      if (callbacks?.onToken) {
-        await callbacks.onToken(value as string)
-      }
-      fullResponse += value
-    },
-    flush: async (controller): Promise<void> => {
-      if (callbacks?.onCompletion) {
-        await callbacks.onCompletion(fullResponse)
-      }
-      controller.terminate()
-    }
-  })
   return {
-    stream: stream.readable.pipeThrough(forkedStream),
+    stream: stream.readable.pipeThrough(createCallbacksTransformer(callbacks)),
     handlers: {
       handleLLMNewToken: async (token: string) => {
         await writer.ready
-        await writer.write(encoder.encode(`${JSON.stringify(token)}\n`))
+        await writer.write(token)
       },
       handleLLMEnd: async () => {
         await writer.ready
