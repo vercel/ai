447416cc45815c37d4a596352de7e7e1049971b0
Shu Ding <g@shud.in>
2023-06-13 23:06:40 +0200

    use .ok instead

--- packages/core/streams/ai-stream.ts
+++ packages/core/streams/ai-stream.ts
@@ -90,11 +90,11 @@ export function AIStream(
   customParser: AIStreamParser,
   callbacks?: AIStreamCallbacks
 ): ReadableStream {
-  // If the response is not 200, we want to throw an error to indicate that
+  // If the response is not OK, we want to throw an error to indicate that
   // the AI service is not available.
   // When catching this error, we can check the status code and return a handled
   // error response to the client.
-  if (res.status !== 200) {
+  if (!res.ok) {
     throw new Error(
       `Failed to convert the response to stream. Received status code: ${res.status}.`
     )
