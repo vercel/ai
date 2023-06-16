583fae4f15098b204d1bdd431acddb81a714673e
Daniel Mirabolghasemi <sirlolcatX@pm.me>
2023-06-16 23:01:56 +0200

    feat: initialize nuxt project

---
+++ examples/nuxt-langchain/nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/eslint-module',
+    'nuxt-icon'
+  ]
+})
