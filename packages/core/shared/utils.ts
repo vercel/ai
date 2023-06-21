7815b117b2d64c2a6ecfd204a5418d84040ccca6
Shu Ding <g@shud.in>
2023-06-21 17:30:41 +0200

    fix type error

--- packages/core/shared/utils.ts
+++ packages/core/shared/utils.ts
@@ -8,7 +8,8 @@ export const nanoid = customAlphabet(
 
 export function createChunkDecoder() {
   const decoder = new TextDecoder()
-  return function (chunk: Uint8Array): string {
+  return function (chunk: Uint8Array | undefined): string {
+    if (!chunk) return ''
     return decoder.decode(chunk, { stream: true })
   }
 }
