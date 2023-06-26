813d9ba9b640ff43188198433a4b1d024067f914
Max Leiter <max.leiter@vercel.com>
2023-06-26 15:29:06 -0700

    rm callbacks

--- packages/core/react/tokens.tsx
+++ packages/core/react/tokens.tsx
@@ -5,42 +5,20 @@ type Props = {
    * A ReadableStream produced by the AI SDK.
    */
   stream: ReadableStream
-  /**
-   * A function that is called at the start of the stream.
-   */
-  onStart?: () => Promise<void>
-  /**
-   * A function that is called for each token processed.
-   */
-  onToken?: (token: string) => Promise<void>
-  /**
-   * A function that is called once the stream has been processed.
-   */
-  onEnd?: () => Promise<void>
 }
 
 /**
  * A React Server Component that recursively renders a stream of tokens.
  */
 export async function Tokens(props: Props) {
-  const { stream, onEnd, onStart, onToken } = props
-
-  if (onStart) {
-    await onStart()
-  }
-
+  const { stream } = props
   const reader = stream.getReader()
 
   return (
     /* @ts-expect-error Suspense */
     <Suspense>
       {/* @ts-expect-error React Server Components */}
-      <RecursiveTokens
-        reader={reader}
-        onEnd={onEnd}
-        onStart={onStart}
-        onToken={onToken}
-      />
+      <RecursiveTokens reader={reader} />
     </Suspense>
   )
 }
@@ -49,27 +27,14 @@ type InternalProps = {
   reader: ReadableStreamDefaultReader
 } & Omit<Props, 'stream'>
 
-async function RecursiveTokens({
-  reader,
-  onEnd,
-  onStart,
-  onToken
-}: InternalProps) {
+async function RecursiveTokens({ reader }: InternalProps) {
   const { done, value } = await reader.read()
 
   if (done) {
-    if (onEnd) {
-      await onEnd()
-    }
-
     return null
   }
 
   const text = new TextDecoder().decode(value)
-
-  if (onToken) {
-    await onToken(text)
-  }
 
   return (
     <>
@@ -77,12 +42,7 @@ async function RecursiveTokens({
       {/* @ts-expect-error Suspense */}
       <Suspense fallback={null}>
         {/* @ts-expect-error React Server Components */}
-        <RecursiveTokens
-          reader={reader}
-          onEnd={onEnd}
-          onStart={onStart}
-          onToken={onToken}
-        />
+        <RecursiveTokens reader={reader} />
       </Suspense>
     </>
   )
