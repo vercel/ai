b6eeec3a832671d272d156f16fd9e4d5c0688ffb
Tomeu Cabot <tomeu@cohere.com>
2023-06-29 13:21:15 +0200

    fix: network buffering for long response

--- packages/core/streams/cohere-stream.ts
+++ packages/core/streams/cohere-stream.ts
@@ -1,6 +1,50 @@
 import { type AIStreamCallbacks, createCallbacksTransformer } from './ai-stream'
 
-const decoder = new TextDecoder()
+const utf8Decoder = new TextDecoder('utf-8')
+
+// Helper function to iterate over the stream due an edge-case  of"network buffering"
+// which can cause the response to be split into multiple chunks or "glued chunks"
+// E.g.: `{ "text": "Hello, " }\n{ "text": "world!" }\n`
+async function* makeTextLineIterator(
+  reader: ReadableStreamDefaultReader<Uint8Array>
+) {
+  let { value: chunk, done: readerDone } = await reader.read()
+  let segment = utf8Decoder.decode(chunk, { stream: true })
+
+  let re = /\r\n|\n|\r/gm
+  let startIndex = 0
+
+  while (true) {
+    const result = re.exec(segment)
+
+    if (!result) {
+      if (readerDone) {
+        break
+      }
+      let remainder = segment.substring(startIndex)
+
+      const next = await reader.read()
+      chunk = next.value
+      readerDone = next.done
+
+      segment = remainder
+      if (chunk) {
+        segment += utf8Decoder.decode(chunk)
+      }
+
+      startIndex = re.lastIndex = 0
+      continue
+    }
+
+    yield segment.substring(startIndex, result.index)
+    startIndex = re.lastIndex
+  }
+
+  if (startIndex < segment.length) {
+    // last line didn't end in a newline char
+    yield segment.substring(startIndex)
+  }
+}
 
 function createParser(res: Response) {
   const reader = res.body?.getReader()
@@ -11,19 +55,17 @@ function createParser(res: Response) {
         return
       }
 
-      const { value, done } = await reader.read()
-      if (done) {
-        controller.close()
-        return
+      for await (const line of makeTextLineIterator(reader)) {
+        const { text, is_finished } = JSON.parse(line)
+
+        if (is_finished === true) {
+          controller.close()
+        } else {
+          controller.enqueue(text)
+        }
       }
 
-      const { text, is_finished } = JSON.parse(decoder.decode(value))
-
-      if (is_finished === true) {
-        controller.close()
-      } else {
-        controller.enqueue(text)
-      }
+      controller.close()
     }
   })
 }
