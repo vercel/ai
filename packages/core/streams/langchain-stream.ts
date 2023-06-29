8bf637adb03c61526ae5fa9d01357652662e539a
Lucas <aranlucas@gmail.com>
2023-06-29 08:46:26 -0700

    fix: langchain stream not closing (#201)
    Co-authored-by: Jared Palmer <jared@jaredpalmer.com>

--- packages/core/streams/langchain-stream.ts
+++ packages/core/streams/langchain-stream.ts
@@ -3,6 +3,28 @@ import { type AIStreamCallbacks, createCallbacksTransformer } from './ai-stream'
 export function LangChainStream(callbacks?: AIStreamCallbacks) {
   const stream = new TransformStream()
   const writer = stream.writable.getWriter()
+
+  const runs = new Set()
+
+  const handleError = async (e: Error, runId: string) => {
+    runs.delete(runId)
+    await writer.ready
+    await writer.abort(e)
+  }
+
+  const handleStart = async (runId: string) => {
+    runs.add(runId)
+  }
+
+  const handleEnd = async (runId: string) => {
+    runs.delete(runId)
+
+    if (runs.size === 0) {
+      await writer.ready
+      await writer.close()
+    }
+  }
+
   return {
     stream: stream.readable.pipeThrough(createCallbacksTransformer(callbacks)),
     handlers: {
@@ -10,13 +32,32 @@ export function LangChainStream(callbacks?: AIStreamCallbacks) {
         await writer.ready
         await writer.write(token)
       },
-      handleChainEnd: async () => {
-        await writer.ready
-        await writer.close()
+      handleLLMStart: async (_llm: any, _prompts: string[], runId: string) => {
+        handleStart(runId)
       },
-      handleLLMError: async (e: any) => {
-        await writer.ready
-        await writer.abort(e)
+      handleLLMEnd: async (_output: any, runId: string) => {
+        await handleEnd(runId)
+      },
+      handleLLMError: async (e: Error, runId: string) => {
+        await handleError(e, runId)
+      },
+      handleChainStart: async (_chain: any, _inputs: any, runId: string) => {
+        handleStart(runId)
+      },
+      handleChainEnd: async (_outputs: any, runId: string) => {
+        await handleEnd(runId)
+      },
+      handleChainError: async (e: Error, runId: string) => {
+        await handleError(e, runId)
+      },
+      handleToolStart: async (_tool: any, _input: string, runId: string) => {
+        handleStart(runId)
+      },
+      handleToolEnd: async (_output: string, runId: string) => {
+        await handleEnd(runId)
+      },
+      handleToolError: async (e: Error, runId: string) => {
+        await handleError(e, runId)
       }
     }
   }
