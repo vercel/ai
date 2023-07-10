3cf83b443765afa946cbc22f0531d853c44342c4
Max Leiter <max.leiter@vercel.com>
2023-07-10 14:39:15 -0700

    rm comment from openai-stream

--- packages/core/streams/openai-stream.ts
+++ packages/core/streams/openai-stream.ts
@@ -118,25 +118,6 @@ function parseOpenAIStream(): (data: string) => string | void {
   }
 }
 
-/**
- * Processes the response of an OpenAI API call and returns a ReadableStream.
- * It employs conditional stream processing to efficiently handle both 2xx and non-2xx HTTP responses.
- *
- * For 2xx HTTP responses:
- * - The function continues with standard stream processing.
- *
- * For non-2xx HTTP responses:
- * - If the response body is not null, it asynchronously extracts and decodes the response body.
- * - It then creates a custom ReadableStream to propagate a detailed error message.
- *
- * This approach ensures that the synchronous nature of the function is preserved while providing
- * detailed error handling for non-2xx responses.
- *
- * @param {Response} res - The Response object from the OpenAI API call.
- * @param {AIStreamCallbacks} [cb] - Optional callback functions to handle stream lifecycle events.
- * @return {ReadableStream} A readable stream with the parsed content from the OpenAI API response or a stream with an error message for non-2xx responses.
- * @throws Will throw an error if the response is not OK and there is no response body.
- */
 export function OpenAIStream(
   res: Response,
   cb?: AIStreamCallbacks
