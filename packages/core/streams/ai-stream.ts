97bd9f1dec529624c300fe70399478dc2b008d24
Max Leiter <max.leiter@vercel.com>
2023-07-10 17:25:55 -0700

    streams/ai-stream: cleanup comment

--- packages/core/streams/ai-stream.ts
+++ packages/core/streams/ai-stream.ts
@@ -137,14 +137,14 @@ export function trimStartOfStreamHelper(): (text: string) => string {
  * Returns a ReadableStream created from the response, parsed and handled with custom logic.
  * The stream goes through two transformation stages, first parsing the events and then
  * invoking the provided callbacks.
- * 
+ *
  * For 2xx HTTP responses:
  * - The function continues with standard stream processing.
  *
  * For non-2xx HTTP responses:
  * - If the response body is defined, it asynchronously extracts and decodes the response body.
  * - It then creates a custom ReadableStream to propagate a detailed error message.
- * 
+ *
  * @param {Response} response - The response.
  * @param {AIStreamParser} customParser - The custom parser function.
  * @param {AIStreamCallbacks} callbacks - The callbacks.
