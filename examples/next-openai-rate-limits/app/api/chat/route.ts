280c745929278244754b6c208857fde5c6a7cd01
Steven Tey <stevensteel97@gmail.com>
2023-08-09 16:50:00 -0500

    Added next-openai-rate-limits example (#431)
    ![CleanShot 2023-08-09 at 16 11 13](https://github.com/vercel-labs/ai/assets/28986134/ddb7dc0c-18c3-4956-b176-99746e40b995)

---
+++ examples/next-openai-rate-limits/app/api/chat/route.ts
@@ -0,0 +1,59 @@
+// ./app/api/chat/route.ts
+import { Configuration, OpenAIApi } from 'openai-edge'
+import { OpenAIStream, StreamingTextResponse } from 'ai'
+import { kv } from '@vercel/kv'
+import { Ratelimit } from '@upstash/ratelimit'
+
+// Create an OpenAI API client (that's edge friendly!)
+const config = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY
+})
+const openai = new OpenAIApi(config)
+
+// IMPORTANT! Set the runtime to edge
+export const runtime = 'edge'
+
+export async function POST(req: Request) {
+  // eslint-disable-next-line turbo/no-undeclared-env-vars
+  if (process.env.KV_REST_API_URL && process.env.KV_REST_API_TOKEN) {
+    const ip = req.headers.get('x-forwarded-for')
+    const ratelimit = new Ratelimit({
+      redis: kv,
+      // rate limit to 5 requests per 10 seconds
+      limiter: Ratelimit.slidingWindow(5, '10s')
+    })
+
+    const { success, limit, reset, remaining } = await ratelimit.limit(
+      `ratelimit_${ip}`
+    )
+
+    if (!success) {
+      return new Response('You have reached your request limit for the day.', {
+        status: 429,
+        headers: {
+          'X-RateLimit-Limit': limit.toString(),
+          'X-RateLimit-Remaining': remaining.toString(),
+          'X-RateLimit-Reset': reset.toString()
+        }
+      })
+    }
+  }
+
+  // Extract the `prompt` from the body of the request
+  const { messages } = await req.json()
+
+  // Ask OpenAI for a streaming chat completion given the prompt
+  const response = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    stream: true,
+    messages: messages.map((message: any) => ({
+      content: message.content,
+      role: message.role
+    }))
+  })
+
+  // Convert the response into a friendly text-stream
+  const stream = OpenAIStream(response)
+  // Respond with the stream
+  return new StreamingTextResponse(stream)
+}
