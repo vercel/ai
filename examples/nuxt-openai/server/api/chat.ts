0fffd25c5d0cfb951a291ae9634f5bd86bb20711
Hebilicious <xsh4k3@gmail.com>
2023-08-11 05:44:48 +0700

    feet: wrap in lazy event handler

--- examples/nuxt-openai/server/api/chat.ts
+++ examples/nuxt-openai/server/api/chat.ts
@@ -5,29 +5,28 @@ import {
 } from 'openai-edge'
 import { OpenAIStream } from 'ai'
 
-let openai: OpenAIApi
+export default defineLazyEventHandler(async () => {
+  const apiKey = useRuntimeConfig().openaiApiKey
+  if (!apiKey) throw new Error('Missing OpenAI API key')
+  const openai = new OpenAIApi(new Configuration({ apiKey }))
 
-export default defineEventHandler(async event => {
-  if (!openai) {
-    const apiKey = useRuntimeConfig(event).openaiApiKey
-    // Create an OpenAI API client (that's edge friendly!)
-    openai = new OpenAIApi(new Configuration({ apiKey }))
-  }
-  // Extract the `prompt` from the body of the request
-  const { messages } = (await readBody(event)) as {
-    messages: ChatCompletionRequestMessage[]
-  }
+  return defineEventHandler(async event => {
+    // Extract the `prompt` from the body of the request
+    const { messages } = (await readBody(event)) as {
+      messages: ChatCompletionRequestMessage[]
+    }
 
-  // Ask OpenAI for a streaming chat completion given the prompt
-  const response = await openai.createChatCompletion({
-    model: 'gpt-3.5-turbo',
-    stream: true,
-    messages: messages.map(message => ({
-      content: message.content,
-      role: message.role
-    }))
+    // Ask OpenAI for a streaming chat completion given the prompt
+    const response = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      stream: true,
+      messages: messages.map(message => ({
+        content: message.content,
+        role: message.role
+      }))
+    })
+
+    // Convert the response into a friendly text-stream
+    return OpenAIStream(response)
   })
-
-  // Convert the response into a friendly text-stream
-  return OpenAIStream(response)
 })
