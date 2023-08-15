f2e3210bfdceec56718278b4d5c307d6d63bb3ba
Max Leiter <max.leiter@vercel.com>
2023-08-15 13:03:34 -0700

    fix test

--- packages/core/shared/utils.test.ts
+++ packages/core/shared/utils.test.ts
@@ -19,9 +19,9 @@ describe('utils', () => {
   it('correctly decodes streamed utf8 chunks in simple mode', () => {
     const decoder = createChunkDecoder(false)
     const encoder = new TextEncoder()
-    const prefixChunkUint8 = encoder.encode('0:')
-    const chunk1 = new Uint8Array([...prefixChunkUint8, 226, 153])
-    const chunk2 = new Uint8Array([...prefixChunkUint8, 165])
+    // const prefixChunkUint8 = encoder.encode('0:')
+    const chunk1 = new Uint8Array([226, 153])
+    const chunk2 = new Uint8Array([165])
     const values = decoder(chunk1)
     const secondValues = decoder(chunk2)
     if (typeof values !== 'string' || typeof secondValues !== 'string') {
