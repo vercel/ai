1d957858a616656335d71c90b413bd4864242d75
Lars Grammel <lars.grammel@gmail.com>
2023-10-24 15:20:20 +0200

    Add characterization tests for createChunkDecoder.

--- packages/core/shared/utils.test.ts
+++ packages/core/shared/utils.test.ts
@@ -1,50 +1,101 @@
 import { createChunkDecoder, getStreamString } from './utils';
 
 describe('utils', () => {
-  it('correctly decodes streamed utf8 chunks in complex mode', () => {
-    const normalDecode = createChunkDecoder();
-    const complexDecode = createChunkDecoder(true);
+  describe('createChunkDecoder', () => {
+    it('should correctly decode text chunk in complex mode', () => {
+      const decoder = createChunkDecoder(true);
 
-    const encoder = new TextEncoder();
+      const encoder = new TextEncoder();
+      const chunk = encoder.encode(getStreamString('text', 'Hello, world!'));
+      const values = decoder(chunk);
 
-    // Original data chunks
-    const chunk1 = new Uint8Array([226, 153]);
-    const chunk2 = new Uint8Array([165]);
+      expect(values).toStrictEqual([
+        { type: 'text', value: 'Hello, world!' },
+        { type: 'text', value: '' },
+      ]);
+    });
 
-    const enqueuedChunks = [];
-    enqueuedChunks.push(
-      encoder.encode(getStreamString('text', normalDecode(chunk1))),
-    );
-    enqueuedChunks.push(
-      encoder.encode(getStreamString('text', normalDecode(chunk2))),
-    );
+    it('should correctly decode function chunk in complex mode', () => {
+      const functionCall = {
+        name: 'get_current_weather',
+        arguments:
+          '{\n"location": "Charlottesville, Virginia",\n"format": "celsius"\n}',
+      };
 
-    let fullDecodedString = '';
-    for (const chunk of enqueuedChunks) {
-      const lines = complexDecode(chunk);
-      for (const line of lines) {
-        if (line.type !== 'text') {
-          throw new Error('Expected line to be text');
+      const decoder = createChunkDecoder(true);
+
+      const encoder = new TextEncoder();
+      const chunk = encoder.encode(
+        getStreamString('function_call', functionCall),
+      );
+      const values = decoder(chunk);
+
+      expect(values).toStrictEqual([
+        { type: 'function_call', value: functionCall },
+        { type: 'text', value: '' },
+      ]);
+    });
+
+    it('should correctly decode data chunk in complex mode', () => {
+      const data = { test: 'value' };
+
+      const decoder = createChunkDecoder(true);
+
+      const encoder = new TextEncoder();
+      const chunk = encoder.encode(getStreamString('data', data));
+      const values = decoder(chunk);
+
+      expect(values).toStrictEqual([
+        { type: 'data', value: data },
+        { type: 'text', value: '' },
+      ]);
+    });
+
+    it('should correctly decode streamed utf8 chunks in complex mode', () => {
+      const normalDecode = createChunkDecoder();
+      const complexDecode = createChunkDecoder(true);
+
+      const encoder = new TextEncoder();
+
+      // Original data chunks
+      const chunk1 = new Uint8Array([226, 153]);
+      const chunk2 = new Uint8Array([165]);
+
+      const enqueuedChunks = [];
+      enqueuedChunks.push(
+        encoder.encode(getStreamString('text', normalDecode(chunk1))),
+      );
+      enqueuedChunks.push(
+        encoder.encode(getStreamString('text', normalDecode(chunk2))),
+      );
+
+      let fullDecodedString = '';
+      for (const chunk of enqueuedChunks) {
+        const lines = complexDecode(chunk);
+        for (const line of lines) {
+          if (line.type !== 'text') {
+            throw new Error('Expected line to be text');
+          }
+          fullDecodedString += line.value;
         }
-        fullDecodedString += line.value;
       }
-    }
 
-    expect(fullDecodedString).toBe('♥');
-  });
+      expect(fullDecodedString).toBe('♥');
+    });
 
-  it('correctly decodes streamed utf8 chunks in simple mode', () => {
-    const decoder = createChunkDecoder(false);
-    const encoder = new TextEncoder();
-    // const prefixChunkUint8 = encoder.encode('0:')
-    const chunk1 = new Uint8Array([226, 153]);
-    const chunk2 = new Uint8Array([165]);
-    const values = decoder(chunk1);
-    const secondValues = decoder(chunk2);
-    if (typeof values !== 'string' || typeof secondValues !== 'string') {
-      throw new Error('Expected values to be strings, not objects');
-    }
+    it('should correctly decode streamed utf8 chunks in simple mode', () => {
+      const decoder = createChunkDecoder(false);
+      const encoder = new TextEncoder();
+      // const prefixChunkUint8 = encoder.encode('0:')
+      const chunk1 = new Uint8Array([226, 153]);
+      const chunk2 = new Uint8Array([165]);
+      const values = decoder(chunk1);
+      const secondValues = decoder(chunk2);
+      if (typeof values !== 'string' || typeof secondValues !== 'string') {
+        throw new Error('Expected values to be strings, not objects');
+      }
 
-    expect(values + secondValues).toBe('♥');
+      expect(values + secondValues).toBe('♥');
+    });
   });
 });
