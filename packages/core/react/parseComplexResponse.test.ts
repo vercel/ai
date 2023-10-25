9d6ff8a9750845fb868f3161a7b396771ce7b83b
Lars Grammel <lars.grammel@gmail.com>
2023-10-25 17:51:54 +0200

    Add test for text message sequences.

--- packages/core/react/parseComplexResponse.test.ts
+++ packages/core/react/parseComplexResponse.test.ts
@@ -1,7 +1,7 @@
 import { parseComplexResponse } from './parseComplexResponse';
 
 describe('parseComplexResponse function', () => {
-  it('should parse a single text message from stream', async () => {
+  it('should parse a single text message', async () => {
     const mockData = '0:"Hello"\n';
 
     const readableStream = new ReadableStream<Uint8Array>({
@@ -27,11 +27,109 @@ describe('parseComplexResponse function', () => {
     };
 
     // check the mockUpdate call:
-    expect(mockUpdate).toHaveBeenCalled();
+    expect(mockUpdate).toHaveBeenCalledTimes(2);
     expect(mockUpdate.mock.calls[0][0]).toEqual([expectedMessage]);
+    expect(mockUpdate.mock.calls[1][0]).toEqual([expectedMessage]);
 
     // check the prefix map:
     expect(result).toHaveProperty('text');
     expect(result.text).toEqual(expectedMessage);
+  });
+
+  it('should parse a sequence of text messages', async () => {
+    const mockData = ['0:"Hello"\n', '0:","\n', '0:" world"\n', '0:"."\n'];
+
+    const readableStream = new ReadableStream<Uint8Array>({
+      async start(controller) {
+        for (const chunk of mockData) {
+          controller.enqueue(Buffer.from(chunk, 'utf-8'));
+        }
+        controller.close();
+      },
+    });
+
+    const mockUpdate = jest.fn();
+
+    const result = await parseComplexResponse({
+      reader: readableStream.getReader(),
+      abortControllerRef: { current: new AbortController() },
+      update: mockUpdate,
+    });
+
+    // check the mockUpdate call:
+    expect(mockUpdate).toHaveBeenCalledTimes(8);
+    expect(mockUpdate.mock.calls[0][0]).toEqual([
+      {
+        id: expect.any(String),
+        role: 'assistant',
+        content: 'Hello',
+        createdAt: expect.any(Date),
+      },
+    ]);
+    expect(mockUpdate.mock.calls[1][0]).toEqual([
+      {
+        id: expect.any(String),
+        role: 'assistant',
+        content: 'Hello',
+        createdAt: expect.any(Date),
+      },
+    ]);
+    expect(mockUpdate.mock.calls[2][0]).toEqual([
+      {
+        id: expect.any(String),
+        role: 'assistant',
+        content: 'Hello,',
+        createdAt: expect.any(Date),
+      },
+    ]);
+    expect(mockUpdate.mock.calls[3][0]).toEqual([
+      {
+        id: expect.any(String),
+        role: 'assistant',
+        content: 'Hello,',
+        createdAt: expect.any(Date),
+      },
+    ]);
+    expect(mockUpdate.mock.calls[4][0]).toEqual([
+      {
+        id: expect.any(String),
+        role: 'assistant',
+        content: 'Hello, world',
+        createdAt: expect.any(Date),
+      },
+    ]);
+    expect(mockUpdate.mock.calls[5][0]).toEqual([
+      {
+        id: expect.any(String),
+        role: 'assistant',
+        content: 'Hello, world',
+        createdAt: expect.any(Date),
+      },
+    ]);
+    expect(mockUpdate.mock.calls[6][0]).toEqual([
+      {
+        id: expect.any(String),
+        role: 'assistant',
+        content: 'Hello, world.',
+        createdAt: expect.any(Date),
+      },
+    ]);
+    expect(mockUpdate.mock.calls[7][0]).toEqual([
+      {
+        id: expect.any(String),
+        role: 'assistant',
+        content: 'Hello, world.',
+        createdAt: expect.any(Date),
+      },
+    ]);
+
+    // check the prefix map:
+    expect(result).toHaveProperty('text');
+    expect(result.text).toEqual({
+      id: expect.any(String),
+      role: 'assistant',
+      content: 'Hello, world.',
+      createdAt: expect.any(Date),
+    });
   });
 });
