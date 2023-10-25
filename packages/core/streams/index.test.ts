7d48c2d7f5de194de1a1a9795d4a0d78b18f4bb4
Lars Grammel <lars.grammel@gmail.com>
2023-10-25 09:40:24 +0200

    Add characterization tests for StreamData. (#667)

--- packages/core/streams/index.test.ts
+++ packages/core/streams/index.test.ts
@@ -14,7 +14,7 @@ describe('AIStream', () => {
     if (typeof Response === 'undefined') {
       it("should skip this test on Node 16 because it doesn't support `Response`", () => {});
     } else {
-      const { OpenAIStream, StreamingTextResponse } =
+      const { OpenAIStream, StreamingTextResponse, experimental_StreamData } =
         require('.') as typeof import('.');
 
       it('should be able to parse SSE and receive the streamed response', async () => {
@@ -101,6 +101,250 @@ describe('AIStream', () => {
         expect(JSON.stringify(server.getRecentFlushed())).toMatchInlineSnapshot(
           `"[{"id":"chatcmpl-7RyNSW2BXkOQQh7NlBc65j5kX8AjC","object":"chat.completion.chunk","created":1686901302,"model":"gpt-3.5-turbo-0301","choices":[{"delta":{"role":"assistant"},"index":0,"finish_reason":null}]},{"id":"chatcmpl-7RyNSW2BXkOQQh7NlBc65j5kX8AjC","object":"chat.completion.chunk","created":1686901302,"model":"gpt-3.5-turbo-0301","choices":[{"delta":{"content":"Hello"},"index":0,"finish_reason":null}]}]"`,
         );
+      });
+
+      describe('StreamData prototcol', () => {
+        it('should send text', async () => {
+          const data = new experimental_StreamData();
+
+          const stream = OpenAIStream(
+            await fetch(server.api, {
+              headers: {
+                'x-mock-service': 'openai',
+                'x-mock-type': 'chat',
+              },
+            }),
+            {
+              onFinal() {
+                data.close();
+              },
+              experimental_streamData: true,
+            },
+          );
+
+          const response = new StreamingTextResponse(stream, {}, data);
+
+          const client = createClient(response);
+          const chunks = await client.readAll();
+
+          expect(chunks).toEqual([
+            '0:"Hello"\n',
+            '0:","\n',
+            '0:" world"\n',
+            '0:"."\n',
+          ]);
+        });
+
+        it('should send function response as text stream when onFunctionCall is not defined', async () => {
+          const data = new experimental_StreamData();
+
+          const stream = OpenAIStream(
+            await fetch(server.api + '/mock-func-call', {
+              headers: {
+                'x-mock-service': 'openai',
+                'x-mock-type': 'func_call',
+              },
+            }),
+            {
+              onFinal() {
+                data.close();
+              },
+              experimental_streamData: true,
+            },
+          );
+
+          const response = new StreamingTextResponse(stream, {}, data);
+
+          const client = createClient(response);
+          const chunks = await client.readAll();
+
+          expect(chunks).toEqual([
+            '0:"{\\"function_call\\": {\\"name\\": \\"get_current_weather\\", \\"arguments\\": \\""\n',
+            '0:"{\\\\n"\n',
+            '0:"\\\\\\""\n',
+            '0:"location"\n',
+            '0:"\\\\\\":"\n',
+            '0:" \\\\\\""\n',
+            '0:"Char"\n',
+            '0:"l"\n',
+            '0:"ottesville"\n',
+            '0:","\n',
+            '0:" Virginia"\n',
+            '0:"\\\\\\",\\\\n"\n',
+            '0:"\\\\\\""\n',
+            '0:"format"\n',
+            '0:"\\\\\\":"\n',
+            '0:" \\\\\\""\n',
+            '0:"c"\n',
+            '0:"elsius"\n',
+            '0:"\\\\\\"\\\\n"\n',
+            '0:"}"\n',
+            '0:"\\"}}"\n',
+          ]);
+        });
+
+        it('should send function response when onFunctionCall is defined and returns undefined', async () => {
+          const data = new experimental_StreamData();
+
+          const stream = OpenAIStream(
+            await fetch(server.api + '/mock-func-call', {
+              headers: {
+                'x-mock-service': 'openai',
+                'x-mock-type': 'func_call',
+              },
+            }),
+            {
+              onFinal() {
+                data.close();
+              },
+              async experimental_onFunctionCall({ name }) {
+                // no response
+              },
+              experimental_streamData: true,
+            },
+          );
+
+          const response = new StreamingTextResponse(stream, {}, data);
+
+          const client = createClient(response);
+          const chunks = await client.readAll();
+
+          expect(chunks).toEqual([
+            '1:"{\\"function_call\\": {\\"name\\": \\"get_current_weather\\", \\"arguments\\": \\"{\\\\n\\\\\\"location\\\\\\": \\\\\\"Charlottesville, Virginia\\\\\\",\\\\n\\\\\\"format\\\\\\": \\\\\\"celsius\\\\\\"\\\\n}\\"}}"\n',
+          ]);
+        });
+
+        it('should send function response and data when onFunctionCall is defined, returns undefined, and data is added', async () => {
+          const data = new experimental_StreamData();
+
+          const stream = OpenAIStream(
+            await fetch(server.api + '/mock-func-call', {
+              headers: {
+                'x-mock-service': 'openai',
+                'x-mock-type': 'func_call',
+              },
+            }),
+            {
+              onFinal() {
+                data.close();
+              },
+              async experimental_onFunctionCall({ name }) {
+                data.append({ fn: name });
+
+                // no response
+              },
+              experimental_streamData: true,
+            },
+          );
+
+          const response = new StreamingTextResponse(stream, {}, data);
+
+          const client = createClient(response);
+          const chunks = await client.readAll();
+
+          expect(chunks).toEqual([
+            '1:"{\\"function_call\\": {\\"name\\": \\"get_current_weather\\", \\"arguments\\": \\"{\\\\n\\\\\\"location\\\\\\": \\\\\\"Charlottesville, Virginia\\\\\\",\\\\n\\\\\\"format\\\\\\": \\\\\\"celsius\\\\\\"\\\\n}\\"}}"\n',
+            '2:"[{\\"fn\\":\\"get_current_weather\\"}]"\n',
+          ]);
+        });
+
+        it('should send return value when onFunctionCall is defined and returns value', async () => {
+          const data = new experimental_StreamData();
+
+          const stream = OpenAIStream(
+            await fetch(server.api + '/mock-func-call', {
+              headers: {
+                'x-mock-service': 'openai',
+                'x-mock-type': 'func_call',
+              },
+            }),
+            {
+              onFinal() {
+                data.close();
+              },
+              async experimental_onFunctionCall({ name }) {
+                return 'experimental_onFunctionCall-return-value';
+              },
+              experimental_streamData: true,
+            },
+          );
+
+          const response = new StreamingTextResponse(stream, {}, data);
+
+          const client = createClient(response);
+          const chunks = await client.readAll();
+
+          expect(chunks).toEqual([
+            '0:"experimental_onFunctionCall-return-value"\n',
+          ]);
+        });
+
+        it('should send return value and data when onFunctionCall is defined, returns value and data is added', async () => {
+          const data = new experimental_StreamData();
+
+          const stream = OpenAIStream(
+            await fetch(server.api + '/mock-func-call', {
+              headers: {
+                'x-mock-service': 'openai',
+                'x-mock-type': 'func_call',
+              },
+            }),
+            {
+              onFinal() {
+                data.close();
+              },
+              async experimental_onFunctionCall({ name }) {
+                data.append({ fn: name });
+                return 'experimental_onFunctionCall-return-value';
+              },
+              experimental_streamData: true,
+            },
+          );
+
+          const response = new StreamingTextResponse(stream, {}, data);
+
+          const client = createClient(response);
+          const chunks = await client.readAll();
+
+          expect(chunks).toEqual([
+            '0:"experimental_onFunctionCall-return-value"\n',
+            '2:"[{\\"fn\\":\\"get_current_weather\\"}]"\n',
+          ]);
+        });
+
+        it('should send text and data', async () => {
+          const data = new experimental_StreamData();
+
+          data.append({ t1: 'v1' });
+
+          const stream = OpenAIStream(
+            await fetch(server.api, {
+              headers: {
+                'x-mock-service': 'openai',
+                'x-mock-type': 'chat',
+              },
+            }),
+            {
+              onFinal() {
+                data.close();
+              },
+              experimental_streamData: true,
+            },
+          );
+
+          const response = new StreamingTextResponse(stream, {}, data);
+
+          const client = createClient(response);
+          const chunks = await client.readAll();
+
+          expect(chunks).toEqual([
+            '0:"Hello"\n',
+            '2:"[{\\"t1\\":\\"v1\\"}]"\n',
+            '0:","\n',
+            '0:" world"\n',
+            '0:"."\n',
+          ]);
+        });
       });
     }
   });
