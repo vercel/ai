a13f38e8e9ef7e8b16758623a682ee5df4b1c33c
Lars Grammel <lars.grammel@gmail.com>
2023-10-27 18:27:40 +0200

    Add HuggingFace tests for StreamData protocol.

--- packages/core/streams/huggingface-stream.test.ts
+++ packages/core/streams/huggingface-stream.test.ts
@@ -1,9 +1,15 @@
 import { HfInference } from '@huggingface/inference';
-import { HuggingFaceStream, StreamingTextResponse } from '.';
+import {
+  HuggingFaceStream,
+  StreamingTextResponse,
+  experimental_StreamData,
+} from '.';
 import { createClient } from '../tests/utils/mock-client';
 import { setup } from '../tests/utils/mock-service';
 
 describe('HuggingFace stream', () => {
+  const Hf = new HfInference();
+
   let server: ReturnType<typeof setup>;
   beforeAll(() => {
     server = setup(3033);
@@ -17,24 +23,21 @@ describe('HuggingFace stream', () => {
   }
 
   it('should be able to parse HuggingFace response and receive the streamed response', async () => {
-    const Hf = new HfInference();
-
-    const hfResponse = Hf.textGenerationStream(
-      { model: 'model', inputs: '' },
-      {
-        fetch() {
-          return fetch(server.api, {
-            headers: {
-              'x-mock-service': 'huggingface',
-              'x-mock-type': 'chat',
-            },
-          });
+    const stream = HuggingFaceStream(
+      Hf.textGenerationStream(
+        { model: 'model', inputs: '' },
+        {
+          fetch() {
+            return fetch(server.api, {
+              headers: {
+                'x-mock-service': 'huggingface',
+                'x-mock-type': 'chat',
+              },
+            });
+          },
         },
-      },
+      ),
     );
-
-    // Convert the async generator into a friendly text-stream
-    const stream = HuggingFaceStream(hfResponse);
 
     const response = new StreamingTextResponse(stream);
 
@@ -44,5 +47,80 @@ describe('HuggingFace stream', () => {
       ' world',
       '.',
     ]);
+  });
+
+  describe('StreamData prototcol', () => {
+    it('should send text', async () => {
+      const data = new experimental_StreamData();
+
+      const stream = HuggingFaceStream(
+        Hf.textGenerationStream(
+          { model: 'model', inputs: '' },
+          {
+            fetch() {
+              return fetch(server.api, {
+                headers: {
+                  'x-mock-service': 'huggingface',
+                  'x-mock-type': 'chat',
+                },
+              });
+            },
+          },
+        ),
+        {
+          onFinal() {
+            data.close();
+          },
+          experimental_streamData: true,
+        },
+      );
+
+      const response = new StreamingTextResponse(stream, {}, data);
+
+      expect(await readAllChunks(response)).toEqual([
+        '0:"Hello"\n',
+        '0:","\n',
+        '0:" world"\n',
+        '0:"."\n',
+      ]);
+    });
+
+    it('should send text and data', async () => {
+      const data = new experimental_StreamData();
+
+      data.append({ t1: 'v1' });
+
+      const stream = HuggingFaceStream(
+        Hf.textGenerationStream(
+          { model: 'model', inputs: '' },
+          {
+            fetch() {
+              return fetch(server.api, {
+                headers: {
+                  'x-mock-service': 'huggingface',
+                  'x-mock-type': 'chat',
+                },
+              });
+            },
+          },
+        ),
+        {
+          onFinal() {
+            data.close();
+          },
+          experimental_streamData: true,
+        },
+      );
+
+      const response = new StreamingTextResponse(stream, {}, data);
+
+      expect(await readAllChunks(response)).toEqual([
+        '2:"[{\\"t1\\":\\"v1\\"}]"\n',
+        '0:"Hello"\n',
+        '0:","\n',
+        '0:" world"\n',
+        '0:"."\n',
+      ]);
+    });
   });
 });
