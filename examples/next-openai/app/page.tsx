e6db317162ba9644f5bd9513210d4d4f51aa380e
Lars Grammel <lars.grammel@gmail.com>
2023-11-08 20:53:18 +0100

    Cleanup.

--- examples/next-openai/app/page.tsx
+++ examples/next-openai/app/page.tsx
@@ -28,7 +28,7 @@ export default function Chat() {
         <input
           className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
           value={input}
-          placeholder="Say say something..."
+          placeholder="Say something..."
           onChange={handleInputChange}
         />
       </form>
