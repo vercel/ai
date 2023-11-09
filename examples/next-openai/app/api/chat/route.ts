9d685cb439cfd387a9cad5b4d34a259c22d18bde
Lars Grammel <lars.grammel@gmail.com>
2023-11-09 09:27:19 +0100

    Revert example change.

--- examples/next-openai/app/api/chat/route.ts
+++ examples/next-openai/app/api/chat/route.ts
@@ -16,7 +16,7 @@ export async function POST(req: Request) {
 
   // Ask OpenAI for a streaming chat completion given the prompt
   const response = await openai.chat.completions.create({
-    model: 'gpt-4-vision-preview',
+    model: 'gpt-3.5-turbo',
     stream: true,
     messages: messages,
   });
