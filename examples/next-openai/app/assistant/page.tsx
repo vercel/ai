5a2b249df1dc278064d6e52bccc3fa83176c8af7
Lars Grammel <lars.grammel@gmail.com>
2023-11-10 11:06:44 +0100

    Automatically focus input when done. Nicer status.

--- examples/next-openai/app/assistant/page.tsx
+++ examples/next-openai/app/assistant/page.tsx
@@ -2,6 +2,7 @@
 
 import { Message } from 'ai/react';
 import { experimental_useAssistant } from './useAssistant';
+import { useEffect, useRef } from 'react';
 
 const roleToColorMap: Record<Message['role'], string> = {
   system: 'red',
@@ -23,12 +24,17 @@ export default function Chat() {
     api: '/api/assistant',
   });
 
+  const inputRef = useRef<HTMLInputElement>(null); // Create a ref for the input element
+
+  useEffect(() => {
+    // When acceptsMessage changes and is true, focus the input
+    if (acceptsMessage) {
+      inputRef.current?.focus();
+    }
+  }, [acceptsMessage]);
+
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-      <pre>
-        <code>{JSON.stringify(status, null, 2)}</code>
-      </pre>
-
       {data && (
         <pre>
           <code>{JSON.stringify(data, null, 2)}</code>
@@ -48,12 +54,17 @@ export default function Chat() {
         </div>
       ))}
 
+      {status?.status === 'in_progress' && (
+        <div className="h-8 w-full max-w-md p-2 mb-8 bg-gray-300 dark:bg-gray-600 rounded-lg animate-pulse" />
+      )}
+
       <form onSubmit={submitMessage}>
         <input
+          ref={inputRef}
           disabled={!acceptsMessage}
           className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
           value={input}
-          placeholder="Say something..."
+          placeholder="What is the temperature in the living room?"
           onChange={handleInputChange}
         />
       </form>
