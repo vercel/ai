6a69684339e63c046e0f23cc33fcc5508603820f
Lars Grammel <lars.grammel@gmail.com>
2023-11-20 16:09:41 +0100

    Fix nuxt/openai example.

--- examples/nuxt-openai/server/api/chat.ts
+++ examples/nuxt-openai/server/api/chat.ts
@@ -1,6 +1,6 @@
 import OpenAI from 'openai';
 import { OpenAIStream } from 'ai';
-import { CreateChatCompletionRequestMessage } from 'openai/resources/chat';
+import { ChatCompletionMessageParam } from 'openai/resources/chat';
 
 export default defineLazyEventHandler(async () => {
   const apiKey = useRuntimeConfig().openaiApiKey;
@@ -9,20 +9,17 @@ export default defineLazyEventHandler(async () => {
     apiKey: apiKey,
   });
 
-  return defineEventHandler(async event => {
+  return defineEventHandler(async (event: any) => {
     // Extract the `prompt` from the body of the request
     const { messages } = (await readBody(event)) as {
-      messages: CreateChatCompletionRequestMessage[];
+      messages: ChatCompletionMessageParam[];
     };
 
     // Ask OpenAI for a streaming chat completion given the prompt
     const response = await openai.chat.completions.create({
       model: 'gpt-3.5-turbo',
       stream: true,
-      messages: messages.map(message => ({
-        content: message.content,
-        role: message.role,
-      })),
+      messages: messages,
     });
 
     // Convert the response into a friendly text-stream
