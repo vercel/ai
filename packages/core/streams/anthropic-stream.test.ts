ce0e33881e5519b97c047bcfe65d5233f7ef9849
Lars Grammel <lars.grammel@gmail.com>
2023-11-21 19:39:21 +0100

    Add characterization tests for Anthropic.

---
+++ packages/core/streams/anthropic-stream.test.ts
@@ -0,0 +1,128 @@
+import {
+  AnthropicStream,
+  StreamingTextResponse,
+  experimental_StreamData,
+} from '.';
+import { readAllChunks } from '../tests/utils/mock-client';
+import { setup } from '../tests/utils/mock-service';
+import Anthropic from '@anthropic-ai/sdk';
+
+describe('AnthropicStream', () => {
+  let server: ReturnType<typeof setup>;
+  beforeAll(() => {
+    server = setup(3034);
+  });
+  afterAll(async () => server.teardown());
+
+  it('should be able to parse SSE and receive the streamed response', async () => {
+    const anthropic = new Anthropic({
+      fetch: () =>
+        fetch(server.api, {
+          headers: {
+            'x-mock-service': 'anthropic',
+            'x-mock-type': 'chat',
+          },
+        }),
+      apiKey: 'sk-doesnt-matter',
+    });
+
+    const anthropicResponse = await anthropic.completions.create({
+      prompt: '',
+      model: 'claude-2',
+      stream: true,
+      max_tokens_to_sample: 300,
+    });
+
+    const stream = AnthropicStream(anthropicResponse);
+
+    const response = new StreamingTextResponse(stream);
+
+    expect(await readAllChunks(response)).toEqual([
+      ' Hello',
+      ',',
+      ' world',
+      '.',
+    ]);
+  });
+
+  describe('StreamData protocol', () => {
+    it('should send text', async () => {
+      const anthropic = new Anthropic({
+        fetch: () =>
+          fetch(server.api, {
+            headers: {
+              'x-mock-service': 'anthropic',
+              'x-mock-type': 'chat',
+            },
+          }),
+        apiKey: 'sk-doesnt-matter',
+      });
+
+      const data = new experimental_StreamData();
+
+      const anthropicResponse = await anthropic.completions.create({
+        prompt: '',
+        model: 'claude-2',
+        stream: true,
+        max_tokens_to_sample: 300,
+      });
+
+      const stream = AnthropicStream(anthropicResponse, {
+        onFinal() {
+          data.close();
+        },
+        experimental_streamData: true,
+      });
+
+      const response = new StreamingTextResponse(stream, {}, data);
+
+      expect(await readAllChunks(response)).toEqual([
+        '0:" Hello"\n',
+        '0:","\n',
+        '0:" world"\n',
+        '0:"."\n',
+      ]);
+    });
+
+    it('should send text and data', async () => {
+      const anthropic = new Anthropic({
+        fetch: () =>
+          fetch(server.api, {
+            headers: {
+              'x-mock-service': 'anthropic',
+              'x-mock-type': 'chat',
+            },
+          }),
+        apiKey: 'sk-doesnt-matter',
+      });
+
+      const data = new experimental_StreamData();
+
+      data.append({ t1: 'v1' });
+
+      const anthropicResponse = await anthropic.completions.create({
+        prompt: '',
+        model: 'claude-2',
+        stream: true,
+        max_tokens_to_sample: 300,
+      });
+
+      const stream = AnthropicStream(anthropicResponse, {
+        onFinal() {
+          data.close();
+        },
+        experimental_streamData: true,
+      });
+
+      const response = new StreamingTextResponse(stream, {}, data);
+
+      expect(await readAllChunks(response)).toEqual([
+        '2:[{"t1":"v1"}]\n',
+        '0:" Hello"\n',
+        '0:","\n',
+        '0:" world"\n',
+        '0:"."\n',
+      ]);
+    });
+  });
+});
---
+++ packages/core/tests/snapshots/anthropic.ts
@@ -0,0 +1,37 @@
+export const anthropicChunks = [
+  {
+    completion: ' Hello',
+    stop_reason: null,
+    stop: null,
+    model: 'claude-2.1',
+    log_id: 'e44a7f8a3dbe7e755c98fc507a70dfb320192bb99059edc8fb3ff8ff068a2ab9',
+  },
+  {
+    completion: ',',
+    stop_reason: null,
+    stop: null,
+    model: 'claude-2.1',
+    log_id: 'e44a7f8a3dbe7e755c98fc507a70dfb320192bb99059edc8fb3ff8ff068a2ab9',
+  },
+  {
+    completion: ' world',
+    stop_reason: null,
+    stop: null,
+    model: 'claude-2.1',
+    log_id: 'e44a7f8a3dbe7e755c98fc507a70dfb320192bb99059edc8fb3ff8ff068a2ab9',
+  },
+  {
+    completion: '.',
+    stop_reason: null,
+    stop: null,
+    model: 'claude-2.1',
+    log_id: 'e44a7f8a3dbe7e755c98fc507a70dfb320192bb99059edc8fb3ff8ff068a2ab9',
+  },
+  {
+    completion: '',
+    stop_reason: 'stop_sequence',
+    stop: '\n\nHuman:',
+    model: 'claude-2.1',
+    log_id: 'e44a7f8a3dbe7e755c98fc507a70dfb320192bb99059edc8fb3ff8ff068a2ab9',
+  },
+];
