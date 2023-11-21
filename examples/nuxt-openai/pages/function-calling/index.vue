b4cbdd547aa168abb8b63ad1a4e1e9e9b8fa38a4
Lars Grammel <lars.grammel@gmail.com>
2023-11-21 14:34:25 +0100

    Add Nuxt function calling example.

---
+++ examples/nuxt-openai/pages/function-calling/index.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { useChat } from 'ai/vue';
+
+const { messages, input, handleSubmit } = useChat({
+  api: '/api/chat-with-functions',
+  headers: { 'Content-Type': 'application/json' },
+});
+</script>
+
+<template>
+  <div class="flex flex-col w-full max-w-md py-24 mx-auto stretch">
+    <div v-for="m in messages" key="m.id" class="whitespace-pre-wrap">
+      {{ m.role === 'user' ? 'User: ' : 'AI: ' }}
+      {{ m.content }}
+    </div>
+
+    <form @submit="handleSubmit">
+      <input
+        class="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
+        v-model="input"
+        placeholder="Say something..."
+      />
+    </form>
+  </div>
+</template>
