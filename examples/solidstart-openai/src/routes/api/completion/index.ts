2522d1ecb092536a31bd9408ec0e429bc4ae933d
Lars Grammel <lars.grammel@gmail.com>
2023-11-24 13:46:43 +0100

    Add completion example for Solid.js

---
+++ examples/solidstart-openai/src/routes/api/completion/index.ts
@@ -0,0 +1,41 @@
+import {
+  OpenAIStream,
+  StreamingTextResponse,
+  experimental_StreamData,
+} from 'ai';
+import OpenAI from 'openai';
+
+import { APIEvent } from 'solid-start/api';
+
+// Create an OpenAI API client
+const openai = new OpenAI({
+  apiKey: process.env['OPENAI_API_KEY'] || '',
+});
+
+export const POST = async (event: APIEvent) => {
+  // Extract the `prompt` from the body of the request
+  const { prompt } = await event.request.json();
+
+  // Ask OpenAI for a streaming chat completion given the prompt
+  const response = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo',
+    stream: true,
+    messages: [{ role: 'user', content: prompt }],
+  });
+
+  // optional: use stream data
+  // const data = new experimental_StreamData();
+
+  // data.append({ test: 'value' });
+
+  // Convert the response into a friendly text-stream
+  const stream = OpenAIStream(response, {
+    onFinal(completion) {
+      // data.close();
+    },
+    // experimental_streamData: true,
+  });
+
+  // Respond with the stream
+  return new StreamingTextResponse(stream); // TODO
+};
