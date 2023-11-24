d5b14d6919df79df7768e3a0f1b583ffa72109be
Lars Grammel <lars.grammel@gmail.com>
2023-11-24 11:22:10 +0100

    Add useCompletion example for Vue.

---
+++ examples/nuxt-openai/pages/completion/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { useCompletion } from 'ai/vue';
+
+const { completion, input, handleSubmit } = useCompletion();
+</script>
+
+<template>
+  <div class="flex flex-col w-full max-w-md py-24 mx-auto stretch">
+    <h4 class="text-xl font-bold text-gray-900 md:text-xl pb-4">
+      useCompletion Example
+    </h4>
+    {{ completion }}
+    <form @submit="handleSubmit">
+      <input
+        class="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
+        v-model="input"
+        placeholder="Say something..."
+      />
+    </form>
+  </div>
+</template>
--- examples/nuxt-openai/server/api/completion.ts
+++ examples/nuxt-openai/server/api/completion.ts
@@ -1,6 +1,5 @@
 import { OpenAIStream, StreamingTextResponse } from 'ai';
 import OpenAI from 'openai';
-import { ChatCompletionMessageParam } from 'openai/resources/chat';
 
 export default defineLazyEventHandler(async () => {
   const apiKey = useRuntimeConfig().openaiApiKey;
@@ -11,15 +10,17 @@ export default defineLazyEventHandler(async () => {
 
   return defineEventHandler(async (event: any) => {
     // Extract the `prompt` from the body of the request
-    const { messages } = (await readBody(event)) as {
-      messages: ChatCompletionMessageParam[];
+    const newVariable = await readBody(event);
+    console.log('nw', newVariable);
+    const { prompt } = newVariable as {
+      prompt: string;
     };
 
     // Ask OpenAI for a streaming chat completion given the prompt
     const response = await openai.chat.completions.create({
       model: 'gpt-3.5-turbo',
       stream: true,
-      messages,
+      messages: [{ role: 'user', content: prompt }],
     });
 
     // Convert the response into a friendly text-stream
