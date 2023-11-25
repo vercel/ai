8b4f7d1219d4a764a3a7226d546fb232d217dd93
Lars Grammel <lars.grammel@gmail.com>
2023-11-25 08:46:35 +0100

    Add data stream support to useCompletion / React. (#771)

---
+++ examples/next-openai/app/api/completion/route.ts
@@ -0,0 +1,28 @@
+import OpenAI from 'openai';
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+
+// Create an OpenAI API client (that's edge friendly!)
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY || '',
+});
+
+// IMPORTANT! Set the runtime to edge
+export const runtime = 'edge';
+
+export async function POST(req: Request) {
+  // Extract the `prompt` from the body of the request
+  const { prompt } = await req.json();
+
+  // Ask OpenAI for a streaming chat completion given the prompt
+  const response = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo',
+    stream: true,
+    messages: [{ role: 'user', content: prompt }],
+  });
+
+  // Convert the response into a friendly text-stream
+  const stream = OpenAIStream(response);
+
+  // Respond with the stream
+  return new StreamingTextResponse(stream);
+}
--- packages/core/tests/utils/mock-fetch.ts
+++ packages/core/tests/utils/mock-fetch.ts
@@ -1,6 +1,12 @@
 import { COMPLEX_HEADER } from '../../shared/utils';
 
-export function mockFetchDataStream(chunks: string[]) {
+export function mockFetchTextStream({
+  url,
+  chunks,
+}: {
+  url: string;
+  chunks: string[];
+}) {
   jest.spyOn(global, 'fetch').mockImplementation(async () => {
     function* generateChunks() {
       for (const chunk of chunks) {
@@ -11,7 +17,44 @@ export function mockFetchDataStream(chunks: string[]) {
     const chunkGenerator = generateChunks();
 
     return {
-      url: 'https://example.com/api/chat',
+      url,
+      ok: true,
+      status: 200,
+      bodyUsed: false,
+      headers: new Map() as any as Headers,
+      body: {
+        getReader() {
+          return {
+            read() {
+              return Promise.resolve(chunkGenerator.next());
+            },
+            releaseLock() {},
+            cancel() {},
+          };
+        },
+      },
+    } as unknown as Response;
+  });
+}
+
+export function mockFetchDataStream({
+  url,
+  chunks,
+}: {
+  url: string;
+  chunks: string[];
+}) {
+  jest.spyOn(global, 'fetch').mockImplementation(async () => {
+    function* generateChunks() {
+      for (const chunk of chunks) {
+        yield new TextEncoder().encode(chunk);
+      }
+    }
+
+    const chunkGenerator = generateChunks();
+
+    return {
+      url,
       ok: true,
       status: 200,
       headers: new Map<string, string>([
