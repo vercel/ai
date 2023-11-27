166f13fcbbb7faba17ad869939e7d4bb54957388
Lars Grammel <lars.grammel@gmail.com>
2023-11-27 16:24:31 +0100

    Add chain example.

--- examples/next-langchain/app/stream-data-basic/page.tsx
+++ examples/next-langchain/app/stream-data-basic/page.tsx
@@ -4,7 +4,7 @@ import { useChat } from 'ai/react';
 
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat({
-    api: '/api/stream-data',
+    api: '/api/stream-data-basic',
   });
 
   return (
