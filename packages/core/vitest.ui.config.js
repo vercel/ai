792fc0c78e603880d5ca6c4aa5c41e57f97fe77c
Lars Grammel <lars.grammel@gmail.com>
2023-11-29 20:22:38 +0100

    Vue UI tests with vitest.

--- packages/core/vitest.ui.config.js
+++ packages/core/vitest.ui.config.js
@@ -1,7 +1,10 @@
+import react from '@vitejs/plugin-react';
+import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [vue(), react()],
   test: {
     environment: 'jsdom',
     include: ['**/*.ui.test.ts', '**/*.ui.test.tsx'],
