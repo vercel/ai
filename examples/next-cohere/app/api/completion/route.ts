ff95055a24be9c8ad44c4d77e2ed9a778f1ab24e
Lars Grammel <lars.grammel@gmail.com>
2023-11-29 17:18:49 +0100

    Add Cohere example.

---
+++ examples/next-cohere/app/api/completion/route.ts
@@ -0,0 +1,39 @@
+import { CohereStream, StreamingTextResponse } from 'ai';
+
+// IMPORTANT! Set the runtime to edge
+export const runtime = 'edge';
+
+export async function POST(req: Request) {
+  // Extract the `prompt` from the body of the request
+  const { prompt } = await req.json();
+
+  const response = await fetch('https://api.cohere.ai/v1/generate', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${process.env.COHERE_API_KEY}`,
+    },
+    body: JSON.stringify({
+      prompt,
+      model: 'command-nightly',
+      max_tokens: 300,
+      stop_sequences: [],
+      temperature: 0.9,
+      return_likelihoods: 'NONE',
+      stream: true,
+    }),
+  });
+
+  // Check for errors
+  if (!response.ok) {
+    return new Response(await response.text(), {
+      status: response.status,
+    });
+  }
+
+  // Extract the text response from the Cohere stream
+  const stream = CohereStream(response);
+
+  // Respond with the stream
+  return new StreamingTextResponse(stream);
+}
