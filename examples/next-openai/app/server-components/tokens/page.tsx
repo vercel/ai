61675ecc61518fe003982c0e0e6453dde39388ba
Lars Grammel <lars.grammel@gmail.com>
2023-11-30 18:46:16 +0100

    Provide default.

--- examples/next-openai/app/server-components/tokens/page.tsx
+++ examples/next-openai/app/server-components/tokens/page.tsx
@@ -6,7 +6,7 @@ export const runtime = 'edge';
 
 export default async function Page() {
   const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY!,
+    apiKey: process.env.OPENAI_API_KEY ?? '',
   });
 
   const response = await openai.chat.completions.create({
