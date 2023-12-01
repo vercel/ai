22386a51055396f0f0ba2fb1252d1d1c75553844
Lars Grammel <lars.grammel@gmail.com>
2023-12-01 20:23:24 +0100

    Prettier.

--- examples/next-openai-pages/pages/_app.tsx
+++ examples/next-openai-pages/pages/_app.tsx
@@ -1,5 +1,5 @@
-import "./globals.css";
-import type { AppProps } from "next/app";
+import './globals.css';
+import type { AppProps } from 'next/app';
 
 export default function ExampleApp({
   Component,
