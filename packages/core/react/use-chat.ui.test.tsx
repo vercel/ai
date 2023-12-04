118b7ce84a8c7211acea4120f5f7100da2160a22
Lars Grammel <lars.grammel@gmail.com>
2023-12-04 21:18:21 +0100

    Add tests for isLoading. (#823)

--- packages/core/react/use-chat.ui.test.tsx
+++ packages/core/react/use-chat.ui.test.tsx
@@ -1,20 +1,20 @@
 import '@testing-library/jest-dom/vitest';
 import { cleanup, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { afterEach, describe, expect, test, vi } from 'vitest';
-import { mockFetchDataStream, mockFetchError } from '../tests/utils/mock-fetch';
+import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest';
+import {
+  mockFetchDataStream,
+  mockFetchDataStreamWithGenerator,
+  mockFetchError,
+} from '../tests/utils/mock-fetch';
 import { useChat } from './use-chat';
 
-// mock nanoid import
-vi.mock('nanoid', () => ({
-  nanoid: () => Math.random().toString(36).slice(2, 9),
-}));
-
 const TestComponent = () => {
-  const { messages, append, error, data } = useChat();
+  const { messages, append, error, data, isLoading } = useChat();
 
   return (
     <div>
+      <div data-testid="loading">{isLoading.toString()}</div>
       {error && <div data-testid="error">{error.toString()}</div>}
       {data && <div data-testid="data">{JSON.stringify(data)}</div>}
       {messages.map((m, idx) => (
@@ -33,60 +33,93 @@ const TestComponent = () => {
   );
 };
 
-describe('useChat', () => {
-  afterEach(() => {
-    vi.restoreAllMocks();
-    cleanup();
+beforeEach(() => {
+  render(<TestComponent />);
+});
+
+afterEach(() => {
+  vi.restoreAllMocks();
+  cleanup();
+});
+
+test('Shows streamed complex text response', async () => {
+  mockFetchDataStream({
+    url: 'https://example.com/api/chat',
+    chunks: ['0:"Hello"\n', '0:","\n', '0:" world"\n', '0:"."\n'],
   });
 
-  test('Shows streamed complex text response', async () => {
-    render(<TestComponent />);
+  await userEvent.click(screen.getByTestId('button'));
 
-    mockFetchDataStream({
+  await screen.findByTestId('message-0');
+  expect(screen.getByTestId('message-0')).toHaveTextContent('User: hi');
+
+  await screen.findByTestId('message-1');
+  expect(screen.getByTestId('message-1')).toHaveTextContent(
+    'AI: Hello, world.',
+  );
+});
+
+test('Shows streamed complex text response with data', async () => {
+  mockFetchDataStream({
+    url: 'https://example.com/api/chat',
+    chunks: ['2:[{"t1":"v1"}]\n', '0:"Hello"\n'],
+  });
+
+  await userEvent.click(screen.getByTestId('button'));
+
+  await screen.findByTestId('data');
+  expect(screen.getByTestId('data')).toHaveTextContent('[{"t1":"v1"}]');
+
+  await screen.findByTestId('message-1');
+  expect(screen.getByTestId('message-1')).toHaveTextContent('AI: Hello');
+});
+
+test('Shows error response', async () => {
+  mockFetchError({ statusCode: 404, errorMessage: 'Not found' });
+
+  await userEvent.click(screen.getByTestId('button'));
+
+  // TODO bug? the user message does not show up
+  // await screen.findByTestId('message-0');
+  // expect(screen.getByTestId('message-0')).toHaveTextContent('User: hi');
+
+  await screen.findByTestId('error');
+  expect(screen.getByTestId('error')).toHaveTextContent('Error: Not found');
+});
+
+describe('loading state', () => {
+  test('should show loading state', async () => {
+    let finishGeneration: ((value?: unknown) => void) | undefined;
+    const finishGenerationPromise = new Promise(resolve => {
+      finishGeneration = resolve;
+    });
+
+    mockFetchDataStreamWithGenerator({
       url: 'https://example.com/api/chat',
-      chunks: ['0:"Hello"\n', '0:","\n', '0:" world"\n', '0:"."\n'],
+      chunkGenerator: (async function* generate() {
+        const encoder = new TextEncoder();
+        yield encoder.encode('0:"Hello"\n');
+        await finishGenerationPromise;
+      })(),
     });
 
     await userEvent.click(screen.getByTestId('button'));
 
-    await screen.findByTestId('message-0');
-    expect(screen.getByTestId('message-0')).toHaveTextContent('User: hi');
+    await screen.findByTestId('loading');
+    expect(screen.getByTestId('loading')).toHaveTextContent('true');
 
-    await screen.findByTestId('message-1');
-    expect(screen.getByTestId('message-1')).toHaveTextContent(
-      'AI: Hello, world.',
-    );
+    finishGeneration?.();
+
+    await screen.findByTestId('loading');
+    expect(screen.getByTestId('loading')).toHaveTextContent('false');
   });
 
-  test('Shows streamed complex text response with data', async () => {
-    render(<TestComponent />);
-
-    mockFetchDataStream({
-      url: 'https://example.com/api/chat',
-      chunks: ['2:[{"t1":"v1"}]\n', '0:"Hello"\n'],
-    });
-
-    await userEvent.click(screen.getByTestId('button'));
-
-    await screen.findByTestId('data');
-    expect(screen.getByTestId('data')).toHaveTextContent('[{"t1":"v1"}]');
-
-    await screen.findByTestId('message-1');
-    expect(screen.getByTestId('message-1')).toHaveTextContent('AI: Hello');
-  });
-
-  test('Shows error response', async () => {
-    render(<TestComponent />);
-
+  test('should reset loading state on error', async () => {
     mockFetchError({ statusCode: 404, errorMessage: 'Not found' });
 
     await userEvent.click(screen.getByTestId('button'));
 
-    // TODO bug? the user message does not show up
-    // await screen.findByTestId('message-0');
-    // expect(screen.getByTestId('message-0')).toHaveTextContent('User: hi');
-
-    await screen.findByTestId('error');
-    expect(screen.getByTestId('error')).toHaveTextContent('Error: Not found');
+    await screen.findByTestId('loading');
+    expect(screen.getByTestId('loading')).toHaveTextContent('false');
   });
 });
--- packages/core/vue/TestComponent.vue
+++ packages/core/vue/TestComponent.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import { useChat } from './use-chat';
 
-const { messages, append, data, error } = useChat();
+const { messages, append, data, error, isLoading } = useChat();
 </script>
 
 <template>
   <div class="flex flex-col w-full max-w-md py-24 mx-auto stretch">
+    <div data-testid="loading">{{ isLoading?.toString() }}</div>
     <div data-testid="error">{{ error?.toString() }}</div>
     <div data-testid="data">{{ JSON.stringify(data) }}</div>
     <div
