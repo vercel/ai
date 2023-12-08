d356799060b1d975ee5adda73de1012feb240c75
Lars Grammel <lars.grammel@gmail.com>
2023-12-08 16:13:32 +0100

    Update test.

--- packages/core/svelte/use-chat.ui.test.tsx
+++ packages/core/svelte/use-chat.ui.test.tsx
@@ -1,68 +1,58 @@
 import '@testing-library/jest-dom/vitest';
 import { render, screen } from '@testing-library/svelte';
 import userEvent from '@testing-library/user-event';
-import { afterEach, describe, expect, test, vi } from 'vitest';
 import { mockFetchDataStream, mockFetchError } from '../tests/utils/mock-fetch';
 import TestComponent from './TestComponent.svelte';
 
-// mock nanoid import
-vi.mock('nanoid', () => ({
-  nanoid: () => Math.random().toString(36).slice(2, 9),
-}));
+beforeEach(() => {
+  render(TestComponent);
+});
 
-describe('useChat', () => {
-  afterEach(() => {
-    vi.restoreAllMocks();
+afterEach(() => {
+  vi.restoreAllMocks();
+});
+
+it('should return messages', async () => {
+  mockFetchDataStream({
+    url: 'https://example.com/api/chat',
+    chunks: ['0:"Hello"\n', '0:","\n', '0:" world"\n', '0:"."\n'],
   });
 
-  test('Shows streamed complex text response', async () => {
-    render(TestComponent);
+  await userEvent.click(screen.getByTestId('button'));
 
-    mockFetchDataStream({
-      url: 'https://example.com/api/chat',
-      chunks: ['0:"Hello"\n', '0:","\n', '0:" world"\n', '0:"."\n'],
-    });
+  await screen.findByTestId('message-0');
+  expect(screen.getByTestId('message-0')).toHaveTextContent('User: hi');
 
-    await userEvent.click(screen.getByTestId('button'));
+  await screen.findByTestId('message-1');
+  expect(screen.getByTestId('message-1')).toHaveTextContent(
+    'AI: Hello, world.',
+  );
+});
 
-    await screen.findByTestId('message-0');
-    expect(screen.getByTestId('message-0')).toHaveTextContent('User: hi');
-
-    await screen.findByTestId('message-1');
-    expect(screen.getByTestId('message-1')).toHaveTextContent(
-      'AI: Hello, world.',
-    );
+it('should return messages and data', async () => {
+  mockFetchDataStream({
+    url: 'https://example.com/api/chat',
+    chunks: ['2:[{"t1":"v1"}]\n', '0:"Hello"\n'],
   });
 
-  test('Shows streamed complex text response with data', async () => {
-    render(TestComponent);
+  await userEvent.click(screen.getByTestId('button'));
 
-    mockFetchDataStream({
-      url: 'https://example.com/api/chat',
-      chunks: ['2:[{"t1":"v1"}]\n', '0:"Hello"\n'],
-    });
+  await screen.findByTestId('data');
+  expect(screen.getByTestId('data')).toHaveTextContent('[{"t1":"v1"}]');
 
-    await userEvent.click(screen.getByTestId('button'));
+  await screen.findByTestId('message-1');
+  expect(screen.getByTestId('message-1')).toHaveTextContent('AI: Hello');
+});
 
-    await screen.findByTestId('data');
-    expect(screen.getByTestId('data')).toHaveTextContent('[{"t1":"v1"}]');
+it('should return error', async () => {
+  mockFetchError({ statusCode: 404, errorMessage: 'Not found' });
 
-    await screen.findByTestId('message-1');
-    expect(screen.getByTestId('message-1')).toHaveTextContent('AI: Hello');
-  });
+  await userEvent.click(screen.getByTestId('button'));
 
-  test('Shows error response', async () => {
-    render(TestComponent);
+  // TODO bug? the user message does not show up
+  // await screen.findByTestId('message-0');
+  // expect(screen.getByTestId('message-0')).toHaveTextContent('User: hi');
 
-    mockFetchError({ statusCode: 404, errorMessage: 'Not found' });
-
-    await userEvent.click(screen.getByTestId('button'));
-
-    // TODO bug? the user message does not show up
-    // await screen.findByTestId('message-0');
-    // expect(screen.getByTestId('message-0')).toHaveTextContent('User: hi');
-
-    await screen.findByTestId('error');
-    expect(screen.getByTestId('error')).toHaveTextContent('Error: Not found');
-  });
+  await screen.findByTestId('error');
+  expect(screen.getByTestId('error')).toHaveTextContent('Error: Not found');
 });
