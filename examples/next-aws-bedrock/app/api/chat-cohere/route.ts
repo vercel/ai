a2acb0bf89d653f301010c6328b9734cb8fa15a6
Lars Grammel <lars.grammel@gmail.com>
2023-12-13 15:54:18 +0100

    Set region default.

--- examples/next-aws-bedrock/app/api/chat-cohere/route.ts
+++ examples/next-aws-bedrock/app/api/chat-cohere/route.ts
@@ -22,7 +22,7 @@ function buildPrompt(
 }
 
 const bedrockClient = new BedrockRuntimeClient({
-  region: process.env.AWS_REGION ?? '',
+  region: process.env.AWS_REGION ?? 'us-east-1',
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? '',
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? '',
--- examples/next-aws-bedrock/app/api/chat-llama2/route.ts
+++ examples/next-aws-bedrock/app/api/chat-llama2/route.ts
@@ -9,7 +9,7 @@ import { experimental_buildLlama2Prompt } from 'ai/prompts';
 export const runtime = 'edge';
 
 const bedrockClient = new BedrockRuntimeClient({
-  region: process.env.AWS_REGION ?? '',
+  region: process.env.AWS_REGION ?? 'us-east-1',
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? '',
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? '',
