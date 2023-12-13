3c2f6a68b7ccdfbeaf4400211e7ae1699aeafa3f
Nick Gomez <nick@inkeep.com>
2023-12-12 21:27:23 -0500

    putting chat session id into body, moving integration id into api route

--- packages/core/streams/inkeep-stream.ts
+++ packages/core/streams/inkeep-stream.ts
@@ -4,66 +4,108 @@ import {
   readableFromAsyncIterable,
   type AIStreamCallbacksAndOptions,
   createCallbacksTransformer,
+  AIStreamParser,
 } from './ai-stream';
+
+import { z } from 'zod';
 import { createStreamDataTransformer } from './stream-data';
-import { createParser } from 'eventsource-parser';
 
-interface InkeepContentChunk {
+export type AIStreamCallbacksAndOptionsWithInkeep =
+  AIStreamCallbacksAndOptions &
+    Pick<InkeepChatResultCallbacks, 'onCompleteMessage'>;
+
+// Schema for an Inkeep Message Chunk
+const InkeepMessageChunkDataSchema = z.object({
+  chat_session_id: z.string(),
+  content_chunk: z.string(),
+  finish_reason: z.union([z.string(), z.null()]).optional(),
+});
+
+export type InkeepMessageChunkData = z.infer<
+  typeof InkeepMessageChunkDataSchema
+>;
+
+export type InkeepMessage = {
+  role: 'user' | 'assistant';
+  content: string;
+};
+
+export type InkeepCompleteMessage = {
   chat_session_id: string;
-  message_chunk: string;
-}
+  message: InkeepMessage;
+};
 
-async function* createInkeepContentChunkIterable(
-  stream: ReadableStream<string>,
-): AsyncIterable<InkeepContentChunk> {
-  const reader = stream.getReader();
-  const queue: InkeepContentChunk[] = [];
-
-  const parser = createParser(event => {
-    if (event.type === 'event') {
-      const inkeepContentChunk = JSON.parse(event.data) as InkeepContentChunk;
-      queue.push(inkeepContentChunk);
-    }
-  });
-
-  while (true) {
-    const { done, value } = await reader.read();
-    if (done) break;
-    parser.feed(value);
-
-    while (queue.length > 0) {
-      yield queue.shift()!;
-    }
-  }
-}
-
-async function* streamableInkeep(stream: ReadableStream<string>) {
-  for await (const chunk of createInkeepContentChunkIterable(stream)) {
-    const text = chunk.message_chunk;
-    if (text) yield text;
-  }
-}
+export type InkeepChatResultCallbacks = {
+  onCompleteMessage?: (completeMessage: InkeepCompleteMessage) => void;
+};
 
 export function InkeepStream(
   res: Response,
-  cb?: AIStreamCallbacksAndOptions,
+  callbacks?: AIStreamCallbacksAndOptionsWithInkeep,
 ): ReadableStream {
   if (!res.body) {
     throw new Error('Response body is null');
   }
 
-  const textDecoder = new TextDecoder();
-  const stringStream = new TransformStream({
-    transform(chunk, controller) {
-      controller.enqueue(textDecoder.decode(chunk));
+  let completeContent = '';
+  let chat_session_id = '';
+
+  const inkeepEventParser: AIStreamParser = (data: string) => {
+    let inkeepContentChunk: InkeepMessageChunkData;
+    try {
+      inkeepContentChunk = InkeepMessageChunkDataSchema.parse(
+        JSON.parse(data),
+      ) as InkeepMessageChunkData;
+    } catch (error) {
+      return;
+    }
+
+    chat_session_id = inkeepContentChunk.chat_session_id;
+    completeContent += inkeepContentChunk.content_chunk;
+
+    return inkeepContentChunk.content_chunk;
+  };
+
+  let onCompleteMessage;
+  let userCoreCallbacks: AIStreamCallbacksAndOptions | undefined;
+
+  if (callbacks) {
+    ({ onCompleteMessage, ...userCoreCallbacks } = callbacks);
+  }
+
+  const inkeepCallbacks: InkeepChatResultCallbacks = {
+    onCompleteMessage,
+  };
+
+  let callbacksCore = { ...userCoreCallbacks };
+
+  callbacksCore = {
+    ...callbacksCore,
+    onEvent: e => {
+      if (userCoreCallbacks?.onEvent) {
+        userCoreCallbacks.onEvent(e);
+      }
+      if (e.type === 'event') {
+        if (e.event === 'message_chunk') {
+          const data = InkeepMessageChunkDataSchema.parse(
+            JSON.parse(e.data),
+          ) as InkeepMessageChunkData;
+          if (data.finish_reason === 'stop') {
+            inkeepCallbacks.onCompleteMessage?.({
+              chat_session_id: data.chat_session_id,
+              message: {
+                role: 'assistant',
+                content: completeContent,
+              },
+            });
+            completeContent = '';
+          }
+        }
+      }
     },
-  });
+  };
 
-  const stringBody = res.body.pipeThrough(stringStream);
-
-  const inkeepStream = readableFromAsyncIterable(streamableInkeep(stringBody));
-
-  return inkeepStream
-    .pipeThrough(createCallbacksTransformer(cb))
-    .pipeThrough(createStreamDataTransformer(cb?.experimental_streamData));
+  return AIStream(res, inkeepEventParser, callbacksCore).pipeThrough(
+    createStreamDataTransformer(callbacksCore?.experimental_streamData),
+  );
 }
