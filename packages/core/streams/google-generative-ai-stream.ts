b78a73e769506a57b47636103f4b06ede12e9f8b
Lars Grammel <lars.grammel@gmail.com>
2023-12-18 21:03:17 +0100

    Google Gemini support using Google Generative AI SDK (#865)
    Co-authored-by: Max Leiter <max.leiter@vercel.com>

---
+++ packages/core/streams/google-generative-ai-stream.ts
@@ -0,0 +1,60 @@
+import {
+  createCallbacksTransformer,
+  readableFromAsyncIterable,
+  type AIStreamCallbacksAndOptions,
+} from './ai-stream';
+import { createStreamDataTransformer } from './stream-data';
+
+interface GenerateContentResponse {
+  candidates?: GenerateContentCandidate[];
+}
+
+interface GenerateContentCandidate {
+  index: number;
+  content: Content;
+}
+
+interface Content {
+  role: string;
+  parts: Part[];
+}
+
+type Part = TextPart | InlineDataPart;
+
+interface InlineDataPart {
+  text?: never;
+}
+
+interface TextPart {
+  text: string;
+  inlineData?: never;
+}
+
+async function* streamable(response: {
+  stream: AsyncIterable<GenerateContentResponse>;
+}) {
+  for await (const chunk of response.stream) {
+    const parts = chunk.candidates?.[0].content.parts;
+
+    if (parts === undefined) {
+      continue;
+    }
+
+    const firstPart = parts[0];
+
+    if (typeof firstPart.text === 'string') {
+      yield firstPart.text;
+    }
+  }
+}
+
+export function GoogleGenerativeAIStream(
+  response: {
+    stream: AsyncIterable<GenerateContentResponse>;
+  },
+  cb?: AIStreamCallbacksAndOptions,
+): ReadableStream {
+  return readableFromAsyncIterable(streamable(response))
+    .pipeThrough(createCallbacksTransformer(cb))
+    .pipeThrough(createStreamDataTransformer(cb?.experimental_streamData));
+}
