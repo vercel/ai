1614801ebf0ef5c449b1b88d88115acf68868935
Lars Grammel <lars.grammel@gmail.com>
2023-12-19 15:45:28 +0100

    Explore edge config.

--- examples/next-google-gemini/app/api/chat/route.ts
+++ examples/next-google-gemini/app/api/chat/route.ts
@@ -1,15 +1,8 @@
-import { VertexAI } from '@google-cloud/vertexai';
 import { GoogleVertexAIStream, Message, StreamingTextResponse } from 'ai';
-
-// Initialize Vertex with your Cloud project and location
-const vertex_ai = new VertexAI({
-  project: process.env.GCP_PROJECT || '',
-  location: 'us-central1',
-});
 
 // NOTE: The Vertex AI client is not compatible with the Edge runtime.
 // IMPORTANT! Set the runtime to edge
-// export const runtime = 'edge';
+export const runtime = 'edge';
 
 const experimental_buildGeminiPrompt = (messages: Message[]) => {
   return {
@@ -25,27 +18,43 @@ const experimental_buildGeminiPrompt = (messages: Message[]) => {
 };
 
 export async function POST(req: Request) {
-  // Extract the `prompt` from the body of the request
-  const { messages } = await req.json();
+  try {
+    const before = globalThis.window;
+    globalThis.window = before || { fetch };
+    const { VertexAI } = await import('@google-cloud/vertexai');
+    globalThis.window = before;
 
-  // Instantiate the models
-  const generativeModel = vertex_ai.preview.getGenerativeModel({
-    model: 'gemini-pro',
-    generation_config: {
-      max_output_tokens: 2048,
-      temperature: 0.4,
-      top_p: 1,
-      top_k: 32,
-    },
-  });
+    // Initialize Vertex with your Cloud project and location
+    const vertex_ai = new VertexAI({
+      project: process.env.GCP_PROJECT || '',
+      location: 'us-central1',
+    });
 
-  const geminiStream = await generativeModel.generateContentStream(
-    experimental_buildGeminiPrompt(messages),
-  );
+    // Extract the `prompt` from the body of the request
+    const { messages } = await req.json();
 
-  // Convert the response into a friendly text-stream
-  const stream = GoogleVertexAIStream(geminiStream);
+    // Instantiate the models
+    const generativeModel = vertex_ai.preview.getGenerativeModel({
+      model: 'gemini-pro',
+      generation_config: {
+        max_output_tokens: 2048,
+        temperature: 0.4,
+        top_p: 1,
+        top_k: 32,
+      },
+    });
 
-  // Respond with the stream
-  return new StreamingTextResponse(stream);
+    const geminiStream = await generativeModel.generateContentStream(
+      experimental_buildGeminiPrompt(messages),
+    );
+
+    // Convert the response into a friendly text-stream
+    const stream = GoogleVertexAIStream(geminiStream);
+
+    // Respond with the stream
+    return new StreamingTextResponse(stream);
+  } catch (e) {
+    console.error(e);
+    return new Response('bla', { status: 500 });
+  }
 }
--- examples/next-google-gemini/next.config.js
+++ examples/next-google-gemini/next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+
+  webpack: config => {
+    config.resolve.alias.child_process = false;
+    config.resolve.alias.crypto = false;
+    config.resolve.alias.fs = false;
+    config.resolve.alias.https = false;
+    config.resolve.alias.os = false;
+    config.resolve.alias.path = false;
+    config.resolve.alias.querystring = false;
+    config.resolve.alias.stream = false;
+
+    config.resolve.alias['https-proxy-agent'] = false;
+
+    return config;
+  },
+};
 
 module.exports = nextConfig;
