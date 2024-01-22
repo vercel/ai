2e0de35bf31c6d46b9e0c8847c664f41fd0a1cd4
Lars Grammel <lars.grammel@gmail.com>
2024-01-22 16:49:29 +0100

    Add styles.

--- examples/next-openai/app/chat-speech-elevenlabs/page.tsx
+++ examples/next-openai/app/chat-speech-elevenlabs/page.tsx
@@ -1,6 +1,17 @@
 'use client';
 
+import { Message } from 'ai';
 import { useChat } from 'ai/react';
+
+// Generate a map of message role to text color
+const roleToColorMap: Record<Message['role'], string> = {
+  system: 'red',
+  user: 'black',
+  function: 'blue',
+  tool: 'purple',
+  assistant: 'green',
+  data: 'orange',
+};
 
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit, speechUrl } =
@@ -10,10 +21,16 @@ export default function Chat() {
 
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-      {messages.map(m => (
-        <div key={m.id} className="whitespace-pre-wrap">
-          {m.role === 'user' ? 'User: ' : 'AI: '}
-          {m.content}
+      {messages.map((m: Message) => (
+        <div
+          key={m.id}
+          className="whitespace-pre-wrap"
+          style={{ color: roleToColorMap[m.role] }}
+        >
+          <strong>{`${m.role}: `}</strong>
+          {m.content || JSON.stringify(m.function_call)}
+          <br />
+          <br />
         </div>
       ))}
 
