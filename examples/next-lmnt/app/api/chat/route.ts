b11498558b9a82febaf277f9b63cce2e06685e24
Lars Grammel <lars.grammel@gmail.com>
2024-02-19 10:49:49 +0100

    Add LMNT chat example.

--- examples/next-lmnt/app/api/chat/route.ts
+++ examples/next-lmnt/app/api/chat/route.ts
@@ -1,13 +1,22 @@
-import { OpenAIStream, StreamingTextResponse } from 'ai';
 import OpenAI from 'openai';
+import {
+  OpenAIStream,
+  StreamingTextResponse,
+  experimental_StreamData,
+  forwardLmntSpeechStream,
+} from 'ai';
+import Speech from 'lmnt-node';
 
 // Create an OpenAI API client (that's edge friendly!)
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY || '',
 });
 
-// IMPORTANT! Set the runtime to edge
-export const runtime = 'edge';
+// Create an LMNT API client
+const speech = new Speech(process.env.LMNT_API_KEY || 'no key');
+
+// Note: The LMNT SDK does not work on edge (v1.1.2)
+// export const runtime = 'edge';
 
 export async function POST(req: Request) {
   const { messages } = await req.json();
@@ -19,9 +28,36 @@ export async function POST(req: Request) {
     messages,
   });
 
+  const speechStream = speech.synthesizeStreaming(
+    '034b632b-df71-46c8-b440-86a42ffc3cf3', // Henry
+    {},
+  );
+
+  const data = new experimental_StreamData();
+
+  // note: no await here, we want to run this in parallel:
+  forwardLmntSpeechStream(speechStream, data, {
+    onFinal() {
+      data.close();
+    },
+  });
+
   // Convert the response into a friendly text-stream
-  const stream = OpenAIStream(response);
+  const stream = OpenAIStream(response, {
+    onToken(token) {
+      speechStream.appendText(token);
+
+      // only flush after each sentence to make it sound more natural:
+      if (token.includes('.')) {
+        speechStream.flush();
+      }
+    },
+    onFinal(completion) {
+      speechStream.finish(); // flush any remaining tokens and close the stream
+    },
+    experimental_streamData: true,
+  });
 
   // Respond with the stream
-  return new StreamingTextResponse(stream);
+  return new StreamingTextResponse(stream, {}, data);
 }
--- examples/next-lmnt/app/page.tsx
+++ examples/next-lmnt/app/page.tsx
@@ -1,17 +1,47 @@
 'use client';
 
+import { Message } from 'ai';
 import { useChat } from 'ai/react';
 
+// Generate a map of message role to text color
+const roleToColorMap: Record<Message['role'], string> = {
+  system: 'red',
+  user: 'black',
+  function: 'blue',
+  tool: 'purple',
+  assistant: 'green',
+  data: 'orange',
+};
+
 export default function Chat() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat();
+  const { messages, input, handleInputChange, handleSubmit, speechUrl } =
+    useChat();
+
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-      {messages.map(m => (
-        <div key={m.id} className="whitespace-pre-wrap">
-          {m.role === 'user' ? 'User: ' : 'AI: '}
-          {m.content}
+      {messages.map((m: Message) => (
+        <div
+          key={m.id}
+          className="whitespace-pre-wrap"
+          style={{ color: roleToColorMap[m.role] }}
+        >
+          <strong>{`${m.role}: `}</strong>
+          {m.content || JSON.stringify(m.function_call)}
+          <br />
+          <br />
         </div>
       ))}
+
+      <div className="flex justify-center mt-4">
+        {speechUrl != null && (
+          <audio
+            controls
+            controlsList="nodownload nofullscreen noremoteplayback"
+            autoPlay={true}
+            src={speechUrl}
+          />
+        )}
+      </div>
 
       <form onSubmit={handleSubmit}>
         <input
