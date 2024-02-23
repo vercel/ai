63ba8969000a632c25e1f47aeea2d7791c116c4c
Lars Grammel <lars.grammel@gmail.com>
2024-02-23 17:52:34 +0100

    Simplify examples.

--- examples/basic/src/stream-object/openai-chat-text-prompt-example.ts
+++ examples/basic/src/stream-object/openai-chat-text-prompt-example.ts
@@ -5,35 +5,30 @@ import { z } from 'zod';
 
 dotenv.config();
 
-async function main() {
-  const result = await streamObject({
-    model: openai.chat({
-      id: 'gpt-4-turbo-preview',
-      maxTokens: 2000,
+const result = await streamObject({
+  model: openai.chat({
+    id: 'gpt-4-turbo-preview',
+    maxTokens: 2000,
+  }),
+
+  schema: zodSchema(
+    z.object({
+      characters: z.array(
+        z.object({
+          name: z.string(),
+          class: z
+            .string()
+            .describe('Character class, e.g. warrior, mage, or thief.'),
+          description: z.string(),
+        }),
+      ),
     }),
+  ),
 
-    schema: zodSchema(
-      z.object({
-        characters: z.array(
-          z.object({
-            name: z.string(),
-            class: z
-              .string()
-              .describe('Character class, e.g. warrior, mage, or thief.'),
-            description: z.string(),
-          }),
-        ),
-      }),
-    ),
+  prompt: 'Generate 3 character descriptions for a fantasy role playing game.',
+});
 
-    prompt:
-      'Generate 3 character descriptions for a fantasy role playing game.',
-  });
-
-  for await (const partialObject of result.objectStream) {
-    console.clear();
-    console.log(partialObject);
-  }
+for await (const partialObject of result.objectStream) {
+  console.clear();
+  console.log(partialObject);
 }
-
-main().catch(console.error);
