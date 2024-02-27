b9cc8ac83552289fd6ee773498dcfa1af1d05c1b
Lars Grammel <lars.grammel@gmail.com>
2024-02-27 10:15:26 +0100

    Add MistralStream.

--- packages/core/tests/snapshots/mistral.ts
+++ packages/core/tests/snapshots/mistral.ts
@@ -1,49 +1,74 @@
 export const mistralChunks = [
   {
-    id: 'chatcmpl-7RyNSW2BXkOQQh7NlBc65j5kX8AjC',
+    id: 'eb7ce8e5329e4f309b7bfc1a958d450f',
     object: 'chat.completion.chunk',
     created: 1686901302,
     model: 'mistral-small',
     choices: [
       {
-        delta: { content: 'Hello' },
         index: 0,
+        delta: { role: 'assistant', content: '' },
         finish_reason: null,
       },
     ],
+    usage: null,
   },
   {
-    id: 'chatcmpl-7RyNSW2BXkOQQh7NlBc65j5kX8AjC',
-    object: 'chat.completion.chunk',
-    created: 1686901302,
-    model: 'mistral-small',
-    choices: [{ delta: { content: ',' }, index: 0, finish_reason: null }],
-  },
-  {
-    id: 'chatcmpl-7RyNSW2BXkOQQh7NlBc65j5kX8AjC',
+    id: 'eb7ce8e5329e4f309b7bfc1a958d450f',
     object: 'chat.completion.chunk',
     created: 1686901302,
     model: 'mistral-small',
     choices: [
       {
-        delta: { content: ' world' },
+        delta: { role: null, content: 'Hello' },
         index: 0,
         finish_reason: null,
       },
     ],
+    usage: null,
   },
   {
-    id: 'chatcmpl-7RyNSW2BXkOQQh7NlBc65j5kX8AjC',
+    id: 'eb7ce8e5329e4f309b7bfc1a958d450f',
     object: 'chat.completion.chunk',
     created: 1686901302,
     model: 'mistral-small',
-    choices: [{ delta: { content: '.' }, index: 0, finish_reason: null }],
+    choices: [
+      { delta: { role: null, content: ',' }, index: 0, finish_reason: null },
+    ],
+    usage: null,
   },
   {
-    id: 'chatcmpl-7RyNSW2BXkOQQh7NlBc65j5kX8AjC',
+    id: 'eb7ce8e5329e4f309b7bfc1a958d450f',
     object: 'chat.completion.chunk',
     created: 1686901302,
     model: 'mistral-small',
-    choices: [{ delta: {}, index: 0, finish_reason: 'stop' }],
+    choices: [
+      {
+        delta: { role: null, content: ' world' },
+        index: 0,
+        finish_reason: null,
+      },
+    ],
+    usage: null,
+  },
+  {
+    id: 'eb7ce8e5329e4f309b7bfc1a958d450f',
+    object: 'chat.completion.chunk',
+    created: 1686901302,
+    model: 'mistral-small',
+    choices: [
+      { delta: { role: null, content: '.' }, index: 0, finish_reason: null },
+    ],
+    usage: null,
+  },
+  {
+    id: 'eb7ce8e5329e4f309b7bfc1a958d450f',
+    object: 'chat.completion.chunk',
+    created: 1686901302,
+    model: 'mistral-small',
+    choices: [
+      { delta: { role: null, content: '' }, index: 0, finish_reason: 'stop' },
+    ],
+    usage: { prompt_tokens: 15, total_tokens: 245, completion_tokens: 230 },
   },
 ];
