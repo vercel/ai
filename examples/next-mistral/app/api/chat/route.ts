657ff0d286c00d4912f7303250e5a19ea01c1f58
Lars Grammel <lars.grammel@gmail.com>
2024-02-27 17:21:44 +0100

    Update Mistral integration to latest Mistral client (#1000)

--- examples/next-mistral/app/api/chat/route.ts
+++ examples/next-mistral/app/api/chat/route.ts
@@ -1,25 +1,23 @@
-import { OpenAIStream, StreamingTextResponse } from 'ai';
-
-// Note: There are no types for the Mistral API client yet.
-// @ts-ignore
 import MistralClient from '@mistralai/mistralai';
+import { MistralStream, StreamingTextResponse } from 'ai';
 
-const client = new MistralClient(process.env.MISTRAL_API_KEY || '');
+export const runtime = 'edge';
+
+const mistral = new MistralClient(process.env.MISTRAL_API_KEY || '');
 
 export async function POST(req: Request) {
   // Extract the `messages` from the body of the request
   const { messages } = await req.json();
 
-  const response = await client.chatStream({
-    model: 'mistral-tiny',
-    stream: true,
-    max_tokens: 1000,
+  const response = mistral.chatStream({
+    model: 'mistral-small',
+    maxTokens: 1000,
     messages,
   });
 
   // Convert the response into a friendly text-stream. The Mistral client responses are
-  // compatible with the Vercel AI SDK OpenAIStream adapter.
-  const stream = OpenAIStream(response);
+  // compatible with the Vercel AI SDK MistralStream adapter.
+  const stream = MistralStream(response);
 
   // Respond with the stream
   return new StreamingTextResponse(stream);
