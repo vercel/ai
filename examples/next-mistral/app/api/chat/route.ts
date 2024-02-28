638e47b09a64903ce0c2180d8bb889b0df0e2863
Lars Grammel <lars.grammel@gmail.com>
2024-02-28 20:14:13 +0100

    Update mistral routes.

--- examples/next-mistral/app/api/chat/route.ts
+++ examples/next-mistral/app/api/chat/route.ts
@@ -1,24 +1,15 @@
-import MistralClient from '@mistralai/mistralai';
-import { MistralStream, StreamingTextResponse } from 'ai';
+import { streamText } from 'ai/core';
+import { mistral } from 'ai/provider';
 
 export const runtime = 'edge';
 
-const mistral = new MistralClient(process.env.MISTRAL_API_KEY || '');
-
 export async function POST(req: Request) {
-  // Extract the `messages` from the body of the request
   const { messages } = await req.json();
 
-  const response = mistral.chatStream({
-    model: 'mistral-small',
-    maxTokens: 1000,
-    messages,
+  const stream = await streamText({
+    model: mistral.chat({ id: 'mistral-small', maxTokens: 1000 }),
+    prompt: { messages },
   });
 
-  // Convert the response into a friendly text-stream. The Mistral client responses are
-  // compatible with the Vercel AI SDK MistralStream adapter.
-  const stream = MistralStream(response);
-
-  // Respond with the stream
-  return new StreamingTextResponse(stream);
+  return stream.toResponse();
 }
--- examples/next-mistral/app/api/completion/route.ts
+++ examples/next-mistral/app/api/completion/route.ts
@@ -1,38 +1,15 @@
-import MistralClient from '@mistralai/mistralai';
-import {
-  MistralStream,
-  StreamingTextResponse,
-  experimental_StreamData,
-} from 'ai';
-
-const mistral = new MistralClient(process.env.MISTRAL_API_KEY || '');
+import { streamText } from 'ai/core';
+import { mistral } from 'ai/provider';
 
 export const runtime = 'edge';
 
 export async function POST(req: Request) {
-  // Extract the `prompt` from the body of the request
   const { prompt } = await req.json();
 
-  // Ask Mistral for a streaming completion given the prompt
-  const response = mistral.chatStream({
-    model: 'mistral-small',
-    maxTokens: 1000,
-    messages: [{ role: 'user', content: prompt }],
+  const stream = await streamText({
+    model: mistral.chat({ id: 'mistral-small', maxTokens: 1000 }),
+    prompt,
   });
 
-  // optional: use stream data
-  const data = new experimental_StreamData();
-
-  data.append({ test: 'value' });
-
-  // Convert the response into a friendly text-stream
-  const stream = MistralStream(response, {
-    onFinal(completion) {
-      data.close();
-    },
-    experimental_streamData: true,
-  });
-
-  // Respond with the stream
-  return new StreamingTextResponse(stream, {}, data);
+  return stream.toResponse();
 }
