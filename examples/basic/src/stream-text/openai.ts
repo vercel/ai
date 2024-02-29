4d4475367f75248a87e99f37eeaa06204857e975
Lars Grammel <lars.grammel@gmail.com>
2024-02-29 16:42:20 +0100

    Refactor openai.doStream.

---
+++ examples/basic/src/stream-text/openai.ts
@@ -0,0 +1,18 @@
+import { streamText } from 'ai/core';
+import { openai } from 'ai/provider';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+async function main() {
+  const result = await streamText({
+    model: openai.chat({ id: 'gpt-3.5-turbo' }),
+    prompt: 'Invent a new holiday and describe its traditions.',
+  });
+
+  for await (const textPart of result.textStream) {
+    process.stdout.write(textPart);
+  }
+}
+
+main();
--- packages/core/provider/openai/openai-chat-language-model.ts
+++ packages/core/provider/openai/openai-chat-language-model.ts
@@ -1,6 +1,9 @@
 import { nanoid } from 'nanoid';
 import OpenAI from 'openai';
-import { ChatCompletionCreateParamsNonStreaming } from 'openai/resources';
+import {
+  ChatCompletionCreateParamsNonStreaming,
+  ChatCompletionCreateParamsStreaming,
+} from 'openai/resources';
 import {
   LanguageModel,
   LanguageModelSettings,
@@ -109,196 +112,42 @@ export class OpenAIChatLanguageModel implements LanguageModel {
     };
   }
 
-  async doStream({
+  private getDoStreamArgs({
     mode,
     prompt,
-  }: Parameters<LanguageModel['doStream']>[0]): Promise<
-    ReadableStream<LanguageModelStreamPart>
-  > {
+  }: Parameters<
+    LanguageModel['doGenerate']
+  >[0]): ChatCompletionCreateParamsStreaming {
     const type = mode.type;
     const messages = convertToOpenAIChatPrompt(prompt);
-    const client = await this.getClient();
 
     switch (type) {
       case 'regular': {
-        const openaiResponse = await client.chat.completions.create({
-          ...this.basePrompt,
+        return {
           stream: true,
-          messages: convertToOpenAIChatPrompt(prompt),
-          tools: mode.tools?.map(tool => ({
-            type: 'function',
-            function: {
-              name: tool.name,
-              description: tool.description,
-              parameters: tool.parameters,
-            },
-          })),
-        });
-
-        const toolCalls: Array<{
-          id?: string;
-          type?: 'function';
-          function?: {
-            name?: string;
-            arguments?: string;
-          };
-        }> = [];
-
-        return readableFromAsyncIterable(openaiResponse).pipeThrough(
-          new TransformStream<
-            OpenAI.Chat.Completions.ChatCompletionChunk,
-            LanguageModelStreamPart
-          >({
-            transform(chunk, controller) {
-              if (chunk.choices?.[0].delta == null) {
-                return;
-              }
-
-              const delta = chunk.choices[0].delta;
-
-              if (delta.content != null) {
-                controller.enqueue({
-                  type: 'text-delta',
-                  textDelta: delta.content,
-                });
-              }
-
-              if (delta.tool_calls != null) {
-                for (const toolCallDelta of delta.tool_calls) {
-                  const index = toolCallDelta.index;
-
-                  // new tool call, add to list
-                  if (toolCalls[index] == null) {
-                    toolCalls[index] = toolCallDelta;
-                    continue;
-                  }
-
-                  // existing tool call, merge
-                  const toolCall = toolCalls[index];
-
-                  if (toolCallDelta.function?.arguments != null) {
-                    toolCall.function!.arguments +=
-                      toolCallDelta.function?.arguments ?? '';
-                  }
-
-                  // check if tool call is complete
-                  if (
-                    toolCall.function?.name == null ||
-                    toolCall.function?.arguments == null
-                  ) {
-                    continue;
-                  }
-
-                  const args = tryParseJSON(toolCall.function.arguments);
-
-                  if (args == null) {
-                    continue;
-                  }
-
-                  controller.enqueue({
-                    type: 'tool-call',
-                    toolCallId: toolCall.id ?? nanoid(),
-                    toolName: toolCall.function.name,
-                    args,
-                  });
-                }
-              }
-            },
-          }),
-        );
+          ...this.basePrompt,
+          messages,
+          // TODO tools
+        };
       }
 
       case 'object-json': {
-        const clientResponse = await client.chat.completions.create({
-          ...this.basePrompt,
+        return {
           stream: true,
+          ...this.basePrompt,
           response_format: { type: 'json_object' },
           messages,
-        });
-
-        return readableFromAsyncIterable(clientResponse).pipeThrough(
-          new TransformStream<
-            OpenAI.Chat.Completions.ChatCompletionChunk,
-            LanguageModelStreamPart
-          >({
-            transform(chunk, controller) {
-              if (chunk.choices?.[0].delta == null) {
-                return;
-              }
-
-              const delta = chunk.choices[0].delta;
-
-              if (delta.content != null) {
-                controller.enqueue({
-                  type: 'text-delta',
-                  textDelta: delta.content,
-                });
-              }
-            },
-          }),
-        );
+        };
       }
 
       case 'object-tool': {
-        const clientResponse = await client.chat.completions.create({
-          ...this.basePrompt,
+        return {
           stream: true,
+          ...this.basePrompt,
           tool_choice: { type: 'function', function: { name: mode.tool.name } },
           tools: [{ type: 'function', function: mode.tool }],
           messages,
-        });
-
-        const toolCalls: Array<{
-          toolCallId: string;
-          toolName: string;
-        }> = [];
-
-        return readableFromAsyncIterable(clientResponse).pipeThrough(
-          new TransformStream<
-            OpenAI.Chat.Completions.ChatCompletionChunk,
-            LanguageModelStreamPart
-          >({
-            transform(chunk, controller) {
-              if (chunk.choices?.[0].delta == null) {
-                return;
-              }
-
-              const delta = chunk.choices[0].delta;
-
-              if (delta.tool_calls == null) {
-                return;
-              }
-
-              for (const toolCallDelta of delta.tool_calls) {
-                const index = toolCallDelta.index;
-
-                if (index !== 0) {
-                  continue;
-                }
-
-                if (toolCalls[index] == null) {
-                  toolCalls[index] = {
-                    toolCallId: toolCallDelta.id ?? '', // TODO empty?
-                    toolName: toolCallDelta.function?.name ?? '', // TODO empty?
-                  };
-                }
-
-                const toolCall = toolCalls[index];
-
-                const argumentsDelta = toolCallDelta.function?.arguments;
-
-                if (argumentsDelta != null) {
-                  controller.enqueue({
-                    type: 'tool-call-delta',
-                    toolCallId: toolCall.toolCallId,
-                    toolName: toolCall.toolName,
-                    argsTextDelta: argumentsDelta,
-                  });
-                }
-              }
-            },
-          }),
-        );
+        };
       }
 
       default: {
@@ -306,5 +155,98 @@ export class OpenAIChatLanguageModel implements LanguageModel {
         throw new Error(`Unsupported type: ${_exhaustiveCheck}`);
       }
     }
+  }
+
+  async doStream({
+    mode,
+    prompt,
+  }: Parameters<LanguageModel['doStream']>[0]): Promise<
+    ReadableStream<LanguageModelStreamPart>
+  > {
+    const client = await this.getClient();
+
+    const response = await client.chat.completions.create(
+      this.getDoStreamArgs({ mode, prompt }),
+    );
+
+    const toolCalls: Array<{
+      id?: string;
+      type?: 'function';
+      function?: {
+        name?: string;
+        arguments?: string;
+      };
+    }> = [];
+
+    return readableFromAsyncIterable(response).pipeThrough(
+      new TransformStream<
+        OpenAI.Chat.Completions.ChatCompletionChunk,
+        LanguageModelStreamPart
+      >({
+        transform(chunk, controller) {
+          if (chunk.choices?.[0].delta == null) {
+            return;
+          }
+
+          const delta = chunk.choices[0].delta;
+
+          if (delta.content != null) {
+            controller.enqueue({
+              type: 'text-delta',
+              textDelta: delta.content,
+            });
+          }
+
+          if (delta.tool_calls != null) {
+            for (const toolCallDelta of delta.tool_calls) {
+              const index = toolCallDelta.index;
+
+              // new tool call, add to list
+              if (toolCalls[index] == null) {
+                toolCalls[index] = toolCallDelta;
+                continue;
+              }
+
+              // existing tool call, merge
+              const toolCall = toolCalls[index];
+
+              if (toolCallDelta.function?.arguments != null) {
+                toolCall.function!.arguments +=
+                  toolCallDelta.function?.arguments ?? '';
+              }
+
+              // send delta
+              controller.enqueue({
+                type: 'tool-call-delta',
+                toolCallId: toolCall.id ?? '', // TODO empty?
+                toolName: toolCall.function?.name ?? '', // TODO empty?
+                argsTextDelta: toolCallDelta.function?.arguments ?? '', // TODO empty?
+              });
+
+              // check if tool call is complete
+              if (
+                toolCall.function?.name == null ||
+                toolCall.function?.arguments == null
+              ) {
+                continue;
+              }
+
+              const args = tryParseJSON(toolCall.function.arguments);
+
+              if (args == null) {
+                continue;
+              }
+
+              controller.enqueue({
+                type: 'tool-call',
+                toolCallId: toolCall.id ?? nanoid(),
+                toolName: toolCall.function.name,
+                args,
+              });
+            }
+          }
+        },
+      }),
+    );
   }
 }
