89b66d0f2ca7f5f422c33cb0136cfdbaf2d8f46c
Lars Grammel <lars.grammel@gmail.com>
2024-02-29 17:10:37 +0100

    Move.

--- packages/core/core/language-model/generate-text/index.ts
+++ packages/core/core/language-model/generate-text/index.ts
@@ -1,1 +1,4 @@
 export * from './generate-text';
+export * from './stream-text';
+export * from './stream-text-http-response';
+export * from './tool-result-stream-part';
--- packages/core/core/language-model/stream-text/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './stream-text';
-export * from './stream-text-http-response';
-export * from './tool-result-stream-part';
