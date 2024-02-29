34bd67b65add23c013621ab0c74977b3cefaf2d1
Lars Grammel <lars.grammel@gmail.com>
2024-02-29 10:59:30 +0100

    Add tool result test.

--- packages/core/core/language-model/stream-text/stream-text.test.ts
+++ packages/core/core/language-model/stream-text/stream-text.test.ts
@@ -4,9 +4,10 @@ import { convertArrayToReadableStream } from '../test/convert-array-to-readable-
 import { convertAsyncIterableToArray } from '../test/convert-async-iterable-to-array';
 import { MockLanguageModel } from '../test/mock-language-model';
 import { streamText } from './stream-text';
+import { createTool } from '../tool';
 
 describe('result.textStream', () => {
-  it('should send text deltas as textStream', async () => {
+  it('should send text deltas', async () => {
     const result = await streamText({
       model: new MockLanguageModel({
         doStream: async ({ prompt }) =>
@@ -27,7 +28,7 @@ describe('result.textStream', () => {
 });
 
 describe('result.fullStream', () => {
-  it('should send all text parts as fullStream', async () => {
+  it('should send text deltas', async () => {
     const result = await streamText({
       model: new MockLanguageModel({
         doStream: async ({ prompt }) =>
@@ -50,7 +51,7 @@ describe('result.fullStream', () => {
     );
   });
 
-  it("should send tool call parts as fullStream if they're present", async () => {
+  it('should send tool calls', async () => {
     const result = await streamText({
       model: new MockLanguageModel({
         doStream: async ({ prompt }) =>
@@ -59,19 +60,17 @@ describe('result.fullStream', () => {
               type: 'tool-call',
               toolCallId: 'call-1',
               toolName: 'tool-1',
-              args: { name: prompt },
+              args: { value: prompt },
             },
           ]),
       }),
       tools: [
         {
           name: 'tool-1',
-          parameters: z.object({
-            name: z.string(),
-          }),
+          parameters: z.object({ value: z.string() }),
         },
       ],
-      prompt: 'test',
+      prompt: 'test-input',
     });
 
     assert.deepStrictEqual(
@@ -81,7 +80,49 @@ describe('result.fullStream', () => {
           type: 'tool-call',
           toolCallId: 'call-1',
           toolName: 'tool-1',
-          args: { name: 'test' },
+          args: { value: 'test-input' },
+        },
+      ],
+    );
+  });
+
+  it('should send tool results', async () => {
+    const result = await streamText({
+      model: new MockLanguageModel({
+        doStream: async ({ prompt }) =>
+          convertArrayToReadableStream([
+            {
+              type: 'tool-call',
+              toolCallId: 'call-1',
+              toolName: 'tool-1',
+              args: { value: prompt },
+            },
+          ]),
+      }),
+      tools: [
+        createTool({
+          name: 'tool-1',
+          parameters: z.object({ value: z.string() }),
+          execute: async ({ value }) => `${value}-result`,
+        }),
+      ],
+      prompt: 'test-input',
+    });
+
+    assert.deepStrictEqual(
+      await convertAsyncIterableToArray(result.fullStream),
+      [
+        {
+          type: 'tool-call',
+          toolCallId: 'call-1',
+          toolName: 'tool-1',
+          args: { value: 'test-input' },
+        },
+        {
+          type: 'tool-result',
+          toolCallId: 'call-1',
+          toolName: 'tool-1',
+          result: 'test-input-result',
         },
       ],
     );
