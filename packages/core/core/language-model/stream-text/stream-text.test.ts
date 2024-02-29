b0636a89931418d5842440e1a2e3d3877ed8f45c
Lars Grammel <lars.grammel@gmail.com>
2024-02-29 10:55:35 +0100

    Add tool call test.

--- packages/core/core/language-model/stream-text/stream-text.test.ts
+++ packages/core/core/language-model/stream-text/stream-text.test.ts
@@ -1,8 +1,9 @@
 import assert from 'node:assert';
-import { streamText } from './stream-text';
-import { MockLanguageModel } from '../test/mock-language-model';
-import { convertAsyncIterableToArray } from '../test/convert-async-iterable-to-array';
+import { z } from 'zod';
 import { convertArrayToReadableStream } from '../test/convert-array-to-readable-stream';
+import { convertAsyncIterableToArray } from '../test/convert-async-iterable-to-array';
+import { MockLanguageModel } from '../test/mock-language-model';
+import { streamText } from './stream-text';
 
 describe('result.textStream', () => {
   it('should send text deltas as textStream', async () => {
@@ -45,6 +46,43 @@ describe('result.fullStream', () => {
         { type: 'text-delta', textDelta: 'Hello' },
         { type: 'text-delta', textDelta: ', ' },
         { type: 'text-delta', textDelta: 'world!' },
+      ],
+    );
+  });
+
+  it("should send tool call parts as fullStream if they're present", async () => {
+    const result = await streamText({
+      model: new MockLanguageModel({
+        doStream: async ({ prompt }) =>
+          convertArrayToReadableStream([
+            {
+              type: 'tool-call',
+              toolCallId: 'call-1',
+              toolName: 'tool-1',
+              args: { name: prompt },
+            },
+          ]),
+      }),
+      tools: [
+        {
+          name: 'tool-1',
+          parameters: z.object({
+            name: z.string(),
+          }),
+        },
+      ],
+      prompt: 'test',
+    });
+
+    assert.deepStrictEqual(
+      await convertAsyncIterableToArray(result.fullStream),
+      [
+        {
+          type: 'tool-call',
+          toolCallId: 'call-1',
+          toolName: 'tool-1',
+          args: { name: 'test' },
+        },
       ],
     );
   });
