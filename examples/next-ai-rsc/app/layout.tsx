3e238ee98b2d7bdb03cb209f8d349833c5beb145
Max Leiter <max.leiter@vercel.com>
2024-02-29 17:29:11 -0800

    prettier

--- examples/next-ai-rsc/app/layout.tsx
+++ examples/next-ai-rsc/app/layout.tsx
@@ -16,7 +16,7 @@ const meta = {
 };
 export const metadata: Metadata = {
   ...meta,
-  metadataBase: new URL(process.env.VERCEL_URL || ""),
+  metadataBase: new URL(process.env.VERCEL_URL || ''),
   title: {
     default: 'AI RSC Demo',
     template: `%s - AI RSC Demo`,
