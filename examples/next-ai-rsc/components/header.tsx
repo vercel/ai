ff426637f32979515d317ed5fcc50b052cc040fe
Max Leiter <max.leiter@vercel.com>
2024-03-01 10:18:39 -0800

    examples: Bump next.js to 14.1.1 (#1023)

--- examples/next-ai-rsc/components/header.tsx
+++ examples/next-ai-rsc/components/header.tsx
@@ -10,14 +10,14 @@ import { Button } from '@/components/ui/button';
 
 export async function Header() {
   return (
-    <header className="sticky top-0 z-50 flex h-14 w-full shrink-0 items-center justify-between border-b bg-background px-4 backdrop-blur-xl">
+    <header className="sticky top-0 z-50 flex items-center justify-between w-full px-4 border-b h-14 shrink-0 bg-background backdrop-blur-xl">
       <span className="inline-flex items-center home-links whitespace-nowrap">
         <a href="https://vercel.com" rel="noopener" target="_blank">
           <IconVercel className="w-5 h-5 sm:h-6 sm:w-6" />
         </a>
-        <IconSeparator className="h-6 w-6 text-muted-foreground/20" />
+        <IconSeparator className="w-6 h-6 text-muted-foreground/20" />
         <Link href="/">
-          <span className="font-bold text-lg">
+          <span className="text-lg font-bold">
             <IconSparkles className="inline mr-0 w-4 sm:w-5 mb-0.5" />
             AI
           </span>
@@ -27,16 +27,16 @@ export async function Header() {
         <Button variant="outline" asChild>
           <a
             target="_blank"
-            href="https://github.com/vercel/ai-rsc-demo"
+            href="https://github.com/vercel/ai/tree/main/examples/next-ai-rsc"
             rel="noopener noreferrer"
           >
             <IconGitHub />
-            <span className="ml-2 hidden md:flex">GitHub</span>
+            <span className="hidden ml-2 md:flex">GitHub</span>
           </a>
         </Button>
         <Button asChild>
           <a
-            href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fvercel%2Fai-rsc-demo"
+            href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fvercel%2Fai%2Fblob%2Fmain%2Fexamples%2Fnext-ai-rsc&env=OPENAI_API_KEY&envDescription=OpenAI+API+Key&envLink=https%3A%2F%2Fplatform.openai.com%2Fapi-keys"
             target="_blank"
           >
             <IconVercel className="mr-2" />
