a3966fe351e54f42207b5cf8a1ed77d16238b4af
Lars Grammel <lars.grammel@gmail.com>
2024-03-04 13:07:33 +0100

    Add test case for generateText tool results.

--- packages/core/core/language-model/generate-text/generate-text.test.ts
+++ packages/core/core/language-model/generate-text/generate-text.test.ts
@@ -99,3 +99,67 @@ describe('result.toolCalls', () => {
     ]);
   });
 });
+
+describe('result.toolResults', () => {
+  it('should contain tool results', async () => {
+    const result = await generateText({
+      model: new MockLanguageModel({
+        doGenerate: async ({ prompt, mode }) => {
+          assert.deepStrictEqual(mode, {
+            type: 'regular',
+            tools: [
+              {
+                name: 'tool1',
+                description: undefined,
+                parameters: {
+                  $schema: 'http://json-schema.org/draft-07/schema#',
+                  additionalProperties: false,
+                  properties: { value: { type: 'string' } },
+                  required: ['value'],
+                  type: 'object',
+                },
+              },
+            ],
+          });
+          assert.deepStrictEqual(prompt, {
+            messages: [{ role: 'user', content: 'test-input' }],
+          });
+
+          return {
+            toolCalls: [
+              {
+                toolCallId: 'call-1',
+                toolName: 'tool1',
+                args: `{ "value": "value" }`,
+              },
+            ],
+          };
+        },
+      }),
+      tools: {
+        tool1: {
+          parameters: z.object({ value: z.string() }),
+          execute: async args => {
+            assert.deepStrictEqual(args, { value: 'value' });
+            return 'result1';
+          },
+        },
+      },
+      prompt: 'test-input',
+    });
+
+    // test type inference
+    if (result.toolResults[0].toolName === 'tool1') {
+      assertType<string>(result.toolResults[0].result);
+    }
+
+    assert.deepStrictEqual(result.toolResults, [
+      {
+        toolCallId: 'call-1',
+        toolName: 'tool1',
+        args: { value: 'value' },
+        result: 'result1',
+      },
+    ]);
+  });
+});
