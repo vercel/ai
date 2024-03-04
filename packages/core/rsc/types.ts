363a6b2fd06e1f6e1907073ecf42fc6be1d23df1
Max Leiter <maxwell.leiter@gmail.com>
2024-03-03 16:49:27 -0800

    types.ts: add warning comment

--- packages/core/rsc/types.ts
+++ packages/core/rsc/types.ts
@@ -31,6 +31,7 @@ export type AIProviderProps<AIState = any, UIState = any, Actions = any> = {
   children: React.ReactNode;
   initialAIState?: AIState;
   initialUIState?: UIState;
+  /** $ActionTypes is only added for type inference and is never used at runtime **/
   $ActionTypes?: Actions;
 };
 
