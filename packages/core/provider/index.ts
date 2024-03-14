e374571999a17b9b525f61b17ad6c2ae08b2e2b5
Lars Grammel <lars.grammel@gmail.com>
2024-03-14 20:37:59 +0100

    Only keep OpenAI.

--- packages/core/provider/index.ts
+++ packages/core/provider/index.ts
@@ -1,4 +1,1 @@
-export * as fireworks from './fireworks/fireworks-facade';
-export * as mistral from './mistral/mistral-facade';
-export * as perplexity from './perplexity/perplexity-facade';
 export * from './openai/openai-facade';
