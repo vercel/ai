da3ac583eef18ae997567f95974ebd93f9d766b5
Aykut Kardaş <aykutkrds@gmail.com>
2024-03-16 11:55:02 +0300

    Remove undefined type from optional properties

--- packages/core/react/use-chat.ts
+++ packages/core/react/use-chat.ts
@@ -70,7 +70,7 @@ export type UseChatHelpers = {
   /** Whether the API request is in progress */
   isLoading: boolean;
   /** Additional data added on the server via StreamData */
-  data?: JSONValue[] | undefined;
+  data?: JSONValue[];
 };
 
 type StreamingReactResponseAction = (payload: {
