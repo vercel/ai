d3132a300b3294a92054d110a12e55b7a0466118
Malte Ubl <malte.ubl@gmail.com>
2024-03-20 14:10:00 -0700

    Added clarity what needs to be updated to use the software

--- examples/next-openai-kasada-bot-protection/app/149e9513-01fa-4fb0-aad4-566afd725d1b/2d206a39-8ed7-437e-a3be-862e0f06eea3/[[...restpath]]/route.ts
+++ examples/next-openai-kasada-bot-protection/app/149e9513-01fa-4fb0-aad4-566afd725d1b/2d206a39-8ed7-437e-a3be-862e0f06eea3/[[...restpath]]/route.ts
@@ -1,19 +1,19 @@
-const KASADA_ENDPOINT = 'vercel-endpoint.kasadapolyform.io'
+const KASADA_ENDPOINT = 'FILL_IN.kasadapolyform.io';
 
-export const runtime = 'edge'
-export const dynamic = 'force-dynamic'
+export const runtime = 'edge';
+export const dynamic = 'force-dynamic';
 
 async function handler(request: Request) {
-  const url = new URL(request.url)
+  const url = new URL(request.url);
 
-  url.protocol = 'https:'
-  url.host = KASADA_ENDPOINT
-  url.port = ''
-  url.searchParams.delete('restpath')
+  url.protocol = 'https:';
+  url.host = KASADA_ENDPOINT;
+  url.port = '';
+  url.searchParams.delete('restpath');
 
-  const headers = new Headers(request.headers)
-  headers.set('X-Forwarded-Host', 'sdk.vercel.ai')
-  headers.delete('host')
+  const headers = new Headers(request.headers);
+  headers.set('X-Forwarded-Host', 'FILL_IN');
+  headers.delete('host');
   const r = await fetch(url.toString(), {
     method: request.method,
     body: request.body,
@@ -22,17 +22,17 @@ async function handler(request: Request) {
     redirect: 'manual',
     // @ts-expect-error
     duplex: 'half',
-  })
-  const responseHeaders = new Headers(r.headers)
-  responseHeaders.set('cdn-cache-control', 'no-cache')
+  });
+  const responseHeaders = new Headers(r.headers);
+  responseHeaders.set('cdn-cache-control', 'no-cache');
   return new Response(r.body, {
     status: r.status,
     statusText: r.statusText,
     headers: responseHeaders,
-  })
+  });
 }
 
-export const GET = handler
-export const POST = handler
-export const OPTIONS = handler
-export const PUT = handler
+export const GET = handler;
+export const POST = handler;
+export const OPTIONS = handler;
+export const PUT = handler;
