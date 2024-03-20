3cfedfee1dae749e75e937b3f01de9a491941dd5
Malte Ubl <malte.ubl@gmail.com>
2024-03-20 14:01:11 -0700

    Add a bot protection template

---
+++ examples/next-openai-kasada-bot-protection/README.md
@@ -0,0 +1,3 @@
+Integration of an AI chat app based on Vercel's AI SDK and Kasada's advanced bot protection solution.
+
+The system hooks into the API calls to the LLM and prevents abusive usage before it occurs.
---
+++ examples/next-openai-kasada-bot-protection/app/149e9513-01fa-4fb0-aad4-566afd725d1b/2d206a39-8ed7-437e-a3be-862e0f06eea3/[[...restpath]]/route.ts
@@ -0,0 +1,38 @@
+const KASADA_ENDPOINT = 'vercel-endpoint.kasadapolyform.io'
+
+export const runtime = 'edge'
+export const dynamic = 'force-dynamic'
+
+async function handler(request: Request) {
+  const url = new URL(request.url)
+
+  url.protocol = 'https:'
+  url.host = KASADA_ENDPOINT
+  url.port = ''
+  url.searchParams.delete('restpath')
+
+  const headers = new Headers(request.headers)
+  headers.set('X-Forwarded-Host', 'sdk.vercel.ai')
+  headers.delete('host')
+  const r = await fetch(url.toString(), {
+    method: request.method,
+    body: request.body,
+    headers,
+    mode: request.mode,
+    redirect: 'manual',
+    // @ts-expect-error
+    duplex: 'half',
+  })
+  const responseHeaders = new Headers(r.headers)
+  responseHeaders.set('cdn-cache-control', 'no-cache')
+  return new Response(r.body, {
+    status: r.status,
+    statusText: r.statusText,
+    headers: responseHeaders,
+  })
+}
+
+export const GET = handler
+export const POST = handler
+export const OPTIONS = handler
+export const PUT = handler
