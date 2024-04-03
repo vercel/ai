6d1941873ce11b79e5ef4e2858a80ee26d11b53a
Lars Grammel <lars.grammel@gmail.com>
2024-04-03 11:37:10 +0200

    Fix build.

--- packages/core/core/util/detect-image-mimetype.ts
+++ packages/core/core/util/detect-image-mimetype.ts
@@ -1,9 +1,9 @@
 const mimeTypeSignatures = [
-  { bytes: [0x47, 0x49, 0x46], mimeType: 'image/gif' },
-  { bytes: [0x89, 0x50, 0x4e, 0x47], mimeType: 'image/png' },
-  { bytes: [0xff, 0xd8], mimeType: 'image/jpeg' },
-  { bytes: [0x52, 0x49, 0x46, 0x46], mimeType: 'image/webp' },
-] as const;
+  { mimeType: 'image/gif' as const, bytes: [0x47, 0x49, 0x46] },
+  { mimeType: 'image/png' as const, bytes: [0x89, 0x50, 0x4e, 0x47] },
+  { mimeType: 'image/jpeg' as const, bytes: [0xff, 0xd8] },
+  { mimeType: 'image/webp' as const, bytes: [0x52, 0x49, 0x46, 0x46] },
+];
 
 export function detectImageMimeType(
   image: Uint8Array,
