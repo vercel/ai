40da751512017b398d26726e6696340e8a4598f0
Lars Grammel <lars.grammel@gmail.com>
2024-04-15 14:53:55 +0200

    add vue/useCompletion support for streamMode.

--- packages/core/vue/use-chat.ui.test.tsx
+++ packages/core/vue/use-chat.ui.test.tsx
@@ -7,95 +7,126 @@ import {
   mockFetchError,
 } from '../tests/utils/mock-fetch';
 import TestChatComponent from './TestChatComponent.vue';
+import TestChatTextStreamComponent from './TestChatTextStreamComponent.vue';
 
-beforeEach(() => {
-  render(TestChatComponent);
-});
-
-afterEach(() => {
-  vi.restoreAllMocks();
-  cleanup();
-});
-
-test('Shows streamed complex text response', async () => {
-  mockFetchDataStream({
-    url: 'https://example.com/api/chat',
-    chunks: ['0:"Hello"\n', '0:","\n', '0:" world"\n', '0:"."\n'],
+describe('stream data stream', () => {
+  beforeEach(() => {
+    render(TestChatComponent);
   });
 
-  await userEvent.click(screen.getByTestId('button'));
-
-  await screen.findByTestId('message-0');
-  expect(screen.getByTestId('message-0')).toHaveTextContent('User: hi');
-
-  await screen.findByTestId('message-1');
-  expect(screen.getByTestId('message-1')).toHaveTextContent(
-    'AI: Hello, world.',
-  );
-});
-
-test('Shows streamed complex text response with data', async () => {
-  mockFetchDataStream({
-    url: 'https://example.com/api/chat',
-    chunks: ['2:[{"t1":"v1"}]\n', '0:"Hello"\n'],
+  afterEach(() => {
+    vi.restoreAllMocks();
+    cleanup();
   });
 
-  await userEvent.click(screen.getByTestId('button'));
-
-  await screen.findByTestId('data');
-  expect(screen.getByTestId('data')).toHaveTextContent('[{"t1":"v1"}]');
-
-  await screen.findByTestId('message-1');
-  expect(screen.getByTestId('message-1')).toHaveTextContent('AI: Hello');
-});
-
-test('Shows error response', async () => {
-  mockFetchError({ statusCode: 404, errorMessage: 'Not found' });
-
-  await userEvent.click(screen.getByTestId('button'));
-
-  // TODO bug? the user message does not show up
-  // await screen.findByTestId('message-0');
-  // expect(screen.getByTestId('message-0')).toHaveTextContent('User: hi');
-
-  await screen.findByTestId('error');
-  expect(screen.getByTestId('error')).toHaveTextContent('Error: Not found');
-});
-
-describe('loading state', () => {
-  test('should show loading state', async () => {
-    let finishGeneration: ((value?: unknown) => void) | undefined;
-    const finishGenerationPromise = new Promise(resolve => {
-      finishGeneration = resolve;
-    });
-
-    mockFetchDataStreamWithGenerator({
+  it('should show streamed complex text response', async () => {
+    mockFetchDataStream({
       url: 'https://example.com/api/chat',
-      chunkGenerator: (async function* generate() {
-        const encoder = new TextEncoder();
-        yield encoder.encode('0:"Hello"\n');
-        await finishGenerationPromise;
-      })(),
+      chunks: ['0:"Hello"\n', '0:","\n', '0:" world"\n', '0:"."\n'],
     });
 
     await userEvent.click(screen.getByTestId('button'));
 
-    await screen.findByTestId('loading');
-    expect(screen.getByTestId('loading')).toHaveTextContent('true');
+    await screen.findByTestId('message-0');
+    expect(screen.getByTestId('message-0')).toHaveTextContent('User: hi');
 
-    finishGeneration?.();
-
-    await findByText(await screen.findByTestId('loading'), 'false');
-
-    expect(screen.getByTestId('loading')).toHaveTextContent('false');
+    await screen.findByTestId('message-1');
+    expect(screen.getByTestId('message-1')).toHaveTextContent(
+      'AI: Hello, world.',
+    );
   });
 
-  test('should reset loading state on error', async () => {
+  it('should show streamed complex text response with data', async () => {
+    mockFetchDataStream({
+      url: 'https://example.com/api/chat',
+      chunks: ['2:[{"t1":"v1"}]\n', '0:"Hello"\n'],
+    });
+
+    await userEvent.click(screen.getByTestId('button'));
+
+    await screen.findByTestId('data');
+    expect(screen.getByTestId('data')).toHaveTextContent('[{"t1":"v1"}]');
+
+    await screen.findByTestId('message-1');
+    expect(screen.getByTestId('message-1')).toHaveTextContent('AI: Hello');
+  });
+
+  it('should show error response', async () => {
     mockFetchError({ statusCode: 404, errorMessage: 'Not found' });
 
     await userEvent.click(screen.getByTestId('button'));
 
-    await screen.findByTestId('loading');
-    expect(screen.getByTestId('loading')).toHaveTextContent('false');
+    // TODO bug? the user message does not show up
+    // await screen.findByTestId('message-0');
+    // expect(screen.getByTestId('message-0')).toHaveTextContent('User: hi');
+
+    await screen.findByTestId('error');
+    expect(screen.getByTestId('error')).toHaveTextContent('Error: Not found');
+  });
+
+  describe('loading state', () => {
+    it('should show loading state', async () => {
+      let finishGeneration: ((value?: unknown) => void) | undefined;
+      const finishGenerationPromise = new Promise(resolve => {
+        finishGeneration = resolve;
+      });
+
+      mockFetchDataStreamWithGenerator({
+        url: 'https://example.com/api/chat',
+        chunkGenerator: (async function* generate() {
+          const encoder = new TextEncoder();
+          yield encoder.encode('0:"Hello"\n');
+          await finishGenerationPromise;
+        })(),
+      });
+
+      await userEvent.click(screen.getByTestId('button'));
+
+      await screen.findByTestId('loading');
+      expect(screen.getByTestId('loading')).toHaveTextContent('true');
+
+      finishGeneration?.();
+
+      await findByText(await screen.findByTestId('loading'), 'false');
+
+      expect(screen.getByTestId('loading')).toHaveTextContent('false');
+    });
+
+    it('should reset loading state on error', async () => {
+      mockFetchError({ statusCode: 404, errorMessage: 'Not found' });
+
+      await userEvent.click(screen.getByTestId('button'));
+
+      await screen.findByTestId('loading');
+      expect(screen.getByTestId('loading')).toHaveTextContent('false');
+    });
+  });
+});
+
+describe('text stream', () => {
+  beforeEach(() => {
+    render(TestChatTextStreamComponent);
+  });
+
+  afterEach(() => {
+    vi.restoreAllMocks();
+    cleanup();
+  });
+
+  it('should show streamed complex text response', async () => {
+    mockFetchDataStream({
+      url: 'https://example.com/api/chat',
+      chunks: ['Hello', ',', ' world', '.'],
+    });
+
+    await userEvent.click(screen.getByTestId('button'));
+
+    await screen.findByTestId('message-0');
+    expect(screen.getByTestId('message-0')).toHaveTextContent('User: hi');
+
+    await screen.findByTestId('message-1');
+    expect(screen.getByTestId('message-1')).toHaveTextContent(
+      'AI: Hello, world.',
+    );
   });
 });
