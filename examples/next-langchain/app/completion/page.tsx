8511aa3dd64973ca0f70a286fb93110317a573a8
Lars Grammel <lars.grammel@gmail.com>
2024-05-10 10:58:37 +0200

    Update content.

--- examples/next-langchain/app/completion/page.tsx
+++ examples/next-langchain/app/completion/page.tsx
@@ -3,7 +3,7 @@
 import { useCompletion } from 'ai/react';
 
 export default function Chat() {
-  const { completion, input, handleInputChange, handleSubmit, error, data } =
+  const { completion, input, handleInputChange, handleSubmit, error } =
     useCompletion();
 
   return (
@@ -11,11 +11,6 @@ export default function Chat() {
       <h4 className="pb-4 text-xl font-bold text-gray-900 md:text-xl">
         useCompletion Example
       </h4>
-      {data && (
-        <pre className="p-4 text-sm bg-gray-100">
-          {JSON.stringify(data, null, 2)}
-        </pre>
-      )}
       {error && (
         <div className="fixed top-0 left-0 w-full p-4 text-center text-white bg-red-500">
           {error.message}
