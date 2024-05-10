0ca941a07a22aa6260db262d5aa3c1f5388e6844
Lars Grammel <lars.grammel@gmail.com>
2024-05-10 10:37:55 +0200

    Update LangChain example.

---
+++ examples/next-langchain/app/api/completion-stream-data/route.ts
@@ -0,0 +1,28 @@
+import { ChatOpenAI } from '@langchain/openai';
+import { LangChainAdapter, StreamData, StreamingTextResponse } from 'ai';
+
+export const dynamic = 'force-dynamic';
+export const maxDuration = 60;
+
+const model = new ChatOpenAI({
+  model: 'gpt-3.5-turbo-0125',
+  temperature: 0,
+});
+
+export async function POST(req: Request) {
+  const { prompt } = await req.json();
+
+  const stream = await model.stream(prompt);
+
+  const data = new StreamData();
+
+  data.append({ test: 'value' });
+
+  const aiStream = LangChainAdapter.toAIStream(stream, {
+    onFinal() {
+      data.close();
+    },
+  });
+
+  return new StreamingTextResponse(aiStream, {}, data);
+}
---
+++ examples/next-langchain/app/api/completion/route.ts
@@ -0,0 +1,18 @@
+import { ChatOpenAI } from '@langchain/openai';
+import { LangChainAdapter, StreamingTextResponse } from 'ai';
+
+export const dynamic = 'force-dynamic';
+export const maxDuration = 60;
+
+const model = new ChatOpenAI({
+  model: 'gpt-3.5-turbo-0125',
+  temperature: 0,
+});
+
+export async function POST(req: Request) {
+  const { prompt } = await req.json();
+
+  const stream = await model.stream(prompt);
+
+  return new StreamingTextResponse(LangChainAdapter.toAIStream(stream));
+}
