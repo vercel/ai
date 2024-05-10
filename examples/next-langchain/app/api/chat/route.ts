9cd0e0df68dea6d1f3a6f0a7d828b6d826872846
Lars Grammel <lars.grammel@gmail.com>
2024-05-10 11:28:18 +0200

    Move model.

--- examples/next-langchain/app/api/chat/route.ts
+++ examples/next-langchain/app/api/chat/route.ts
@@ -5,17 +5,17 @@ import { AIMessage, HumanMessage } from 'langchain/schema';
 export const dynamic = 'force-dynamic';
 export const maxDuration = 60;
 
-const model = new ChatOpenAI({
-  model: 'gpt-3.5-turbo-0125',
-  temperature: 0,
-});
-
 export async function POST(req: Request) {
   const {
     messages,
   }: {
     messages: Message[];
   } = await req.json();
+
+  const model = new ChatOpenAI({
+    model: 'gpt-3.5-turbo-0125',
+    temperature: 0,
+  });
 
   const stream = await model.stream(
     messages.map(message =>
