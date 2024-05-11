60ae839a0de6a49ec1e26f7fde985002ce961f49
Jake Hall <jake.hallslm@gmail.com>
2024-05-11 16:23:16 +0100

    add svelte kit useAssitant example

---
+++ examples/sveltekit-openai/src/routes/api/assistant/assistant-setup.md
@@ -0,0 +1,22 @@
+# Home Automation Assistant Example
+
+## Setup
+
+### Create OpenAI Assistant
+
+[OpenAI Assistant Website](https://platform.openai.com/assistants)
+
+Create a new assistant. Add the following functions and instructions to the assistant.
+
+Then add the assistant id to the `.env` file as `ASSISTANT_ID=your-assistant-id`.
+
+### Instructions
+
+```
+You are an assistant who always answers like Kanye West. Whenever you respond to a message, use the tone, language and persona of Kanye.
+```
+
+## Run
+
+1. Run `pnpm run dev` in `examples/sveltekit-openai`
+2. Go to http://localhost:3000/assistant
---
+++ examples/sveltekit-openai/src/routes/assistant/+page.svelte
@@ -0,0 +1,39 @@
+<script>
+	import { useAssistant } from '../../../../../packages/core/svelte/use-assistant'
+
+	const { messages, input, submitMessage } = useAssistant({
+		api: '/api/assistant',
+	});
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>useAssistant</h1>
+	<ul>
+		{#each $messages as message}
+			<li>{message.role}: {message.content}</li>
+		{/each}
+	</ul>
+	<form on:submit={submitMessage}>
+		<input bind:value={$input} />
+		<button type="submit">Send</button>
+	</form>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+</style>
