ead5729ae7a76deebc522657698016c762e48303
Lars Grammel <lars.grammel@gmail.com>
2024-05-14 16:07:33 +0200

    chore: update language model v1 jsdoc content & formatting.

--- packages/provider/src/language-model/v1/language-model-v1.ts
+++ packages/provider/src/language-model/v1/language-model-v1.ts
@@ -5,65 +5,64 @@ import { LanguageModelV1FunctionToolCall } from './language-model-v1-function-to
 import { LanguageModelV1LogProbs } from './language-model-v1-logprobs';
 
 /**
- * Experimental: Specification for a language model that implements the language model
- * interface version 1.
+Specification for a language model that implements the language model interface version 1.
  */
 export type LanguageModelV1 = {
   /**
-   * The language model must specify which language model interface
-   * version it implements. This will allow us to evolve the language
-   * model interface and retain backwards compatibility. The different
-   * implementation versions can be handled as a discriminated union
-   * on our side.
+The language model must specify which language model interface
+version it implements. This will allow us to evolve the language
+model interface and retain backwards compatibility. The different
+implementation versions can be handled as a discriminated union
+on our side.
    */
   readonly specificationVersion: 'v1';
 
   /**
-   * Name of the provider for logging purposes.
+Name of the provider for logging purposes.
    */
   readonly provider: string;
 
   /**
-   * Provider-specific model ID for logging purposes.
+Provider-specific model ID for logging purposes.
    */
   readonly modelId: string;
 
   /**
-   * Default object generation mode that should be used with this model when
-   * no mode is specified. Should be the mode with the best results for this
-   * model. `undefined` can be returned if object generation is not supported.
-   *
-   * This is needed to generate the best objects possible w/o requiring the
-   * user to explicitly specify the object generation mode.
+Default object generation mode that should be used with this model when
+no mode is specified. Should be the mode with the best results for this
+model. `undefined` can be returned if object generation is not supported.
+
+This is needed to generate the best objects possible w/o requiring the
+user to explicitly specify the object generation mode.
    */
   readonly defaultObjectGenerationMode: 'json' | 'tool' | 'grammar' | undefined;
 
   /**
-   * Generates a language model output (non-streaming).
-   *
-   * Naming: "do" prefix to prevent accidental direct usage of the method
-   * by the user.
+Generates a language model output (non-streaming).
+
+Naming: "do" prefix to prevent accidental direct usage of the method
+by the user.
    */
   doGenerate(options: LanguageModelV1CallOptions): PromiseLike<{
     /**
-     * Text that the model has generated. Can be undefined if the model
-     * has only generated tool calls.
+Text that the model has generated. Can be undefined if the model
+has only generated tool calls.
      */
     text?: string;
 
     /**
-     * Tool calls that the model has generated. Can be undefined if the
-     * model has only generated text.
+Tool calls that the model has generated. Can be undefined if the
+model has only generated text.
      */
     toolCalls?: Array<LanguageModelV1FunctionToolCall>;
 
     /**
-     * Finish reason.
+Finish reason.
      */
     finishReason: LanguageModelV1FinishReason;
 
     /**
-     * Usage information.
+  Usage information.
      */
     usage: {
       promptTokens: number;
@@ -71,75 +70,75 @@ export type LanguageModelV1 = {
     };
 
     /**
-     * Raw prompt and setting information for observability provider integration.
+Raw prompt and setting information for observability provider integration.
      */
     rawCall: {
       /**
-       * Raw prompt after expansion and conversion to the format that the
-       * provider uses to send the information to their API.
+Raw prompt after expansion and conversion to the format that the
+provider uses to send the information to their API.
        */
       rawPrompt: unknown;
 
       /**
-       * Raw settings that are used for the API call. Includes provider-specific
-       * settings.
+Raw settings that are used for the API call. Includes provider-specific
+settings.
        */
       rawSettings: Record<string, unknown>;
     };
 
     /**
-     * Optional raw response information for debugging purposes.
+Optional raw response information for debugging purposes.
      */
     rawResponse?: {
       /**
-       * Response headers.
-       */
+Response headers.
+      */
       headers?: Record<string, string>;
     };
 
     warnings?: LanguageModelV1CallWarning[];
 
     /**
-     * Logprobs for the completion.
-     * `undefined` if the mode does not support logprobs or if was not enabled
+  Logprobs for the completion.
+  `undefined` if the mode does not support logprobs or if was not enabled
      */
     logprobs?: LanguageModelV1LogProbs;
   }>;
 
   /**
-   * Generates a language model output (streaming).
+Generates a language model output (streaming).
+
+Naming: "do" prefix to prevent accidental direct usage of the method
+by the user.
    *
-   * Naming: "do" prefix to prevent accidental direct usage of the method
-   * by the user.
-   *
-   * @return A stream of higher-level language model output parts.
+@return A stream of higher-level language model output parts.
    */
   doStream(options: LanguageModelV1CallOptions): PromiseLike<{
     stream: ReadableStream<LanguageModelV1StreamPart>;
 
     /**
-     * Raw prompt and setting information for observability provider integration.
+Raw prompt and setting information for observability provider integration.
      */
     rawCall: {
       /**
-       * Raw prompt after expansion and conversion to the format that the
-       * provider uses to send the information to their API.
+Raw prompt after expansion and conversion to the format that the
+provider uses to send the information to their API.
        */
       rawPrompt: unknown;
 
       /**
-       * Raw settings that are used for the API call. Includes provider-specific
-       * settings.
+Raw settings that are used for the API call. Includes provider-specific
+settings.
        */
       rawSettings: Record<string, unknown>;
     };
 
     /**
-     * Optional raw response data.
+Optional raw response data.
      */
     rawResponse?: {
       /**
-       * Response headers.
+Response headers.
        */
       headers?: Record<string, string>;
     };
