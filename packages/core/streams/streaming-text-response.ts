ae05fb77ddfcedcea6da82f3566020c191d77807
Lars Grammel <lars.grammel@gmail.com>
2024-05-17 11:15:04 +0200

    feat (ai/streams): add StreamData support to streamToResponse (#1615)
    Co-authored-by: Valtteri Karesto <valtteri.karesto@gmail.com>

--- packages/core/streams/streaming-text-response.ts
+++ packages/core/streams/streaming-text-response.ts
@@ -1,6 +1,5 @@
-import type { ServerResponse } from 'node:http';
-import { StreamData } from './stream-data';
 import { mergeStreams } from '../core/util/merge-streams';
+import { StreamData } from './stream-data';
 
 /**
  * A utility class for streaming text responses.
@@ -22,31 +21,4 @@ export class StreamingTextResponse extends Response {
       },
     });
   }
-}
-
-/**
- * A utility function to stream a ReadableStream to a Node.js response-like object.
- */
-export function streamToResponse(
-  res: ReadableStream,
-  response: ServerResponse,
-  init?: { headers?: Record<string, string>; status?: number },
-) {
-  response.writeHead(init?.status || 200, {
-    'Content-Type': 'text/plain; charset=utf-8',
-    ...init?.headers,
-  });
-
-  const reader = res.getReader();
-  function read() {
-    reader.read().then(({ done, value }: { done: boolean; value?: any }) => {
-      if (done) {
-        response.end();
-        return;
-      }
-      response.write(value);
-      read();
-    });
-  }
-  read();
 }
