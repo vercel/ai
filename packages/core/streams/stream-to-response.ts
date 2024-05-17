0bf1bd84022fe01639e6f65cfed8874bf23a31d2
Lars Grammel <lars.grammel@gmail.com>
2024-05-17 09:15:44 +0200

    move to separate file. use mergeStreams.

---
+++ packages/core/streams/stream-to-response.ts
@@ -0,0 +1,37 @@
+import type { ServerResponse } from 'node:http';
+import { StreamData } from './stream-data';
+import { mergeStreams } from '../core/util/merge-streams';
+
+/**
+ * A utility function to stream a ReadableStream to a Node.js response-like object.
+ */
+export function streamToResponse(
+  res: ReadableStream,
+  response: ServerResponse,
+  init?: { headers?: Record<string, string>; status?: number },
+  data?: StreamData,
+) {
+  response.writeHead(init?.status || 200, {
+    'Content-Type': 'text/plain; charset=utf-8',
+    ...init?.headers,
+  });
+
+  let processedStream = res;
+
+  if (data) {
+    processedStream = mergeStreams(data.stream, res);
+  }
+
+  const reader = processedStream.getReader();
+  function read() {
+    reader.read().then(({ done, value }: { done: boolean; value?: any }) => {
+      if (done) {
+        response.end();
+        return;
+      }
+      response.write(value);
+      read();
+    });
+  }
+  read();
+}
--- packages/core/streams/streaming-text-response.ts
+++ packages/core/streams/streaming-text-response.ts
@@ -1,6 +1,5 @@
-import type { ServerResponse } from 'node:http';
-import { StreamData } from './stream-data';
 import { mergeStreams } from '../core/util/merge-streams';
+import { StreamData } from './stream-data';
 
 /**
  * A utility class for streaming text responses.
@@ -22,38 +21,4 @@ export class StreamingTextResponse extends Response {
       },
     });
   }
-}
-
-/**
- * A utility function to stream a ReadableStream to a Node.js response-like object.
- */
-export function streamToResponse(
-  res: ReadableStream,
-  response: ServerResponse,
-  init?: { headers?: Record<string, string>; status?: number },
-  data?: StreamData,
-) {
-  response.writeHead(init?.status || 200, {
-    'Content-Type': 'text/plain; charset=utf-8',
-    ...init?.headers,
-  });
-
-  let processedStream = res;
-
-  if (data) {
-    processedStream = res.pipeThrough(data.stream);
-  }
-
-  const reader = processedStream.getReader();
-  function read() {
-    reader.read().then(({ done, value }: { done: boolean; value?: any }) => {
-      if (done) {
-        response.end();
-        return;
-      }
-      response.write(value);
-      read();
-    });
-  }
-  read();
 }
