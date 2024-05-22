4f1f1954f636ddb05f5a3b9f83ee27a25ae7a6be
Lars Grammel <lars.grammel@gmail.com>
2024-05-22 15:53:33 +0200

    examples: fix and improve next pages router example (#1667)

---
+++ examples/next-openai-pages/app/api/chat-app-route/route.ts
@@ -0,0 +1,19 @@
+import { openai } from '@ai-sdk/openai';
+import { streamText } from 'ai';
+
+export const dynamic = 'force-dynamic';
+export const maxDuration = 60;
+
+export async function POST(req: Request) {
+  // Extract the `messages` from the body of the request
+  const { messages } = await req.json();
+
+  // Call the language model
+  const result = await streamText({
+    model: openai('gpt-4-turbo'),
+    messages,
+  });
+
+  // Respond with the stream
+  return result.toAIStreamResponse();
+}
