864d915fd3bf62c2c28cd3067709462c38084f75
Andrew Risse <andrewrisse@gmail.com>
2024-05-29 13:56:52 -0600

    fix: spread data in append function

--- packages/core/svelte/use-chat.ts
+++ packages/core/svelte/use-chat.ts
@@ -260,6 +260,7 @@ export function useChat({
       function_call,
       tools,
       tool_choice,
+      data
     }: ChatRequestOptions = {},
   ) => {
     if (!message.id) {
@@ -273,6 +274,7 @@ export function useChat({
       ...(function_call !== undefined && { function_call }),
       ...(tools !== undefined && { tools }),
       ...(tool_choice !== undefined && { tool_choice }),
+      ...(data !== undefined && { data })
     };
     return triggerRequest(chatRequest);
   };
