7de628e93599fc86629839271ffdb96c48a5b503
Lars Grammel <lars.grammel@gmail.com>
2024-05-29 11:56:24 +0200

    feat (ui): add onToolCall callback to useChat (#1729)

---
+++ .changeset/three-foxes-dream.md
@@ -0,0 +1,5 @@
+---
+'ai': patch
+---
+
+feat (ui): add onToolCall handler to useChat
--- examples/next-openai/app/api/use-chat-tools/route.ts
+++ examples/next-openai/app/api/use-chat-tools/route.ts
@@ -10,21 +10,31 @@ export async function POST(req: Request) {
 
   const result = await streamText({
     model: openai('gpt-4-turbo'),
-    system:
-      'You are a weather bot that can use the weather tool to get the weather in a given city. ' +
-      'Respond to the user with weather information in a friendly and helpful manner.',
     messages: convertToCoreMessages(messages),
     tools: {
-      weather: {
+      // server-side tool with execute function:
+      getWeatherInformation: {
         description: 'show the weather in a given city to the user',
         parameters: z.object({ city: z.string() }),
         execute: async ({}: { city: string }) => {
-          // Random weather:
           const weatherOptions = ['sunny', 'cloudy', 'rainy', 'snowy', 'windy'];
           return weatherOptions[
             Math.floor(Math.random() * weatherOptions.length)
           ];
         },
+      },
+      // client-side tool that starts user interaction:
+      askForConfirmation: {
+        description: 'Ask the user for confirmation.',
+        parameters: z.object({
+          message: z.string().describe('The message to ask for confirmation.'),
+        }),
+      },
+      // client-side tool that is automatically executed on the client:
+      getLocation: {
+        description:
+          'Get the user location. Always ask for confirmation before using this tool.',
+        parameters: z.object({}),
       },
     },
   });
