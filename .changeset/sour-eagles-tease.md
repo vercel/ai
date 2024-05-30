0e1da47608080875c73f292ddb4f5d7c4d550449
Lars Grammel <lars.grammel@gmail.com>
2024-05-30 11:24:36 +0200

    feat (core): add maxAutomaticRoundtrips setting to generateText (#1750)

---
+++ .changeset/sour-eagles-tease.md
@@ -0,0 +1,5 @@
+---
+'ai': patch
+---
+
+feat (core): add maxAutomaticRoundtrips setting to generateText
--- packages/core/core/prompt/convert-to-language-model-prompt.ts
+++ packages/core/core/prompt/convert-to-language-model-prompt.ts
@@ -4,6 +4,7 @@ import {
   LanguageModelV1Prompt,
   LanguageModelV1TextPart,
 } from '@ai-sdk/provider';
+import { CoreMessage } from '../prompt/message';
 import { detectImageMimeType } from '../util/detect-image-mimetype';
 import { convertDataContentToUint8Array } from './data-content';
 import { ValidatedPrompt } from './get-validated-prompt';
@@ -17,7 +18,8 @@ export function convertToLanguageModelPrompt(
     languageModelMessages.push({ role: 'system', content: prompt.system });
   }
 
-  switch (prompt.type) {
+  const promptType = prompt.type;
+  switch (promptType) {
     case 'prompt': {
       languageModelMessages.push({
         role: 'user',
@@ -28,82 +30,86 @@ export function convertToLanguageModelPrompt(
 
     case 'messages': {
       languageModelMessages.push(
-        ...prompt.messages.map((message): LanguageModelV1Message => {
-          switch (message.role) {
-            case 'system': {
-              return { role: 'system', content: message.content };
-            }
-
-            case 'user': {
-              if (typeof message.content === 'string') {
-                return {
-                  role: 'user',
-                  content: [{ type: 'text', text: message.content }],
-                };
-              }
-
-              return {
-                role: 'user',
-                content: message.content.map(
-                  (
-                    part,
-                  ): LanguageModelV1TextPart | LanguageModelV1ImagePart => {
-                    switch (part.type) {
-                      case 'text': {
-                        return part;
-                      }
-
-                      case 'image': {
-                        if (part.image instanceof URL) {
-                          return {
-                            type: 'image',
-                            image: part.image,
-                            mimeType: part.mimeType,
-                          };
-                        }
-
-                        const imageUint8 = convertDataContentToUint8Array(
-                          part.image,
-                        );
-
-                        return {
-                          type: 'image',
-                          image: imageUint8,
-                          mimeType:
-                            part.mimeType ?? detectImageMimeType(imageUint8),
-                        };
-                      }
-                    }
-                  },
-                ),
-              };
-            }
-
-            case 'assistant': {
-              if (typeof message.content === 'string') {
-                return {
-                  role: 'assistant',
-                  content: [{ type: 'text', text: message.content }],
-                };
-              }
-
-              return { role: 'assistant', content: message.content };
-            }
-
-            case 'tool': {
-              return message;
-            }
-          }
-        }),
+        ...prompt.messages.map(convertToLanguageModelMessage),
       );
       break;
     }
 
     default: {
-      const _exhaustiveCheck: never = prompt;
+      const _exhaustiveCheck: never = promptType;
       throw new Error(`Unsupported prompt type: ${_exhaustiveCheck}`);
     }
   }
 
   return languageModelMessages;
+}
+
+export function convertToLanguageModelMessage(
+  message: CoreMessage,
+): LanguageModelV1Message {
+  switch (message.role) {
+    case 'system': {
+      return { role: 'system', content: message.content };
+    }
+
+    case 'user': {
+      if (typeof message.content === 'string') {
+        return {
+          role: 'user',
+          content: [{ type: 'text', text: message.content }],
+        };
+      }
+
+      return {
+        role: 'user',
+        content: message.content.map(
+          (part): LanguageModelV1TextPart | LanguageModelV1ImagePart => {
+            switch (part.type) {
+              case 'text': {
+                return part;
+              }
+
+              case 'image': {
+                if (part.image instanceof URL) {
+                  return {
+                    type: 'image',
+                    image: part.image,
+                    mimeType: part.mimeType,
+                  };
+                }
+
+                const imageUint8 = convertDataContentToUint8Array(part.image);
+
+                return {
+                  type: 'image',
+                  image: imageUint8,
+                  mimeType: part.mimeType ?? detectImageMimeType(imageUint8),
+                };
+              }
+            }
+          },
+        ),
+      };
+    }
+
+    case 'assistant': {
+      if (typeof message.content === 'string') {
+        return {
+          role: 'assistant',
+          content: [{ type: 'text', text: message.content }],
+        };
+      }
+
+      return { role: 'assistant', content: message.content };
+    }
+
+    case 'tool': {
+      return message;
+    }
+
+    default: {
+      const _exhaustiveCheck: never = message;
+      throw new Error(`Unsupported message role: ${_exhaustiveCheck}`);
+    }
+  }
 }
