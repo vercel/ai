3a12ea7d1c34b3bc021be6d0e38a78ba4d855fb2
Lars Grammel <lars.grammel@gmail.com>
2024-06-05 18:19:28 +0200

    vertex

--- packages/google-vertex/src/convert-to-google-vertex-content-request.test.ts
+++ packages/google-vertex/src/convert-to-google-vertex-content-request.test.ts
@@ -1,19 +1,28 @@
 import { convertToGoogleVertexContentRequest } from './convert-to-google-vertex-content-request';
 
 describe('user message', () => {
-  it('should convert uint8 image parts', async () => {
-    const result = convertToGoogleVertexContentRequest([
-      {
-        role: 'user',
-        content: [
-          {
-            type: 'image',
-            image: new Uint8Array([0, 1, 2, 3]),
-            mimeType: 'image/png',
-          },
-        ],
+  it('should download images for user image parts with URLs', async () => {
+    const result = await convertToGoogleVertexContentRequest({
+      prompt: [
+        {
+          role: 'user',
+          content: [
+            {
+              type: 'image',
+              image: new URL('https://example.com/image.png'),
+            },
+          ],
+        },
+      ],
+      downloadImplementation: async ({ url }) => {
+        expect(url).toEqual(new URL('https://example.com/image.png'));
+
+        return {
+          data: new Uint8Array([0, 1, 2, 3]),
+          mimeType: 'image/png',
+        };
       },
-    ]);
+    });
 
     expect(result).toEqual({
       contents: [
@@ -32,36 +41,59 @@ describe('user message', () => {
     });
   });
 
-  it('should throw an error for URL image parts', async () => {
-    expect(() => {
-      convertToGoogleVertexContentRequest([
+  it('should add image parts for UInt8Array images', async () => {
+    const result = await convertToGoogleVertexContentRequest({
+      prompt: [
         {
           role: 'user',
           content: [
             {
               type: 'image',
-              image: new URL('https://example.com/image.png'),
+              image: new Uint8Array([0, 1, 2, 3]),
+              mimeType: 'image/png',
             },
           ],
         },
-      ]);
-    }).toThrow('URL image parts');
+      ],
+
+      downloadImplementation: async ({ url }) => {
+        throw new Error('Unexpected download call');
+      },
+    });
+
+    expect(result).toEqual({
+      contents: [
+        {
+          role: 'user',
+          parts: [
+            {
+              inlineData: {
+                mimeType: 'image/png',
+                data: 'AAECAw==',
+              },
+            },
+          ],
+        },
+      ],
+    });
   });
 });
 
 describe('assistant message', () => {
   it("should convert text parts that aren't empty", async () => {
-    const result = convertToGoogleVertexContentRequest([
-      {
-        role: 'assistant',
-        content: [
-          {
-            type: 'text',
-            text: 'Hello, world!',
-          },
-        ],
-      },
-    ]);
+    const result = await convertToGoogleVertexContentRequest({
+      prompt: [
+        {
+          role: 'assistant',
+          content: [
+            {
+              type: 'text',
+              text: 'Hello, world!',
+            },
+          ],
+        },
+      ],
+    });
 
     expect(result).toEqual({
       contents: [
@@ -78,17 +110,19 @@ describe('assistant message', () => {
   });
 
   it('should exclude text parts that are empty', async () => {
-    const result = convertToGoogleVertexContentRequest([
-      {
-        role: 'assistant',
-        content: [
-          {
-            type: 'text',
-            text: '',
-          },
-        ],
-      },
-    ]);
+    const result = await convertToGoogleVertexContentRequest({
+      prompt: [
+        {
+          role: 'assistant',
+          content: [
+            {
+              type: 'text',
+              text: '',
+            },
+          ],
+        },
+      ],
+    });
 
     expect(result).toEqual({
       contents: [
@@ -101,19 +135,21 @@ describe('assistant message', () => {
   });
 
   it('should convert tool call parts', async () => {
-    const result = convertToGoogleVertexContentRequest([
-      {
-        role: 'assistant',
-        content: [
-          {
-            type: 'tool-call',
-            toolName: 'tool',
-            toolCallId: 'id',
-            args: { arg: 'value' },
-          },
-        ],
-      },
-    ]);
+    const result = await convertToGoogleVertexContentRequest({
+      prompt: [
+        {
+          role: 'assistant',
+          content: [
+            {
+              type: 'tool-call',
+              toolName: 'tool',
+              toolCallId: 'id',
+              args: { arg: 'value' },
+            },
+          ],
+        },
+      ],
+    });
 
     expect(result).toEqual({
       contents: [
@@ -135,19 +171,21 @@ describe('assistant message', () => {
 
 describe('tool message', () => {
   it('should convert tool response parts', async () => {
-    const result = convertToGoogleVertexContentRequest([
-      {
-        role: 'tool',
-        content: [
-          {
-            type: 'tool-result',
-            toolName: 'tool',
-            toolCallId: 'id',
-            result: { result: 'value' },
-          },
-        ],
-      },
-    ]);
+    const result = await convertToGoogleVertexContentRequest({
+      prompt: [
+        {
+          role: 'tool',
+          content: [
+            {
+              type: 'tool-result',
+              toolName: 'tool',
+              toolCallId: 'id',
+              result: { result: 'value' },
+            },
+          ],
+        },
+      ],
+    });
 
     expect(result).toEqual({
       contents: [
