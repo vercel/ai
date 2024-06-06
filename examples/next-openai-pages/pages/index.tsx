2492db23a8fb17f9f228f7914a60de0d9a44672b
jeremyphilemon <git@jrmyphlmn.com>
2024-06-07 01:26:09 +0300

    Fix build issue

--- examples/next-openai-pages/pages/index.tsx
+++ examples/next-openai-pages/pages/index.tsx
@@ -58,7 +58,7 @@ export default function Home() {
   return (
     <main className={`flex flex-col gap-2 p-2 ${inter.className}`}>
       {examples.map((example, index) => (
-        <Link className="flex flex-row" href={example.link}>
+        <Link key={example.link} className="flex flex-row" href={example.link}>
           <div className="w-6 text-zinc-400">{index + 1}.</div>
           <div className="hover:underline">{example.title}</div>
         </Link>
