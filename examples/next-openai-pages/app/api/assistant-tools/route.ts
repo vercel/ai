55e06da0d7a3916b47ad38951686406a16bad48d
Jeremy <git@jrmyphlmn.com>
2024-06-07 15:18:54 +0300

    Add next pages examples to folder (#1876)

---
+++ examples/next-openai-pages/app/api/assistant-tools/route.ts
@@ -0,0 +1,71 @@
+import { AssistantResponse } from 'ai';
+import OpenAI from 'openai';
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY || '',
+});
+
+export async function POST(req: Request) {
+  const input: {
+    threadId: string | null;
+    message: string;
+  } = await req.json();
+
+  const threadId = input.threadId ?? (await openai.beta.threads.create({})).id;
+
+  const createdMessage = await openai.beta.threads.messages.create(threadId, {
+    role: 'user',
+    content: input.message,
+  });
+
+  return AssistantResponse(
+    { threadId, messageId: createdMessage.id },
+    async ({ forwardStream }) => {
+      const runStream = openai.beta.threads.runs.stream(threadId, {
+        assistant_id:
+          process.env.ASSISTANT_ID ??
+          (() => {
+            throw new Error('ASSISTANT_ID is not set');
+          })(),
+      });
+
+      let runResult = await forwardStream(runStream);
+
+      while (
+        runResult?.status === 'requires_action' &&
+        runResult.required_action?.type === 'submit_tool_outputs'
+      ) {
+        const tool_outputs =
+          runResult.required_action.submit_tool_outputs.tool_calls.map(
+            (toolCall: any) => {
+              const parameters = JSON.parse(toolCall.function.arguments);
+
+              switch (toolCall.function.name) {
+                case 'celsiusToFahrenheit':
+                  const celsius = parseFloat(parameters.value);
+                  const fahrenheit = celsius * (9 / 5) + 32;
+
+                  return {
+                    tool_call_id: toolCall.id,
+                    output: `${celsius}°C is ${fahrenheit.toFixed(2)}°F`,
+                  };
+
+                default:
+                  throw new Error(
+                    `Unknown tool call function: ${toolCall.function.name}`,
+                  );
+              }
+            },
+          );
+
+        runResult = await forwardStream(
+          openai.beta.threads.runs.submitToolOutputsStream(
+            threadId,
+            runResult.id,
+            { tool_outputs },
+          ),
+        );
+      }
+    },
+  );
+}
