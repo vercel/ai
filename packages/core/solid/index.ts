20e65a7f2de1a31897a82835be2d97ee1631d535
Lars Grammel <lars.grammel@gmail.com>
2024-06-10 13:00:20 +0200

    solid integration

--- packages/core/solid/index.ts
+++ packages/core/solid/index.ts
@@ -1,2 +1,14 @@
-export * from './use-chat';
-export * from './use-completion';
+import {
+  useChat as useChatSolid,
+  useCompletion as useCompletionSolid,
+} from '@ai-sdk/solid';
+
+/**
+ * @deprecated Use `useChat` from `@ai-sdk/solid` instead.
+ */
+export const useChat = useChatSolid;
+
+/**
+ * @deprecated Use `useCompletion` from `@ai-sdk/solid` instead.
+ */
+export const useCompletion = useCompletionSolid;
