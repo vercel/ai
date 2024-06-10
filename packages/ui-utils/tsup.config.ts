d69e65ed083bc372778693822854043c527de5d0
Lars Grammel <lars.grammel@gmail.com>
2024-06-10 09:52:30 +0200

    add files

--- packages/ui-utils/tsup.config.ts
+++ packages/ui-utils/tsup.config.ts
@@ -7,11 +7,4 @@ export default defineConfig([
     dts: true,
     sourcemap: true,
   },
-  {
-    entry: ['src/test/index.ts'],
-    outDir: 'test/dist',
-    format: ['cjs', 'esm'],
-    dts: true,
-    sourcemap: true,
-  },
 ]);
