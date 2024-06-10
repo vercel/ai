cc2679bf12077cefed3bb7604ecbb605a63ade85
Lars Grammel <lars.grammel@gmail.com>
2024-06-10 13:25:58 +0200

    fix build, react

--- packages/core/react/index.ts
+++ packages/core/react/index.ts
@@ -1,3 +1,20 @@
-export * from './use-chat';
-export * from './use-completion';
-export * from './use-assistant';
+import {
+  useChat as useChatReact,
+  useCompletion as useCompletionReact,
+  useAssistant as useAssistantReact,
+} from '@ai-sdk/react';
+
+/**
+ * @deprecated Use `useChat` from `@ai-sdk/react` instead.
+ */
+export const useChat = useChatReact;
+
+/**
+ * @deprecated Use `useCompletion` from `@ai-sdk/react` instead.
+ */
+export const useCompletion = useCompletionReact;
+
+/**
+ * @deprecated Use `useAssistant` from `@ai-sdk/react` instead.
+ */
+export const useAssistant = useAssistantReact;
