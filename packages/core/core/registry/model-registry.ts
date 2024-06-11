807074dc9c58772e2a79f37f52c8972345297ba6
Lars Grammel <lars.grammel@gmail.com>
2024-06-11 10:42:42 +0200

    jsdoc

--- packages/core/core/registry/model-registry.ts
+++ packages/core/core/registry/model-registry.ts
@@ -2,13 +2,22 @@ import { LanguageModel } from '../types';
 import { NoSuchModelError } from './no-such-model-error';
 import { NoSuchProviderError } from './no-such-provider-error';
 
+/**
+Registry for managing models. It enables getting a model with a string id.
+ */
 export class ModelRegistry {
-  // model id -> model
+  // Mapping of model id to model
   private models: Record<string, LanguageModel> = {};
 
-  // provider id -> provider
+  // Mapping of provider id to provider
   private providers: Record<string, (id: string) => LanguageModel> = {};
 
+  /**
+Registers a language model with a given id.
+
+@param {string} id - The id of the model.
+@param {LanguageModel} model - The language model to register.
+   */
   registerLanguageModel({
     id,
     model,
@@ -19,6 +28,12 @@ export class ModelRegistry {
     this.models[id] = model;
   }
 
+  /**
+Registers a language model provider with a given id.
+
+@param {string} id - The id of the provider.
+@param {(id: string) => LanguageModel} provider - The provider function to register.
+   */
   registerLanguageModelProvider({
     id,
     provider,
@@ -31,9 +46,16 @@ export class ModelRegistry {
 
   /**
 Returns the language model with the given id.
-The id can either be a registered model id or use a provider prefix
+The id can either be a registered model id or use a provider prefix.
+Provider ids are separated from the model id by a colon: `providerId:modelId`.
+The model id is then passed to the provider function to get the model.
 
-@param id - The id of the model to return.
+@param {string} id - The id of the model to return.
+
+@throws {NoSuchModelError} If no model with the given id exists.
+@throws {NoSuchProviderError} If no provider with the given id exists.
+
+@returns {LanguageModel} The language model associated with the id.
    */
   languageModel(id: string): LanguageModel {
     let model = this.models[id];
