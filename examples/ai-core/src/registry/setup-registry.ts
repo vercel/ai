0fbf0070343c4411bcb02282cd51ff723765cc53
Lars Grammel <lars.grammel@gmail.com>
2024-06-12 17:56:33 +0200

    experiemntal

--- examples/ai-core/src/registry/setup-registry.ts
+++ examples/ai-core/src/registry/setup-registry.ts
@@ -1,11 +1,11 @@
 import { anthropic } from '@ai-sdk/anthropic';
 import { createOpenAI } from '@ai-sdk/openai';
-import { experimental_createModelRegistry } from 'ai';
+import { experimental_createModelRegistry as createModelRegistry } from 'ai';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const registry = experimental_createModelRegistry({
+export const registry = createModelRegistry({
   // register provider with prefix and default setup:
   anthropic,
 
