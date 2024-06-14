584c69a9dddd69024e282779e4766e6b37c3e983
Lars Grammel <lars.grammel@gmail.com>
2024-06-14 14:18:24 +0200

    parse error msg

--- packages/cohere/src/cohere-error.ts
+++ packages/cohere/src/cohere-error.ts
@@ -2,11 +2,7 @@ import { createJsonErrorResponseHandler } from '@ai-sdk/provider-utils';
 import { z } from 'zod';
 
 const cohereErrorDataSchema = z.object({
-  object: z.literal('error'),
   message: z.string(),
-  type: z.string(),
-  param: z.string().nullable(),
-  code: z.string().nullable(),
 });
 
 export type CohereErrorData = z.infer<typeof cohereErrorDataSchema>;
