cce6c247f01dd64dd70a3a158bb95cf4b42d7b36
Lars Grammel <lars.grammel@gmail.com>
2024-06-14 13:32:18 +0200

    test

---
+++ packages/cohere/src/cohere-chat-language-model.test.ts
@@ -0,0 +1,151 @@
+import { LanguageModelV1Prompt } from '@ai-sdk/provider';
+import { JsonTestServer } from '@ai-sdk/provider-utils/test';
+import { createCohere } from './cohere-provider';
+
+const TEST_PROMPT: LanguageModelV1Prompt = [
+  { role: 'user', content: [{ type: 'text', text: 'Hello' }] },
+];
+
+const provider = createCohere({ apiKey: 'test-api-key' });
+const model = provider.chat('command-r-plus');
+
+describe('doGenerate', () => {
+  const server = new JsonTestServer('https://api.cohere.com/v1/chat');
+
+  server.setupTestEnvironment();
+
+  function prepareJsonResponse({
+    input = '',
+    text = '',
+    tokens = {
+      input_tokens: 4,
+      output_tokens: 30,
+    },
+  }: {
+    input?: string;
+    text?: string;
+    tokens?: {
+      input_tokens: number;
+      output_tokens: number;
+    };
+  }) {
+    server.responseBodyJson = {
+      response_id: '0cf61ae0-1f60-4c18-9802-be7be809e712',
+      text,
+      generation_id: 'dad0c7cd-7982-42a7-acfb-706ccf598291',
+      chat_history: [
+        { role: 'USER', message: input },
+        { role: 'CHATBOT', message: text },
+      ],
+      finish_reason: 'COMPLETE',
+      meta: {
+        api_version: { version: '1' },
+        billed_units: { input_tokens: 9, output_tokens: 415 },
+        tokens,
+      },
+    };
+  }
+
+  it('should extract text response', async () => {
+    prepareJsonResponse({ text: 'Hello, World!' });
+
+    const { text } = await model.doGenerate({
+      inputFormat: 'prompt',
+      mode: { type: 'regular' },
+      prompt: TEST_PROMPT,
+    });
+
+    expect(text).toStrictEqual('Hello, World!');
+  });
+
+  it('should extract usage', async () => {
+    prepareJsonResponse({
+      tokens: { input_tokens: 20, output_tokens: 5 },
+    });
+
+    const { usage } = await model.doGenerate({
+      inputFormat: 'prompt',
+      mode: { type: 'regular' },
+      prompt: TEST_PROMPT,
+    });
+
+    expect(usage).toStrictEqual({
+      promptTokens: 20,
+      completionTokens: 5,
+    });
+  });
+
+  it('should expose the raw response headers', async () => {
+    prepareJsonResponse({});
+
+    server.responseHeaders = {
+      'test-header': 'test-value',
+    };
+
+    const { rawResponse } = await model.doGenerate({
+      inputFormat: 'prompt',
+      mode: { type: 'regular' },
+      prompt: TEST_PROMPT,
+    });
+
+    expect(rawResponse?.headers).toStrictEqual({
+      // default headers:
+      'content-type': 'application/json',
+
+      // custom header
+      'test-header': 'test-value',
+    });
+  });
+
+  it('should pass model, message, and chat history', async () => {
+    prepareJsonResponse({});
+
+    await model.doGenerate({
+      inputFormat: 'prompt',
+      mode: { type: 'regular' },
+      prompt: TEST_PROMPT,
+    });
+
+    expect(await server.getRequestBodyJson()).toStrictEqual({
+      model: 'command-r-plus',
+      message: 'Hello',
+      chat_history: [],
+    });
+  });
+
+  it('should pass custom headers', async () => {
+    prepareJsonResponse({});
+
+    const provider = createCohere({
+      apiKey: 'test-api-key',
+      headers: {
+        'Custom-Header': 'test-header',
+      },
+    });
+
+    await provider.chat('command-r-plus').doGenerate({
+      inputFormat: 'prompt',
+      mode: { type: 'regular' },
+      prompt: TEST_PROMPT,
+    });
+
+    const requestHeaders = await server.getRequestHeaders();
+    expect(requestHeaders.get('Custom-Header')).toStrictEqual('test-header');
+  });
+
+  it('should pass the api key as Authorization header', async () => {
+    prepareJsonResponse({});
+
+    const provider = createCohere({ apiKey: 'test-api-key' });
+
+    await provider.chat('command-r-plus').doGenerate({
+      inputFormat: 'prompt',
+      mode: { type: 'regular' },
+      prompt: TEST_PROMPT,
+    });
+
+    expect(
+      (await server.getRequestHeaders()).get('Authorization'),
+    ).toStrictEqual('Bearer test-api-key');
+  });
+});
