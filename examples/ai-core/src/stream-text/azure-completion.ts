fb9607cb6965ecb983beb11948d2c602e74f27e0
Lars Grammel <lars.grammel@gmail.com>
2024-06-17 18:53:29 +0200

    add example

---
+++ examples/ai-core/src/stream-text/azure-completion.ts
@@ -0,0 +1,18 @@
+import { azure } from '@ai-sdk/azure';
+import { streamText } from 'ai';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+async function main() {
+  const result = await streamText({
+    model: azure.completion('gpt-4o'), // use your own deployment
+    prompt: 'Invent a new holiday and describe its traditions.',
+  });
+
+  for await (const textPart of result.textStream) {
+    process.stdout.write(textPart);
+  }
+}
+
+main().catch(console.error);
