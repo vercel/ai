ec9ddbe10edaeeecc5678fbbf48480ecb605f8da
Jon Spaeth <jon.spaeth@caylent.com>
2024-06-20 16:32:13 -0500

    adding claude 3.5 sonnet

--- packages/amazon-bedrock/src/bedrock-chat-settings.ts
+++ packages/amazon-bedrock/src/bedrock-chat-settings.ts
@@ -4,6 +4,7 @@ export type BedrockChatModelId =
   | 'amazon.titan-text-express-v1'
   | 'anthropic.claude-v2:1'
   | 'anthropic.claude-3-sonnet-20240229-v1:0'
+  | 'anthropic.claude-3-5-sonnet-20240620-v1:0'
   | 'anthropic.claude-3-haiku-20240307-v1:0'
   | 'anthropic.claude-3-opus-20240229-v1:0'
   | 'cohere.command-r-v1:0'
