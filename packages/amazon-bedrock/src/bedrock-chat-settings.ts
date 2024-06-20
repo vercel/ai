531ed890f438a76f447573336f01f803230d8666
Jon Spaeth <jon.spaeth@caylent.com>
2024-06-20 14:13:23 -0500

    removing unsupported chat models

--- packages/amazon-bedrock/src/bedrock-chat-settings.ts
+++ packages/amazon-bedrock/src/bedrock-chat-settings.ts
@@ -1,18 +1,7 @@
-import {
-  SystemContentBlock,
-  ToolConfiguration,
-} from '@aws-sdk/client-bedrock-runtime';
-
 // https://docs.aws.amazon.com/bedrock/latest/userguide/model-ids.html
 export type BedrockChatModelId =
   | 'amazon.titan-tg1-large'
   | 'amazon.titan-text-express-v1'
-  | 'ai21.j2-grande-instruct'
-  | 'ai21.j2-jumbo-instruct'
-  | 'ai21.j2-mid'
-  | 'ai21.j2-mid-v1'
-  | 'ai21.j2-ultra'
-  | 'ai21.j2-ultra-v1'
   | 'anthropic.claude-v2:1'
   | 'anthropic.claude-3-sonnet-20240229-v1:0'
   | 'anthropic.claude-3-haiku-20240307-v1:0'
