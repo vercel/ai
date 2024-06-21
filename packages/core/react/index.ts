9f15b840f8c9a33d74a05c14def48ab7c0ba2703
Lars Grammel <lars.grammel@gmail.com>
2024-06-21 14:51:24 +0200

    chore (ai): remove deprecation from ai/react imports, add experimental_useObject

--- packages/core/react/index.ts
+++ packages/core/react/index.ts
@@ -2,26 +2,13 @@ import {
   useChat as useChatReact,
   useCompletion as useCompletionReact,
   useAssistant as useAssistantReact,
+  experimental_useObject as experimental_useObjectReact,
 } from '@ai-sdk/react';
 
-/**
- * @deprecated Use `useChat` from `@ai-sdk/react` instead.
- */
 export const useChat = useChatReact;
-
-/**
- * @deprecated Use `useCompletion` from `@ai-sdk/react` instead.
- */
 export const useCompletion = useCompletionReact;
-
-/**
- * @deprecated Use `useAssistant` from `@ai-sdk/react` instead.
- */
 export const useAssistant = useAssistantReact;
-
-/**
- * @deprecated Use `@ai-sdk/react` instead.
- */
+export const experimental_useObject = experimental_useObjectReact;
 export type {
   CreateMessage,
   Message,
