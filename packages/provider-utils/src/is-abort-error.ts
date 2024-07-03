28f95925df09a9aa58fefde4799f48f3cdd8efc0
ocavue <ocavue@gmail.com>
2024-07-04 01:52:59 +1000

    fix (provider-utils): check DOMException existence

--- packages/provider-utils/src/is-abort-error.ts
+++ packages/provider-utils/src/is-abort-error.ts
@@ -1,5 +1,6 @@
 export function isAbortError(error: unknown): error is DOMException {
   return (
+    typeof DOMException !== 'undefined' &&
     error instanceof DOMException &&
     (error.name === 'AbortError' || error.name === 'TimeoutError')
   );
