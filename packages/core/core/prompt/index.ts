b33095250b729bebcadcf896d09ecc926892835d
Aaron Friel <mayreply@aaronfriel.com>
2024-07-07 19:10:59 -0700

    fix (ai): Make public conversion CoreMessage to Provider messages
    These APIs were previously private, which made using the `doGenerate` and
    `doStream` APIs cumbersome from outside the AI SDK.
    
    Fixes #2196

--- packages/core/core/prompt/index.ts
+++ packages/core/core/prompt/index.ts
@@ -3,3 +3,6 @@ export * from './convert-to-core-messages';
 export * from './data-content';
 export * from './invalid-message-role-error';
 export * from './message';
+export * from './prompt';
+export * from './get-validated-prompt';
+export * from './convert-to-language-model-prompt';
