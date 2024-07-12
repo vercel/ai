baa28528db0084cea52bd5c3adc557e9c26f61b2
jeremyphilemon <git@jrmyphlmn.com>
2024-07-12 19:31:47 +0530

    Add JSDoc comments for attachment type

--- packages/ui-utils/src/types.ts
+++ packages/ui-utils/src/types.ts
@@ -121,8 +121,20 @@ export type ToolInvocation =
  * An attachment that can be sent along with a message.
  */
 export interface Attachment {
+  /**
+   * The name of the attachment.
+   */
   name?: string;
+
+  /**
+   * A string indicating the [media type](https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Type).
+   * By default, it's extracted from the pathname's extension.
+   */
   mimeType?: string;
+
+  /**
+   * The URL of the attachment. It can either be a URL to a hosted file or a [Data URL](https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/Data_URLs).
+   */
   url: string;
 }
 
