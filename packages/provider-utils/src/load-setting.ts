a5379c25523cde1c843d002125dcd17455287629
karl-richter <karl@langdock.com>
2024-07-17 11:45:15 +0200

    feat: modified settings loading

--- packages/provider-utils/src/load-setting.ts
+++ packages/provider-utils/src/load-setting.ts
@@ -1,16 +1,54 @@
 import { LoadSettingError } from '@ai-sdk/provider';
 
+// Added an overload for the `failOnMissing` parameter to be `true` by default.
 export function loadSetting({
   settingValue,
   environmentVariableName,
   settingName,
   description,
+  defaultValue,
+  failOnMissing,
 }: {
   settingValue: string | undefined;
   environmentVariableName: string;
   settingName: string;
   description: string;
-}): string {
+  defaultValue: string | undefined;
+  failOnMissing: true;
+}): string;
+
+// Added an overload for the `failOnMissing` parameter to be `false`.
+export function loadSetting({
+  settingValue,
+  environmentVariableName,
+  settingName,
+  description,
+  defaultValue,
+  failOnMissing,
+}: {
+  settingValue: string | undefined;
+  environmentVariableName: string;
+  settingName: string;
+  description: string;
+  defaultValue: string | undefined;
+  failOnMissing: false;
+}): string | undefined;
+
+export function loadSetting({
+  settingValue,
+  environmentVariableName,
+  settingName,
+  description,
+  defaultValue,
+  failOnMissing,
+}: {
+  settingValue: string | undefined;
+  environmentVariableName: string;
+  settingName: string;
+  description: string;
+  defaultValue: string | undefined;
+  failOnMissing: boolean;
+}) {
   if (typeof settingValue === 'string') {
     return settingValue;
   }
@@ -21,7 +59,7 @@ export function loadSetting({
     });
   }
 
-  if (typeof process === 'undefined') {
+  if (typeof process === 'undefined' && failOnMissing) {
     throw new LoadSettingError({
       message: `${description} setting is missing. Pass it using the '${settingName}' parameter. Environment variables is not supported in this environment.`,
     });
@@ -29,17 +67,17 @@ export function loadSetting({
 
   settingValue = process.env[environmentVariableName];
 
-  if (settingValue == null) {
+  if (settingValue == null && failOnMissing) {
     throw new LoadSettingError({
       message: `${description} setting is missing. Pass it using the '${settingName}' parameter or the ${environmentVariableName} environment variable.`,
     });
   }
 
-  if (typeof settingValue !== 'string') {
+  if (typeof settingValue !== 'string' && failOnMissing) {
     throw new LoadSettingError({
       message: `${description} setting must be a string. The value of the ${environmentVariableName} environment variable is not a string.`,
     });
   }
 
-  return settingValue;
+  return settingValue ?? defaultValue ?? undefined;
 }
