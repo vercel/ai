a78f44f29885ff11e3adf1b1633f7befb847a50f
Lars Grammel <lars.grammel@gmail.com>
2024-07-29 14:19:51 +0200

    send heder

--- packages/core/core/util/prepare-response-headers.ts
+++ packages/core/core/util/prepare-response-headers.ts
@@ -1,11 +1,18 @@
 export function prepareResponseHeaders(
   init: ResponseInit | undefined,
-  { contentType }: { contentType: string },
+  {
+    contentType,
+    aiStreamVersion,
+  }: { contentType: string; aiStreamVersion?: string | undefined },
 ) {
   const headers = new Headers(init?.headers ?? {});
 
   if (!headers.has('Content-Type')) {
     headers.set('Content-Type', contentType);
+  }
+
+  if (aiStreamVersion !== undefined) {
+    headers.set('X-Vercel-AI-Stream', aiStreamVersion);
   }
 
   return headers;
