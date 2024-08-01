9102be23b0dd0a432d0f330b51ca472b10d1ec56
Lars Grammel <lars.grammel@gmail.com>
2024-08-01 10:44:34 +0200

    feat (examples): expense tracker demo

---
+++ examples/next-openai/app/api/use-object-expense-tracker/route.ts
@@ -0,0 +1,41 @@
+import { openai } from '@ai-sdk/openai';
+import { streamObject } from 'ai';
+import { expenseSchema } from './schema';
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+
+export async function POST(req: Request) {
+  const { expense }: { expense: string } = await req.json();
+
+  console.log(
+    new Date().toLocaleDateString('en-US', {
+      year: 'numeric',
+      month: 'short',
+      day: '2-digit',
+    }),
+  );
+
+  const result = await streamObject({
+    model: openai('gpt-4-turbo'),
+    system:
+      'You categorize expenses into one of the following categories: ' +
+      'TRAVEL, MEALS, ENTERTAINMENT, OFFICE SUPPLIES, OTHER.' +
+      'The current date is: ' +
+      new Date()
+        .toLocaleDateString('en-US', {
+          year: 'numeric',
+          month: 'short',
+          day: '2-digit',
+        })
+        .replace(/(\w+) (\d+), (\d+)/, '$3-$1-$2') +
+      '. When no date is supplied, use the current date.',
+    prompt: `Please categorize the following expense: "${expense}"`,
+    schema: expenseSchema,
+    onFinish({ object }) {
+      // save object to database
+    },
+  });
+
+  return result.toTextStreamResponse();
+}
