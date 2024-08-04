6f7dacc3724b9f2eddd11b2e7b4c32a9bb55e5e9
Bhanu Nexus <4593794+BhanuNexus@users.noreply.github.com>
2024-08-05 02:35:21 +0530

    feat (provider/azure): update Azure API version to 2024-06-01

--- packages/azure/src/azure-openai-provider.test.ts
+++ packages/azure/src/azure-openai-provider.test.ts
@@ -58,7 +58,7 @@ describe('chat', () => {
 
       const searchParams = await server.getRequestUrlSearchParams();
       expect(searchParams.get('api-version')).toStrictEqual(
-        '2024-05-01-preview',
+        '2024-06-01',
       );
     });
 
@@ -108,7 +108,7 @@ describe('chat', () => {
 
       const requestUrl = await server.getRequestUrl();
       expect(requestUrl).toStrictEqual(
-        'https://test-resource.openai.azure.com/openai/deployments/test-deployment/chat/completions?api-version=2024-05-01-preview',
+        'https://test-resource.openai.azure.com/openai/deployments/test-deployment/chat/completions?api-version=2024-06-01',
       );
     });
   });
@@ -173,7 +173,7 @@ describe('completion', () => {
 
       const searchParams = await server.getRequestUrlSearchParams();
       expect(searchParams.get('api-version')).toStrictEqual(
-        '2024-05-01-preview',
+        '2024-06-01',
       );
     });
 
@@ -251,7 +251,7 @@ describe('embedding', () => {
 
       const searchParams = await server.getRequestUrlSearchParams();
       expect(searchParams.get('api-version')).toStrictEqual(
-        '2024-05-01-preview',
+        '2024-06-01',
       );
     });
 
