886d404801ed22ca65cd747bb6a43474b9b25590
Lars Grammel <lars.grammel@gmail.com>
2024-08-08 13:34:46 +0200

    chore (docs): improve deprecation jsdoc for StreamTextResponse.

--- packages/ai/streams/streaming-text-response.ts
+++ packages/ai/streams/streaming-text-response.ts
@@ -5,7 +5,15 @@ import { StreamData } from './stream-data';
 /**
  * A utility class for streaming text responses.
  *
- * @deprecated Use `streamText.toDataStreamResponse()` instead.
+ * @deprecated Use `streamText.toDataStreamResponse()` (if you did send StreamData)
+ * or a regular `Response` instead (if you did not send any StreamData):
+ *
+ * ```ts
+ * return new Response(stream, {
+ *   status: 200,
+ *   contentType: 'text/plain; charset=utf-8',
+ * })
+ * ```
  */
 export class StreamingTextResponse extends Response {
   constructor(res: ReadableStream, init?: ResponseInit, data?: StreamData) {
