405d2a0dbbadc46e1d4ec207dcd0a4994fcb5695
Lars Grammel <lars.grammel@gmail.com>
2024-08-13 13:43:49 +0200

    refactoring (ai/rsc): reorganize streamable value code in streamable-value folder (#2647)

--- packages/ai/rsc/rsc-server.ts
+++ packages/ai/rsc/rsc-server.ts
@@ -1,5 +1,5 @@
 export { getAIState, getMutableAIState } from './ai-state';
 export { createStreamableUI } from './create-streamable-ui';
-export { createStreamableValue } from './create-streamable-value';
 export { createAI } from './provider';
 export { render, streamUI } from './stream-ui';
+export { createStreamableValue } from './streamable-value/create-streamable-value';
