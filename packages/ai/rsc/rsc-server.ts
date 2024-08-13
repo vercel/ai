c4a1ad857f659e25dbad984965a1a7f69bff2665
Lars Grammel <lars.grammel@gmail.com>
2024-08-13 13:15:52 +0200

    refactoring (ai/rsc): move render into stream-ui folder (#2646)

--- packages/ai/rsc/rsc-server.ts
+++ packages/ai/rsc/rsc-server.ts
@@ -2,5 +2,4 @@ export { getAIState, getMutableAIState } from './ai-state';
 export { createStreamableUI } from './create-streamable-ui';
 export { createStreamableValue } from './create-streamable-value';
 export { createAI } from './provider';
-export { render } from './render';
-export { streamUI } from './stream-ui';
+export { render, streamUI } from './stream-ui';
