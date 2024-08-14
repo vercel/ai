eccbd8eb36e11b83a3ee30a9fda4d6f87684ce89
Lars Grammel <lars.grammel@gmail.com>
2024-08-14 11:24:50 +0200

    feat (ai/core): add onChunk callback to streamText (#2660)

---
+++ examples/ai-core/src/stream-text/openai-on-chunk.ts
@@ -0,0 +1,21 @@
+import { openai } from '@ai-sdk/openai';
+import { streamText } from 'ai';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+async function main() {
+  const result = await streamText({
+    model: openai('gpt-3.5-turbo'),
+    onChunk({ chunk }) {
+      console.log('onChunk', chunk);
+    },
+    prompt: 'Invent a new holiday and describe its traditions.',
+  });
+
+  // consume stream:
+  for await (const textPart of result.textStream) {
+  }
+}
+
+main().catch(console.error);
