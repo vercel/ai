5bd14bee6b7c90503e59fb7ca7b29c506405363a
Lars Grammel <lars.grammel@gmail.com>
2024-08-22 11:55:06 +0200

    test and tweak inject json

--- packages/ai/core/generate-object/inject-json-instruction.ts
+++ packages/ai/core/generate-object/inject-json-instruction.ts
@@ -3,15 +3,15 @@ import { JSONSchema7 } from 'json-schema';
 const DEFAULT_SCHEMA_PREFIX = 'JSON schema:';
 const DEFAULT_SCHEMA_SUFFIX =
   'You MUST answer with a JSON object that matches the JSON schema above.';
+const DEFAULT_GENERIC_SUFFIX = 'You MUST answer with JSON.';
 
-// TODO test
 export function injectJsonInstruction({
   prompt,
   schema,
   schemaPrefix = schema != null ? DEFAULT_SCHEMA_PREFIX : undefined,
   schemaSuffix = schema != null
     ? DEFAULT_SCHEMA_SUFFIX
-    : 'You MUST answer with JSON',
+    : DEFAULT_GENERIC_SUFFIX,
 }: {
   prompt?: string;
   schema?: JSONSchema7;
@@ -19,10 +19,10 @@ export function injectJsonInstruction({
   schemaSuffix?: string;
 }): string {
   return [
-    prompt,
-    prompt != null ? '' : null, // add a newline if prompt is not null
-    schemaPrefix ?? null,
-    schema != null ? JSON.stringify(schema) : null,
+    prompt != null && prompt.length > 0 ? prompt : undefined,
+    prompt != null && prompt.length > 0 ? '' : undefined, // add a newline if prompt is not null
+    schemaPrefix,
+    schema != null ? JSON.stringify(schema) : undefined,
     schemaSuffix,
   ]
     .filter(line => line != null)
