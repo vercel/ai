73788eb64fc9ff3221003583af050ecbd861b4f7
Lars Grammel <lars.grammel@gmail.com>
2024-08-23 14:32:17 +0200

    tweak example

--- examples/ai-core/src/stream-object/openai-array.ts
+++ examples/ai-core/src/stream-object/openai-array.ts
@@ -16,12 +16,11 @@ async function main() {
         .describe('Character class, e.g. warrior, mage, or thief.'),
       description: z.string(),
     }),
-    prompt:
-      'Generate 3 character descriptions for a fantasy role playing game.',
+    prompt: 'Generate 3 hero descriptions for a fantasy role playing game.',
   });
 
-  for await (const element of result.elementStream) {
-    console.log(element);
+  for await (const hero of result.elementStream) {
+    console.log(hero);
   }
 }
 
