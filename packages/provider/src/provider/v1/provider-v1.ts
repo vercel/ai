f3e32cf7ab524f1b083057966cf96a9114344686
Lars Grammel <lars.grammel@gmail.com>
2024-08-26 10:09:22 +0200

    add provider v1 spec

---
+++ packages/provider/src/provider/v1/provider-v1.ts
@@ -0,0 +1,27 @@
+import { EmbeddingModelV1 } from '../../embedding-model/v1/embedding-model-v1';
+import { LanguageModelV1 } from '../../language-model/v1/language-model-v1';
+
+/**
+ * Provides for language and text embedding models.
+ */
+export interface ProviderV1 {
+  /**
+Returns the language model with the given id in the format `providerId:modelId`.
+The model id is then passed to the provider function to get the model.
+
+@param {string} id - The id of the model to return.
+
+@returns {LanguageModel} The language model associated with the id, or undefined if no such model exists.
+   */
+  languageModel(modelId: string): LanguageModelV1 | undefined;
+
+  /**
+Returns the text embedding model with the given id in the format `providerId:modelId`.
+The model id is then passed to the provider function to get the model.
+
+@param {string} id - The id of the model to return.
+
+@returns {LanguageModel} The language model associated with the id, or undefined if no such model exists.
+   */
+  textEmbeddingModel(modelId: string): EmbeddingModelV1<string> | undefined;
+}
