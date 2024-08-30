11166c4aef09293cd4c26afbc502e8f10679e0c0
Lars Grammel <lars.grammel@gmail.com>
2024-08-30 08:50:35 +0200

    improve examples

--- examples/ai-core/src/stream-text/openai-tool-call.ts
+++ examples/ai-core/src/stream-text/openai-tool-call.ts
@@ -1,18 +1,29 @@
 import { openai } from '@ai-sdk/openai';
 import dotenv from 'dotenv';
 import { weatherTool } from '../tools/weather-tool';
-import { streamText } from 'ai';
+import { streamText, tool } from 'ai';
+import { z } from 'zod';
 
 dotenv.config();
 
 async function main() {
   const result = await streamText({
-    model: openai('gpt-3.5-turbo'),
+    model: openai('gpt-4-turbo'),
     maxToolRoundtrips: 5,
     tools: {
+      currentLocation: tool({
+        description: 'Get the current location.',
+        parameters: z.object({}),
+        execute: async () => {
+          const locations = ['New York', 'London', 'Paris'];
+          return {
+            location: locations[Math.floor(Math.random() * locations.length)],
+          };
+        },
+      }),
       weather: weatherTool,
     },
-    prompt: 'What is the weather in San Francisco?',
+    prompt: 'What is the weather in my current location and in Rome?',
   });
 
   for await (const chunk of result.fullStream) {
@@ -23,28 +34,16 @@ async function main() {
       }
 
       case 'tool-call': {
-        switch (chunk.toolName) {
-          case 'weather': {
-            console.log('TOOL CALL weather');
-            console.log(`location: ${chunk.args.location}`); // string
-            break;
-          }
-        }
-
+        console.log(
+          `TOOL CALL ${chunk.toolName} ${JSON.stringify(chunk.args)}`,
+        );
         break;
       }
 
       case 'tool-result': {
-        switch (chunk.toolName) {
-          case 'weather': {
-            console.log();
-            console.log('TOOL RESULT weather');
-            console.log(`location: ${chunk.args.location}`); // string
-            console.log(`temperature: ${chunk.result.temperature}`); // number
-            break;
-          }
-        }
-
+        console.log(
+          `TOOL RESULT ${chunk.toolName} ${JSON.stringify(chunk.result)}`,
+        );
         break;
       }
 
