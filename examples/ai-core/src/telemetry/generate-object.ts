ba0b865790a58a599acb8af79ccebc8dfd5cb504
Lars Grammel <lars.grammel@gmail.com>
2024-09-05 09:56:49 +0200

    example

--- examples/ai-core/src/telemetry/generate-object.ts
+++ examples/ai-core/src/telemetry/generate-object.ts
@@ -1,9 +1,18 @@
+import 'dotenv/config';
+
 import { openai } from '@ai-sdk/openai';
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+import { NodeSDK } from '@opentelemetry/sdk-node';
+import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-node';
 import { generateObject } from 'ai';
-import dotenv from 'dotenv';
 import { z } from 'zod';
 
-dotenv.config();
+const sdk = new NodeSDK({
+  traceExporter: new ConsoleSpanExporter(),
+  instrumentations: [getNodeAutoInstrumentations()],
+});
+
+sdk.start();
 
 async function main() {
   const result = await generateObject({
@@ -21,12 +30,19 @@ async function main() {
       }),
     }),
     prompt: 'Generate a lasagna recipe.',
+    experimental_telemetry: {
+      isEnabled: true,
+      functionId: 'my-awesome-function',
+      metadata: {
+        something: 'custom',
+        someOtherThing: 'other-value',
+      },
+    },
   });
 
   console.log(JSON.stringify(result.object.recipe, null, 2));
-  console.log();
-  console.log('Token usage:', result.usage);
-  console.log('Finish reason:', result.finishReason);
+
+  await sdk.shutdown();
 }
 
 main().catch(console.error);
