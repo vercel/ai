8d248dfb675335b6162c3f3516cab41fc65938b5
Lars Grammel <lars.grammel@gmail.com>
2024-09-05 10:05:12 +0200

    exports

--- packages/ai/core/generate-text/index.ts
+++ packages/ai/core/generate-text/index.ts
@@ -1,4 +1,4 @@
-export { generateText } from './generate-text';
+export { generateText, experimental_generateText } from './generate-text';
 export type { GenerateTextResult } from './generate-text-result';
-export { streamText } from './stream-text';
+export { streamText, experimental_streamText } from './stream-text';
 export type { StreamTextResult, TextStreamPart } from './stream-text-result';
