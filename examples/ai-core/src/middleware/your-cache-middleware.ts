13a147e168838ba4ed413043c66a548daaf3a98e
Lars Grammel <lars.grammel@gmail.com>
2024-09-06 09:45:17 +0200

    cache

--- examples/ai-core/src/middleware/your-cache-middleware.ts
+++ examples/ai-core/src/middleware/your-cache-middleware.ts
@@ -1,34 +1,21 @@
-import type {
-  LanguageModelV1,
-  LanguageModelV1CallOptions,
-  Experimental_LanguageModelV1Middleware as LanguageModelV1Middleware,
-} from 'ai';
+import type { Experimental_LanguageModelV1Middleware as LanguageModelV1Middleware } from 'ai';
+
+const cache = new Map<string, any>();
 
 export const yourCacheMiddleware: LanguageModelV1Middleware = {
   wrapGenerate: async ({ doGenerate, params }) => {
-    if (isCached(params)) {
-      return fromCache(params);
+    const cacheKey = JSON.stringify(params);
+
+    if (cache.has(cacheKey)) {
+      return cache.get(cacheKey);
     }
 
     const result = await doGenerate();
 
-    saveToCache(params, result);
+    cache.set(cacheKey, result);
 
     return result;
   },
+
+  // here you would implement the caching logic for streaming
 };
-
-function isCached(params: LanguageModelV1CallOptions): boolean {
-  return false;
-}
-
-function fromCache(
-  params: LanguageModelV1CallOptions,
-): Awaited<ReturnType<LanguageModelV1['doGenerate']>> {
-  return {} as any;
-}
-
-function saveToCache(
-  params: LanguageModelV1CallOptions,
-  result: Awaited<ReturnType<LanguageModelV1['doGenerate']>>,
-) {}
