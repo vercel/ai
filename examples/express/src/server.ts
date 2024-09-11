2328bcf3e0ea6af0cd5417320a1766bd60a755cc
Lars Grammel <lars.grammel@gmail.com>
2024-09-11 11:58:46 +0200

    impl

--- examples/express/src/server.ts
+++ examples/express/src/server.ts
@@ -1,9 +1,25 @@
+import { openai } from '@ai-sdk/openai';
+import { StreamData, streamText } from 'ai';
+import 'dotenv/config';
 import express, { Request, Response } from 'express';
 
 const app = express();
 
-app.post('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+app.post('/', async (req: Request, res: Response) => {
+  // use stream data (optional):
+  const data = new StreamData();
+  data.append('initialized call');
+
+  const result = await streamText({
+    model: openai('gpt-4o'),
+    prompt: 'Invent a new holiday and describe its traditions.',
+    onFinish() {
+      data.append('call completed');
+      data.close();
+    },
+  });
+
+  result.pipeDataStreamToResponse(res, { data });
 });
 
 app.listen(8080, () => {
