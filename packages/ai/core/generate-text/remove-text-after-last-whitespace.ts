ba13420d0f6595452c9028e4b6fac9b6172d35af
Lars Grammel <lars.grammel@gmail.com>
2024-09-26 09:44:22 +0200

    siplify

--- packages/ai/core/generate-text/remove-text-after-last-whitespace.ts
+++ packages/ai/core/generate-text/remove-text-after-last-whitespace.ts
@@ -1,6 +1,6 @@
-const lastWhitespaceRegexp = /^([\s\S]*?)(\s+)(\S*)$/;
+const lastWhitespaceRegexp = /^([\s\S]*?\s+)(\S*)$/;
 
 export function removeTextAfterLastWhitespace(text: string): string {
   const match = text.match(lastWhitespaceRegexp);
-  return match ? match[1] + match[2] : text;
+  return match ? match[1] : text;
 }
