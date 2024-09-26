0342b6dd3eec253b2288bf5483af67fc71758e79
Nico Albanese <nico.albanese@vercel.com>
2024-09-26 10:50:41 +0100

    add showcase projects

---
+++ content/showcase/projects.ts
@@ -0,0 +1,48 @@
+type Project = {
+  name: string
+  image: string
+  link: string
+}
+
+export const showcaseProjects: Project[] = [
+  {
+    name: "v0",
+    image: "v0.png",
+    link: "https://v0.dev/chat"
+  },
+  {
+    name: "Postgres.new",
+    image: "postgresnew.png",
+    link: "https://postgres.new"
+  },
+  {
+    name: "Midday",
+    image: "midday.png",
+    link: "https://www.midday.ai"
+  },
+  {
+    name: "AI Playground",
+    image: "aiplayground.png",
+    link: "https://sdk.vercel.ai/playground"
+  },
+  {
+    name: "Morphic",
+    link: "https://morphic.sh",
+    image: "morphic.png"
+  },
+  {
+    name: "Dub.sh",
+    link: "https://dub.sh",
+    image: "dubsh.png"
+  },
+  {
+    name: "Chatbase",
+    link: "https://chatbase.co",
+    image: "chatbase.png"
+  },
+  {
+    name: "2txt",
+    link: "https://2txt.vercel.app",
+    image: "2txt.png"
+  }
+]
