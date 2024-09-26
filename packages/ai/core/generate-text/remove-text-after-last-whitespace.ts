a424d7f50e9afc7e303e968b5f3137069f3ca7fd
Lars Grammel <lars.grammel@gmail.com>
2024-09-26 09:41:25 +0200

    fix

--- packages/ai/core/generate-text/remove-text-after-last-whitespace.ts
+++ packages/ai/core/generate-text/remove-text-after-last-whitespace.ts
@@ -1,4 +1,4 @@
-const lastWhitespaceRegexp = /^(.*?)(\s+)(\S*)$/s;
+const lastWhitespaceRegexp = /^([\s\S]*?)(\s+)(\S*)$/;
 
 export function removeTextAfterLastWhitespace(text: string): string {
   const match = text.match(lastWhitespaceRegexp);
