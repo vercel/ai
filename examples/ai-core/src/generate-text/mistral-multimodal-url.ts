1525c087977da36afa62756017283a0aa25e2aa5
Lars Grammel <lars.grammel@gmail.com>
2024-09-27 09:01:39 +0200

    revert

--- examples/ai-core/src/generate-text/mistral-multimodal-url.ts
+++ examples/ai-core/src/generate-text/mistral-multimodal-url.ts
@@ -1,18 +1,6 @@
-import { createMistral } from '@ai-sdk/mistral';
+import { mistral } from '@ai-sdk/mistral';
 import { generateText } from 'ai';
 import 'dotenv/config';
-
-const mistral = createMistral({
-  // example fetch wrapper that logs the input to the API call:
-  fetch: async (url, options) => {
-    console.log('URL', url);
-    console.log('Headers', JSON.stringify(options!.headers, null, 2));
-    console.log(
-      `Body ${JSON.stringify(JSON.parse(options!.body! as string), null, 2)}`,
-    );
-    return await fetch(url, options);
-  },
-});
 
 async function main() {
   const result = await generateText({
