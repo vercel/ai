5386f45ca760fe5633e0bf97bbf7c0e430ac9c36
Lars Grammel <lars.grammel@gmail.com>
2024-10-23 11:41:23 +0200

    feat (provider/anthropic): update model ids

--- packages/anthropic/src/anthropic-messages-settings.ts
+++ packages/anthropic/src/anthropic-messages-settings.ts
@@ -1,6 +1,9 @@
 // https://docs.anthropic.com/claude/docs/models-overview
 export type AnthropicMessagesModelId =
+  | 'claude-3-5-sonnet-latest'
+  | 'claude-3-5-sonnet-20241022'
   | 'claude-3-5-sonnet-20240620'
+  | 'claude-3-opus-latest'
   | 'claude-3-opus-20240229'
   | 'claude-3-sonnet-20240229'
   | 'claude-3-haiku-20240307'
