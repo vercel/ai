73a201396aa4d5618d91a30c9349fe0581607475
Lars Grammel <lars.grammel@gmail.com>
2024-10-25 16:36:41 +0200

    update bash example

--- examples/ai-core/src/generate-text/anthropic-computer-use-bash.ts
+++ examples/ai-core/src/generate-text/anthropic-computer-use-bash.ts
@@ -8,6 +8,7 @@ async function main() {
     tools: {
       bash: anthropic.tools.bash_20241022({
         async execute({ command }) {
+          console.log('COMMAND', command);
           return [
             {
               type: 'text',
@@ -19,30 +20,12 @@ async function main() {
           ];
         },
       }),
-      str_replace_editor: anthropic.tools.textEditor_20241022({
-        async execute({ command, path, old_str, new_str }) {
-          return [{ type: 'text', text: '' }];
-        },
-      }),
     },
     prompt: 'List the files in my home directory.',
     maxSteps: 2,
   });
 
-  for (const toolResult of result.toolResults) {
-    switch (toolResult.toolName) {
-      case 'bash': {
-        toolResult.args.command; // string
-        toolResult.result; // string
-        break;
-      }
-    }
-  }
-
   console.log(result.text);
-  console.log(result.finishReason);
-  console.log(JSON.stringify(result.toolCalls, null, 2));
-  console.log(JSON.stringify(result.steps, null, 2));
 }
 
 main().catch(console.error);
