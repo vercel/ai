61d3cbbf094bdaeea10d70edb354e15b3cdd5d1e
Lars Grammel <lars.grammel@gmail.com>
2024-10-25 16:24:39 +0200

    txt editr

--- examples/ai-core/src/generate-text/anthropic-computer-use-editor.ts
+++ examples/ai-core/src/generate-text/anthropic-computer-use-editor.ts
@@ -3,23 +3,44 @@ import { generateText } from 'ai';
 import 'dotenv/config';
 
 async function main() {
+  let editorContent = `
+## README
+This is a test file.
+  `;
+
   const result = await generateText({
     model: anthropic('claude-3-5-sonnet-20241022'),
     tools: {
       str_replace_editor: anthropic.tools.textEditor_20241022({
         async execute({ command, path, old_str, new_str }) {
-          return 'some text';
+          console.log({ command, path, old_str, new_str });
+          switch (command) {
+            case 'view': {
+              return editorContent;
+            }
+            case 'create': {
+              editorContent = new_str!;
+              return editorContent;
+            }
+            case 'str_replace': {
+              editorContent = editorContent.replace(old_str!, new_str!);
+              return editorContent;
+            }
+            case 'insert': {
+              editorContent = new_str!;
+              return editorContent;
+            }
+          }
         },
       }),
     },
-    prompt: 'List the files in my home directory.',
-    maxSteps: 2,
+    prompt: 'Update my README file to talk about AI.',
+    maxSteps: 5,
   });
 
-  console.log(result.text);
-  console.log(result.finishReason);
-  console.log(JSON.stringify(result.toolCalls, null, 2));
-  console.log(JSON.stringify(result.steps, null, 2));
+  console.log('TEXT', result.text);
+  console.log();
+  console.log('EDITOR CONTENT', editorContent);
 }
 
 main().catch(console.error);
