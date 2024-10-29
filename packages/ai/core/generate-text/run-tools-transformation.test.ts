46103bfaa109d716b7bd7bdd1c436b8296d63e6f
Lars Grammel <lars.grammel@gmail.com>
2024-10-29 09:28:44 +0100

    add test

---
+++ packages/ai/core/generate-text/run-tools-transformation.test.ts
@@ -0,0 +1,46 @@
+import {
+  convertArrayToReadableStream,
+  convertReadableStreamToArray,
+} from '@ai-sdk/provider-utils/test';
+import { MockTracer } from '../test/mock-tracer';
+import { runToolsTransformation } from './run-tools-transformation';
+import { LanguageModelV1StreamPart } from '@ai-sdk/provider';
+
+it('should forward text deltas correctly', async () => {
+  const inputStream: ReadableStream<LanguageModelV1StreamPart> =
+    convertArrayToReadableStream([
+      { type: 'text-delta', textDelta: 'Hello' },
+      { type: 'text-delta', textDelta: ', ' },
+      { type: 'text-delta', textDelta: 'world!' },
+      {
+        type: 'finish',
+        finishReason: 'stop',
+        logprobs: undefined,
+        usage: { completionTokens: 10, promptTokens: 3 },
+      },
+    ]);
+
+  const transformedStream = runToolsTransformation({
+    tools: undefined,
+    generatorStream: inputStream,
+    toolCallStreaming: false,
+    tracer: new MockTracer(),
+    telemetry: undefined,
+    abortSignal: undefined,
+  });
+
+  const result = await convertReadableStreamToArray(transformedStream);
+
+  expect(result).toEqual([
+    { type: 'text-delta', textDelta: 'Hello' },
+    { type: 'text-delta', textDelta: ', ' },
+    { type: 'text-delta', textDelta: 'world!' },
+    {
+      type: 'finish',
+      finishReason: 'stop',
+      logprobs: undefined,
+      usage: { completionTokens: 10, promptTokens: 3, totalTokens: 13 },
+      experimental_providerMetadata: undefined,
+    },
+  ]);
+});
