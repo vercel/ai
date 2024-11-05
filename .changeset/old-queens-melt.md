e6042b187ded593eacd206bd8d10c0f88b7d6ada
Lars Grammel <lars.grammel@gmail.com>
2024-11-05 09:29:46 +0100

    feat (provider/anthropic): add haiku 3.5 model ids (#3473)

---
+++ .changeset/old-queens-melt.md
@@ -0,0 +1,6 @@
+---
+'@ai-sdk/amazon-bedrock': patch
+'@ai-sdk/anthropic': patch
+---
+
+feat (provider/anthropic): add haiku 3.5 model ids
--- packages/anthropic/src/anthropic-messages-settings.ts
+++ packages/anthropic/src/anthropic-messages-settings.ts
@@ -3,6 +3,8 @@ export type AnthropicMessagesModelId =
   | 'claude-3-5-sonnet-latest'
   | 'claude-3-5-sonnet-20241022'
   | 'claude-3-5-sonnet-20240620'
+  | 'claude-3-5-haiku-latest'
+  | 'claude-3-5-haiku-20241022'
   | 'claude-3-opus-latest'
   | 'claude-3-opus-20240229'
   | 'claude-3-sonnet-20240229'
