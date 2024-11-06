f0e67b64b45c994c1d3cd65c28e163dc0b88de55
Lars Grammel <lars.grammel@gmail.com>
2024-11-06 13:47:15 +0100

    chore (core): remove deprecated constants

--- packages/ai/core/prompt/ui-message.ts
+++ packages/ai/core/prompt/ui-message.ts
@@ -2,13 +2,7 @@ import { Attachment, ToolInvocation } from '@ai-sdk/ui-utils';
 
 // only for internal use - should be removed when we fully migrate to core messages
 export type UIMessage = {
-  role:
-    | 'system'
-    | 'user'
-    | 'assistant'
-    | 'function' // @deprecated
-    | 'data'
-    | 'tool'; // @deprecated
+  role: 'system' | 'user' | 'assistant' | 'data';
 
   content: string;
   toolInvocations?: ToolInvocation[];
