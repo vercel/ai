0957ae7319cace936851ec2ba8228c0af6801e06
Lars Grammel <lars.grammel@gmail.com>
2024-11-07 14:32:50 +0100

    update

--- examples/ai-core/src/stream-text/openai-on-finish.ts
+++ examples/ai-core/src/stream-text/openai-on-finish.ts
@@ -6,15 +6,7 @@ async function main() {
   const result = await streamText({
     model: openai('gpt-4-turbo'),
     prompt: 'Invent a new holiday and describe its traditions.',
-    onFinish({
-      usage,
-      finishReason,
-      text,
-      toolCalls,
-      toolResults,
-      rawResponse,
-      warnings,
-    }) {
+    onFinish({ usage, finishReason, text, toolCalls, toolResults, response }) {
       console.log();
       console.log('onFinish');
       console.log('Token usage:', usage);
