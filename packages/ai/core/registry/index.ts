7bed2b6a8d6d693b47881ca41b8628d24c916f9d
Lars Grammel <lars.grammel@gmail.com>
2024-11-08 11:21:46 +0100

    fx

--- packages/ai/core/registry/index.ts
+++ packages/ai/core/registry/index.ts
@@ -1,4 +1,3 @@
 export { experimental_customProvider } from './custom-provider';
 export * from './no-such-provider-error';
-export type { experimental_Provider } from './provider';
 export * from './provider-registry';
