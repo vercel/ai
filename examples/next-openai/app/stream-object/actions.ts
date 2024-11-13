4eb788b07d40a71568c0e6bb127776f139db3f23
Lars Grammel <lars.grammel@gmail.com>
2024-11-13 15:43:17 +0100

    fx

--- examples/next-openai/app/stream-object/actions.ts
+++ examples/next-openai/app/stream-object/actions.ts
@@ -8,19 +8,19 @@ import { PartialNotification, notificationSchema } from './schema';
 export async function generateNotifications(context: string) {
   const notificationsStream = createStreamableValue<PartialNotification>();
 
-  streamObject({
+  const result = streamObject({
     model: openai('gpt-4-turbo'),
     prompt: `Generate 3 notifications for a messages app in this context: ${context}`,
     schema: notificationSchema,
-  })
-    .then(async ({ partialObjectStream }) => {
-      for await (const partialObject of partialObjectStream) {
-        notificationsStream.update(partialObject);
-      }
-    })
-    .finally(() => {
-      notificationsStream.done();
-    });
+  });
+
+  try {
+    for await (const partialObject of result.partialObjectStream) {
+      notificationsStream.update(partialObject);
+    }
+  } finally {
+    notificationsStream.done();
+  }
 
   return notificationsStream.value;
 }
