a2e0f02d6d21808b49a4755015ddcd0103e8672c
Walter Korman <shaper@vercel.com>
2024-11-17 08:44:24 -0800

    fix (packages/codemod): Ignore code under dot-prefixed dirs. (#3721)

--- packages/codemod/src/lib/transform.ts
+++ packages/codemod/src/lib/transform.ts
@@ -4,6 +4,9 @@ import fs from 'fs';
 import path from 'path';
 import { TransformOptions } from './transform-options';
 
+const log = debug('codemod:transform');
+const error = debug('codemod:transform:error');
+
 function getJscodeshift(): string {
   const localJscodeshift = path.resolve(
     __dirname,
@@ -12,20 +15,18 @@ function getJscodeshift(): string {
   return fs.existsSync(localJscodeshift) ? localJscodeshift : 'jscodeshift';
 }
 
-const log = debug('codemod:transform');
-const error = debug('codemod:transform:error');
-
-export function transform(
-  codemod: string,
-  source: string,
+function buildCommand(
+  codemodPath: string,
+  targetPath: string,
+  jscodeshift: string,
   options: TransformOptions,
-) {
-  log(`Applying codemod '${codemod}': ${source}`);
-
-  const codemodPath = path.resolve(__dirname, `../codemods/${codemod}.js`);
-  const targetPath = path.resolve(source);
-  const jscodeshift = getJscodeshift();
-  let command = `${jscodeshift} -t ${codemodPath} ${targetPath} --parser tsx --ignore-pattern="**/node_modules/**" --ignore-pattern="**/.next/**"`;
+): string {
+  // Ignoring everything under `.*/` covers `.next/` along with any other
+  // framework build related or otherwise intended-to-be-hidden directories.
+  let command = `${jscodeshift} -t ${codemodPath} ${targetPath} \
+    --parser tsx \
+    --ignore-pattern="**/node_modules/**" \
+    --ignore-pattern="**/.*/**"`;
 
   if (options.dry) {
     command += ' --dry';
@@ -43,6 +44,19 @@ export function transform(
     command += ` ${options.jscodeshift}`;
   }
 
+  return command;
+}
+
+export function transform(
+  codemod: string,
+  source: string,
+  options: TransformOptions,
+) {
+  log(`Applying codemod '${codemod}': ${source}`);
+  const codemodPath = path.resolve(__dirname, `../codemods/${codemod}.js`);
+  const targetPath = path.resolve(source);
+  const jscodeshift = getJscodeshift();
+  const command = buildCommand(codemodPath, targetPath, jscodeshift, options);
   try {
     execSync(command, { stdio: 'inherit' });
     log('Codemod applied successfully.');
