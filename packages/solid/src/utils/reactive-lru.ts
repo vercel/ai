242828722a7636ccf00eb0897c250a332bd42b3e
Ian Pascoe <ian.g.pascoe@gmail.com>
2024-11-21 08:38:45 -0500

    fix: type error

--- packages/solid/src/utils/reactive-lru.ts
+++ packages/solid/src/utils/reactive-lru.ts
@@ -61,7 +61,7 @@ export class ReactiveLRU<K, V> extends Map<K, V> {
     return super.size;
   }
 
-  *keys(): IterableIterator<K> {
+  *keys(): MapIterator<K> {
     for (const key of super.keys()) {
       this.#keyTriggers.track(key);
       yield key;
@@ -69,7 +69,7 @@ export class ReactiveLRU<K, V> extends Map<K, V> {
     this.#keyTriggers.track($KEYS);
   }
 
-  *values(): IterableIterator<V> {
+  *values(): MapIterator<V> {
     for (const [key, v] of super.entries()) {
       this.#valueTriggers.track(key);
       yield v;
@@ -77,7 +77,7 @@ export class ReactiveLRU<K, V> extends Map<K, V> {
     this.#keyTriggers.track($KEYS);
   }
 
-  *entries(): IterableIterator<[K, V]> {
+  *entries(): MapIterator<[K, V]> {
     for (const entry of super.entries()) {
       this.#valueTriggers.track(entry[0]);
       yield entry;
@@ -144,7 +144,7 @@ export class ReactiveLRU<K, V> extends Map<K, V> {
     }
   }
 
-  [Symbol.iterator](): IterableIterator<[K, V]> {
+  [Symbol.iterator](): MapIterator<[K, V]> {
     return this.entries();
   }
 }
