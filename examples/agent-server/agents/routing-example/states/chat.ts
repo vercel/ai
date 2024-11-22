1ef4ef09c6fe47e1763da3ec389be02233091f67
Lars Grammel <lars.grammel@gmail.com>
2024-11-22 14:14:21 +0100

    example

---
+++ examples/agent-server/agents/routing-example/states/chat.ts
@@ -0,0 +1,24 @@
+import { StreamState } from '@ai-sdk/agent-server';
+import { openai } from '@ai-sdk/openai';
+import { streamText } from 'ai';
+import { Context } from '../context';
+
+export default {
+  type: 'stream',
+  async execute({ context }) {
+    const result = streamText({
+      model: openai('gpt-4o'),
+      system: 'You are a friendly chatbot. Respond briefly and concisely.',
+      prompt: context.prompt,
+    });
+
+    return {
+      // streamText will expose streams specifically for the agent server
+      // in the future:
+      // stream: result.toAgentStream()
+      // for now we need to decode:
+      stream: result.toDataStream().pipeThrough(new TextDecoderStream()),
+      nextState: 'END',
+    };
+  },
+} satisfies StreamState<Context, string>;
