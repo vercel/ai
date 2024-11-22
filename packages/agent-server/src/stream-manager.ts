b6078404f90b131128d8c6f4cac260d471236d49
Lars Grammel <lars.grammel@gmail.com>
2024-11-22 14:12:11 +0100

    notes

--- packages/agent-server/src/stream-manager.ts
+++ packages/agent-server/src/stream-manager.ts
@@ -1,7 +1,15 @@
 import { createStitchableStream } from './util/create-stitchable-stream';
+import { JSONValue } from '@ai-sdk/provider';
 
 export class StreamManager {
-  private streams: Map<string, ReturnType<typeof createStitchableStream>>;
+  private streams: Map<
+    string,
+    {
+      stream: ReadableStream<JSONValue>;
+      addStream: (stream: ReadableStream<JSONValue>) => void;
+      close: () => void;
+    }
+  >;
 
   constructor() {
     this.streams = new Map();
@@ -22,13 +30,20 @@ export class StreamManager {
     stitchableStream.addStream(stream);
   }
 
-  getStream(runId: string): ReadableStream {
+  getStream(runId: string) {
     const stitchableStream = this.streams.get(runId);
     if (!stitchableStream) {
       throw new Error(`No stream found for run ${runId}`);
     }
 
-    // TODO how to support multiple consumers? if we tee, the stitching breaks
+    // how to support multiple consumers? if we tee, the stitching breaks
+    // because there is no second consumer, and therefore the stitched
+    // stream will not continue to the 2nd stream.
+    // therefore we must only tee if there is already a consumer for
+    // the stream.
+
+    // TODO what if one of the stream consumers breaks or stops consuming?
+
     return stitchableStream.stream;
   }
 
