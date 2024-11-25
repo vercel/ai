dc60f46061339c073f248c833b6041760e177e1d
Lars Grammel <lars.grammel@gmail.com>
2024-11-25 16:20:50 +0100

    example

--- examples/ai-core/src/generate-text/openai-output-object.ts
+++ examples/ai-core/src/generate-text/openai-output-object.ts
@@ -1,18 +1,32 @@
 import { openai } from '@ai-sdk/openai';
-import { generateText, Output } from 'ai';
+import { generateText, Output, tool } from 'ai';
 import 'dotenv/config';
 import { z } from 'zod';
 
 async function main() {
   const { usage, experimental_output } = await generateText({
     model: openai('gpt-4o-mini'), // TODO { structuredOutputs: true }),
-    prompt: 'Invent a new holiday and describe its traditions.',
+    tools: {
+      weather: tool({
+        description: 'Get the weather in a location',
+        parameters: z.object({
+          location: z.string().describe('The location to get the weather for'),
+        }),
+        // location below is inferred to be a string:
+        execute: async ({ location }) => ({
+          location,
+          temperature: 72 + Math.floor(Math.random() * 21) - 10,
+        }),
+      }),
+    },
     experimental_output: Output.object({
       schema: z.object({
-        holiday: z.string(),
-        traditions: z.array(z.string()),
+        location: z.string(),
+        temperature: z.number(),
       }),
     }),
+    maxSteps: 2,
+    prompt: 'What is the weather in San Francisco?',
   });
 
   console.log(experimental_output);
