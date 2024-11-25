63b839d197bf5b7c505a4e9bc62f6dc52ab988b9
Lars Grammel <lars.grammel@gmail.com>
2024-11-25 09:06:23 +0100

    fix (docs): fix hono streaming examples (headers)

--- examples/hono/src/server.ts
+++ examples/hono/src/server.ts
@@ -7,27 +7,25 @@ import { stream } from 'hono/streaming';
 
 const app = new Hono();
 
-app.post('/', async c =>
-  stream(c, async stream => {
-    // use stream data (optional):
-    const data = new StreamData();
-    data.append('initialized call');
+app.post('/', async c => {
+  // Mark the response as a v1 data stream:
+  c.header('X-Vercel-AI-Data-Stream', 'v1');
+  c.header('Content-Type', 'text/plain; charset=utf-8');
 
-    const result = streamText({
-      model: openai('gpt-4o'),
-      prompt: 'Invent a new holiday and describe its traditions.',
-      onFinish() {
-        data.append('call completed');
-        data.close();
-      },
-    });
+  // use stream data (optional):
+  const data = new StreamData();
+  data.append('initialized call');
 
-    // Mark the response as a v1 data stream:
-    c.header('X-Vercel-AI-Data-Stream', 'v1');
-    c.header('Content-Type', 'text/plain; charset=utf-8');
+  const result = streamText({
+    model: openai('gpt-4o'),
+    prompt: 'Invent a new holiday and describe its traditions.',
+    onFinish() {
+      data.append('call completed');
+      data.close();
+    },
+  });
 
-    await stream.pipe(result.toDataStream({ data }));
-  }),
-);
+  return stream(c, stream => stream.pipe(result.toDataStream({ data })));
+});
 
 serve({ fetch: app.fetch, port: 8080 });
