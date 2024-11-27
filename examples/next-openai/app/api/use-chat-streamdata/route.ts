55bcbed539fae47741b36f910e9bb0cea4fbce9f
Lars Grammel <lars.grammel@gmail.com>
2024-11-27 14:23:53 +0100

    route

--- examples/next-openai/app/api/use-chat-streamdata/route.ts
+++ examples/next-openai/app/api/use-chat-streamdata/route.ts
@@ -1,35 +1,31 @@
 import { openai } from '@ai-sdk/openai';
-import { generateId, StreamData, streamText } from 'ai';
-
-// Allow streaming responses up to 30 seconds
-export const maxDuration = 30;
+import { generateId, sendDataStreamResponse, streamText } from 'ai';
 
 export async function POST(req: Request) {
   const { messages } = await req.json();
 
-  // use stream data
-  const data = new StreamData();
-  data.append('initialized call');
+  // immediately start streaming (solves RAG issues with status, etc.)
+  return sendDataStreamResponse(dataStream => {
+    dataStream.appendData('initialized call');
 
-  const result = streamText({
-    model: openai('gpt-4o'),
-    messages,
-    onChunk() {
-      data.appendMessageAnnotation({ chunk: '123' });
-    },
-    onFinish() {
-      // message annotation:
-      data.appendMessageAnnotation({
-        id: generateId(), // e.g. id from saved DB record
-        other: 'information',
-      });
+    const result = streamText({
+      model: openai('gpt-4o'),
+      messages,
+      onChunk() {
+        dataStream.appendMessageAnnotation({ chunk: '123' });
+      },
+      onFinish() {
+        // message annotation:
+        dataStream.appendMessageAnnotation({
+          id: generateId(), // e.g. id from saved DB record
+          other: 'information',
+        });
 
-      // call annotation:
-      data.append('call completed');
+        // call annotation:
+        dataStream.appendData('call completed');
+      },
+    });
 
-      data.close();
-    },
+    result.forwardToDataStream(dataStream);
   });
-
-  return result.toDataStreamResponse({ data });
 }
