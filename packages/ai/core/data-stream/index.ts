b22fdfde6ec7ef16d65a9ec9a7d6a77052047b74
Lars Grammel <lars.grammel@gmail.com>
2024-11-27 16:45:40 +0100

    dx

--- packages/ai/core/data-stream/index.ts
+++ packages/ai/core/data-stream/index.ts
@@ -1,4 +1,4 @@
 export { createDataStream } from './create-data-stream';
+export { createDataStreamResponse } from './create-data-stream-response';
 export type { DataStream } from './data-stream';
 export { pipeDataStreamToResponse } from './pipe-data-stream-to-response';
-export { sendDataStreamResponse } from './send-data-stream-response';
