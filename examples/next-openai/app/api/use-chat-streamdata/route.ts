87cab2643c0e3990f56b34199b80c77913e08af7
Lars Grammel <lars.grammel@gmail.com>
2024-11-27 16:48:34 +0100

    fx

--- examples/next-openai/app/api/use-chat-streamdata/route.ts
+++ examples/next-openai/app/api/use-chat-streamdata/route.ts
@@ -1,34 +1,34 @@
 import { openai } from '@ai-sdk/openai';
-import { generateId, sendDataStreamResponse, streamText } from 'ai';
+import { generateId, createDataStreamResponse, streamText } from 'ai';
 
 export async function POST(req: Request) {
   const { messages } = await req.json();
 
   // immediately start streaming (solves RAG issues with status, etc.)
-  return sendDataStreamResponse({
+  return createDataStreamResponse({
     execute: dataStream => {
-      dataStream.appendData('initialized call');
+      dataStream.writeData('initialized call');
 
       const result = streamText({
         model: openai('gpt-4o'),
         messages,
         onChunk() {
-          dataStream.appendMessageAnnotation({ chunk: '123' });
+          dataStream.writeMessageAnnotation({ chunk: '123' });
         },
         onFinish() {
           // message annotation:
-          dataStream.appendMessageAnnotation({
+          dataStream.writeMessageAnnotation({
             id: generateId(), // e.g. id from saved DB record
             other: 'information',
           });
 
           // call annotation:
-          dataStream.appendData('call completed');
+          dataStream.writeData('call completed');
         },
       });
 
-      // TODO: result.forwardToDataStream(dataStream);
-      dataStream.forward(
+      // TODO: result.mergeIntoDataStream(dataStream);
+      dataStream.merge(
         result.toDataStream().pipeThrough(new TextDecoderStream()),
       );
     },
