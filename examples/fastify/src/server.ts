df9cb6758e03c61f4b2d445cd4d4433680f0cbc0
Lars Grammel <lars.grammel@gmail.com>
2024-11-28 14:20:57 +0100

    fastify

--- examples/fastify/src/server.ts
+++ examples/fastify/src/server.ts
@@ -1,21 +1,36 @@
 import { openai } from '@ai-sdk/openai';
-import { StreamData, streamText } from 'ai';
+import { createDataStream, streamText } from 'ai';
 import 'dotenv/config';
 import Fastify from 'fastify';
 
 const fastify = Fastify({ logger: true });
 
 fastify.post('/', async function (request, reply) {
-  // use stream data (optional):
-  const data = new StreamData();
-  data.append('initialized call');
-
   const result = streamText({
     model: openai('gpt-4o'),
     prompt: 'Invent a new holiday and describe its traditions.',
-    onFinish() {
-      data.append('call completed');
-      data.close();
+  });
+
+  // Mark the response as a v1 data stream:
+  reply.header('X-Vercel-AI-Data-Stream', 'v1');
+  reply.header('Content-Type', 'text/plain; charset=utf-8');
+
+  return reply.send(result.toDataStream());
+});
+
+fastify.post('/stream-data', async function (request, reply) {
+  // immediately start streaming the response
+  const stream = createDataStream({
+    execute: async dataStream => {
+      // send stream data:
+      dataStream.writeData('initialized call');
+
+      const result = streamText({
+        model: openai('gpt-4o'),
+        prompt: 'Invent a new holiday and describe its traditions.',
+      });
+
+      result.mergeIntoDataStream(dataStream);
     },
   });
 
@@ -23,7 +38,7 @@ fastify.post('/', async function (request, reply) {
   reply.header('X-Vercel-AI-Data-Stream', 'v1');
   reply.header('Content-Type', 'text/plain; charset=utf-8');
 
-  return reply.send(result.toDataStream({ data }));
+  return reply.send(stream);
 });
 
 fastify.listen({ port: 8080 });
