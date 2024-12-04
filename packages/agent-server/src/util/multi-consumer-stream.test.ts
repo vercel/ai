6fc17281f0cd4e431d47dcf8dca885862fbb1948
Lars Grammel <lars.grammel@gmail.com>
2024-12-04 12:13:57 +0100

    scenario 3

--- packages/agent-server/src/util/multi-consumer-stream.test.ts
+++ packages/agent-server/src/util/multi-consumer-stream.test.ts
@@ -34,4 +34,43 @@ describe('MultiConsumerStream', () => {
       '3',
     ]);
   });
+
+  describe('scenario: first consumer cancels after reading some chunks, second consumer connects and reads remaining chunks', async () => {
+    const multiConsumerStream = new MultiConsumerStream({
+      stream: convertArrayToReadableStream(['1', '2', '3']),
+    });
+
+    const stream1 = multiConsumerStream.split();
+    const result1: string[] = [];
+    const reader1 = stream1.getReader();
+
+    async function pull1() {
+      const { value, done } = await reader1.read();
+      if (!done) {
+        result1.push(value!);
+      }
+    }
+
+    await pull1();
+    await pull1();
+
+    // disconnect through cancel
+    reader1.cancel();
+
+    const stream2 = multiConsumerStream.split();
+    const result2: string[] = [];
+    const reader2 = stream2.getReader();
+
+    async function pull2() {
+      const { value, done } = await reader2.read();
+      if (!done) {
+        result2.push(value);
+      }
+    }
+
+    await pull2();
+
+    expect(result1).toEqual(['1', '2']);
+    expect(result2).toEqual(['3']);
+  });
 });
--- packages/agent-server/src/util/multi-consumer-stream.ts
+++ packages/agent-server/src/util/multi-consumer-stream.ts
@@ -1,6 +1,8 @@
+import { generateId } from '@ai-sdk/provider-utils';
+
 export class MultiConsumerStream<CHUNK> {
   private readonly reader: ReadableStreamDefaultReader<CHUNK>;
-  private readonly consumers: Consumer<CHUNK>[] = [];
+  private consumers: Consumer<CHUNK>[] = [];
 
   constructor({ stream }: { stream: ReadableStream<CHUNK> }) {
     this.reader = stream.getReader();
@@ -8,24 +10,40 @@ export class MultiConsumerStream<CHUNK> {
 
   private async pull() {
     const result = await this.reader.read();
+
+    // TODO how to wait here?? other streams must not block
     this.consumers.forEach(consumer => {
       consumer.consumeResult(result);
     });
   }
 
+  private removeConsumer(consumerId: string) {
+    this.consumers = this.consumers.filter(
+      consumer => consumer.id !== consumerId,
+    );
+  }
+
   split(): ReadableStream<CHUNK> {
-    const doPull = this.pull.bind(this);
+    const self = this;
+
     let controller!: ReadableStreamDefaultController<CHUNK>;
+    const consumerId = generateId();
+
     const stream = new ReadableStream<CHUNK>({
       start(controllerArg) {
         controller = controllerArg;
       },
+
       async pull() {
-        doPull(); // TODO catch errors
+        return self.pull(); // TODO catch errors
+      },
+
+      cancel() {
+        self.removeConsumer(consumerId);
       },
     });
 
-    this.consumers.push(new Consumer(controller));
+    this.consumers.push(new Consumer({ controller, id: consumerId }));
 
     return stream;
   }
@@ -33,9 +51,17 @@ export class MultiConsumerStream<CHUNK> {
 
 class Consumer<CHUNK> {
   private readonly controller: ReadableStreamDefaultController<CHUNK>;
+  readonly id: string;
 
-  constructor(controller: ReadableStreamDefaultController<CHUNK>) {
+  constructor({
+    controller,
+    id,
+  }: {
+    controller: ReadableStreamDefaultController<CHUNK>;
+    id: string;
+  }) {
     this.controller = controller;
+    this.id = id;
   }
 
   consumeResult(result: ReadableStreamReadResult<CHUNK>) {
