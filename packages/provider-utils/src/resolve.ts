bfb8e78ff812e2ccf0122f13be8bfcf5c9201fd5
Lars Grammel <lars.grammel@gmail.com>
2024-12-04 09:10:05 +0100

    fix (ai/ui-utils): limit resolvable type to never resolve to a function

--- packages/provider-utils/src/resolve.ts
+++ packages/provider-utils/src/resolve.ts
@@ -1,19 +1,28 @@
-export type Resolvable<T> =
-  | T // Raw value
-  | Promise<T> // Promise of value
-  | (() => T) // Function returning value
-  | (() => Promise<T>); // Function returning promise of value
+/**
+ * A type that can be resolved to a value.
+ * The resolved value cannot be a function.
+ *
+ * 4 variants are possible:
+ * - T: Raw value
+ * - Promise<T>: Promise of value
+ * - (() => T): Function returning value
+ * - (() => Promise<T>): Function returning promise of value
+ */
+export type Resolvable<T extends object> =
+  | T
+  | Promise<T>
+  | (() => T)
+  | (() => Promise<T>);
 
 /**
  * Resolves a value that could be a raw value, a Promise, a function returning a value,
  * or a function returning a Promise.
  */
-export async function resolve<T>(value: Resolvable<T>): Promise<T> {
-  // If it's a function, call it to get the value/promise
+export async function resolve<T extends object>(
+  value: Resolvable<T>,
+): Promise<T> {
   if (typeof value === 'function') {
-    value = (value as Function)();
+    value = value();
   }
-
-  // Otherwise just resolve whatever we got (value or promise)
-  return Promise.resolve(value as T);
+  return Promise.resolve(value);
 }
