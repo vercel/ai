9ee38be3aca1dba9c42de42daaedee44ebe8da3a
Lars Grammel <lars.grammel@gmail.com>
2024-12-10 11:52:44 +0100

    exampe

--- examples/ai-core/src/generate-image/openai.ts
+++ examples/ai-core/src/generate-image/openai.ts
@@ -1,15 +1,19 @@
 import { openai } from '@ai-sdk/openai';
 import { generateImage } from 'ai';
 import 'dotenv/config';
+import fs from 'fs';
 
 async function main() {
-  const { images } = await generateImage({
-    model: openai.image('dall-e-3'),
+  const { imageAsUint8Array: image } = await generateImage({
+    model: openai.image('dall-e-2'),
     prompt: 'Santa Claus driving a Cadillac',
+    size: '512x512',
     maxRetries: 0,
   });
 
-  console.log(images);
+  const filename = `image-${Date.now()}.png`;
+  fs.writeFileSync(filename, image);
+  console.log(`Image saved to ${filename}`);
 }
 
 main().catch(console.error);
