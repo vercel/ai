255587e7be2241164db007f8a41826d6da29d731
Walter Korman <shaper@vercel.com>
2024-12-17 12:05:53 -0800

    Add new grok-2 models and update docs w/ capabilities.

---
+++ .changeset/lemon-cobras-thank.md
@@ -0,0 +1,5 @@
+---
+'@ai-sdk/xai': patch
+---
+
+feat (provider/xai): Add grok-2 models, use openai-compatible base impl.
--- packages/xai/src/xai-chat-settings.ts
+++ packages/xai/src/xai-chat-settings.ts
@@ -1,6 +1,11 @@
 import { OpenAICompatibleChatSettings } from '@ai-sdk/openai-compatible';
 
 // https://console.x.ai and see "View models"
-export type XaiChatModelId = 'grok-beta' | 'grok-vision-beta' | (string & {});
+export type XaiChatModelId =
+  | 'grok-2-1212'
+  | 'grok-2-vision-1212'
+  | 'grok-beta'
+  | 'grok-vision-beta'
+  | (string & {});
 
 export interface XaiChatSettings extends OpenAICompatibleChatSettings {}
