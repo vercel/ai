45ebae8036646762bcfa06600e06158aaba34af5
Lars Grammel <lars.grammel@gmail.com>
2025-01-02 16:27:43 +0100

    example

---
+++ examples/ai-core/src/stream-text/openai-output-object.ts
@@ -0,0 +1,45 @@
+import { openai } from '@ai-sdk/openai';
+import { Output, streamText, tool } from 'ai';
+import 'dotenv/config';
+import { z } from 'zod';
+
+async function main() {
+  const { experimental_partialOutputStream: partialOutputStream } = streamText({
+    model: openai('gpt-4o-mini', { structuredOutputs: true }),
+    tools: {
+      weather: tool({
+        description: 'Get the weather in a location',
+        parameters: z.object({
+          location: z.string().describe('The location to get the weather for'),
+        }),
+        // location below is inferred to be a string:
+        execute: async ({ location }) => ({
+          location,
+          temperature: 72 + Math.floor(Math.random() * 21) - 10,
+        }),
+      }),
+    },
+    experimental_output: Output.object({
+      schema: z.object({
+        elements: z.array(
+          z.object({
+            location: z.string(),
+            temperature: z.number(),
+            touristAttraction: z.string(),
+          }),
+        ),
+      }),
+    }),
+    maxSteps: 2,
+    prompt:
+      'What is the weather and the main tourist attraction in San Francisco, London Paris, and Berlin?',
+  });
+
+  // { location: 'San Francisco', temperature: 81 }
+  for await (const partialOutput of partialOutputStream) {
+    console.clear();
+    console.log(partialOutput);
+  }
+}
+
+main().catch(console.error);
