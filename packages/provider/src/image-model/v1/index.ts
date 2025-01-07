bf0034d366aec6b8a06393c86a9999d0e12d8ae9
Lars Grammel <lars.grammel@gmail.com>
2025-01-07 12:23:12 +0100

    add export

--- packages/provider/src/image-model/v1/index.ts
+++ packages/provider/src/image-model/v1/index.ts
@@ -1,2 +1,3 @@
 export type { ImageModelV1 } from './image-model-v1';
 export type { ImageModelV1CallOptions } from './image-model-v1-call-options';
+export type { ImageModelV1Warning } from './image-model-v1-call-warning';
