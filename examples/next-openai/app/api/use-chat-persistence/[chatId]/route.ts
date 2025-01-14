a077372e0cbeb089a2d8eb7145dd9259cd27407d
Lars Grammel <lars.grammel@gmail.com>
2025-01-14 15:36:10 +0100

    rm

--- examples/next-openai/app/api/use-chat-persistence/[chatId]/route.ts
+++
@@ -1,8 +0,0 @@
-import { loadChat } from '../chat-store';
-
-export async function GET(
-  req: Request,
-  { params }: { params: { chatId: string } },
-) {
-  return Response.json(await loadChat({ id: params.chatId }));
-}
