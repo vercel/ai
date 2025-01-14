04b05967189654c979580ba9c1d5bfc11d209584
Walter Korman <shaper@vercel.com>
2025-01-13 16:33:34 -0800

    add add'l model to docs

--- packages/cerebras/src/cerebras-chat-settings.ts
+++ packages/cerebras/src/cerebras-chat-settings.ts
@@ -3,7 +3,7 @@ import { OpenAICompatibleChatSettings } from '@ai-sdk/openai-compatible';
 // https://inference-docs.cerebras.ai/introduction
 export type CerebrasChatModelId =
   | 'llama3.1-8b'
-  | 'llama3.1-70b' // available in the playground?
+  | 'llama3.1-70b'
   | 'llama-3.3-70b'
   | (string & {});
 
