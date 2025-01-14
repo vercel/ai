1362cfaffd801dbd33c393e421ed8ba14d14aca7
Lars Grammel <lars.grammel@gmail.com>
2025-01-14 14:58:52 +0100

    example

---
+++ examples/next-openai/app/api/use-chat-persistence/chat-store.ts
@@ -0,0 +1,30 @@
+import { Message } from 'ai';
+import { existsSync, mkdirSync } from 'fs';
+import { readFile, writeFile } from 'fs/promises';
+import path from 'path';
+
+// example implementation for demo purposes
+// in a real app, you would save the chat to a database.
+export async function saveChat({
+  id,
+  messages,
+}: {
+  id: string;
+  messages: Message[];
+}): Promise<void> {
+  await writeFile(getChatFile(id), JSON.stringify(messages, null, 2));
+}
+
+export async function loadChat({ id }: { id: string }): Promise<Message[]> {
+  return JSON.parse(await readFile(getChatFile(id), 'utf8'));
+}
+
+function getChatFile(chatId: string): string {
+  const chatDir = path.join(process.cwd(), '.chats');
+
+  if (!existsSync(chatDir)) {
+    mkdirSync(chatDir, { recursive: true });
+  }
+
+  return path.join(chatDir, `${chatId}.json`);
+}
--- examples/next-openai/app/api/use-chat-persistence/route.ts
+++ examples/next-openai/app/api/use-chat-persistence/route.ts
@@ -1,16 +1,17 @@
 import { openai } from '@ai-sdk/openai';
 import { streamText } from 'ai';
+import { saveChat } from './chat-store';
 
 export async function POST(req: Request) {
   const { messages, id } = await req.json();
 
-  console.log('messages', messages);
-  console.log('id', id);
-
   const result = streamText({
-    model: openai('gpt-4o'),
+    model: openai('gpt-4o-mini'),
     messages,
-    async onFinish({ text, toolCalls, toolResults, usage, finishReason }) {},
+    async onFinish({ response }) {
+      // TODO merge response messages
+      await saveChat({ id, messages });
+    },
   });
 
   return result.toDataStreamResponse();
