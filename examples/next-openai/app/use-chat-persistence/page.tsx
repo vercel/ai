5bbf0a6ce4339dce532b76f1c4686a525e38c38b
Lars Grammel <lars.grammel@gmail.com>
2025-01-15 10:28:55 +0100

    better

--- examples/next-openai/app/use-chat-persistence/page.tsx
+++ examples/next-openai/app/use-chat-persistence/page.tsx
@@ -1,14 +1,6 @@
-'use client';
-
+import { redirect } from 'next/navigation';
 import { generateId } from 'ai';
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
 
 export default function ChatPage() {
-  const router = useRouter();
-
-  // redirect to a chat with a new, random id:
-  useEffect(() => {
-    router.replace(`/use-chat-persistence/${generateId()}`);
-  }, [router]);
+  redirect(`/use-chat-persistence/${generateId()}`);
 }
