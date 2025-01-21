c59e2639cd786bcd94783a2579629736ac8be1cc
Lars Grammel <lars.grammel@gmail.com>
2025-01-21 09:45:16 +0100

    explore

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,4 +1,5 @@
 export * from './use-assistant';
 export * from './use-chat';
 export * from './use-completion';
+export { inDevelopment_useMessages } from './use-messages';
 export * from './use-object';
