f5959d0c846fb437429f5043fb8b66f7ec2d5402
Lars Grammel <lars.grammel@gmail.com>
2025-01-27 13:42:28 +0100

    5

--- packages/ai/core/middleware/extract-reasoning-middleware.test.ts
+++ packages/ai/core/middleware/extract-reasoning-middleware.test.ts
@@ -1,62 +1,169 @@
-import { generateText } from '../generate-text';
+import {
+  convertArrayToReadableStream,
+  convertAsyncIterableToArray,
+} from '@ai-sdk/provider-utils/test';
+import { generateText, streamText } from '../generate-text';
 import { experimental_wrapLanguageModel } from '../middleware/wrap-language-model';
 import { MockLanguageModelV1 } from '../test/mock-language-model-v1';
 import { extractReasoningMiddleware } from './extract-reasoning-middleware';
+import { mockId } from '../test/mock-id';
 
 describe('extractReasoningMiddleware', () => {
-  it('should extract reasoning from <think> tags during generation', async () => {
-    const mockModel = new MockLanguageModelV1({
-      async doGenerate() {
-        return {
-          text: '<think>analyzing the request</think>Here is the response',
-          finishReason: 'stop',
-          usage: { promptTokens: 10, completionTokens: 10 },
-          rawCall: {
-            rawPrompt: 'Hello, how can I help?',
-            rawSettings: {},
-          },
-        };
-      },
+  describe('wrapGenerate', () => {
+    it('should extract reasoning from <think> tags during generation', async () => {
+      const mockModel = new MockLanguageModelV1({
+        async doGenerate() {
+          return {
+            text: '<think>analyzing the request</think>Here is the response',
+            finishReason: 'stop',
+            usage: { promptTokens: 10, completionTokens: 10 },
+            rawCall: { rawPrompt: '', rawSettings: {} },
+          };
+        },
+      });
+
+      const result = await generateText({
+        model: experimental_wrapLanguageModel({
+          model: mockModel,
+          middleware: extractReasoningMiddleware({ tagName: 'think' }),
+        }),
+        prompt: 'Hello, how can I help?',
+      });
+
+      expect(result.reasoning).toStrictEqual('analyzing the request');
+      expect(result.text).toStrictEqual('Here is the response');
     });
 
-    const result = await generateText({
-      model: experimental_wrapLanguageModel({
-        model: mockModel,
-        middleware: extractReasoningMiddleware({ tagName: 'think' }),
-      }),
-      prompt: 'Hello, how can I help?',
-    });
+    it('should extract multiple reasoning from <think> tags during generation', async () => {
+      const mockModel = new MockLanguageModelV1({
+        async doGenerate() {
+          return {
+            text: '<think>analyzing the request</think>Here is the response<think>thinking about the response</think>more',
+            finishReason: 'stop',
+            usage: { promptTokens: 10, completionTokens: 10 },
+            rawCall: { rawPrompt: '', rawSettings: {} },
+          };
+        },
+      });
 
-    expect(result.reasoning).toStrictEqual('analyzing the request');
-    expect(result.text).toStrictEqual('Here is the response');
+      const result = await generateText({
+        model: experimental_wrapLanguageModel({
+          model: mockModel,
+          middleware: extractReasoningMiddleware({ tagName: 'think' }),
+        }),
+        prompt: 'Hello, how can I help?',
+      });
+
+      expect(result.reasoning).toStrictEqual(
+        'analyzing the request\nthinking about the response',
+      );
+      expect(result.text).toStrictEqual('Here is the response\nmore');
+    });
   });
 
-  it('should extract multiple reasoning from <think> tags during generation', async () => {
-    const mockModel = new MockLanguageModelV1({
-      async doGenerate() {
-        return {
-          text: '<think>analyzing the request</think>Here is the response<think>thinking about the response</think>more',
+  describe('wrapStream', () => {
+    it('should extract reasoning from <think> tags during streaming', async () => {
+      const mockModel = new MockLanguageModelV1({
+        async doStream() {
+          return {
+            stream: convertArrayToReadableStream([
+              {
+                type: 'response-metadata',
+                id: 'id-0',
+                modelId: 'mock-model-id',
+                timestamp: new Date(0),
+              },
+              { type: 'text-delta', textDelta: '<thi' },
+              { type: 'text-delta', textDelta: 'nk>ana' },
+              { type: 'text-delta', textDelta: 'lyzing the request' },
+              { type: 'text-delta', textDelta: '</thi' },
+              { type: 'text-delta', textDelta: 'nk>Here' },
+              { type: 'text-delta', textDelta: ' is the response' },
+              {
+                type: 'finish',
+                finishReason: 'stop',
+                logprobs: undefined,
+                usage: { completionTokens: 10, promptTokens: 3 },
+              },
+            ]),
+            rawCall: { rawPrompt: '', rawSettings: {} },
+          };
+        },
+      });
+
+      const result = streamText({
+        model: experimental_wrapLanguageModel({
+          model: mockModel,
+          middleware: extractReasoningMiddleware({ tagName: 'think' }),
+        }),
+        prompt: 'Hello, how can I help?',
+        experimental_generateMessageId: mockId({ prefix: 'msg' }),
+      });
+
+      expect(
+        await convertAsyncIterableToArray(result.fullStream),
+      ).toStrictEqual([
+        {
+          messageId: 'msg-0',
+          request: {},
+          type: 'step-start',
+          warnings: [],
+        },
+        {
+          type: 'reasoning',
+          textDelta: 'ana',
+        },
+        {
+          type: 'reasoning',
+          textDelta: 'lyzing the request',
+        },
+        {
+          type: 'text-delta',
+          textDelta: 'Here',
+        },
+        {
+          type: 'text-delta',
+          textDelta: ' is the response',
+        },
+        {
+          experimental_providerMetadata: undefined,
           finishReason: 'stop',
-          usage: { promptTokens: 10, completionTokens: 10 },
-          rawCall: {
-            rawPrompt: 'Hello, how can I help?',
-            rawSettings: {},
+          isContinued: false,
+          logprobs: undefined,
+          messageId: 'msg-0',
+          request: {},
+          response: {
+            headers: undefined,
+            id: 'id-0',
+            modelId: 'mock-model-id',
+            timestamp: new Date(0),
           },
-        };
-      },
+          type: 'step-finish',
+          usage: {
+            completionTokens: 10,
+            promptTokens: 3,
+            totalTokens: 13,
+          },
+          warnings: undefined,
+        },
+        {
+          experimental_providerMetadata: undefined,
+          finishReason: 'stop',
+          logprobs: undefined,
+          response: {
+            headers: undefined,
+            id: 'id-0',
+            modelId: 'mock-model-id',
+            timestamp: new Date(0),
+          },
+          type: 'finish',
+          usage: {
+            completionTokens: 10,
+            promptTokens: 3,
+            totalTokens: 13,
+          },
+        },
+      ]);
     });
-
-    const result = await generateText({
-      model: experimental_wrapLanguageModel({
-        model: mockModel,
-        middleware: extractReasoningMiddleware({ tagName: 'think' }),
-      }),
-      prompt: 'Hello, how can I help?',
-    });
-
-    expect(result.reasoning).toStrictEqual(
-      'analyzing the request\nthinking about the response',
-    );
-    expect(result.text).toStrictEqual('Here is the response\nmore');
   });
 });
--- packages/ai/core/middleware/extract-reasoning-middleware.ts
+++ packages/ai/core/middleware/extract-reasoning-middleware.ts
@@ -1,5 +1,15 @@
+import { LanguageModelV1StreamPart } from '@ai-sdk/provider';
 import { Experimental_LanguageModelV1Middleware } from './language-model-v1-middleware';
+import { getPotentialStartIndex } from '../util/get-potential-start-index';
+import { read } from 'fs';
 
+/**
+ * Extract an XML-tagged reasoning section from the generated text and exposes it
+ * as a `reasoning` property on the result.
+ *
+ * @param tagName - The name of the XML tag to extract reasoning from.
+ * @param separator - The separator to use between reasoning and text sections.
+ */
 export function extractReasoningMiddleware({
   tagName,
   separator = '\n',
@@ -7,24 +17,27 @@ export function extractReasoningMiddleware({
   tagName: string;
   separator?: string;
 }): Experimental_LanguageModelV1Middleware {
+  const openingTag = `<${tagName}>`;
+  const closingTag = `<\/${tagName}>`;
+
   return {
     wrapGenerate: async ({ doGenerate }) => {
-      const result = await doGenerate();
+      const { text, ...rest } = await doGenerate();
 
-      if (result.text == null) {
-        return result;
+      if (text == null) {
+        return { text, ...rest };
       }
 
-      const regexp = new RegExp(`<${tagName}>(.*?)<\/${tagName}>`, 'gs');
-      const matches = Array.from(result.text.matchAll(regexp));
+      const regexp = new RegExp(`${openingTag}(.*?)${closingTag}`, 'gs');
+      const matches = Array.from(text.matchAll(regexp));
 
       if (!matches.length) {
-        return result;
+        return { text, ...rest };
       }
 
-      result.reasoning = matches.map(match => match[1]).join(separator);
+      const reasoning = matches.map(match => match[1]).join(separator);
 
-      let textWithoutReasoning = result.text;
+      let textWithoutReasoning = text;
       for (let i = matches.length - 1; i >= 0; i--) {
         const match = matches[i];
 
@@ -39,9 +52,68 @@ export function extractReasoningMiddleware({
           afterMatch;
       }
 
-      result.text = textWithoutReasoning;
+      return { text: textWithoutReasoning, reasoning, ...rest };
+    },
 
-      return result;
+    wrapStream: async ({ doStream }) => {
+      const { stream, ...rest } = await doStream();
+
+      let isReasoning: boolean = false;
+      let buffer = '';
+
+      return {
+        stream: stream.pipeThrough(
+          new TransformStream<
+            LanguageModelV1StreamPart,
+            LanguageModelV1StreamPart
+          >({
+            transform: (chunk, controller) => {
+              if (chunk.type !== 'text-delta') {
+                controller.enqueue(chunk);
+                return;
+              }
+
+              buffer += chunk.textDelta;
+
+              function publish(text: string) {
+                if (text.length > 0) {
+                  controller.enqueue({
+                    type: isReasoning ? 'reasoning' : 'text-delta',
+                    textDelta: text,
+                  });
+                }
+              }
+
+              do {
+                const nextTag = isReasoning ? closingTag : openingTag;
+                const startIndex = getPotentialStartIndex(buffer, nextTag);
+
+                // no opening or closing tag found, publish the buffer
+                if (startIndex == null) {
+                  publish(buffer);
+                  buffer = '';
+                  break;
+                }
+
+                // publish text before the tag
+                publish(buffer.slice(0, startIndex));
+
+                const foundFullMatch =
+                  startIndex + nextTag.length <= buffer.length;
+
+                if (foundFullMatch) {
+                  buffer = buffer.slice(startIndex + nextTag.length);
+                  isReasoning = !isReasoning;
+                } else {
+                  buffer = buffer.slice(startIndex);
+                  break;
+                }
+              } while (true);
+            },
+          }),
+        ),
+        ...rest,
+      };
     },
   };
 }
