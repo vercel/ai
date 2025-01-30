a869224c04f172051b8074922bd8ee7cb7c75473
Lars Grammel <lars.grammel@gmail.com>
2025-01-30 15:58:18 +0100

    feat (examples): add human in the loop example (#4610)

---
+++ examples/next-openai/app/use-chat-human-in-the-loop/page.tsx
@@ -0,0 +1,73 @@
+'use client';
+
+import { ToolInvocation } from 'ai';
+import { Message, useChat } from 'ai/react';
+
+export default function Chat() {
+  const { messages, input, handleInputChange, handleSubmit, addToolResult } =
+    useChat({
+      api: '/api/use-chat-human-in-the-loop',
+      maxSteps: 5,
+    });
+
+  return (
+    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
+      {messages?.map((m: Message) => (
+        <div key={m.id} className="whitespace-pre-wrap">
+          <strong>{`${m.role}: `}</strong>
+          {m.content}
+          {m.toolInvocations?.map((toolInvocation: ToolInvocation) => {
+            const toolCallId = toolInvocation.toolCallId;
+
+            // render confirmation tool (client-side tool with user interaction)
+            if (
+              toolInvocation.toolName === 'getWeatherInformation' &&
+              toolInvocation.state === 'call'
+            ) {
+              return (
+                <div key={toolCallId} className="text-gray-500">
+                  Get weather information for {toolInvocation.args.city}?
+                  <div className="flex gap-2">
+                    <button
+                      className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
+                      onClick={() =>
+                        addToolResult({
+                          toolCallId,
+                          result: 'Yes, confirmed.',
+                        })
+                      }
+                    >
+                      Yes
+                    </button>
+                    <button
+                      className="px-4 py-2 font-bold text-white bg-red-500 rounded hover:bg-red-700"
+                      onClick={() =>
+                        addToolResult({
+                          toolCallId,
+                          result: 'No, denied.',
+                        })
+                      }
+                    >
+                      No
+                    </button>
+                  </div>
+                </div>
+              );
+            }
+          })}
+          <br />
+          <br />
+        </div>
+      ))}
+
+      <form onSubmit={handleSubmit}>
+        <input
+          className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
+          value={input}
+          placeholder="Say something..."
+          onChange={handleInputChange}
+        />
+      </form>
+    </div>
+  );
+}
