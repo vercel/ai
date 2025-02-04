980083da3d998ef1970e2ae78b3cf3854e98b37e
Lars Grammel <lars.grammel@gmail.com>
2025-02-04 09:49:05 +0100

    x

--- packages/ui-utils/src/fill-message-parts.ts
+++ packages/ui-utils/src/fill-message-parts.ts
@@ -4,6 +4,7 @@ import { Message, UIMessage } from './types';
 export function fillMessageParts(messages: Message[]): UIMessage[] {
   return messages.map(message => ({
     ...message,
+    createdAt: message.createdAt!,
     parts: getMessageParts(message),
   }));
 }
