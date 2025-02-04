2bbaa515fba1705b6a0a46fb0776b7524d027bdc
Lars Grammel <lars.grammel@gmail.com>
2025-02-04 14:17:54 +0100

    change order

--- packages/ui-utils/src/get-message-parts.ts
+++ packages/ui-utils/src/get-message-parts.ts
@@ -12,17 +12,17 @@ export function getMessageParts(
 ): (TextUIPart | ReasoningUIPart | ToolInvocationUIPart)[] {
   return (
     message.parts ?? [
-      ...(message.reasoning
-        ? [{ type: 'reasoning' as const, reasoning: message.reasoning }]
-        : []),
-      ...(message.content
-        ? [{ type: 'text' as const, text: message.content }]
-        : []),
       ...(message.toolInvocations
         ? message.toolInvocations.map(toolInvocation => ({
             type: 'tool-invocation' as const,
             toolInvocation,
           }))
+        : []),
+      ...(message.reasoning
+        ? [{ type: 'reasoning' as const, reasoning: message.reasoning }]
+        : []),
+      ...(message.content
+        ? [{ type: 'text' as const, text: message.content }]
         : []),
     ]
   );
