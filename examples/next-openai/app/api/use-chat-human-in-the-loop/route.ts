00aa441c04c2283a1d511f819af78cada0a92b68
Nico Albanese <nico.albanese@vercel.com>
2025-02-05 18:30:54 +0000

    update hitl example

--- examples/next-openai/app/api/use-chat-human-in-the-loop/route.ts
+++ examples/next-openai/app/api/use-chat-human-in-the-loop/route.ts
@@ -1,12 +1,7 @@
 import { openai } from '@ai-sdk/openai';
-import {
-  createDataStreamResponse,
-  formatDataStreamPart,
-  Message,
-  streamText,
-  tool,
-} from 'ai';
-import { z } from 'zod';
+import { createDataStreamResponse, Message, streamText } from 'ai';
+import { processToolCalls } from './utils';
+import { tools } from './tools';
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
@@ -16,74 +11,30 @@ export async function POST(req: Request) {
 
   return createDataStreamResponse({
     execute: async dataStream => {
-      const lastMessage = messages[messages.length - 1];
-      lastMessage.parts = await Promise.all(
-        lastMessage.parts?.map(async part => {
-          if (part.type !== 'tool-invocation') {
-            return part;
-          }
-          const toolInvocation = part.toolInvocation;
-          if (
-            toolInvocation.toolName !== 'getWeatherInformation' ||
-            toolInvocation.state !== 'result'
-          ) {
-            return { ...part, toolInvocation };
-          }
-
-          switch (toolInvocation.result) {
-            case 'Yes, confirmed.': {
-              const result = await executeWeatherTool(toolInvocation.args);
-
-              // forward updated tool result to the client:
-              dataStream.write(
-                formatDataStreamPart('tool_result', {
-                  toolCallId: toolInvocation.toolCallId,
-                  result,
-                }),
-              );
-
-              // update the messages:
-              return { ...part, toolInvocation: { ...toolInvocation, result } };
-            }
-            case 'No, denied.': {
-              const result = 'Error: User denied access to weather information';
-
-              // forward updated tool result to the client:
-              dataStream.write(
-                formatDataStreamPart('tool_result', {
-                  toolCallId: toolInvocation.toolCallId,
-                  result,
-                }),
-              );
-
-              // update the messages:
-              return { ...part, toolInvocation: { ...toolInvocation, result } };
-            }
-            default:
-              return { ...part, toolInvocation };
-          }
-        }) ?? [],
+      // Utility function to handle tools that require human confirmation
+      const processedMessages = await processToolCalls(
+        {
+          messages,
+          dataStream,
+          tools,
+        },
+        {
+          getWeatherInformation: async ({ city }) => {
+            const conditions = ['sunny', 'cloudy', 'rainy', 'snowy'];
+            return `The weather in ${city} is ${
+              conditions[Math.floor(Math.random() * conditions.length)]
+            }.`;
+          },
+        },
       );
 
       const result = streamText({
         model: openai('gpt-4o'),
-        messages,
-        tools: {
-          // server-side tool with human in the loop:
-          getWeatherInformation: tool({
-            description: 'show the weather in a given city to the user',
-            parameters: z.object({ city: z.string() }),
-            // no execute function, we want human in the loop
-          }),
-        },
+        messages: processedMessages,
+        tools,
       });
 
       result.mergeIntoDataStream(dataStream);
     },
   });
-}
-
-async function executeWeatherTool({}: { city: string }) {
-  const weatherOptions = ['sunny', 'cloudy', 'rainy', 'snowy', 'windy'];
-  return weatherOptions[Math.floor(Math.random() * weatherOptions.length)];
 }
--- examples/next-openai/app/use-chat-human-in-the-loop/page.tsx
+++ examples/next-openai/app/use-chat-human-in-the-loop/page.tsx
@@ -1,7 +1,11 @@
 'use client';
 
-import { ToolInvocation } from 'ai';
 import { Message, useChat } from 'ai/react';
+import {
+  APPROVAL,
+  getToolsRequiringConfirmation,
+} from '../api/use-chat-human-in-the-loop/utils';
+import { tools } from '../api/use-chat-human-in-the-loop/tools';
 
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit, addToolResult } =
@@ -10,38 +14,59 @@ export default function Chat() {
       maxSteps: 5,
     });
 
+  const toolsRequiringConfirmation = getToolsRequiringConfirmation(tools);
+
+  const pendingToolCallConfirmation = messages.some((m: Message) =>
+    m.parts?.some(
+      part =>
+        part.type === 'tool-invocation' &&
+        part.toolInvocation.state === 'call' &&
+        toolsRequiringConfirmation.includes(part.toolInvocation.toolName),
+    ),
+  );
+
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
       {messages?.map((m: Message) => (
-        <div key={m.id} className="whitespace-pre-wrap">
+        <div key={m.id} id={m.id} className="whitespace-pre-wrap">
           <strong>{`${m.role}: `}</strong>
           {m.parts?.map((part, i) => {
             switch (part.type) {
               case 'text':
                 return (
-                  <div key={i} className="text-gray-500">
+                  <div key={i} className="">
                     {part.text}
                   </div>
                 );
               case 'tool-invocation':
                 const toolInvocation = part.toolInvocation;
                 const toolCallId = toolInvocation.toolCallId;
+                const dynamicInfoStyles = 'font-mono bg-gray-100 p-1 text-sm';
 
                 // render confirmation tool (client-side tool with user interaction)
                 if (
-                  toolInvocation.toolName === 'getWeatherInformation' &&
+                  toolsRequiringConfirmation.includes(
+                    toolInvocation.toolName,
+                  ) &&
                   toolInvocation.state === 'call'
                 ) {
                   return (
                     <div key={toolCallId} className="text-gray-500">
-                      Get weather information for {toolInvocation.args.city}?
-                      <div className="flex gap-2">
+                      Run{' '}
+                      <span className={dynamicInfoStyles}>
+                        {toolInvocation.toolName}
+                      </span>{' '}
+                      with args:{' '}
+                      <span className={dynamicInfoStyles}>
+                        {JSON.stringify(toolInvocation.args)}
+                      </span>
+                      <div className="flex gap-2 pt-2">
                         <button
                           className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
                           onClick={() =>
                             addToolResult({
                               toolCallId,
-                              result: 'Yes, confirmed.',
+                              result: APPROVAL.YES,
                             })
                           }
                         >
@@ -52,7 +77,7 @@ export default function Chat() {
                           onClick={() =>
                             addToolResult({
                               toolCallId,
-                              result: 'No, denied.',
+                              result: APPROVAL.NO,
                             })
                           }
                         >
@@ -65,12 +90,12 @@ export default function Chat() {
             }
           })}
           <br />
-          <br />
         </div>
       ))}
 
       <form onSubmit={handleSubmit}>
         <input
+          disabled={pendingToolCallConfirmation}
           className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
           value={input}
           placeholder="Say something..."
