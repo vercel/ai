f62e86bcf7c5347fcd7cf724260f3490743014fb
Walter Korman <shaper@vercel.com>
2025-02-11 17:11:40 -0800

    rm reporter stragglers

--- tools/capability-checker/vitest.config.js
+++ tools/capability-checker/vitest.config.js
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite';
-// import TestResultReporter from './src/test-result-reporter'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,6 +6,6 @@ export default defineConfig({
     environment: 'node',
     globals: true,
     include: ['**/*.test.ts', '**/*.test.tsx', '**/*.test.mts'],
-    reporters: ['dot'], // new TestResultReporter()
+    reporters: ['dot'],
   },
 });
