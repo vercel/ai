03a0b1ee5e622dd7a402daca6dfd63f2df2ddc75
Lars Grammel <lars.grammel@gmail.com>
2025-02-13 15:45:23 +0100

    mocid

--- packages/ai/test/index.ts
+++ packages/ai/test/index.ts
@@ -1,6 +1,8 @@
-export { convertArrayToReadableStream } from '@ai-sdk/provider-utils/test';
+export {
+  convertArrayToReadableStream,
+  mockId,
+} from '@ai-sdk/provider-utils/test';
 export { MockEmbeddingModelV1 } from '../core/test/mock-embedding-model-v1';
-export { mockId } from '../core/test/mock-id';
 export { MockLanguageModelV1 } from '../core/test/mock-language-model-v1';
 export { mockValues } from '../core/test/mock-values';
 
