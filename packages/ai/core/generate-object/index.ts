ef2e23b8bbb020b4b6b0710040901a23924c3b31
Lars Grammel <lars.grammel@gmail.com>
2025-02-19 15:21:25 +0100

    feat (ai/core): add experimental repairText function to generateObject (#4937)
    Co-authored-by: Edoardo Barbieri <edoardo@twinlogix.com>

--- packages/ai/core/generate-object/index.ts
+++ packages/ai/core/generate-object/index.ts
@@ -1,4 +1,5 @@
 export { generateObject } from './generate-object';
+export type { RepairTextFunction } from './generate-object';
 export type { GenerateObjectResult } from './generate-object-result';
 export { streamObject } from './stream-object';
 export type { StreamObjectOnFinishCallback } from './stream-object';
