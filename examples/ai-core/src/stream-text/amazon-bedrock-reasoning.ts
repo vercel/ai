8eed8cdc3c67d213ab1aa7e3b0503a2e7e43c892
Und3rf10w <jechavarria@reliaquest.com>
2025-03-05 11:25:14 -0500

    feat(provider/amazon-bedrock): Add examples for amazon-bedrock reasoning

---
+++ examples/ai-core/src/stream-text/amazon-bedrock-reasoning.ts
@@ -0,0 +1,35 @@
+import { bedrock } from '@ai-sdk/amazon-bedrock';
+import { streamText } from 'ai';
+import 'dotenv/config';
+
+async function main() {
+  const result = streamText({
+    model: bedrock('anthropic.claude-3-7-sonnet-20250219-v1:0'),
+    prompt: 'How many "r"s are in the word "strawberry"?',
+    temperature: 0.5, // should get ignored (warning)
+    onError: error => {
+      console.error(error);
+    },
+    providerOptions: {
+      bedrock: {
+        reasoning_config: { type: 'enabled', budget_tokens: 12000 },
+      },
+    },
+    maxRetries: 0,
+  });
+
+  for await (const part of result.fullStream) {
+    if (part.type === 'reasoning') {
+      process.stdout.write('\x1b[34m' + part.textDelta + '\x1b[0m');
+    } else if (part.type === 'redacted-reasoning') {
+      process.stdout.write('\x1b[31m' + '<redacted>' + '\x1b[0m');
+    } else if (part.type === 'text-delta') {
+      process.stdout.write(part.textDelta);
+    }
+  }
+
+  console.log();
+  console.log('Warnings:', await result.warnings);
+}
+
+main().catch(console.error);
