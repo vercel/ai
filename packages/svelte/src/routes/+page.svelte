1f3f1bb75b5f431d80395adb583d2f83a0b829b7
S. Elliott Johnson <sejohnson@torchcloudconsulting.com>
2025-03-08 00:00:19 -0700

    just putting this in git history in case I need to get it later

--- packages/svelte/src/routes/+page.svelte
+++ packages/svelte/src/routes/+page.svelte
@@ -1,3 +1,1 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>TODO: This can be an example</p>
