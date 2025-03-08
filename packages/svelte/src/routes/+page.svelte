c45c2295c567dc2fd48edf89faa9a805940d6149
S. Elliott Johnson <sejohnson@torchcloudconsulting.com>
2025-03-08 00:00:27 -0700

    Revert "just putting this in git history in case I need to get it later"
    This reverts commit 6ce72278f8c2f8cc933063018645572c66e4e390.

--- packages/svelte/src/routes/+page.svelte
+++ packages/svelte/src/routes/+page.svelte
@@ -1,1 +1,3 @@
-<p>TODO: This can be an example</p>
+<h1>Welcome to your library project</h1>
+<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
