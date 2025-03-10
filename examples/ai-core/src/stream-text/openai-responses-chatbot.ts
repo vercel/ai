ebef9f782b0905f865bc38145ff602e6a5a93610
Lars Grammel <lars.grammel@gmail.com>
2025-03-10 14:19:25 +0100

    x

--- examples/ai-core/src/stream-text/openai-responses-chatbot.ts
+++ examples/ai-core/src/stream-text/openai-responses-chatbot.ts
@@ -20,18 +20,18 @@ async function main() {
     const result = streamText({
       model: openai.responses('gpt-4o-mini'),
       tools: {
-        // weather: tool({
-        //   description: 'Get the weather in a location',
-        //   parameters: z.object({
-        //     location: z
-        //       .string()
-        //       .describe('The location to get the weather for'),
-        //   }),
-        //   execute: async ({ location }) => ({
-        //     location,
-        //     temperature: 72 + Math.floor(Math.random() * 21) - 10,
-        //   }),
-        // }),
+        weather: tool({
+          description: 'Get the weather in a location',
+          parameters: z.object({
+            location: z
+              .string()
+              .describe('The location to get the weather for'),
+          }),
+          execute: async ({ location }) => ({
+            location,
+            temperature: 72 + Math.floor(Math.random() * 21) - 10,
+          }),
+        }),
       },
       maxSteps: 5,
       messages,
