88da5f4f27d0976825324c9be5f0c63692a593cd
Lars Grammel <lars.grammel@gmail.com>
2025-03-11 17:16:34 +0100

    rm2

--- packages/openai/src/openai-tools.ts
+++ packages/openai/src/openai-tools.ts
@@ -1,26 +1,5 @@
 import { z } from 'zod';
 
-// Copied from ai package
-type ExecuteFunction<PARAMETERS, RESULT> =
-  | undefined
-  | ((
-      args: PARAMETERS,
-      options: { abortSignal?: AbortSignal },
-    ) => Promise<RESULT>);
-
-// Copied from ai package
-export type ToolResultContent = Array<
-  | {
-      type: 'text';
-      text: string;
-    }
-  | {
-      type: 'image';
-      data: string; // base64 encoded png image, e.g. screenshot
-      mimeType?: string; // e.g. 'image/png';
-    }
->;
-
 const WebSearchPreviewParameters = z.object({});
 
 function webSearchPreviewTool({
