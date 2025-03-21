cdfa4bb9126d42bbde59c615c956cef5337861f6
Jake Talgard <jake.talgard@zapier.com>
2025-03-21 14:14:23 -0600

    feat(mcp) - allow for implementing of SseMCPTransport class for easier custom transport

--- packages/ai/core/tool/index.ts
+++ packages/ai/core/tool/index.ts
@@ -1,4 +1,7 @@
 export { createMCPClient as experimental_createMCPClient } from './mcp/mcp-client';
 export type { MCPTransport } from './mcp/mcp-transport';
+export { SseMCPTransport } from './mcp/mcp-sse-transport';
+export { JSONRPCMessageSchema } from './mcp/json-rpc-message';
+export type { JSONRPCMessage } from './mcp/json-rpc-message';
 export { tool } from './tool';
 export type { CoreTool, Tool, ToolExecutionOptions } from './tool';
