7f0f6dc2c43ecb3259b9880a2b363781e9631b1e
Grace Yun <graceyunn@gmail.com>
2025-03-26 15:40:53 -0400

    chore: azure provider options

---
+++ examples/ai-core/src/generate-text/azure-image.ts
@@ -0,0 +1,39 @@
+import { azure } from '@ai-sdk/azure';
+import { generateText } from 'ai';
+import 'dotenv/config';
+import fs from 'node:fs';
+
+async function main() {
+  const imageData = fs.readFileSync('/Desktop/sonny-angel.jpg');
+  const imageBase64_string = imageData.toString('base64');
+
+  const { text, usage } = await generateText({
+    model: azure('v0-gpt-35-turbo'), // use your own deployment
+    messages: [
+      {
+        role: 'user',
+        content: [
+          { type: 'text', text: 'Describe the image in detail.' },
+          {
+            type: 'image',
+            image: imageBase64_string,
+            // Even if you mislabel the mime type, internally the AI SDK will detect and use the correct mime type:
+            mimeType: 'image/svg+xml',
+            providerOptions: {
+              // When using the Azure OpenAI provider, the imageDetail option can be configured under the `openai` key:
+              openai: {
+                imageDetail: 'low',
+              },
+            },
+          },
+        ],
+      },
+    ],
+  });
+
+  console.log(text);
+  console.log();
+  console.log('Usage:', usage);
+}
+
+main().catch(console.error);
