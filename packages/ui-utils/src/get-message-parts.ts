802694338c0da9e748831b23615cb833f5d1b07d
Lars Grammel <lars.grammel@gmail.com>
2025-03-27 10:38:01 +0100

    add get message parts test

--- packages/ui-utils/src/get-message-parts.ts
+++ packages/ui-utils/src/get-message-parts.ts
@@ -4,6 +4,7 @@ import {
   Message,
   ReasoningUIPart,
   SourceUIPart,
+  StepBoundaryUIPart,
   TextUIPart,
   ToolInvocationUIPart,
   UIMessage,
@@ -17,6 +18,7 @@ export function getMessageParts(
   | ToolInvocationUIPart
   | SourceUIPart
   | FileUIPart
+  | StepBoundaryUIPart
 )[] {
   return (
     message.parts ?? [
