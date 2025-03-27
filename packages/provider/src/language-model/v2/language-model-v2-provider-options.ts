f13923679d662850fce83d6444dc05ca9e923d5c
Lars Grammel <lars.grammel@gmail.com>
2025-03-27 13:59:58 +0100

    rename to provider options

--- packages/provider/src/language-model/v2/language-model-v2-provider-options.ts
+++ packages/provider/src/language-model/v2/language-model-v2-provider-options.ts
@@ -1,14 +1,15 @@
 import { JSONValue } from '../../json-value/json-value';
 
 /**
- * Additional provider-specific metadata. They are returned from
- * the provider and enable provider-specific result information.
+ * Additional provider-specific options. They are passed through
+ * to the provider from the AI SDK and enable provider-specific
+ * functionality that can be fully encapsulated in the provider.
  *
  * This enables us to quickly ship provider-specific functionality
  * without affecting the core AI SDK.
  *
  * The outer record is keyed by the provider name, and the inner
- * record is keyed by the provider-specific metadata key.
+ * record is keyed by the provider-specific options key.
  *
  * ```ts
  * {
@@ -18,7 +19,7 @@ import { JSONValue } from '../../json-value/json-value';
  * }
  * ```
  */
-export type LanguageModelV2ProviderMetadata = Record<
+export type LanguageModelV2ProviderOptions = Record<
   string,
   Record<string, JSONValue>
 >;
