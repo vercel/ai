fe0843520f689bdae9b363614b3367c473f82b93
Sam Denty <sam@samdenty.com>
2025-04-04 19:28:38 +0100

    fix

--- packages/ai/rsc/rsc-client.ts
+++ packages/ai/rsc/rsc-client.ts
@@ -1,8 +1,26 @@
 export {
+  /**
+   * @deprecated Use `@ai-sdk/react/rsc` instead.
+   */
   readStreamableValue,
+  /**
+   * @deprecated Use `@ai-sdk/react/rsc` instead.
+   */
   useStreamableValue,
+  /**
+   * @deprecated Use `@ai-sdk/react/rsc` instead.
+   */
   useUIState,
+  /**
+   * @deprecated Use `@ai-sdk/react/rsc` instead.
+   */
   useAIState,
+  /**
+   * @deprecated Use `@ai-sdk/react/rsc` instead.
+   */
   useActions,
+  /**
+   * @deprecated Use `@ai-sdk/react/rsc` instead.
+   */
   useSyncUIState,
 } from './rsc-shared.mjs';
--- packages/ai/rsc/rsc-server.ts
+++ packages/ai/rsc/rsc-server.ts
@@ -1,5 +1,35 @@
-export { getAIState, getMutableAIState } from './ai-state';
-export { createAI } from './provider';
-export { streamUI } from './stream-ui';
-export { createStreamableUI } from './streamable-ui/create-streamable-ui';
-export { createStreamableValue } from './streamable-value/create-streamable-value';
+export {
+  /**
+   * @deprecated Use `@ai-sdk/react/rsc` instead.
+   */
+  getAIState,
+  /**
+   * @deprecated Use `@ai-sdk/react/rsc` instead.
+   */
+  getMutableAIState,
+} from './ai-state';
+export {
+  /**
+   * @deprecated Use `@ai-sdk/react/rsc` instead.
+   */
+  createAI,
+} from './provider';
+export {
+  /**
+   * @deprecated Use `@ai-sdk/react/rsc` instead.
+   */
+  streamUI,
+} from './stream-ui';
+
+export {
+  /**
+   * @deprecated Use `@ai-sdk/react/rsc` instead.
+   */
+  createStreamableUI,
+} from './streamable-ui/create-streamable-ui';
+export {
+  /**
+   * @deprecated Use `@ai-sdk/react/rsc` instead.
+   */
+  createStreamableValue,
+} from './streamable-value/create-streamable-value';
