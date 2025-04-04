7b30aff1e58f08e940a947015a55e56d814dbbaa
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2025-04-04 15:28:48 -0700

    remove incorrect "AI" copyright header
    @lgrammel is that what you meant? I am not sure where the AI header came from

--- packages/provider-utils/src/generate-id-custom-alphabet.ts
+++ packages/provider-utils/src/generate-id-custom-alphabet.ts
@@ -3,7 +3,7 @@
 //
 // MIT License
 //
-// Copyright (c) 2020–2024 AI
+// Copyright (c) 2017 Andrey Sitnik <andrey@sitnik.ru>
 // Copyright (c) Vercel, Inc. (https://vercel.com)
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
