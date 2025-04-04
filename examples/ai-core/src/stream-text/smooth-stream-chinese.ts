7c7951382cb53198ffb8b8a4f43bcbadf7b64932
Sam Denty <sam@samdenty.com>
2025-04-04 18:42:02 +0100

    fix: lint

--- examples/ai-core/src/stream-text/smooth-stream-chinese.ts
+++ examples/ai-core/src/stream-text/smooth-stream-chinese.ts
@@ -6,27 +6,28 @@ async function main() {
     model: new MockLanguageModelV1({
       doStream: async () => ({
         stream: simulateReadableStream({
-          chunks:[({ type: 'text-delta', textDelta: '你好你好你好你好你好' }),
-          { type: 'text-delta', textDelta: '你好你好你好你好你好' },
-          { type: 'text-delta', textDelta: '你好你好你好你好你好' },
-          { type: 'text-delta', textDelta: '你好你好你好你好你好' },
-          { type: 'text-delta', textDelta: '你好你好你好你好你好' },
-          {
-            type: 'finish',
-            finishReason: 'stop',
-            logprobs: undefined,
-            usage: { completionTokens: 10, promptTokens: 3 },
-          },
-        ],
-        chunkDelayInMs: 400,
-      }),
+          chunks: [
+            { type: 'text-delta', textDelta: '你好你好你好你好你好' },
+            { type: 'text-delta', textDelta: '你好你好你好你好你好' },
+            { type: 'text-delta', textDelta: '你好你好你好你好你好' },
+            { type: 'text-delta', textDelta: '你好你好你好你好你好' },
+            { type: 'text-delta', textDelta: '你好你好你好你好你好' },
+            {
+              type: 'finish',
+              finishReason: 'stop',
+              logprobs: undefined,
+              usage: { completionTokens: 10, promptTokens: 3 },
+            },
+          ],
+          chunkDelayInMs: 400,
+        }),
         rawCall: { rawPrompt: null, rawSettings: {} },
       }),
     }),
 
     prompt: 'Say hello in Chinese!',
     experimental_transform: smoothStream({
-      chunking: /[\u4E00-\u9FFF]|\S+\s+/
+      chunking: /[\u4E00-\u9FFF]|\S+\s+/,
     }),
   });
 
