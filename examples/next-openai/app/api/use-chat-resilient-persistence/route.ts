2818730a2da8c300a3957a100a248e02867352fa
Grace Yun <graceyunn@gmail.com>
2025-04-05 13:04:12 -0400

    fix example

--- examples/next-openai/app/api/use-chat-resilient-persistence/route.ts
+++ examples/next-openai/app/api/use-chat-resilient-persistence/route.ts
@@ -27,8 +27,10 @@ export async function POST(req: Request) {
   // consume the stream to ensure it runs to completion and triggers onFinish
   // even when the client response is aborted (e.g. when the browser tab is closed).
   // no await
-  result.consumeStream(error => {
-    console.log('Error during background stream consumption: ', error); // optional error callback
+  result.consumeStream({
+    onError: error => {
+      console.log('Error during background stream consumption: ', error); // optional error callback
+    },
   });
 
   return result.toDataStreamResponse();
