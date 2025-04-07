320985f13da18ad8e37d470348dd3478cd138bf6
Lars Grammel <lars.grammel@gmail.com>
2025-04-07 11:01:37 +0200

    rm

--- packages/provider/src/language-model/v2/language-model-v2-call-options.ts
+++ packages/provider/src/language-model/v2/language-model-v2-call-options.ts
@@ -1,12 +1,11 @@
 import { JSONSchema7 } from 'json-schema';
-import { LanguageModelV2CallSettings } from './language-model-v2-call-settings';
 import { LanguageModelV2FunctionTool } from './language-model-v2-function-tool';
 import { LanguageModelV2Prompt } from './language-model-v2-prompt';
 import { LanguageModelV2ProviderDefinedTool } from './language-model-v2-provider-defined-tool';
 import { LanguageModelV2ProviderOptions } from './language-model-v2-provider-options';
 import { LanguageModelV2ToolChoice } from './language-model-v2-tool-choice';
 
-export type LanguageModelV2CallOptions = LanguageModelV2CallSettings & {
+export type LanguageModelV2CallOptions = {
   /**
 Whether the user provided the input as messages or as
 a prompt. This can help guide non-chat models in the
@@ -16,39 +15,71 @@ chat/non-chat use cases.
   inputFormat: 'messages' | 'prompt';
 
   /**
-The mode affects the behavior of the language model. It is required to
-support provider-independent streaming and generation of structured objects.
-The model can take this information and e.g. configure json mode, the correct
-low level grammar, etc. It can also be used to optimize the efficiency of the
-streaming, e.g. tool-delta stream parts are only needed in the
-object-tool mode.
+A language mode prompt is a standardized prompt type.
 
-@deprecated mode will be removed in v2.
-All necessary settings will be directly supported through the call settings,
-in particular responseFormat, toolChoice, and tools.
+Note: This is **not** the user-facing prompt. The AI SDK methods will map the
+user-facing prompt types such as chat or instruction prompts to this format.
+That approach allows us to evolve the user  facing prompts without breaking
+the language model interface.
    */
-  mode:
-    | {
-        // stream text & complete tool calls
-        type: 'regular';
+  prompt: LanguageModelV2Prompt;
 
-        /**
-The tools that are available for the model.
-         */
-        // TODO Spec V2: move to call settings
-        tools?: Array<
-          LanguageModelV2FunctionTool | LanguageModelV2ProviderDefinedTool
-        >;
+  /**
+Maximum number of tokens to generate.
+   */
+  // TODO rename to maxOutputTokens
+  maxTokens?: number;
 
-        /**
-Specifies how the tool should be selected. Defaults to 'auto'.
-         */
-        // TODO Spec V2: move to call settings
-        toolChoice?: LanguageModelV2ToolChoice;
-      }
+  /**
+Temperature setting.
+
+It is recommended to set either `temperature` or `topP`, but not both.
+ */
+  temperature?: number;
+
+  /**
+Stop sequences.
+If set, the model will stop generating text when one of the stop sequences is generated.
+Providers may have limits on the number of stop sequences.
+ */
+  stopSequences?: string[];
+
+  /**
+Nucleus sampling.
+
+It is recommended to set either `temperature` or `topP`, but not both.
+ */
+  topP?: number;
+
+  /**
+Only sample from the top K options for each subsequent token.
+
+Used to remove "long tail" low probability responses.
+Recommended for advanced use cases only. You usually only need to use temperature.
+ */
+  topK?: number;
+
+  /**
+Presence penalty setting. It affects the likelihood of the model to
+repeat information that is already in the prompt.
+ */
+  presencePenalty?: number;
+
+  /**
+Frequency penalty setting. It affects the likelihood of the model
+to repeatedly use the same words or phrases.
+ */
+  frequencyPenalty?: number;
+
+  /**
+Response format. The output can either be text or JSON. Default is text.
+
+If JSON is selected, a schema can optionally be provided to guide the LLM.
+ */
+  responseFormat?:
+    | { type: 'text' }
     | {
-        // object generation with json mode enabled (streaming: text delta)
-        type: 'object-json';
+        type: 'json';
 
         /**
          * JSON schema that the generated output should conform to.
@@ -64,22 +95,36 @@ Specifies how the tool should be selected. Defaults to 'auto'.
          * Description of the output that should be generated. Used by some providers for additional LLM guidance.
          */
         description?: string;
-      }
-    | {
-        // object generation with tool mode enabled (streaming: tool call deltas)
-        type: 'object-tool';
-        tool: LanguageModelV2FunctionTool;
       };
 
   /**
-A language mode prompt is a standardized prompt type.
+The seed (integer) to use for random sampling. If set and supported
+by the model, calls will generate deterministic results.
+ */
+  seed?: number;
 
-Note: This is **not** the user-facing prompt. The AI SDK methods will map the
-user-facing prompt types such as chat or instruction prompts to this format.
-That approach allows us to evolve the user  facing prompts without breaking
-the language model interface.
-   */
-  prompt: LanguageModelV2Prompt;
+  /**
+The tools that are available for the model.
+  */
+  tools?: Array<
+    LanguageModelV2FunctionTool | LanguageModelV2ProviderDefinedTool
+  >;
+
+  /**
+Specifies how the tool should be selected. Defaults to 'auto'.
+*/
+  toolChoice?: LanguageModelV2ToolChoice;
+
+  /**
+Abort signal for cancelling the operation.
+ */
+  abortSignal?: AbortSignal;
+
+  /**
+Additional HTTP headers to be sent with the request.
+Only applicable for HTTP-based providers.
+ */
+  headers?: Record<string, string | undefined>;
 
   /**
    * Additional provider-specific options. They are passed through
