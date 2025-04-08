fa2111441222155fcad348e9975ab219bf902905
Hayden Bleasel <hello@haydenbleasel.com>
2025-04-08 16:25:49 -0700

    Create index.ts

---
+++ packages/elevenlabs/src/index.ts
@@ -0,0 +1,3 @@
+export { createElevenLabs, elevenlabs } from './elevenlabs-provider';
+export type { ElevenLabsProvider, ElevenLabsProviderSettings } from './elevenlabs-provider';
+export type { ElevenLabsResponsesProviderOptions } from './responses/elevenlabs-responses-language-model';
