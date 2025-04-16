1f1bd1aa0f552a4bfda61ebf7b151ababcb5c85f
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2025-04-16 11:51:30 -0700

    docs: remove comment with reference to this PR from packages/ai/core/middleware/default-settings-middleware.test.ts

--- packages/ai/core/middleware/default-settings-middleware.test.ts
+++ packages/ai/core/middleware/default-settings-middleware.test.ts
@@ -41,7 +41,6 @@ describe('defaultSettingsMiddleware', () => {
       expect(result.temperature).toBe(0.5);
     });
 
-    // https://github.com/vercel/ai/pull/5769
     it('should default temparature to undefined', async () => {
       const middleware = defaultSettingsMiddleware({
         settings: {},
