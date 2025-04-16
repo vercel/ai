b9a6121c7ed3878c27e199a0cdadcca62fe90e3c
Lars Grammel <lars.grammel@gmail.com>
2025-04-16 13:01:59 +0200

    fix (provider/openai-compatible): change tool_call type schema to nulish (#5751) (#5800)
    Co-authored-by: wong2 <wonderfuly@gmail.com>

--- packages/openai-compatible/src/openai-compatible-chat-language-model.ts
+++ packages/openai-compatible/src/openai-compatible-chat-language-model.ts
@@ -690,7 +690,7 @@ const createOpenAICompatibleChatChunkSchema = <ERROR_SCHEMA extends z.ZodType>(
                   z.object({
                     index: z.number(),
                     id: z.string().nullish(),
-                    type: z.literal('function').optional(),
+                    type: z.literal('function').nullish(),
                     function: z.object({
                       name: z.string().nullish(),
                       arguments: z.string().nullish(),
