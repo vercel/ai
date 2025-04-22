754a04d292db9c88c7983b656ca61a70b3246d9e
Lars Grammel <lars.grammel@gmail.com>
2025-04-22 07:33:44 +0200

    mistral

--- packages/mistral/src/mistral-chat-language-model.ts
+++ packages/mistral/src/mistral-chat-language-model.ts
@@ -35,7 +35,6 @@ type MistralChatConfig = {
 
 export class MistralChatLanguageModel implements LanguageModelV2 {
   readonly specificationVersion = 'v2';
-  readonly supportsImageUrls = false;
 
   readonly modelId: MistralChatModelId;
 
@@ -50,8 +49,10 @@ export class MistralChatLanguageModel implements LanguageModelV2 {
     return this.config.provider;
   }
 
-  supportsUrl(url: URL): boolean {
-    return url.protocol === 'https:';
+  async getSupportedUrls(): Promise<Record<string, RegExp[]>> {
+    return {
+      'application/pdf': [/^https:\/\/.*$/],
+    };
   }
 
   private getArgs({
