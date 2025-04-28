f188bc41113bab65c191e409bbb6591a7c0db32c
Sam Denty <sam@samdenty.com>
2025-04-28 16:36:09 +0100

    fix: build

--- packages/ai/core/util/index.ts
+++ packages/ai/core/util/index.ts
@@ -1,4 +1,4 @@
-export { generateId, jsonSchema } from '@ai-sdk/provider-utils';
+export { generateId, jsonSchema, type Schema, asSchema } from '@ai-sdk/provider-utils';
 
 // Export stream data utilities for custom stream implementations,
 // both on the client and server side.
