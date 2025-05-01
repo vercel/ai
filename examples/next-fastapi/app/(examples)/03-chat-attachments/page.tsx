235b7249325bf3de9d352f84df7d4d625b56851f
Lars Grammel <lars.grammel@gmail.com>
2025-05-01 10:52:41 +0200

    4

--- examples/next-fastapi/app/(examples)/03-chat-attachments/page.tsx
+++ examples/next-fastapi/app/(examples)/03-chat-attachments/page.tsx
@@ -2,7 +2,6 @@
 
 import { Card } from '@/app/components';
 /* eslint-disable @next/next/no-img-element */
-import { getTextFromDataUrl } from 'ai';
 import { useChat } from '@ai-sdk/react';
 import { useRef, useState } from 'react';
 
@@ -20,26 +19,22 @@ export default function Page() {
         {messages.map(message => (
           <div key={message.id} className="flex flex-row gap-2">
             <div className="flex-shrink-0 w-24 text-zinc-500">{`${message.role}: `}</div>
-
             <div className="flex flex-col gap-2">
-              {message.content}
-
-              <div className="flex flex-row gap-2">
-                {message.experimental_attachments?.map((attachment, index) =>
-                  attachment.contentType?.includes('image/') ? (
-                    <img
-                      key={`${message.id}-${index}`}
-                      className="w-24 rounded-md"
-                      src={attachment.url}
-                      alt={attachment.name}
-                    />
-                  ) : attachment.contentType?.includes('text/') ? (
-                    <div className="w-32 h-24 p-2 overflow-hidden text-xs border rounded-md ellipsis text-zinc-500">
-                      {getTextFromDataUrl(attachment.url)}
+              {message.parts.map((part, index) => {
+                if (part.type === 'text') {
+                  return <div key={index}>{part.text}</div>;
+                }
+                if (
+                  part.type === 'file' &&
+                  part.mediaType?.startsWith('image/')
+                ) {
+                  return (
+                    <div key={index}>
+                      <img className="rounded-md w-60" src={part.url} />
                     </div>
-                  ) : null,
-                )}
-              </div>
+                  );
+                }
+              })}
             </div>
           </div>
         ))}
@@ -49,9 +44,7 @@ export default function Page() {
 
       <form
         onSubmit={event => {
-          handleSubmit(event, {
-            experimental_attachments: files,
-          });
+          handleSubmit(event, { files });
           setFiles(undefined);
 
           if (fileInputRef.current) {
