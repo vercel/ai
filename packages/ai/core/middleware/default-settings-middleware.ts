52e4dd3334edaeeedd9fb43d62b8277531cd8b71
Lars Grammel <lars.grammel@gmail.com>
2025-05-08 11:30:19 +0200

    x

--- packages/ai/core/middleware/default-settings-middleware.ts
+++ packages/ai/core/middleware/default-settings-middleware.ts
@@ -29,52 +29,7 @@ export function defaultSettingsMiddleware({
   return {
     middlewareVersion: 'v2',
     transformParams: async ({ params }) => {
-      return {
-        ...params,
-
-        // apply defaults to undefined values
-        maxOutputTokens:
-          params.maxOutputTokens === undefined
-            ? settings.maxOutputTokens
-            : params.maxOutputTokens,
-        temperature:
-          params.temperature === undefined
-            ? settings.temperature
-            : params.temperature,
-        stopSequences:
-          params.stopSequences === undefined
-            ? settings.stopSequences
-            : params.stopSequences,
-        topP: params.topP === undefined ? settings.topP : params.topP,
-        topK: params.topK === undefined ? settings.topK : params.topK,
-        presencePenalty:
-          params.presencePenalty === undefined
-            ? settings.presencePenalty
-            : params.presencePenalty,
-        frequencyPenalty:
-          params.frequencyPenalty === undefined
-            ? settings.frequencyPenalty
-            : params.frequencyPenalty,
-        responseFormat:
-          params.responseFormat === undefined
-            ? settings.responseFormat
-            : params.responseFormat,
-        seed: params.seed === undefined ? settings.seed : params.seed,
-        tools: params.tools === undefined ? settings.tools : params.tools,
-        toolChoice:
-          params.toolChoice === undefined
-            ? settings.toolChoice
-            : params.toolChoice,
-
-        // headers: deep merge
-        headers: mergeObjects(settings.headers, params.headers),
-
-        // provider options: deep merge
-        providerOptions: mergeObjects(
-          settings.providerOptions,
-          params.providerOptions,
-        ),
-      };
+      return mergeObjects(settings, params) as LanguageModelV2CallOptions;
     },
   };
 }
