cc0a0cd44228e67dc7077357a0dafa7a9073cae0
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2025-05-14 22:51:59 -0700

    add todo comment

--- packages/ai/src/ui/process-ui-message-stream.ts
+++ packages/ai/src/ui/process-ui-message-stream.ts
@@ -105,6 +105,7 @@ export function processUIMessageStream<MESSAGE_METADATA = unknown>({
               if (messageMetadataSchema != null) {
                 await validateTypes({
                   value: mergedMetadata,
+                  // @ts-ignore TODO: Couldn't figure this one out yet ~@gr2m
                   schema: messageMetadataSchema,
                 });
               }
