d266dac429aefaec97cfed741aa9b35554453db5
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2025-05-23 09:31:41 -0700

    zod3Schema, zod4Sschema, isZod4Schema

--- packages/provider-utils/src/zod-schema.ts
+++ packages/provider-utils/src/zod-schema.ts
@@ -4,8 +4,8 @@ import * as z4 from 'zod/v4/core';
 import zodToJsonSchema from 'zod-to-json-schema';
 import { jsonSchema, Schema } from './schema';
 
-export function zodSchema<OBJECT>(
-  zodSchema: z4.$ZodType<OBJECT, any> | z3.Schema<OBJECT, z3.ZodTypeDef, any>,
+export function zod3Schema<OBJECT>(
+  zodSchema: z3.Schema<OBJECT, z3.ZodTypeDef, any>,
   options?: {
     /**
      * Enables support for references in the schema.
@@ -19,31 +19,72 @@ export function zodSchema<OBJECT>(
   // default to no references (to support openapi conversion for google)
   const useReferences = options?.useReferences ?? false;
 
-  // https://zod.dev/library-authors?id=how-to-support-zod-3-and-zod-4-simultaneously
-  const isZod4 = '_zod' in zodSchema;
-
-  const converted = isZod4
-    ? (z4.toJSONSchema(zodSchema, { target: 'draft-7' }) as JSONSchema7)
-    : (zodToJsonSchema(zodSchema, {
-        $refStrategy: useReferences ? 'root' : 'none',
-        target: 'jsonSchema7', // note: openai mode breaks various gemini conversions
-      }) as JSONSchema7);
-
-  return jsonSchema(converted, {
-    validate: value => {
-      let result;
-      if (isZod4) {
-        // Zod 4 schema
-        z4.safeParse;
-        result = z4.safeParse(zodSchema, value);
-      } else {
-        // Zod 3 schema
-        result = zodSchema.safeParse(value);
-      }
-
-      return result.success
-        ? { success: true, value: result.data }
-        : { success: false, error: result.error };
+  return jsonSchema(
+    zodToJsonSchema(zodSchema, {
+      $refStrategy: useReferences ? 'root' : 'none',
+      target: 'jsonSchema7', // note: openai mode breaks various gemini conversions
+    }) as JSONSchema7,
+    {
+      validate: value => {
+        const result = zodSchema.safeParse(value);
+        return result.success
+          ? { success: true, value: result.data }
+          : { success: false, error: result.error };
+      },
     },
-  });
+  );
+}
+
+export function zod4Schema<OBJECT>(
+  zodSchema: z4.$ZodType<OBJECT, any>,
+  options?: {
+    /**
+     * Enables support for references in the schema.
+     * This is required for recursive schemas, e.g. with `z.lazy`.
+     * However, not all language models and providers support such references.
+     * Defaults to `false`.
+     */
+    useReferences?: boolean;
+  },
+): Schema<OBJECT> {
+  // default to no references (to support openapi conversion for google)
+  const useReferences = options?.useReferences ?? false;
+
+  return jsonSchema(
+    z4.toJSONSchema(zodSchema, { target: 'draft-7' }) as JSONSchema7,
+    {
+      validate: value => {
+        const result = z4.safeParse(zodSchema, value);
+        return result.success
+          ? { success: true, value: result.data }
+          : { success: false, error: result.error };
+      },
+    },
+  );
+}
+
+export function isZod4Schema(
+  zodSchema: z4.$ZodType<any, any> | z3.Schema<any, z3.ZodTypeDef, any>,
+): zodSchema is z4.$ZodType<any, any> {
+  // https://zod.dev/library-authors?id=how-to-support-zod-3-and-zod-4-simultaneously
+  return '_zod' in zodSchema;
+}
+
+export function zodSchema<OBJECT>(
+  zodSchema: z4.$ZodType<OBJECT, any> | z3.Schema<OBJECT, z3.ZodTypeDef, any>,
+  options?: {
+    /**
+     * Enables support for references in the schema.
+     * This is required for recursive schemas, e.g. with `z.lazy`.
+     * However, not all language models and providers support such references.
+     * Defaults to `false`.
+     */
+    useReferences?: boolean;
+  },
+): Schema<OBJECT> {
+  if (isZod4Schema(zodSchema)) {
+    return zod4Schema(zodSchema, options);
+  } else {
+    return zod3Schema(zodSchema, options);
+  }
 }
