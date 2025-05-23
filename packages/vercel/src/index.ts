6b009bad31785355d003b270aded1d171a8f6db5
Walter Korman <shaper@vercel.com>
2025-05-23 13:19:15 -0700

    feat (vercel): initial vercel provider

---
+++ packages/vercel/src/index.ts
@@ -0,0 +1,3 @@
+export { createVercel, vercel } from './vercel-provider';
+export type { VercelProvider, VercelProviderSettings } from './vercel-provider';
+export type { OpenAICompatibleErrorData as VercelErrorData } from '@ai-sdk/openai-compatible';
---
+++ packages/vercel/src/vercel-chat-options.ts
@@ -0,0 +1,2 @@
+// https://vercel.com/docs/v0/api
+export type VercelChatModelId = 'v0-1.0-md' | (string & {});
