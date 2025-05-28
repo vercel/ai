4676d9744ee8b1198dc0949475cd260d9920a8fb
Lars Grammel <lars.grammel@gmail.com>
2025-05-28 14:35:25 +0800

    6

--- packages/ai/src/ui/call-chat-api.ts
+++ packages/ai/src/ui/call-chat-api.ts
@@ -3,7 +3,6 @@ import {
   UIMessageStreamPart,
   uiMessageStreamPartSchema,
 } from '../ui-message-stream/ui-message-stream-parts';
-import { transformTextToUiMessageStream } from './transform-text-to-ui-message-stream';
 
 // use function to allow for mocking in tests:
 const getOriginalFetch = () => fetch;
@@ -70,58 +69,4 @@ export async function fetchUIMessageStream({
       },
     }),
   );
-}
-
-export async function fetchTextStream({
-  api,
-  body,
-  credentials,
-  headers,
-  abortController,
-  fetch = getOriginalFetch(),
-  requestType = 'generate',
-}: {
-  api: string;
-  body: Record<string, any>;
-  credentials: RequestCredentials | undefined;
-  headers: HeadersInit | undefined;
-  abortController: (() => AbortController | null) | undefined;
-  fetch: ReturnType<typeof getOriginalFetch> | undefined;
-  requestType?: 'generate' | 'resume';
-}): Promise<ReadableStream<UIMessageStreamPart>> {
-  const response =
-    requestType === 'resume'
-      ? await fetch(`${api}?chatId=${body.chatId}`, {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-            ...headers,
-          },
-          signal: abortController?.()?.signal,
-          credentials,
-        })
-      : await fetch(api, {
-          method: 'POST',
-          body: JSON.stringify(body),
-          headers: {
-            'Content-Type': 'application/json',
-            ...headers,
-          },
-          signal: abortController?.()?.signal,
-          credentials,
-        });
-
-  if (!response.ok) {
-    throw new Error(
-      (await response.text()) ?? 'Failed to fetch the chat response.',
-    );
-  }
-
-  if (!response.body) {
-    throw new Error('The response body is empty.');
-  }
-
-  return transformTextToUiMessageStream({
-    stream: response.body.pipeThrough(new TextDecoderStream()),
-  });
 }
