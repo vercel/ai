3b897023a877aaaa60ca1e5eb3fea45c35b84e20
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2025-06-02 13:57:57 -0700

    codemod: import `LanguageModelV2` from `@ai-sdk/provider` - account for v1 imports

--- packages/codemod/src/codemods/import-LanguageModelV2-from-provider-package.ts
+++ packages/codemod/src/codemods/import-LanguageModelV2-from-provider-package.ts
@@ -1,5 +1,12 @@
 import { createTransformer } from './lib/create-transformer';
 
+const ImportMappings: Record<string, string> = {
+  LanguageModelV1: 'LanguageModelV2',
+  LanguageModelV2: 'LanguageModelV2',
+  LanguageModelV1Middleware: 'LanguageModelV2Middleware',
+  LanguageModelV2Middleware: 'LanguageModelV2Middleware',
+};
+
 export default createTransformer((fileInfo, api, options, context) => {
   const { j, root } = context;
 
@@ -8,23 +15,34 @@ export default createTransformer((fileInfo, api, options, context) => {
     const node = importPath.node;
 
     // Check if the source value is exactly 'ai'
-    if (node.source.value === 'ai') {
-      // Check if the named import includes 'LanguageModelV2'
-      const specifier = node.specifiers?.find(
+    if (node.source.value !== 'ai') return;
+
+    // Check if the named import includes 'LanguageModelV1' or 'LanguageModelV2'
+    const specifiers =
+      node.specifiers?.filter(
         s =>
-          j.ImportSpecifier.check(s) && s.imported.name === 'LanguageModelV2',
-      );
+          j.ImportSpecifier.check(s) &&
+          Object.keys(ImportMappings).includes(s.imported.name),
+      ) ?? [];
 
-      if (!specifier) return;
-      // Set hasChanges to true since we will modify the AST
-      context.hasChanges = true;
-
-      // Change the module source from 'ai' to '@ai-sdk/provider'
-      node.source = j.stringLiteral('@ai-sdk/provider');
-
-      context.messages.push(
-        `Updated import of LanguageModelV2 from 'ai' to '@ai-sdk/provider'`,
-      );
+    for (const specifier of specifiers) {
+      // If the import is 'LanguageModelV1', we will change it to 'LanguageModelV2'
+      if (
+        specifier.type === 'ImportSpecifier' &&
+        ImportMappings[specifier.imported.name]
+      ) {
+        specifier.imported.name = ImportMappings[specifier.imported.name];
+      }
     }
+
+    // Set hasChanges to true since we will modify the AST
+    context.hasChanges = true;
+
+    // Change the module source from 'ai' to '@ai-sdk/provider'
+    node.source = j.stringLiteral('@ai-sdk/provider');
+
+    context.messages.push(
+      `Updated import of LanguageModelV2 from 'ai' to '@ai-sdk/provider'`,
+    );
   });
 });
--- packages/codemod/src/test/import-LanguageModelV2-from-provider-package.test.ts
+++ packages/codemod/src/test/import-LanguageModelV2-from-provider-package.test.ts
@@ -3,10 +3,19 @@ import transformer from '../codemods/import-LanguageModelV2-from-provider-packag
 import { testTransform } from './test-utils';
 
 describe('import-LanguageModelV2-from-provider-package', () => {
-  it('transforms correctly', () => {
+  it('transforms package name correctly', () => {
     testTransform(transformer, 'import-LanguageModelV2-from-provider-package');
   });
+  it('transforms import name correctly', () => {
+    testTransform(
+      transformer,
+      'import-LanguageModelV2-from-provider-package-v1',
+    );
+  });
   it('does not transform non ai package imports', () => {
-    testTransform(transformer, 'import-LanguageModelV2-from-provider-package-not-ai');
+    testTransform(
+      transformer,
+      'import-LanguageModelV2-from-provider-package-not-ai',
+    );
   });
 });
