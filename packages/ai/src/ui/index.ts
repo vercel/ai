fe2cc96657353e9da2d9167fd0f52c8e7c4cc68f
Lars Grammel <lars.grammel@gmail.com>
2025-06-06 11:49:49 +0200

    a

--- packages/ai/src/ui/index.ts
+++ packages/ai/src/ui/index.ts
@@ -3,8 +3,9 @@ export {
   AbstractChat,
   type BaseChatInit,
   type ChatEvent,
-  type ChatStatus,
+  type ChatRequestOptions,
   type ChatState,
+  type ChatStatus,
 } from './chat';
 export { type ChatTransport } from './chat-transport';
 export { convertFileListToFileUIParts } from './convert-file-list-to-file-ui-parts';
@@ -16,20 +17,20 @@ export { DefaultChatTransport } from './default-chat-transport';
 export { getToolInvocations } from './get-tool-invocations';
 export { TextStreamChatTransport } from './text-stream-chat-transport';
 export {
-  type InferUIDataParts,
-  type UIDataPartSchemas,
-  type ToolInvocation,
-  type UIMessage,
   type CreateUIMessage,
-  type UIDataTypes,
   type DataUIPart,
-  type StepStartUIPart,
-  type SourceUrlUIPart,
   type FileUIPart,
-  type UIMessagePart,
-  type TextUIPart,
+  type InferUIDataParts,
   type ReasoningUIPart,
+  type SourceUrlUIPart,
+  type StepStartUIPart,
+  type TextUIPart,
+  type ToolInvocation,
   type ToolInvocationUIPart,
+  type UIDataPartSchemas,
+  type UIDataTypes,
+  type UIMessage,
+  type UIMessagePart,
 } from './ui-messages';
 export {
   type CompletionRequestOptions,
