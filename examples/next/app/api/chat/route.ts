e9daa1985d12b52dc61fc452df534d51ae949fb0
Lars Grammel <lars.grammel@gmail.com>
2025-06-08 11:28:51 +0200

    2

--- examples/next/app/api/chat/route.ts
+++ examples/next/app/api/chat/route.ts
@@ -1,7 +1,13 @@
 import { callWeatherApi } from '@/util/call-weather-api';
 import { MyUIMessage } from '@/util/chat-schema';
 import { readChat, saveChat } from '@util/chat-store';
-import { convertToModelMessages, streamText, tool } from 'ai';
+import {
+  convertToModelMessages,
+  createUIMessageStream,
+  createUIMessageStreamResponse,
+  streamText,
+  tool,
+} from 'ai';
 import z from 'zod/v4';
 
 export async function POST(req: Request) {
@@ -11,30 +17,40 @@ export async function POST(req: Request) {
   const chat = await readChat(id);
   const messages = [...chat.messages, message];
 
-  const result = streamText({
-    model: 'openai/gpt-4o',
-    messages: convertToModelMessages(messages),
-    tools: {
-      getWeather: tool({
-        description: 'show the weather in a given city to the user',
-        parameters: z.object({ city: z.string() }),
-        execute: callWeatherApi,
-      }),
+  const stream = createUIMessageStream({
+    execute: async ({ writer }) => {
+      const result = streamText({
+        model: 'openai/gpt-4o',
+        messages: convertToModelMessages(messages),
+        tools: {
+          getWeather: tool({
+            description: 'show the weather in a given city to the user',
+            parameters: z.object({ city: z.string() }),
+            execute: callWeatherApi,
+          }),
+        },
+      });
+
+      result.consumeStream(); // TODO always consume the stream even when the client disconnects
+
+      writer.merge(
+        result.toUIMessageStream({
+          messageMetadata: ({ part }) => {
+            if (part.type === 'start') {
+              return { createdAt: Date.now() };
+            }
+          },
+        }),
+      );
     },
-  });
 
-  result.consumeStream(); // TODO always consume the stream even when the client disconnects
-
-  return result.toUIMessageStreamResponse({
+    // save the chat when the stream is finished
     originalMessages: messages,
-    messageMetadata: ({ part }) => {
-      if (part.type === 'start') {
-        return { createdAt: Date.now() };
-      }
-    },
     onFinish: ({ messages }) => {
       // TODO fix type safety
       saveChat({ id, messages: messages as MyUIMessage[] });
     },
   });
+
+  return createUIMessageStreamResponse({ stream });
 }
