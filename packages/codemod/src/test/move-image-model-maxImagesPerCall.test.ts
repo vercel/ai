593f923fc05ccc4d9001dd17476ae5b51c43eadb
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2025-06-09 11:48:13 -0700

    codemod: `move-image-model-maxImagesPerCall`

---
+++ packages/codemod/src/test/move-image-model-maxImagesPerCall.test.ts
@@ -0,0 +1,9 @@
+import { describe, it } from 'vitest';
+import transformer from '../codemods/move-image-model-maxImagesPerCall';
+import { testTransform } from './test-utils';
+
+describe('move-image-model-maxImagesPerCall', () => {
+  it('transforms correctly', () => {
+    testTransform(transformer, 'move-image-model-maxImagesPerCall');
+  });
+}); 
