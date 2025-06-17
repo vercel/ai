e08504fbe41ec7c8bdbc73a27fb9d897a56d5eee
Lars Grammel <lars.grammel@gmail.com>
2025-06-17 10:45:46 +0200

    1

--- packages/ai/core/prompt/resolve-language-model.ts
+++ packages/ai/core/prompt/resolve-language-model.ts
@@ -2,18 +2,16 @@ import { gateway } from '@ai-sdk/gateway';
 import { LanguageModelV2, ProviderV2 } from '@ai-sdk/provider';
 import { LanguageModel } from '../types/language-model';
 
-export const GLOBAL_DEFAULT_PROVIDER = Symbol(
-  'vercel.ai.global.defaultProvider',
-);
+// add type of the global default provider variable to the globalThis object
+declare global {
+  var VERCEL_AI_GLOBAL_DEFAULT_PROVIDER: ProviderV2 | undefined;
+}
 
 export function resolveLanguageModel(model: LanguageModel): LanguageModelV2 {
   if (typeof model !== 'string') {
     return model;
   }
 
-  const globalProvider = (globalThis as any)[GLOBAL_DEFAULT_PROVIDER] as
-    | ProviderV2
-    | undefined;
-
+  const globalProvider = globalThis.VERCEL_AI_GLOBAL_DEFAULT_PROVIDER;
   return (globalProvider ?? gateway).languageModel(model);
 }
