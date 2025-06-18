2a1bb2b1fee34f76ea416cd282ff728632172d1f
Lars Grammel <lars.grammel@gmail.com>
2025-06-18 10:08:20 +0200

    chore (provider-utils): use eventsource-parser library

--- packages/provider-utils/src/parse-json-event-stream.ts
+++ packages/provider-utils/src/parse-json-event-stream.ts
@@ -1,8 +1,8 @@
-import { ZodSchema } from 'zod';
 import {
-  createEventSourceParserStream,
-  EventSourceChunk,
-} from './event-source-parser-stream';
+  EventSourceMessage,
+  EventSourceParserStream,
+} from 'eventsource-parser/stream';
+import { ZodSchema } from 'zod';
 import { ParseResult, safeParseJSON } from './parse-json';
 
 /**
@@ -17,9 +17,9 @@ export function parseJsonEventStream<T>({
 }): ReadableStream<ParseResult<T>> {
   return stream
     .pipeThrough(new TextDecoderStream())
-    .pipeThrough(createEventSourceParserStream())
+    .pipeThrough(new EventSourceParserStream())
     .pipeThrough(
-      new TransformStream<EventSourceChunk, ParseResult<T>>({
+      new TransformStream<EventSourceMessage, ParseResult<T>>({
         async transform({ data }, controller) {
           // ignore the 'DONE' event that e.g. OpenAI sends:
           if (data === '[DONE]') {
