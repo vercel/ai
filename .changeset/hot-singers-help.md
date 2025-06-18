879ba5b24e6efa855038ca5d17bacce787fd4b31
Lars Grammel <lars.grammel@gmail.com>
2025-06-18 08:35:02 +0200

    chore (ai): rename default provider global to AI_SDK_DEFAULT_PROVIDER

---
+++ .changeset/hot-singers-help.md
@@ -0,0 +1,5 @@
+---
+'ai': major
+---
+
+chore (ai): rename default provider global to AI_SDK_DEFAULT_PROVIDER
--- packages/ai/src/global.ts
+++ packages/ai/src/global.ts
@@ -2,5 +2,5 @@ import { ProviderV2 } from '@ai-sdk/provider';
 
 // add type of the global default provider variable to the globalThis object
 declare global {
-  var VERCEL_AI_GLOBAL_DEFAULT_PROVIDER: ProviderV2 | undefined;
+  var AI_SDK_DEFAULT_PROVIDER: ProviderV2 | undefined;
 }
