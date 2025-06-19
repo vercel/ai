vercel/ai

commit f2b041e5efddd96b9ce90dc1ff2b3868f874993b

fix(ai): Use custom fetch if provided (#6787)
## Background

Fixes a bug where `fetch` was not used and it always called the global
`fetch`. This was introduced in https://github.com/vercel/ai/pull/6746

## Summary

Call `this.fetch()` instead of `fetch()`

## Verification

I verified in a patched package locally.

packages/ai/src/ui/http-chat-transport.ts
@@ -154,7 +154,7 @@ export abstract class HttpChatTransport<UI_MESSAGE extends UIMessage>
           };
     const credentials = preparedRequest?.credentials ?? this.credentials;
 
-    const response = await fetch(api, {
+    const response = await this.fetch(api, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -197,7 +197,7 @@ export abstract class HttpChatTransport<UI_MESSAGE extends UIMessage>
         : { ...this.headers, ...options.headers };
     const credentials = preparedRequest?.credentials ?? this.credentials;
 
-    const response = await fetch(api, {
+    const response = await this.fetch(api, {
       method: 'GET',
       headers,
       credentials,

commit 102b066167ecb0da2410d5b44269bddd94ad2506

fix (ai): fix invalid fetch call (#6801)
## Background

`useChat` threw an invalid invocation error.

## Summary

Invoke fetch without `this` arg.

## Verification

Test next-openai manually.

## Related Issues

Caused by #6787

packages/ai/src/ui/http-chat-transport.ts
@@ -154,7 +154,7 @@ export abstract class HttpChatTransport<UI_MESSAGE extends UIMessage>
           };
     const credentials = preparedRequest?.credentials ?? this.credentials;
 
-    const response = await this.fetch(api, {
+    const response = await this.fetch.call(undefined, api, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -197,7 +197,7 @@ export abstract class HttpChatTransport<UI_MESSAGE extends UIMessage>
         : { ...this.headers, ...options.headers };
     const credentials = preparedRequest?.credentials ?? this.credentials;
 
-    const response = await this.fetch(api, {
+    const response = await this.fetch.call(undefined, api, {
       method: 'GET',
       headers,
       credentials,