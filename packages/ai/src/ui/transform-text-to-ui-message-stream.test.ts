8f455f5f7c9d9b56aeb2442089b0e25111508b01
Lars Grammel <lars.grammel@gmail.com>
2025-06-21 14:21:12 +0200

    t

--- packages/ai/src/ui/transform-text-to-ui-message-stream.test.ts
+++ packages/ai/src/ui/transform-text-to-ui-message-stream.test.ts
@@ -12,33 +12,60 @@ describe('transformTextToUiMessageStream', () => {
 
     expect(await convertReadableStreamToArray(transformedStream))
       .toMatchInlineSnapshot(`
-        [
-          {
-            "type": "start",
-          },
-          {
-            "type": "start-step",
-          },
-          {
-            "text": "Hello",
-            "type": "text",
-          },
-          {
-            "text": " ",
-            "type": "text",
-          },
-          {
-            "text": "World",
-            "type": "text",
-          },
-          {
-            "type": "finish-step",
-          },
-          {
-            "type": "finish",
-          },
-        ]
-      `);
+      [
+        {
+          "type": "start",
+        },
+        {
+          "type": "start-step",
+        },
+        {
+          "id": "text-1",
+          "type": "text-start",
+        },
+        {
+          "delta": "Hello",
+          "id": "text-1",
+          "type": "text-delta",
+        },
+        {
+          "id": "text-1",
+          "type": "text-end",
+        },
+        {
+          "id": "text-1",
+          "type": "text-start",
+        },
+        {
+          "delta": " ",
+          "id": "text-1",
+          "type": "text-delta",
+        },
+        {
+          "id": "text-1",
+          "type": "text-end",
+        },
+        {
+          "id": "text-1",
+          "type": "text-start",
+        },
+        {
+          "delta": "World",
+          "id": "text-1",
+          "type": "text-delta",
+        },
+        {
+          "id": "text-1",
+          "type": "text-end",
+        },
+        {
+          "type": "finish-step",
+        },
+        {
+          "type": "finish",
+        },
+      ]
+    `);
   });
 
   it('should handle empty streams correctly', async () => {
@@ -48,21 +75,21 @@ describe('transformTextToUiMessageStream', () => {
 
     expect(await convertReadableStreamToArray(transformedStream))
       .toMatchInlineSnapshot(`
-        [
-          {
-            "type": "start",
-          },
-          {
-            "type": "start-step",
-          },
-          {
-            "type": "finish-step",
-          },
-          {
-            "type": "finish",
-          },
-        ]
-      `);
+      [
+        {
+          "type": "start",
+        },
+        {
+          "type": "start-step",
+        },
+        {
+          "type": "finish-step",
+        },
+        {
+          "type": "finish",
+        },
+      ]
+    `);
   });
 
   it('should handle single chunk streams', async () => {
@@ -72,24 +99,33 @@ describe('transformTextToUiMessageStream', () => {
 
     expect(await convertReadableStreamToArray(transformedStream))
       .toMatchInlineSnapshot(`
-        [
-          {
-            "type": "start",
-          },
-          {
-            "type": "start-step",
-          },
-          {
-            "text": "Complete message",
-            "type": "text",
-          },
-          {
-            "type": "finish-step",
-          },
-          {
-            "type": "finish",
-          },
-        ]
-      `);
+      [
+        {
+          "type": "start",
+        },
+        {
+          "type": "start-step",
+        },
+        {
+          "id": "text-1",
+          "type": "text-start",
+        },
+        {
+          "delta": "Complete message",
+          "id": "text-1",
+          "type": "text-delta",
+        },
+        {
+          "id": "text-1",
+          "type": "text-end",
+        },
+        {
+          "type": "finish-step",
+        },
+        {
+          "type": "finish",
+        },
+      ]
+    `);
   });
 });
