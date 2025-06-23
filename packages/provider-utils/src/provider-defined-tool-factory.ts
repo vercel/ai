9f9a22ae1f5e46caf40b168c8c53d184b330742d
Lars Grammel <lars.grammel@gmail.com>
2025-06-23 09:56:26 +0200

    1

--- packages/provider-utils/src/provider-defined-tool-factory.ts
+++ packages/provider-utils/src/provider-defined-tool-factory.ts
@@ -1,9 +1,12 @@
 import { tool, Tool, ToolExecuteFunction } from './types/tool';
 import { FlexibleSchema } from './schema';
 
-export type ProviderDefinedToolFactory<INPUT, ARGS extends object> = <OUTPUT>(
+export type ProviderDefinedClientToolFactory<INPUT, ARGS extends object> = <
+  OUTPUT,
+>(
   options: ARGS & {
     execute?: ToolExecuteFunction<INPUT, OUTPUT>;
+    outputSchema?: FlexibleSchema<OUTPUT>;
     toModelOutput?: Tool<INPUT, OUTPUT>['toModelOutput'];
     onInputStart?: Tool<INPUT, OUTPUT>['onInputStart'];
     onInputDelta?: Tool<INPUT, OUTPUT>['onInputDelta'];
@@ -11,13 +14,16 @@ export type ProviderDefinedToolFactory<INPUT, ARGS extends object> = <OUTPUT>(
   },
 ) => Tool<INPUT, OUTPUT>;
 
-export function createProviderDefinedToolFactory<INPUT, ARGS extends object>({
+export function createProviderDefinedClientToolFactory<
+  INPUT,
+  ARGS extends object,
+>({
   id,
   inputSchema,
 }: {
   id: `${string}.${string}`;
   inputSchema: FlexibleSchema<INPUT>;
-}): ProviderDefinedToolFactory<INPUT, ARGS> {
+}): ProviderDefinedClientToolFactory<INPUT, ARGS> {
   return <OUTPUT>({
     execute,
     outputSchema,
@@ -29,6 +35,61 @@ export function createProviderDefinedToolFactory<INPUT, ARGS extends object>({
   }: ARGS & {
     execute?: ToolExecuteFunction<INPUT, OUTPUT>;
     outputSchema?: FlexibleSchema<OUTPUT>;
+    toModelOutput?: Tool<INPUT, OUTPUT>['toModelOutput'];
+    onInputStart?: Tool<INPUT, OUTPUT>['onInputStart'];
+    onInputDelta?: Tool<INPUT, OUTPUT>['onInputDelta'];
+    onInputAvailable?: Tool<INPUT, OUTPUT>['onInputAvailable'];
+  }): Tool<INPUT, OUTPUT> =>
+    tool({
+      type: 'provider-defined',
+      id,
+      args,
+      inputSchema,
+      outputSchema,
+      execute,
+      toModelOutput,
+      onInputStart,
+      onInputDelta,
+      onInputAvailable,
+    });
+}
+
+export type ProviderDefinedServerToolFactory<
+  INPUT,
+  OUTPUT,
+  ARGS extends object,
+> = (
+  options: ARGS & {
+    execute?: ToolExecuteFunction<INPUT, OUTPUT>;
+    toModelOutput?: Tool<INPUT, OUTPUT>['toModelOutput'];
+    onInputStart?: Tool<INPUT, OUTPUT>['onInputStart'];
+    onInputDelta?: Tool<INPUT, OUTPUT>['onInputDelta'];
+    onInputAvailable?: Tool<INPUT, OUTPUT>['onInputAvailable'];
+  },
+) => Tool<INPUT, OUTPUT>;
+
+export function createProviderDefinedServerToolFactory<
+  INPUT,
+  OUTPUT,
+  ARGS extends object,
+>({
+  id,
+  inputSchema,
+  outputSchema,
+}: {
+  id: `${string}.${string}`;
+  inputSchema: FlexibleSchema<INPUT>;
+  outputSchema: FlexibleSchema<OUTPUT>;
+}): ProviderDefinedServerToolFactory<INPUT, OUTPUT, ARGS> {
+  return ({
+    execute,
+    toModelOutput,
+    onInputStart,
+    onInputDelta,
+    onInputAvailable,
+    ...args
+  }: ARGS & {
+    execute?: ToolExecuteFunction<INPUT, OUTPUT>;
     toModelOutput?: Tool<INPUT, OUTPUT>['toModelOutput'];
     onInputStart?: Tool<INPUT, OUTPUT>['onInputStart'];
     onInputDelta?: Tool<INPUT, OUTPUT>['onInputDelta'];
