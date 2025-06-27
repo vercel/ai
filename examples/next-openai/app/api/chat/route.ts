eba54eab3eee0e6bb2719a84c460188027c28a9a
Lars Grammel <lars.grammel@gmail.com>
2025-06-27 16:10:14 +0200

    rev

--- examples/next-openai/app/api/chat/route.ts
+++ examples/next-openai/app/api/chat/route.ts
@@ -1,41 +1,17 @@
 import { openai } from '@ai-sdk/openai';
-import {
-  convertToModelMessages,
-  createUIMessageStream,
-  createUIMessageStreamResponse,
-  streamText,
-  UIMessage,
-} from 'ai';
+import { convertToModelMessages, streamText, UIMessage } from 'ai';
 
 export const maxDuration = 30;
 
 export async function POST(req: Request) {
   const { messages }: { messages: UIMessage[] } = await req.json();
 
-  const modelMessages = convertToModelMessages(messages);
+  const prompt = convertToModelMessages(messages);
 
-  const stream = createUIMessageStream({
-    execute: ({ writer }) => {
-      throw new Error('test');
-      // const result = streamText({
-      //   model: anthropic('claude-sonnet-4-20250514'),
-      //   messages: modelMessages,
-      //   onError: (err) => {
-      //     const errorMessage = 'Error during streamText';
-      //     logger.error({ err }, errorMessage);
-      //   },
-      // });
-      // writer.merge(result.toUIMessageStream());
-    },
-    onFinish: () => {
-      // error message doesn't show if onFinish isn't defined
-    },
-    onError: err => {
-      const errorMessage = 'Error during createUIMessageStream';
-      console.error(errorMessage, err);
-      return errorMessage;
-    },
+  const result = streamText({
+    model: openai('gpt-4o'),
+    prompt,
   });
 
-  return createUIMessageStreamResponse({ stream });
+  return result.toUIMessageStreamResponse();
 }
