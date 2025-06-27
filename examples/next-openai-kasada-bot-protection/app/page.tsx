fc51c3b0521e77abea1c2afa9294d963498166c8
Lars Grammel <lars.grammel@gmail.com>
2025-06-27 16:06:51 +0200

    f

--- examples/next-openai-kasada-bot-protection/app/page.tsx
+++ examples/next-openai-kasada-bot-protection/app/page.tsx
@@ -8,12 +8,12 @@ export default function Chat() {
   const [input, setInput] = useState('');
   const { messages, sendMessage, status } = useChat({
     onError: err => {
-      toast.error(err.message);
+      toast.error(err as any);
     },
   });
 
   return (
-    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
+    <div className="flex flex-col py-24 mx-auto w-full max-w-md stretch">
       {messages.length > 0
         ? messages.map(m => (
             <div key={m.id} className="whitespace-pre-wrap">
@@ -33,7 +33,7 @@ export default function Chat() {
         }}
       >
         <input
-          className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
+          className="fixed bottom-0 p-2 mb-8 w-full max-w-md rounded border border-gray-300 shadow-xl"
           value={input}
           placeholder="Say something..."
           onChange={e => setInput(e.target.value)}
