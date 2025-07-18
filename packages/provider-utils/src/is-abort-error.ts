3a7c26cda80655aaa1e226f335ef8960027347bf
Lars Grammel <lars.grammel@gmail.com>
2025-07-18 17:56:48 +0200

    x

--- packages/provider-utils/src/is-abort-error.ts
+++ packages/provider-utils/src/is-abort-error.ts
@@ -1,6 +1,8 @@
 export function isAbortError(error: unknown): error is Error {
   return (
     error instanceof Error &&
-    (error.name === 'AbortError' || error.name === 'TimeoutError')
+    (error.name === 'AbortError' ||
+      error.name === 'ResponseAborted' || // Next.js
+      error.name === 'TimeoutError')
   );
 }
