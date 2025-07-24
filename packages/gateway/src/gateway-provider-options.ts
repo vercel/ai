519ae45503eb896c07b2e68efbe2e7e7c8a17261
R-Taneja <47066511+R-Taneja@users.noreply.github.com>
2025-07-24 09:48:11 -0700

    Markdown nit

--- packages/gateway/src/gateway-provider-options.ts
+++ packages/gateway/src/gateway-provider-options.ts
@@ -5,7 +5,7 @@ export const gatewayProviderOptions = z.object({
   /**
    * Array of provider slugs that specifies the sequence in which providers should be attempted.
    *
-   * Example: ['bedrock', 'anthropic'] will try Amazon Bedrock first, then Anthropic as fallback.
+   * Example: `['bedrock', 'anthropic']` will try Amazon Bedrock first, then Anthropic as fallback.
    */
   order: z.array(z.string()).optional(),
 });
