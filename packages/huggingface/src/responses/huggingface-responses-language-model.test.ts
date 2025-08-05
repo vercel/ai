6ad8a1afe4618535203a89cf75f4b916cd680e85
josh <josh@afterima.ge>
2025-08-05 17:38:03 +0100

    x

--- packages/huggingface/src/responses/huggingface-responses-language-model.test.ts
+++ packages/huggingface/src/responses/huggingface-responses-language-model.test.ts
@@ -16,7 +16,6 @@ function createModel(modelId: string) {
     url: ({ path }) => `https://router.huggingface.co/v1${path}`,
     headers: () => ({ Authorization: `Bearer APIKEY` }),
     generateId: mockId(),
-    
   });
 }
 
