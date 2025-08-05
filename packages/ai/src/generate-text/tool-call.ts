43456260dad865527184bdf6ebb2df49f7e0b682
Lars Grammel <lars.grammel@gmail.com>
2025-08-05 15:15:37 +0200

    1

--- packages/ai/src/generate-text/tool-call.ts
+++ packages/ai/src/generate-text/tool-call.ts
@@ -11,6 +11,7 @@ export type StaticToolCall<TOOLS extends ToolSet> = ValueOf<{
     input: TOOLS[NAME] extends Tool<infer PARAMETERS> ? PARAMETERS : never;
     providerExecuted?: boolean;
     dynamic?: false | undefined;
+    invalid?: false | undefined;
     providerMetadata?: ProviderMetadata;
   };
 }>;
@@ -23,6 +24,14 @@ export type DynamicToolCall = {
   providerExecuted?: boolean;
   dynamic: true;
   providerMetadata?: ProviderMetadata;
+
+  /**
+   * True if this is caused by an unparsable tool call or
+   * a tool that does not exist.
+   */
+  // Added into DynamicToolCall to avoid breaking changes.
+  // TODO AI SDK 6: separate into a new InvalidToolCall type
+  invalid?: boolean;
 };
 
 export type TypedToolCall<TOOLS extends ToolSet> =
