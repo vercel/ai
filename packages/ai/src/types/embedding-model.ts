581926e318788dff50f60a1ca9d342eab69a4eff
Lars Grammel <lars.grammel@gmail.com>
2025-08-07 11:29:44 +0200

    xx

--- packages/ai/src/types/embedding-model.ts
+++ packages/ai/src/types/embedding-model.ts
@@ -3,7 +3,7 @@ import { EmbeddingModelV2, EmbeddingModelV2Embedding } from '@ai-sdk/provider';
 /**
 Embedding model that is used by the AI SDK Core functions.
 */
-export type EmbeddingModel<VALUE> = string | EmbeddingModelV2<VALUE>;
+export type EmbeddingModel<VALUE = string> = string | EmbeddingModelV2<VALUE>;
 
 /**
 Embedding.
