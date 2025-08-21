21b5e721b75e3d884237a4cf7885f45ce6423327
Rajaniraiyn R <72294760+Rajaniraiyn@users.noreply.github.com>
2025-08-21 17:04:11 +0530

    make prompt type bit strict

--- packages/ai/src/prompt/prompt.ts
+++ packages/ai/src/prompt/prompt.ts
@@ -9,18 +9,18 @@ export type Prompt = {
 System message to include in the prompt. Can be used with `prompt` or `messages`.
    */
   system?: string;
-
+} & ({
   /**
 A prompt. It can be either a text prompt or a list of messages.
 
 You can either use `prompt` or `messages` but not both.
 */
   prompt?: string | Array<ModelMessage>;
-
+} | {
   /**
 A list of messages.
 
 You can either use `prompt` or `messages` but not both.
    */
   messages?: Array<ModelMessage>;
-};
+});
