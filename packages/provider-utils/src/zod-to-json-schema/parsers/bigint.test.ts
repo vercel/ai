99bd60a84fa64be4ef1960c17b3f075586afbc32
Lars Grammel <lars.grammel@gmail.com>
2025-08-25 11:50:20 +0200

    t1

---
+++ packages/provider-utils/src/zod-to-json-schema/parsers/bigint.test.ts
@@ -0,0 +1,72 @@
+import { z } from 'zod/v3';
+import { parseBigintDef } from './bigint';
+import { getRefs } from '../refs';
+import { JSONSchema7 } from '@ai-sdk/provider';
+
+describe('bigint', () => {
+  it('should be possible to use bigint', () => {
+    const parsedSchema = parseBigintDef(z.bigint()._def, getRefs());
+
+    expect(parsedSchema).toStrictEqual({
+      type: 'integer',
+      format: 'int64',
+    } satisfies JSONSchema7);
+  });
+
+  it('should be possible to define gt/lt', () => {
+    const parsedSchema = parseBigintDef(
+      z.bigint().gte(BigInt(10)).lte(BigInt(20))._def,
+      getRefs(),
+    );
+
+    expect(parsedSchema).toStrictEqual({
+      type: 'integer',
+      format: 'int64',
+      minimum: BigInt(10) as any, // json schema type does not support bigint
+      maximum: BigInt(20) as any, // json schema type does not support bigint
+    } satisfies JSONSchema7);
+  });
+
+  it('should be possible to define gt/lt (jsonSchema2019-09)', () => {
+    const parsedSchema = parseBigintDef(
+      z.bigint().gte(BigInt(10)).lte(BigInt(20))._def,
+      getRefs({
+        target: 'jsonSchema2019-09',
+      }),
+    );
+
+    expect(parsedSchema).toStrictEqual({
+      type: 'integer',
+      format: 'int64',
+      minimum: BigInt(10) as any, // json schema type does not support bigint
+      maximum: BigInt(20) as any, // json schema type does not support bigint
+    } satisfies JSONSchema7);
+  });
+
+  it('should be possible to define gt/lt', () => {
+    const parsedSchema = parseBigintDef(
+      z.bigint().gt(BigInt(10)).lt(BigInt(20))._def,
+      getRefs(),
+    );
+
+    expect(parsedSchema).toStrictEqual({
+      type: 'integer',
+      format: 'int64',
+      exclusiveMinimum: BigInt(10) as any, // json schema type does not support bigint
+      exclusiveMaximum: BigInt(20) as any, // json schema type does not support bigint
+    } satisfies JSONSchema7);
+  });
+
+  it('should be possible to define multipleOf', () => {
+    const parsedSchema = parseBigintDef(
+      z.bigint().multipleOf(BigInt(5))._def,
+      getRefs(),
+    );
+
+    expect(parsedSchema).toStrictEqual({
+      type: 'integer',
+      format: 'int64',
+      multipleOf: BigInt(5) as any, // json schema type does not support bigint
+    } satisfies JSONSchema7);
+  });
+});
