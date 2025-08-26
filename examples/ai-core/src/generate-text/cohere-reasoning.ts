13ac81da66886f8080c9f1bcf6ddfb5bf6f6dcae
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2025-08-25 20:44:26 -0700

    fixup! docs: `generatText()` example

--- examples/ai-core/src/generate-text/cohere-reasoning.ts
+++ examples/ai-core/src/generate-text/cohere-reasoning.ts
@@ -1,4 +1,4 @@
-import { cohere } from '@ai-sdk/cohere';
+import { cohere, type CohereChatModelOptions } from '@ai-sdk/cohere';
 import { generateText } from 'ai';
 import 'dotenv/config';
 
@@ -7,6 +7,15 @@ async function main() {
     model: cohere('command-a-reasoning-08-2025'),
     prompt:
       "Alice has 3 brothers and she also has 2 sisters. How many sisters does Alice's brother have?",
+    // optional
+    providerOptions: {
+      cohere: {
+        thinking: {
+          type: 'enabled',
+          tokenBudget: 1000,
+        },
+      } satisfies CohereChatModelOptions,
+    }
   });
 
   console.log(JSON.stringify(result.request.body, null, 2));
