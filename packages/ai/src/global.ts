f0687da2d6865f8170d4ffbc461956812e9d6b25
Lars Grammel <lars.grammel@gmail.com>
2025-08-28 11:16:49 +0200

    2

--- packages/ai/src/global.ts
+++ packages/ai/src/global.ts
@@ -1,6 +1,22 @@
 import { ProviderV2 } from '@ai-sdk/provider';
+import { LogWarningsFunction } from './logger/log-warnings';
 
-// add type of the global default provider variable to the globalThis object
+// add AI SDK default provider to the globalThis object
 declare global {
+  /**
+   * The default provider to use for the AI SDK.
+   * String model ids are resolved to the default provider and model id.
+   *
+   * If not set, the default provider is the Vercel AI gateway provider.
+   *
+   * @see https://ai-sdk.dev/docs/ai-sdk-core/provider-management#global-provider-configuration
+   */
   var AI_SDK_DEFAULT_PROVIDER: ProviderV2 | undefined;
+
+  /**
+   * The warning logger to use for the AI SDK.
+   *
+   * If not set, the default logger is the console.warn function.
+   */
+  var AI_SDK_LOG_WARNINGS: LogWarningsFunction | undefined;
 }
