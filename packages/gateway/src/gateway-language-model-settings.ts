350a328b2651a4a8cd09e900660685dab08fb977
Rohan Taneja <47066511+R-Taneja@users.noreply.github.com>
2025-09-08 19:23:39 -0400

    feat(provider/gateway): Add stealth models to Gateway autocomplete (#8517)
    ## Background
    
    New stealth models
    
    ## Summary
    
    Adds them to Gateway autocomplete
    
    ## Manual Verification
    
    Autocomplete works
    
    ## Tasks
    
    <!--
    This task list is intended to help you keep track of what you need to
    do.
    Feel free to add tasks and remove unnecessary tasks or this section as
    needed.
    
    Please check if the PR fulfills the following requirements:
    -->
    
    - [X] A _patch_ changeset for relevant packages has been added (for bug
    fixes / features - run `pnpm changeset` in the project root)
    - [X] Formatting issues have been fixed (run `pnpm prettier-fix` in the
    project root)
    - [X] I have reviewed this pull request (self-review)

--- packages/gateway/src/gateway-language-model-settings.ts
+++ packages/gateway/src/gateway-language-model-settings.ts
@@ -82,6 +82,8 @@ export type GatewayModelId =
   | 'perplexity/sonar-pro'
   | 'perplexity/sonar-reasoning'
   | 'perplexity/sonar-reasoning-pro'
+  | 'stealth/sonoma-dusk-alpha'
+  | 'stealth/sonoma-sky-alpha'
   | 'vercel/v0-1.0-md'
   | 'vercel/v0-1.5-md'
   | 'xai/grok-2'
