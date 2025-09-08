5804a79ba413d8b23dc0620243fd79cb6025b861
Lars Grammel <lars.grammel@gmail.com>
2025-09-08 13:53:08 +0200

    3

--- packages/openai/src/openai-tools.ts
+++ packages/openai/src/openai-tools.ts
@@ -29,6 +29,28 @@ export const openaiTools = {
    */
   fileSearch,
 
+  /**
+   * Web search allows models to access up-to-date information from the internet
+   * and provide answers with sourced citations.
+   *
+   * Must have name `web_search_preview`.
+   *
+   * @param searchContextSize - The search context size to use for the web search.
+   * @param userLocation - The user location to use for the web search.
+   *
+   * @deprecated Use `webSearch` instead.
+   */
   webSearchPreview,
+
+  /**
+   * Web search allows models to access up-to-date information from the internet
+   * and provide answers with sourced citations.
+   *
+   * Must have name `web_search`.
+   *
+   * @param filters - The filters to use for the web search.
+   * @param searchContextSize - The search context size to use for the web search.
+   * @param userLocation - The user location to use for the web search.
+   */
   webSearch,
 };
