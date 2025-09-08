9fe1f52101cc4ae371faabd7178b8bef75e20eb0
Lars Grammel <lars.grammel@gmail.com>
2025-09-08 13:49:06 +0200

    2

--- packages/openai/src/openai-tools.ts
+++ packages/openai/src/openai-tools.ts
@@ -4,8 +4,31 @@ import { webSearch } from './tool/web-search';
 import { webSearchPreview } from './tool/web-search-preview';
 
 export const openaiTools = {
+  /**
+   * The Code Interpreter tool allows models to write and run Python code in a
+   * sandboxed environment to solve complex problems in domains like data analysis,
+   * coding, and math.
+   *
+   * @param container - The container to use for the code interpreter.
+   *
+   * Must have name `code_interpreter`.
+   */
   codeInterpreter,
+
+  /**
+   * File search is a tool available in the Responses API. It enables models to
+   * retrieve information in a knowledge base of previously uploaded files through
+   * semantic and keyword search.
+   *
+   * Must have name `file_search`.
+   *
+   * @param vectorStoreIds - The vector store IDs to use for the file search.
+   * @param maxNumResults - The maximum number of results to return.
+   * @param ranking - The ranking options to use for the file search.
+   * @param filters - The filters to use for the file search.
+   */
   fileSearch,
+
   webSearchPreview,
   webSearch,
 };
