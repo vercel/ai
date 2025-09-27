f6ce532214c255bc7ccc5eb5601b9ee43f137f5d
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2025-09-27 16:09:31 -0700

    revert no longer needed changes to packages/gateway/tsup.config.ts

--- packages/gateway/tsup.config.ts
+++ packages/gateway/tsup.config.ts
@@ -1,34 +1,16 @@
-import { defineConfig, type Options } from 'tsup';
-
-const commonConfig: Options = {
-  entry: ['src/index.ts'],
-  format: ['cjs', 'esm'],
-  dts: true,
-  sourcemap: true,
-  define: {
-    __PACKAGE_VERSION__: JSON.stringify(
-      (await import('./package.json', { with: { type: 'json' } })).default
-        .version,
-    ),
-  },
-};
+import { defineConfig } from 'tsup';
 
 export default defineConfig([
   {
-    ...commonConfig,
-    outDir: 'dist/node',
-    esbuildOptions(options) {
-      // for some reason, using just "node" does not work
-      // When using `"node"` as key in the `imports` field of package.json,
-      // it gets used by `default` below, too. ¯\_(ツ)_/¯
-      options.conditions = ['just-node-workaround'];
-    },
-  },
-  {
-    ...commonConfig,
-    outDir: 'dist/default',
-    esbuildOptions(options) {
-      options.conditions = ['default'];
+    entry: ['src/index.ts'],
+    format: ['cjs', 'esm'],
+    dts: true,
+    sourcemap: true,
+    define: {
+      __PACKAGE_VERSION__: JSON.stringify(
+        (await import('./package.json', { with: { type: 'json' } })).default
+          .version,
+      ),
     },
   },
 ]);
