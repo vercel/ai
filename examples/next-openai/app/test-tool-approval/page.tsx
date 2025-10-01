aa31bb9db5e59fd6c1beea701fd19a7b8e2e4752
Lars Grammel <lars.grammel@gmail.com>
2025-10-01 09:53:37 +0200

    p

---
+++ examples/next-openai/app/test-tool-approval/page.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { useChat } from '@ai-sdk/react';
+import { DefaultChatTransport } from 'ai';
+import ChatInput from '@/component/chat-input';
+import { WeatherWithApprovalAgentUIMessage } from '@/agent/weather-with-approval-agent';
+import WeatherWithApprovalView from '@/component/weather-with-approval-view';
+
+export default function TestToolApproval() {
+  const { status, sendMessage, messages } =
+    useChat<WeatherWithApprovalAgentUIMessage>({
+      transport: new DefaultChatTransport({ api: '/api/chat-tool-approval' }),
+    });
+
+  return (
+    <div className="flex flex-col py-24 mx-auto w-full max-w-md stretch">
+      <h1 className="mb-4 text-xl font-bold">Tool Approval Test</h1>
+
+      {messages.map(message => (
+        <div key={message.id} className="whitespace-pre-wrap">
+          {message.role === 'user' ? 'User: ' : 'AI: '}
+          {message.parts.map((part, index) => {
+            switch (part.type) {
+              case 'text':
+                return <div key={index}>{part.text}</div>;
+              case 'tool-weather':
+                return (
+                  <WeatherWithApprovalView
+                    key={index}
+                    invocation={part}
+                    sendApprovalResponse={() => {
+                      console.log('sendApprovalResponse');
+                    }}
+                  />
+                );
+            }
+          })}
+        </div>
+      ))}
+
+      <ChatInput status={status} onSubmit={text => sendMessage({ text })} />
+    </div>
+  );
+}
