b3ece297fd947617a77f53e756958979bb4acadf
Lars Grammel <lars.grammel@gmail.com>
2025-10-01 14:33:49 +0200

    p

--- examples/next-openai/agent/weather-with-approval-agent.ts
+++ examples/next-openai/agent/weather-with-approval-agent.ts
@@ -4,6 +4,11 @@ import { Agent, InferAgentUIMessage } from 'ai';
 
 export const weatherWithApprovalAgent = new Agent({
   model: openai('gpt-5-mini'),
+  // context engineering required to make sure the model does not retry
+  // the tool execution if it is not approved:
+  system:
+    'When a tool execution is not approved by the user, do not retry it.' +
+    'Just say that the tool execution was not approved.',
   tools: {
     weather: weatherToolWithApproval,
   },
