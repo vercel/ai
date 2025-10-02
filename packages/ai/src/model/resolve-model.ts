286b37e126e866fc79b2b03139389a8066380476
Aayush Kapoor <aayushkapoor34@gmail.com>
2025-10-02 14:23:22 -0400

    fix

--- packages/ai/src/model/resolve-model.ts
+++ packages/ai/src/model/resolve-model.ts
@@ -11,31 +11,23 @@ import { EmbeddingModel } from '../types/embedding-model';
 import { LanguageModel } from '../types/language-model';
 
 function transformToV3LanguageModel(model: LanguageModelV2): LanguageModelV3 {
-  // return new Proxy(model, {
-  //   get(target, prop: keyof LanguageModelV2) {
-  //     if (prop === 'specificationVersion') return 'v3';
-  //     return target[prop];
-  //   },
-  // }) as unknown as LanguageModelV3;
-  return {
-    ...model,
-    specificationVersion: 'v3',
-  };
+  return new Proxy(model, {
+    get(target, prop: keyof LanguageModelV2) {
+      if (prop === 'specificationVersion') return 'v3';
+      return target[prop];
+    },
+  }) as unknown as LanguageModelV3;
 }
 
 function transformToV3EmbeddingModel<VALUE>(
   model: EmbeddingModelV2<VALUE>,
 ): EmbeddingModelV3<VALUE> {
-  // return new Proxy(model, {
-  //   get(target, prop: keyof EmbeddingModelV2<VALUE>) {
-  //     if (prop === 'specificationVersion') return 'v3';
-  //     return target[prop];
-  //   },
-  // }) as unknown as EmbeddingModelV3<VALUE>;
-  return {
-    ...model,
-    specificationVersion: 'v3',
-  };
+  return new Proxy(model, {
+    get(target, prop: keyof EmbeddingModelV2<VALUE>) {
+      if (prop === 'specificationVersion') return 'v3';
+      return target[prop];
+    },
+  }) as unknown as EmbeddingModelV3<VALUE>;
 }
 
 export function resolveLanguageModel(model: LanguageModel): LanguageModelV3 {
