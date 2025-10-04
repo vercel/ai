e20fb016756fc2982e5ddb5ed46668c7caba8d6e
tsuzaki430 <tsuzaki430@users.noreply.github.com>
2025-10-04 22:51:19 +0900

    add test assistant- prefix file id

---
+++ examples/ai-core/src/stream-text/azure-responses-file-id.ts
@@ -0,0 +1,46 @@
+import { azure } from '@ai-sdk/azure';
+import { streamText } from 'ai';
+import 'dotenv/config';
+
+/**
+ * prepare 1
+ * Please add parameters in your .env file for initialize Azure OpenAI.
+ * AZURE_RESOURCE_NAME="<your_resource_name>"
+ * AZURE_API_KEY="<your_api_key>"
+ *
+ * prepare 2
+ * Please create vector store and put file in your vector.
+ * URL:AOAI vector store portal
+ * https://oai.azure.com/resource/datafile
+ */
+
+const fileId = 'assistant-xxxxxxxxxxxxxxxxxxxxxx'; // put your vector store id.
+
+async function main() {
+  const result = streamText({
+    model: azure.responses('gpt-4.1-mini'), // please question about your documents.
+    messages: [
+      {
+        role: 'user',
+        content: [
+          {
+            type: 'text',
+            text: 'Plese give me the short summary in the document.',
+          },
+          {
+            type: 'file',
+            data: fileId,
+            mediaType: 'application/pdf',
+            // filename: 'ai.pdf',
+          },
+        ],
+      },
+    ],
+  });
+
+  for await (const textPart of result.textStream) {
+    process.stdout.write(textPart);
+  }
+}
+
+main().catch(console.error);
