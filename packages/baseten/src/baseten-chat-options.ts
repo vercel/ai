f68af26b34e7ef249e4e844bb3c3791769d4b330
AlexKer <AlexKer@users.noreply.github.com>
2025-10-07 16:47:40 -0400

    ordering

--- packages/baseten/src/baseten-chat-options.ts
+++ packages/baseten/src/baseten-chat-options.ts
@@ -2,9 +2,9 @@
 // Below is the current list of models supported by Baseten model APIs.
 // Ohter dedicated models are also supported, but not listed here.
 export type BasetenChatModelId =
-  | 'deepseek-ai/DeepSeek-V3.1'
   | 'deepseek-ai/DeepSeek-R1-0528'
   | 'deepseek-ai/DeepSeek-V3-0324'
+  | 'deepseek-ai/DeepSeek-V3.1'
   | 'moonshotai/Kimi-K2-Instruct-0905'
   | 'Qwen/Qwen3-235B-A22B-Instruct-2507'
   | 'Qwen/Qwen3-Coder-480B-A35B-Instruct'
