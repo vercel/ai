2b5d6610541d117ccf7ca5d0643baa298aa0cfee
Lars Grammel <lars.grammel@gmail.com>
2025-10-08 11:44:21 +0200

    2

--- packages/provider-utils/src/parse-provider-options.ts
+++ packages/provider-utils/src/parse-provider-options.ts
@@ -1,21 +1,26 @@
 import { InvalidArgumentError } from '@ai-sdk/provider';
+import type { StandardSchemaV1 } from '@standard-schema/spec';
+import { LazyValidator } from './lazy-validator';
 import { safeValidateTypes } from './validate-types';
-import { z } from 'zod/v4';
+import { Validator } from './validator';
 
-export async function parseProviderOptions<T>({
+export async function parseProviderOptions<OPTIONS>({
   provider,
   providerOptions,
   schema,
 }: {
   provider: string;
   providerOptions: Record<string, unknown> | undefined;
-  schema: z.core.$ZodType<T, any>;
-}): Promise<T | undefined> {
+  schema:
+    | StandardSchemaV1<unknown, OPTIONS>
+    | Validator<OPTIONS>
+    | LazyValidator<OPTIONS>;
+}): Promise<OPTIONS | undefined> {
   if (providerOptions?.[provider] == null) {
     return undefined;
   }
 
-  const parsedProviderOptions = await safeValidateTypes<T | undefined>({
+  const parsedProviderOptions = await safeValidateTypes<OPTIONS | undefined>({
     value: providerOptions[provider],
     schema,
   });
