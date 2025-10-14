50659920bc853c61579d219274cba2fac79c55a1
Lars Grammel <lars.grammel@gmail.com>
2025-10-14 10:24:58 +0200

    tool spec1

--- packages/anthropic/src/tool/code-execution_20250825.ts
+++ packages/anthropic/src/tool/code-execution_20250825.ts
@@ -7,37 +7,89 @@ import { z } from 'zod/v4';
 
 export const codeExecution_20250825OutputSchema = lazySchema(() =>
   zodSchema(
-    z.object({
-      type: z.literal('code_execution_result'),
-      stdout: z.string(),
-      stderr: z.string(),
-      return_code: z.number(),
-    }),
+    z.discriminatedUnion('type', [
+      z.object({
+        type: z.literal('bash_code_execution_tool_result'),
+        content: z.object({
+          type: z.literal('bash_code_execution_output'),
+          fileId: z.string(),
+        }),
+        stdout: z.string(),
+        stderr: z.string(),
+        returnCode: z.number(),
+      }),
+      z.object({
+        type: z.literal('bash_code_execution_tool_result_error'),
+        errorCode: z.string(),
+      }),
+      z.object({
+        type: z.literal('text_editor_code_execution_result'),
+        // TODO additional fields
+      }),
+    ]),
   ),
 );
 
 const codeExecution_20250825InputSchema = lazySchema(() =>
   zodSchema(
-    z.object({
-      code: z.string(),
-    }),
+    z.discriminatedUnion('type', [
+      z.object({
+        type: z.literal('bash_code_execution'),
+        command: z.string(),
+      }),
+      z.object({
+        type: z.literal('text_editor_code_execution'),
+        // TODO what is supported here?
+      }),
+    ]),
   ),
 );
 
 const factory = createProviderDefinedToolFactoryWithOutputSchema<
+  | {
+      type: 'bash_code_execution';
+
+      /**
+       * Shell command to execute.
+       */
+      command: string;
+    }
+  | {
+      type: 'text_editor_code_execution';
+    },
+  | {
+      type: 'bash_code_execution_tool_result';
+
+      /**
+       * Output from successful execution
+       */
+      stdout: string;
+
+      /**
+       * Error messages if execution fails
+       */
+      stderr: string;
+
+      /**
+       * 0 for success, non-zero for failure
+       */
+      returnCode: number;
+    }
+  | {
+      type: 'bash_code_execution_tool_result_error';
+
+      /**
+       * Error code
+       */
+      errorCode: string;
+    }
+  | {
+      type: 'text_editor_code_execution_result';
+      // TODO
+    },
   {
-    /**
-     * The code to execute. Supports both Python and Bash commands.
-     */
-    code: string;
-  },
-  {
-    type: 'code_execution_result';
-    stdout: string;
-    stderr: string;
-    return_code: number;
-  },
-  {}
+    // no arguments
+  }
 >({
   id: 'anthropic.code_execution_20250825',
   name: 'code_execution',
