35613621cc93809b1ea2870451a5fb200daff304
Lars Grammel <lars.grammel@gmail.com>
2025-10-14 13:30:28 +0200

    example

--- examples/next-openai/components/tool/anthropic-code-execution-view.tsx
+++ examples/next-openai/components/tool/anthropic-code-execution-view.tsx
@@ -10,25 +10,116 @@ export default function AnthropicCodeExecutionView({
 }) {
   switch (invocation.state) {
     case 'input-streaming':
-    case 'input-available':
-      return (
-        <div className="mb-2 bg-gray-900 rounded-xl border border-gray-600 shadow-lg">
-          Executing the following:
-          <pre>{JSON.stringify(invocation.input, null, 2)}</pre>
-        </div>
-      );
+    case 'input-available': {
+      return <InputView input={invocation.input} />;
+    }
     case 'output-available':
+      console.log(invocation.output);
       return (
         <>
-          <div className="mb-2 bg-gray-900 rounded-xl border border-gray-600 shadow-lg">
-            Executing the following:
-            <pre>{JSON.stringify(invocation.input, null, 2)}</pre>
-          </div>
-          <div className="mb-2 bg-gray-900 rounded-xl border border-gray-600 shadow-lg">
-            Output:
-            <pre>{JSON.stringify(invocation.output, null, 2)}</pre>
+          <InputView input={invocation.input} />
+
+          <div className="mb-2 bg-gray-600 rounded-xl border border-gray-900 shadow-lg">
+            <pre className="overflow-x-auto p-4 text-sm text-gray-100 whitespace-pre-wrap">
+              {invocation.output.type === 'bash_code_execution_result' && (
+                <>
+                  <span className="font-semibold">Stdout:</span>
+                  <br />
+                  {invocation.output.stdout}
+                  <br />
+                  {invocation.output.stderr && (
+                    <>
+                      <span className="font-semibold">Stderr:</span>
+                      <br />
+                      {invocation.output.stderr}
+                      <br />
+                    </>
+                  )}
+                  {invocation.output.return_code != null && (
+                    <>
+                      <span className="font-semibold">Return Code:</span>
+                      <br />
+                      {invocation.output.return_code}
+                      <br />
+                    </>
+                  )}
+                </>
+              )}
+              {invocation.output.type ===
+                'text_editor_code_execution_create_result' && (
+                <>
+                  <span className="font-semibold">File Create Result</span>
+                  <br />
+                  <span className="font-semibold">Is File Update:</span>{' '}
+                  {invocation.output.is_file_update ? 'Yes' : 'No'}
+                  <br />
+                </>
+              )}
+            </pre>
           </div>
         </>
       );
+  }
+}
+
+function InputView({
+  input,
+}: {
+  input: UIToolInvocation<
+    ReturnType<typeof anthropic.tools.codeExecution_20250825>
+  >['input'];
+}) {
+  switch (input?.type) {
+    case 'text_editor_code_execution': {
+      const command = input.command;
+      const path = input.path;
+      const file_text = input.file_text;
+
+      return (
+        <div className="mb-2 text-gray-100 bg-gray-600 rounded-xl border border-gray-900 shadow-lg">
+          <pre className="overflow-x-auto p-4 text-sm text-gray-100 whitespace-pre-wrap">
+            <span className="font-semibold">Text Editor</span>
+            <br />
+            {command && (
+              <>
+                <span className="font-semibold">Command:</span> {command}
+                <br />
+              </>
+            )}
+            {path && (
+              <>
+                <span className="font-semibold">File Path:</span> {path}
+                <br />
+              </>
+            )}
+            {file_text && (
+              <>
+                <span className="font-semibold">File Text:</span> {file_text}
+                <br />
+              </>
+            )}
+          </pre>
+        </div>
+      );
+    }
+
+    case 'bash_code_execution': {
+      const command = input.command;
+
+      return (
+        <div className="mb-2 bg-gray-600 rounded-xl border border-gray-900 shadow-lg">
+          <pre className="overflow-x-auto p-4 text-sm text-gray-100 whitespace-pre-wrap">
+            <span className="font-semibold">Bash</span>
+            <br />
+            {command && (
+              <>
+                <span className="font-semibold">Command:</span> {command}
+                <br />
+              </>
+            )}
+          </pre>
+        </div>
+      );
+    }
   }
 }
