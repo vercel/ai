442c0c59094d62a51d3611ea8d6d50023ea190de
Lars Grammel <lars.grammel@gmail.com>
2025-10-14 15:48:46 +0200

    view

--- examples/next-openai/components/tool/anthropic-code-execution-view.tsx
+++ examples/next-openai/components/tool/anthropic-code-execution-view.tsx
@@ -45,12 +45,71 @@ export default function AnthropicCodeExecutionView({
                 </>
               )}
               {invocation.output.type ===
+                'bash_code_execution_tool_result_error' && (
+                <>
+                  <span className="font-semibold">Bash Tool Result Error</span>
+                  <br />
+                  <span className="font-semibold">Error Code:</span>{' '}
+                  {invocation.output.error_code}
+                  <br />
+                </>
+              )}
+              {invocation.output.type ===
                 'text_editor_code_execution_create_result' && (
                 <>
                   <span className="font-semibold">File Create Result</span>
                   <br />
                   <span className="font-semibold">Is File Update:</span>{' '}
                   {invocation.output.is_file_update ? 'Yes' : 'No'}
+                  <br />
+                </>
+              )}
+              {invocation.output.type ===
+                'text_editor_code_execution_view_result' && (
+                <>
+                  <span className="font-semibold">File View Result</span>
+                  <br />
+                  <span className="font-semibold">File Type:</span>{' '}
+                  {invocation.output.file_type}
+                  <br />
+                  <span className="font-semibold">Content:</span>
+                  <br />
+                  {invocation.output.content}
+                  <br />
+                </>
+              )}
+              {invocation.output.type ===
+                'text_editor_code_execution_str_replace_result' && (
+                <>
+                  <span className="font-semibold">File Str Replace Result</span>
+                  <br />
+                  <span className="font-semibold">New Start:</span>{' '}
+                  {invocation.output.new_start}
+                  <br />
+                  <span className="font-semibold">New Lines:</span>{' '}
+                  {invocation.output.new_lines}
+                  <br />
+                  <span className="font-semibold">Old Start:</span>{' '}
+                  {invocation.output.old_start}
+                  <br />
+                  <span className="font-semibold">Old Lines:</span>{' '}
+                  {invocation.output.old_lines}
+                  <br />
+                  <span className="font-semibold">Lines:</span>
+                  <br />
+                  {invocation.output.lines?.join('\n')}
+                  <br />
+                </>
+              )}
+              {invocation.output.type ===
+                'text_editor_code_execution_tool_result_error' && (
+                <>
+                  <span className="font-semibold">
+                    Text Editor Tool Result Error
+                  </span>
+                  <br />
+                  <span className="font-semibold">Error Code:</span>{' '}
+                  {invocation.output.error_code}
                   <br />
                 </>
               )}
@@ -76,6 +135,7 @@ function InputView({
             <div className="mb-2 text-gray-100 bg-gray-600 rounded-xl border border-gray-900 shadow-lg">
               <pre className="overflow-x-auto p-4 text-sm text-gray-100 whitespace-pre-wrap">
                 <span className="font-semibold">Text Editor (View)</span>
+                <br />
                 {input.path && (
                   <>
                     <span className="font-semibold">File Path:</span>{' '}
@@ -92,6 +152,7 @@ function InputView({
             <div className="mb-2 text-gray-100 bg-gray-600 rounded-xl border border-gray-900 shadow-lg">
               <pre className="overflow-x-auto p-4 text-sm text-gray-100 whitespace-pre-wrap">
                 <span className="font-semibold">Text Editor (Create)</span>
+                <br />
                 {input.path && (
                   <>
                     <span className="font-semibold">File Path:</span>{' '}
@@ -115,6 +176,7 @@ function InputView({
             <div className="mb-2 text-gray-100 bg-gray-600 rounded-xl border border-gray-900 shadow-lg">
               <pre className="overflow-x-auto p-4 text-sm text-gray-100 whitespace-pre-wrap">
                 <span className="font-semibold">Text Editor (Replace)</span>
+                <br />
                 {input.path && (
                   <>
                     <span className="font-semibold">File Path:</span>{' '}
@@ -124,14 +186,16 @@ function InputView({
                 )}
                 {input.old_str && (
                   <>
-                    <span className="font-semibold">Old String:</span>{' '}
+                    <span className="font-semibold">Old String:</span>
+                    <br />
                     {input.old_str}
                     <br />
                   </>
                 )}
                 {input.new_str && (
                   <>
-                    <span className="font-semibold">New String:</span>{' '}
+                    <span className="font-semibold">New String:</span>
+                    <br />
                     {input.new_str}
                     <br />
                   </>
