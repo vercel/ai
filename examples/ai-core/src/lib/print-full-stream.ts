103cb9984290e4f3fbb181dbe0326a924f922a39
Lars Grammel <lars.grammel@gmail.com>
2025-10-20 13:27:25 +0200

    printstream

---
+++ examples/ai-core/src/lib/print-full-stream.ts
@@ -0,0 +1,34 @@
+import { StreamTextResult } from 'ai';
+
+export async function printFullStream({
+  result,
+}: {
+  result: StreamTextResult<any, any>;
+}) {
+  for await (const chunk of result.fullStream) {
+    switch (chunk.type) {
+      case 'text-delta': {
+        process.stdout.write(chunk.text);
+        break;
+      }
+
+      case 'tool-call': {
+        console.log(
+          `\x1b[32m\x1b[1mTool call:\x1b[22m ${JSON.stringify(chunk, null, 2)}\x1b[0m`,
+        );
+        break;
+      }
+
+      case 'tool-result': {
+        console.log(
+          `\x1b[32m\x1b[1mTool result:\x1b[22m ${JSON.stringify(chunk, null, 2)}\x1b[0m`,
+        );
+        break;
+      }
+
+      case 'error':
+        console.error('Error:', chunk.error);
+        break;
+    }
+  }
+}
