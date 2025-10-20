482dc69b88896501cb77219c88702914a198d1b1
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2025-10-20 13:45:31 -0700

    update lint

--- examples/nest/.eslintrc.cjs
+++ examples/nest/.eslintrc.cjs
@@ -15,7 +15,7 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.cjs'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
