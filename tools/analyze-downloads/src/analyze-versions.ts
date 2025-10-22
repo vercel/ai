38b8ec7fb10907243eaa61e04348c51e9fac47c6
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2025-10-22 11:00:36 -0700

    build: add `%` to `analyze-versions` script

--- tools/analyze-downloads/src/analyze-versions.ts
+++ tools/analyze-downloads/src/analyze-versions.ts
@@ -5,11 +5,11 @@
  * to its "major.minor" or "major.minor (channel)" part, e.g. "1.2" or "1.2 (alpha)".
  */
 function toMinorVersion(semanticVersion: string): string {
-  const [versionPart, channelPart] = semanticVersion.split("-");
-  const [major, minor] = versionPart.split(".");
+  const [versionPart, channelPart] = semanticVersion.split('-');
+  const [major, minor] = versionPart.split('.');
   let minorVersion = `${major}.${minor}`;
   if (channelPart) {
-    const [channel] = channelPart.split(".");
+    const [channel] = channelPart.split('.');
     minorVersion += ` (${channel})`;
   }
   return minorVersion;
@@ -25,10 +25,10 @@ function aggregateByMinor(
   for (const [versionString, downloads] of Object.entries(data)) {
     const minor = toMinorVersion(versionString);
     // ignore versions < 1.0
-    if (minor.startsWith("0.")) continue;
+    if (minor.startsWith('0.')) continue;
 
     // ignore versions that have channel but where channel is not beta
-    if (minor.includes("(") && !minor.includes("(beta)")) continue;
+    if (minor.includes('(') && !minor.includes('(beta)')) continue;
 
     output[minor] = (output[minor] || 0) + downloads;
   }
@@ -41,18 +41,33 @@ function aggregateByMinor(
 async function main() {
   const response = await fetch(`https://api.npmjs.org/versions/ai/last-week`);
   const { downloads } = await response.json();
+  const totalDownloads = Object.values(downloads).reduce(
+    (acc, curr) => acc + curr,
+    0,
+  );
+  console.log(`Total weekly downloads: ${totalDownloads.toLocaleString()}`);
+  console.log(
+    `For simplicity, we remove versions < 1.0 and non-beta channels from the table below.`,
+  );
 
   const aggregated = aggregateByMinor(downloads);
-  console.table(Object.entries(aggregated)
-    // sort by version string
-    .sort(([a], [b]) => {
-      // sort 5.0 beta after 5.0
-      if (a === b.replace(" (beta)", "")) return -1;
-      if (b === a.replace(" (beta)", "")) return 1;
-      return b.localeCompare(a)
-    })
-    // map to objects for better console.table formatting
-    .map(([version, count]) => ({ version, count })));
+  console.table(
+    Object.entries(aggregated)
+      // sort by version string
+      .sort(([a], [b]) => {
+        // sort 5.0 beta after 5.0
+        if (a === b.replace(' (beta)', '')) return -1;
+        if (b === a.replace(' (beta)', '')) return 1;
+        return b.localeCompare(a);
+      })
+      // map to objects for better console.table formatting
+      .map(([version, count]) => {
+        return { 
+          version,
+          count,
+          '%': ((count / totalDownloads) * 100).toFixed(2) + '%' };
+      }),
+  );
 }
 
 main();
