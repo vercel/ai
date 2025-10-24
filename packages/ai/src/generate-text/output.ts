1a09eb0666eb3c0b23d8a6a226658111e3177471
Lars Grammel <lars.grammel@gmail.com>
2025-10-24 10:36:37 +0200

    sep

--- packages/ai/src/generate-text/output.ts
+++ packages/ai/src/generate-text/output.ts
@@ -175,9 +175,3 @@ export const array = <ELEMENT>({
     },
   };
 };
-
-export type InferGenerateOutput<OUTPUT extends Output> =
-  OUTPUT extends Output<infer T, any> ? T : never;
-
-export type InferStreamOutput<OUTPUT extends Output> =
-  OUTPUT extends Output<any, infer P> ? P : never;
