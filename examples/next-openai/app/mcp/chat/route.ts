0494c26442937903c5773299b5abaf6d0c08a6e4
Aayush Kapoor <83492835+aayush-kapoor@users.noreply.github.com>
2025-10-24 09:42:35 -0700

    feat(ai): add OAuth for MCP clients + refactor to new package (#9127)
    ## Background
    
    Refactor mcp into it's own separate package + add OAuth for MCP clients
    
    ## Summary
    
    Created new package `@ai-sdk/mcp`, added OAuth provider
    
    ## Manual Verification
    
    Manual tests added, e2e example added to verify oauth works in
    `example/mcp/mcp-with-auth/client.ts`
    
    ## Checklist
    
    - [x] Tests have been added / updated (for bug fixes / features)
    - [x] Documentation has been added / updated (for bug fixes / features)
    - [x] A _patch_ changeset for relevant packages has been added (for bug
    fixes / features - run `pnpm changeset` in the project root)
    - [x] Formatting issues have been fixed (run `pnpm prettier-fix` in the
    project root)
    
    ## Future Work
    
    - Continue refactoring, add docs
    - log warning or throw error if wrong transport protocol is set for an
    mcp server
    
    ## Related Issues
    
    towards #8717, #9337 and #6717
    
    ---------
    
    Co-authored-by: Gregor Martynus <39992+gr2m@users.noreply.github.com>

--- examples/next-openai/app/mcp/chat/route.ts
+++ examples/next-openai/app/mcp/chat/route.ts
@@ -1,11 +1,7 @@
 import { openai } from '@ai-sdk/openai';
 import { StreamableHTTPClientTransport } from '@modelcontextprotocol/sdk/client/streamableHttp.js';
-import {
-  convertToModelMessages,
-  experimental_createMCPClient,
-  stepCountIs,
-  streamText,
-} from 'ai';
+import { convertToModelMessages, stepCountIs, streamText } from 'ai';
+import { experimental_createMCPClient } from '@ai-sdk/mcp';
 
 export async function POST(req: Request) {
   const requestUrl = new URL(req.url);
