f661f74229162f2c4b9df84c7ae12c04ce62f57b
Lars Grammel <lars.grammel@gmail.com>
2025-10-24 09:53:19 +0200

    t1

--- packages/ai/src/generate-text/output.test.ts
+++ packages/ai/src/generate-text/output.test.ts
@@ -22,42 +22,44 @@ const context = {
 };
 
 describe('Output.object', () => {
-  const output = object({ schema: z.object({ content: z.string() }) });
+  const output1 = object({ schema: z.object({ content: z.string() }) });
 
-  it('should parse the output of the model', async () => {
-    const result = await output.parseOutput(
-      { text: `{ "content": "test" }` },
-      context,
-    );
+  describe('parseOutput', () => {
+    it('should parse the output of the model', async () => {
+      const result = await output1.parseOutput(
+        { text: `{ "content": "test" }` },
+        context,
+      );
 
-    expect(result).toStrictEqual({ content: 'test' });
-  });
+      expect(result).toStrictEqual({ content: 'test' });
+    });
 
-  it('should throw NoObjectGeneratedError when parsing fails', async () => {
-    try {
-      await output.parseOutput({ text: '{ broken json' }, context);
-      fail('must throw error');
-    } catch (error) {
-      verifyNoObjectGeneratedError(error, {
-        message: 'No object generated: could not parse the response.',
-        response: context.response,
-        usage: context.usage,
-        finishReason: context.finishReason,
-      });
-    }
-  });
+    it('should throw NoObjectGeneratedError when parsing fails', async () => {
+      try {
+        await output1.parseOutput({ text: '{ broken json' }, context);
+        fail('must throw error');
+      } catch (error) {
+        verifyNoObjectGeneratedError(error, {
+          message: 'No object generated: could not parse the response.',
+          response: context.response,
+          usage: context.usage,
+          finishReason: context.finishReason,
+        });
+      }
+    });
 
-  it('should throw NoObjectGeneratedError when schema validation fails', async () => {
-    try {
-      await output.parseOutput({ text: `{ "content": 123 }` }, context);
-      fail('must throw error');
-    } catch (error) {
-      verifyNoObjectGeneratedError(error, {
-        message: 'No object generated: response did not match schema.',
-        response: context.response,
-        usage: context.usage,
-        finishReason: context.finishReason,
-      });
-    }
+    it('should throw NoObjectGeneratedError when schema validation fails', async () => {
+      try {
+        await output1.parseOutput({ text: `{ "content": 123 }` }, context);
+        fail('must throw error');
+      } catch (error) {
+        verifyNoObjectGeneratedError(error, {
+          message: 'No object generated: response did not match schema.',
+          response: context.response,
+          usage: context.usage,
+          finishReason: context.finishReason,
+        });
+      }
+    });
   });
 });
