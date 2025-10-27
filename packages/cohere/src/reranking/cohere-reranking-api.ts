efd7e32a4de8c147e0eb9a5a4bd64f6a2b5b5d0e
Lars Grammel <lars.grammel@gmail.com>
2025-10-27 12:48:40 +0100

    rm id

--- packages/cohere/src/reranking/cohere-reranking-api.ts
+++ packages/cohere/src/reranking/cohere-reranking-api.ts
@@ -11,12 +11,9 @@ export type CohereRerankingInput = {
   priority: number | undefined;
 };
 
-// minimal version of the schema, focussed on what is needed for the implementation
-// this approach limits breakages when the API changes and increases efficiency
 export const cohereRerankingResponseSchema = lazySchema(() =>
   zodSchema(
     z.object({
-      id: z.string(),
       results: z.array(
         z.object({
           index: z.number(),
