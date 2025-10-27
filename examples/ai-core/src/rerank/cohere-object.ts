6a62d6c3c182bccef0d1a9bcbb94c9399b39ec8a
Lars Grammel <lars.grammel@gmail.com>
2025-10-27 15:11:50 +0100

    ref

--- examples/ai-core/src/rerank/cohere-object.ts
+++ examples/ai-core/src/rerank/cohere-object.ts
@@ -2,52 +2,7 @@ import { cohere, CohereRerankingOptions } from '@ai-sdk/cohere';
 import { rerank } from 'ai';
 import { run } from '../lib/run';
 import { print } from '../lib/print';
-
-const documents = [
-  {
-    from: 'Paul Doe <paul_fake_doe@oracle.com>',
-    to: ['Steve <steve@me.com>', 'lisa@example.com'],
-    date: '2024-03-27',
-    subject: 'Follow-up',
-    text: 'We are happy to give you the following pricing for your project.',
-  },
-  {
-    from: 'John McGill <john_fake_mcgill@microsoft.com>',
-    to: ['Steve <steve@me.com>'],
-    date: '2024-03-28',
-    subject: 'Missing Information',
-    text: 'Sorry, but here is the pricing you asked for for the newest line of your models.',
-  },
-  {
-    from: 'John McGill <john_fake_mcgill@microsoft.com>',
-    to: ['Steve <steve@me.com>'],
-    date: '2024-02-15',
-    subject: 'Commited Pricing Strategy',
-    text: 'I know we went back and forth on this during the call but the pricing for now should follow the agreement at hand.',
-  },
-  {
-    from: 'Generic Airline Company<no_reply@generic_airline_email.com>',
-    to: ['Steve <steve@me.com>'],
-    date: '2023-07-25',
-    subject: 'Your latest flight travel plans',
-    text: 'Thank you for choose to fly Generic Airline Company. Your booking status is confirmed.',
-  },
-  {
-    from: 'Generic SaaS Company<marketing@generic_saas_email.com>',
-    to: ['Steve <steve@me.com>'],
-    date: '2024-01-26',
-    subject:
-      'How to build generative AI applications using Generic Company Name',
-    text: 'Hey Steve! Generative AI is growing so quickly and we know you want to build fast!',
-  },
-  {
-    from: 'Paul Doe <paul_fake_doe@oracle.com>',
-    to: ['Steve <steve@me.com>', 'lisa@example.com'],
-    date: '2024-04-09',
-    subject: 'Price Adjustment',
-    text: "Re: our previous correspondence on 3/27 we'd like to make an amendment on our pricing proposal. We'll have to decrease the expected base price by 5%.",
-  },
-];
+import { documents } from './documents';
 
 run(async () => {
   const result = await rerank({
