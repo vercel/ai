8e38686dbf51f9d9329d581b92d6d1d72b5843cb
Lars Grammel <lars.grammel@gmail.com>
2025-10-27 13:40:35 +0100

    x

--- packages/ai/src/test/mock-reranking-model-v3.ts
+++ packages/ai/src/test/mock-reranking-model-v3.ts
@@ -6,24 +6,20 @@ export class MockRerankingModelV3 implements RerankingModelV3 {
 
   readonly provider: RerankingModelV3['provider'];
   readonly modelId: RerankingModelV3['modelId'];
-  readonly maxDocumentsPerCall: RerankingModelV3['maxDocumentsPerCall'];
 
   doRerank: RerankingModelV3['doRerank'];
 
   constructor({
     provider = 'mock-provider',
     modelId = 'mock-model-id',
-    maxDocumentsPerCall = undefined,
     doRerank = notImplemented,
   }: {
     provider?: RerankingModelV3['provider'];
     modelId?: RerankingModelV3['modelId'];
-    maxDocumentsPerCall?: RerankingModelV3['maxDocumentsPerCall'];
     doRerank?: RerankingModelV3['doRerank'];
   } = {}) {
     this.provider = provider;
     this.modelId = modelId;
-    this.maxDocumentsPerCall = maxDocumentsPerCall;
     this.doRerank = doRerank;
   }
 }
