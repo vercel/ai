80309682ec35878f09f0a61cb45260d662cc6ede
Aayush Kapoor <aayushkapoor34@gmail.com>
2025-10-30 18:24:48 -0400

    api mismatch fix

--- examples/next-openai/app/test-openai-code-interpreter-annotation-download/page.tsx
+++ examples/next-openai/app/test-openai-code-interpreter-annotation-download/page.tsx
@@ -10,7 +10,7 @@ export default function TestOpenAIWebSearch() {
   const { status, sendMessage, messages } =
     useChat<OpenAICodeInterpreterMessage>({
       transport: new DefaultChatTransport({
-        api: '/api/chat-openai-code-interpreter',
+        api: '/api/chat-openai-code-interpreter-annotation-download',
       }),
     });
 
