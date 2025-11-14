7a2abb955d27084b98bc4959ae70fa5059865577
Daniel Amitay <damitay@atmasciences.com>
2025-11-14 17:11:59 -0500

    prettier-fix

--- examples/ai-core/src/stream-text/google-vertex.ts
+++ examples/ai-core/src/stream-text/google-vertex.ts
@@ -18,7 +18,10 @@ async function main() {
   console.log('Finish reason:', await result.finishReason);
 
   const usageMetadata = (await result.providerMetadata)?.google?.usageMetadata;
-  const trafficType = usageMetadata instanceof Object ? (usageMetadata as { trafficType?: string | undefined }).trafficType : undefined;
+  const trafficType =
+    usageMetadata instanceof Object
+      ? (usageMetadata as { trafficType?: string | undefined }).trafficType
+      : undefined;
   console.log('Traffic type:', trafficType);
 }
 
