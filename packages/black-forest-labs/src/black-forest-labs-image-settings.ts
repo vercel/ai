f0be28454c096eff29b2932b1bf17fbde21c3b87
R-Taneja <47066511+R-Taneja@users.noreply.github.com>
2025-11-14 13:23:49 -0800

    Add BFL tools

--- packages/black-forest-labs/src/black-forest-labs-image-settings.ts
+++ packages/black-forest-labs/src/black-forest-labs-image-settings.ts
@@ -3,6 +3,7 @@ export type BlackForestLabsImageModelId =
   | 'flux-kontext-max'
   | 'flux-pro-1.1-ultra'
   | 'flux-pro-1.1'
+  | 'flux-pro-1.0-fill'
   | (string & {});
 
 export type BlackForestLabsAspectRatio = `${number}:${number}`;
