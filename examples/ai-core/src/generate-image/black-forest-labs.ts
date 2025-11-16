4c417d501d7fa1c480f4a9a6e8d0a82c71ff337e
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2025-11-16 09:06:33 -0800

    simplify example and use providerOptions with statisfies

--- examples/ai-core/src/generate-image/black-forest-labs.ts
+++ examples/ai-core/src/generate-image/black-forest-labs.ts
@@ -1,30 +1,19 @@
-import { createBlackForestLabs } from '@ai-sdk/black-forest-labs';
+import { BlackForestLabsImageProviderOptions, blackForestLabs } from '@ai-sdk/black-forest-labs';
 import { experimental_generateImage as generateImage } from 'ai';
 import { presentImages } from '../lib/present-image';
 import { run } from '../lib/run';
 
-const blackForestLabs = createBlackForestLabs({
-  fetch: async (...args) => {
-    console.log('Fetching:', ...args);
-    const response = await fetch(...args);
-    const responseBody = await response.clone().text();
-    try {
-      const body = JSON.parse(responseBody);
-      console.log('Response body:', body);
-    } catch {
-      console.log('non-json response');
-    }
-    return response;
-  },
-});
-
 run(async () => {
   const response = await generateImage({
     model: blackForestLabs.image('flux-pro-1.1'),
-    n: 2,
     prompt:
       'A cat wearing an intricate robe while gesticulating wildly, in the style of 80s pop art',
     aspectRatio: '1:1',
+    providerOptions: {
+      blackForestLabs: {
+        output_format: 'png'
+      } satisfies BlackForestLabsImageProviderOptions,
+    }
   });
   await presentImages(response.images);
 
