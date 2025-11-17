9af9437937e17db2d863551c4977d431e72faf16
R-Taneja <47066511+R-Taneja@users.noreply.github.com>
2025-11-17 13:06:56 -0800

    Update gateway image model settings

--- packages/gateway/src/gateway-image-model-settings.ts
+++ packages/gateway/src/gateway-image-model-settings.ts
@@ -1,1 +1,8 @@
-export type GatewayImageModelId = 'google/imagen-4.0-generate' | (string & {});
+export type GatewayImageModelId =
+  | 'google/imagen-4.0-generate'
+  | 'bfl/flux-kontext-max'
+  | 'bfl/flux-kontext-pro'
+  | 'bfl/flux-pro-1.0-fill'
+  | 'bfl/flux-pro-1.1'
+  | 'bfl/flux-pro-1.1-ultra'
+  | (string & {});
