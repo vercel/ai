a3e91c9147c2d324dbcad6a527c77fbc0d6c87b7
Hayden Bleasel <hello@haydenbleasel.com>
2025-11-23 13:36:46 -0800

    Update Tailwind configs

--- examples/next-openai/tailwind.config.js
+++ examples/next-openai/tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/streamdown/dist/*.js',
   ],
   theme: {
     extend: {
