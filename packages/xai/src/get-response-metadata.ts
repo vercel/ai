c9e2c43ff29cf3908cde2dcf8fc516183fc0a40c
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2025-11-22 21:19:12 -0800

    support both created and created_at

--- packages/xai/src/get-response-metadata.ts
+++ packages/xai/src/get-response-metadata.ts
@@ -1,15 +1,22 @@
 export function getResponseMetadata({
   id,
   model,
+  created,
   created_at,
 }: {
   id?: string | undefined | null;
+  created?: number | undefined | null;
   created_at?: number | undefined | null;
   model?: string | undefined | null;
 }) {
   return {
     id: id ?? undefined,
     modelId: model ?? undefined,
-    timestamp: created_at != null ? new Date(created_at * 1000) : undefined,
+    timestamp:
+      created != null
+        ? new Date(created * 1000)
+        : created_at != null
+          ? new Date(created_at * 1000)
+          : undefined,
   };
 }
