d76fceafd3c590a5e47425e442b96a8b601b12e0
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2025-11-22 21:08:41 -0800

    fix timestamp meta data

--- packages/xai/src/get-response-metadata.ts
+++ packages/xai/src/get-response-metadata.ts
@@ -1,15 +1,15 @@
 export function getResponseMetadata({
   id,
   model,
-  created,
+  created_at,
 }: {
   id?: string | undefined | null;
-  created?: number | undefined | null;
+  created_at?: number | undefined | null;
   model?: string | undefined | null;
 }) {
   return {
     id: id ?? undefined,
     modelId: model ?? undefined,
-    timestamp: created != null ? new Date(created * 1000) : undefined,
+    timestamp: created_at != null ? new Date(created_at * 1000) : undefined,
   };
 }
