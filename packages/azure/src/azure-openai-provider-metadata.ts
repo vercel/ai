061c9ba822a4c7c316832a76c118ee9e1dd0b667
tsuzaki430 <tsuzaki430@users.noreply.github.com>
2025-11-25 21:25:29 +0900

    remove Future Work comment

--- packages/azure/src/azure-openai-provider-metadata.ts
+++ packages/azure/src/azure-openai-provider-metadata.ts
@@ -4,12 +4,6 @@ import {
   responsesSourceDocumentProviderMetadataSchema,
 } from '@ai-sdk/openai/internal';
 
-/**
- * TODO:
- * After PR #10252 is merged, the keys of these Zod objects will need to be changed from `openai` to `azure`.
- * With Zod parsing, the provider can be identified more reliably, making it easier to determine which endpoint should be used.
- */
-
 // zod parse for text providerMetadata come from annotation(ex.code interpreter)
 export const azureResponsesOutputTextProviderMetadataSchema = z.object({
   azure: responsesOutputTextProviderMetadataSchema,
