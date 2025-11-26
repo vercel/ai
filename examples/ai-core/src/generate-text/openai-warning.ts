91fc76fbcd1b17ad2f3df94e41b0849568b92e8c
Lars Grammel <lars.grammel@gmail.com>
2025-11-26 12:17:14 +0100

    e

--- examples/ai-core/src/generate-text/openai-warning.ts
+++ examples/ai-core/src/generate-text/openai-warning.ts
@@ -1,18 +1,19 @@
 import { openai } from '@ai-sdk/openai';
-import { generateText, type Warning } from 'ai';
+import { generateText } from 'ai';
 import { run } from '../lib/run';
 
-// globalThis.AI_SDK_LOG_WARNINGS = false;
+globalThis.AI_SDK_LOG_WARNINGS = false;
 
-// globalThis.AI_SDK_LOG_WARNINGS = (warnings: Warning[]) => {
-//   console.log('WARNINGS:', warnings);
-// };
+globalThis.AI_SDK_LOG_WARNINGS = ({ warnings, provider, model }) => {
+  console.log('WARNINGS:', warnings, provider, model);
+};
 
 run(async () => {
   const result = await generateText({
     model: openai('gpt-5-nano'),
     prompt: 'Invent a new holiday and describe its traditions.',
     seed: 123, // causes warning with gpt-5-nano
+    maxOutputTokens: 1000,
   });
 
   console.log(result.text);
