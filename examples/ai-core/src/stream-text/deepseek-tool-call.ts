f913fa88404c024f61823c02f5ba07f1167d1b9f
Lars Grammel <lars.grammel@gmail.com>
2025-12-02 09:34:01 +0100

    e

--- examples/ai-core/src/stream-text/deepseek-tool-call.ts
+++ examples/ai-core/src/stream-text/deepseek-tool-call.ts
@@ -1,78 +1,23 @@
-import { deepseek } from '@ai-sdk/deepseek';
-import { streamText, ModelMessage, ToolCallPart, ToolResultPart } from 'ai';
+import { deepseek, DeepSeekChatOptions } from '@ai-sdk/deepseek';
+import { streamText } from 'ai';
 import 'dotenv/config';
+import { printFullStream } from '../lib/print-full-stream';
+import { run } from '../lib/run';
 import { weatherTool } from '../tools/weather-tool';
 
-const messages: ModelMessage[] = [];
-
-async function main() {
-  let toolResponseAvailable = false;
-
+run(async () => {
   const result = streamText({
-    model: deepseek('deepseek-chat'),
-    maxOutputTokens: 512,
-    tools: {
-      weather: weatherTool,
+    model: deepseek('deepseek-reasoner'),
+    tools: { weather: weatherTool },
+    providerOptions: {
+      deepseek: {
+        thinking: { type: 'enabled' },
+      } satisfies DeepSeekChatOptions,
     },
-    toolChoice: 'required',
-    prompt:
-      'What is the weather in San Francisco and what attractions should I visit?',
+    prompt: 'What is the weather in San Francisco?',
   });
 
-  let fullResponse = '';
-  const toolCalls: ToolCallPart[] = [];
-  const toolResponses: ToolResultPart[] = [];
+  printFullStream({ result });
 
-  for await (const delta of result.fullStream) {
-    switch (delta.type) {
-      case 'text-delta': {
-        fullResponse += delta.text;
-        process.stdout.write(delta.text);
-        break;
-      }
-
-      case 'tool-call': {
-        toolCalls.push(delta);
-
-        process.stdout.write(
-          `\nTool call: '${delta.toolName}' ${JSON.stringify(delta.input)}`,
-        );
-        break;
-      }
-
-      case 'tool-result': {
-        if (delta.dynamic) {
-          continue;
-        }
-
-        const transformedDelta: ToolResultPart = {
-          ...delta,
-          output: { type: 'json', value: delta.output },
-        };
-        toolResponses.push(transformedDelta);
-
-        process.stdout.write(
-          `\nTool response: '${delta.toolName}' ${JSON.stringify(
-            delta.output,
-          )}`,
-        );
-        break;
-      }
-    }
-  }
-  process.stdout.write('\n\n');
-
-  messages.push({
-    role: 'assistant',
-    content: [{ type: 'text', text: fullResponse }, ...toolCalls],
-  });
-
-  if (toolResponses.length > 0) {
-    messages.push({ role: 'tool', content: toolResponses });
-  }
-
-  toolResponseAvailable = toolCalls.length > 0;
-  console.log('Messages:', messages[0].content);
-}
-
-main().catch(console.error);
+  // TODO 2nd query
+});
