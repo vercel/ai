c5d17cb7b04768526d5847fcf4c637a5e3a1f9cd
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2025-12-11 20:36:43 -0800

    throw error without stack

--- packages/ai/src/prompt/wrap-gateway-error.ts
+++ packages/ai/src/prompt/wrap-gateway-error.ts
@@ -14,18 +14,18 @@ export function wrapGatewayError(error: unknown): unknown {
     });
   }
 
-  console.log(
-    '\n\u001b[1m\u001b[31mError: Unauthenticated request to AI Gateway.\u001b[0m\n',
-  );
-  console.log(
-    'To authenticate, set the \u001b[33mAI_GATEWAY_API_KEY\u001b[0m environment variable with your API key.\n',
-  );
-  console.log(
-    'Alternatively, you can configure and use a provider module instead of the AI Gateway.\n',
-  );
-  console.log(
-    'Learn more: \u001b[34mhttps://vercel.link/unauthenticated-ai-gateway-v6\u001b[0m\n',
-  );
+  const message = `
+\u001b[1m\u001b[31mError: Unauthenticated request to AI Gateway.\u001b[0m
 
-  process.exit(1);
+To authenticate, set the \u001b[33mAI_GATEWAY_API_KEY\u001b[0m environment variable with your API key.
+
+Alternatively, you can configure and use a provider module instead of the AI Gateway.
+
+Learn more: \u001b[34mhttps://vercel.link/unauthenticated-ai-gateway-v6\u001b[0m
+`);
+
+  throw Object.assign(new AISDKError({
+    name: 'GatewayError',
+    message,
+  }), { stack: ''});
 }
