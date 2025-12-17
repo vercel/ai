5bbab948d562dbca47a6a0b3eb94dcfeedf45e87
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2025-12-16 22:19:56 -0800

    comment formatting

--- packages/provider/src/image-model/v3/image-model-v3-file.ts
+++ packages/provider/src/image-model/v3/image-model-v3-file.ts
@@ -1,27 +1,27 @@
 import { SharedV3ProviderMetadata } from '../../shared';
 
 /**
-An image file that can be used for image editing or variation generation.
+ * An image file that can be used for image editing or variation generation.
  */
 export type ImageModelV3File =
   | {
       type: 'file';
 
       /**
-The IANA media type of the file, e.g. `image/png`. Any string is supported.
-
-@see https://www.iana.org/assignments/media-types/media-types.xhtml
+       * The IANA media type of the file, e.g. `image/png`. Any string is supported.
+       *
+       * @see https://www.iana.org/assignments/media-types/media-types.xhtml
        */
       mediaType: string;
 
       /**
-Generated file data as base64 encoded strings or binary data.
-
-The file data should be returned without any unnecessary conversion.
-If the API returns base64 encoded strings, the file data should be returned
-as base64 encoded strings. If the API returns binary data, the file data should
-be returned as binary data.
- */
+       * Generated file data as base64 encoded strings or binary data.
+       *
+       * The file data should be returned without any unnecessary conversion.
+       * If the API returns base64 encoded strings, the file data should be returned
+       * as base64 encoded strings. If the API returns binary data, the file data should
+       * be returned as binary data.
+       */
       data: string | Uint8Array;
 
       /**
