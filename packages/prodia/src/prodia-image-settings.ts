a72bb4bd166249964271d9466d32010c2be175bd
R-Taneja <47066511+R-Taneja@users.noreply.github.com>
2025-12-24 11:23:10 -0500

    Remove v1 models

--- packages/prodia/src/prodia-image-settings.ts
+++ packages/prodia/src/prodia-image-settings.ts
@@ -4,7 +4,4 @@
 export type ProdiaImageModelId =
   | 'inference.flux-fast.schnell.txt2img.v2'
   | 'inference.flux.schnell.txt2img.v2'
-  | 'inference.flux.dev.txt2img.v1'
-  | 'inference.flux.schnell.img2img.v1'
-  | 'inference.flux-2.flex.img2img.v1'
   | (string & {});
