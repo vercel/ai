624c337bfdb9782ebd442fb49f7f5ad6ddd3c856
Rohan Taneja <47066511+R-Taneja@users.noreply.github.com>
2026-01-05 14:47:05 -0800

    Backport conflicts for PR #11412 to release-v5.0

---
+++ packages/prodia/src/index.ts
@@ -0,0 +1,5 @@
+export type { ProdiaImageProviderOptions } from './prodia-image-model';
+export type { ProdiaImageModelId } from './prodia-image-settings';
+export type { ProdiaProvider, ProdiaProviderSettings } from './prodia-provider';
+export { createProdia, prodia } from './prodia-provider';
+export { VERSION } from './version';
