dc620f8873a9a7a3f4b565a6742043db26eb02ba
Lars Grammel <lars.grammel@gmail.com>
2026-01-21 16:20:11 +0100

    c

---
+++ examples/ai-functions/src/stream-text/lmstudio-chatbot.ts
@@ -0,0 +1,88 @@
+import { createOpenResponses } from '@ai-sdk/open-responses';
+import { stepCountIs, ModelMessage, streamText, APICallError } from 'ai';
+import * as readline from 'node:readline/promises';
+import { run } from '../lib/run';
+import { weatherTool } from '../tools/weather-tool';
+
+const lmstudio = createOpenResponses({
+  name: 'lmstudio',
+  url: 'http://localhost:1234/v1/responses',
+});
+
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const messages: ModelMessage[] = [];
+
+run(async () => {
+  while (true) {
+    const userInput = await terminal.question('You: ');
+
+    messages.push({ role: 'user', content: userInput });
+
+    const result = streamText({
+      model: lmstudio('zai-org/glm-4.7-flash'),
+      tools: { weatherTool },
+      system: `You are a helpful, respectful and honest assistant.`,
+      stopWhen: stepCountIs(5),
+      messages,
+      onError: ({ error }) => {
+        console.log('onError');
+        console.error(error);
+
+        if (APICallError.isInstance(error)) {
+          console.error(JSON.stringify(error.requestBodyValues, null, 2));
+        }
+      },
+    });
+
+    process.stdout.write('\nAssistant: ');
+    for await (const chunk of result.fullStream) {
+      switch (chunk.type) {
+        case 'tool-input-start':
+          process.stdout.write('\x1b[33m');
+          console.log('Tool call:', chunk.toolName);
+          process.stdout.write('Tool args: ');
+          break;
+
+        case 'tool-input-delta':
+          process.stdout.write(chunk.delta);
+          break;
+
+        case 'tool-input-end':
+          console.log();
+          break;
+
+        case 'tool-result':
+          console.log('Tool result:', chunk.output);
+          process.stdout.write('\x1b[0m');
+          break;
+
+        case 'tool-error':
+          process.stdout.write('\x1b[0m');
+          process.stderr.write('\x1b[31m');
+          console.error('Tool error:', chunk.error);
+          process.stderr.write('\x1b[0m');
+          break;
+
+        case 'text-start':
+          process.stdout.write('\x1b[32m');
+          break;
+
+        case 'text-delta':
+          process.stdout.write(chunk.text);
+          break;
+
+        case 'text-end':
+          process.stdout.write('\x1b[0m');
+          console.log();
+          break;
+      }
+    }
+    process.stdout.write('\n\n');
+
+    messages.push(...(await result.response).messages);
+  }
+});
