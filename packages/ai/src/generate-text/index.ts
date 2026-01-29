ffbab80934727500b474def73c520f0e09bb97d4
Felix Arntz <felix.arntz@vercel.com>
2026-01-29 13:24:22 -0600

    update todo

--- packages/ai/src/generate-text/index.ts
+++ packages/ai/src/generate-text/index.ts
@@ -7,7 +7,7 @@ export type { ContentPart } from './content-part';
 export type { GenerateTextResult } from './generate-text-result';
 export {
   DefaultGeneratedFile,
-  type GeneratedFile as Experimental_GeneratedImage, // Image for backwards compatibility, TODO remove in v5
+  type GeneratedFile as Experimental_GeneratedImage, // Image for backwards compatibility, TODO remove in v7
   type GeneratedFile,
 } from './generated-file';
 export * as Output from './output';
