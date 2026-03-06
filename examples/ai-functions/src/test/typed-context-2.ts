920adb5aac8ffde76ff381fbfb1525656dfea30e
Lars Grammel <lars.grammel@gmail.com>
2026-03-06 15:50:43 +0100

    x

---
+++ examples/ai-functions/src/test/typed-context-2.ts
@@ -0,0 +1,72 @@
+import { FlexibleSchema } from 'ai';
+import { run } from '../lib/run';
+import { z } from 'zod';
+
+interface Tool<INPUT = any, CONTEXT = any> {
+  inputSchema: FlexibleSchema<INPUT>;
+  contextSchema: FlexibleSchema<CONTEXT>;
+  execute: (input: NoInfer<INPUT>, context: NoInfer<CONTEXT>) => unknown;
+}
+
+function tool<INPUT, CONTEXT>(options: {
+  inputSchema: FlexibleSchema<INPUT>;
+  contextSchema: FlexibleSchema<CONTEXT>;
+  execute: (input: NoInfer<INPUT>, context: NoInfer<CONTEXT>) => unknown;
+}) {
+  return options;
+}
+
+type InferToolInput<TOOL extends Tool> =
+  TOOL extends Tool<infer INPUT, any> ? INPUT : never;
+type InferToolContext<TOOL extends Tool> =
+  TOOL extends Tool<any, infer CONTEXT> ? CONTEXT : never;
+
+export type ToolSet = Record<string, Tool<any, any>>;
+
+// should be a union of all the context types of the tools
+type InferToolSetContext<TOOLS extends ToolSet> = {
+  [K in keyof TOOLS]: InferToolContext<TOOLS[K]>;
+}[keyof TOOLS];
+
+function executeTool<CONTEXT, TOOLS extends ToolSet>({
+  tools,
+  toolName,
+  input,
+  context,
+}: {
+  tools: TOOLS;
+  toolName: keyof TOOLS;
+  input: InferToolInput<TOOLS[typeof toolName]>;
+  context: InferToolSetContext<TOOLS>;
+}) {
+  const tool = tools[toolName];
+  return tool.execute(input, context);
+}
+
+run(async () => {
+  const tool1 = tool({
+    inputSchema: z.object({ input1: z.string() }),
+    contextSchema: z.object({ context1: z.number() }),
+    execute: async ({ input1 }, { context1 }) => {
+      console.log(input1, context1);
+    },
+  });
+
+  const tool2 = tool({
+    inputSchema: z.object({ input2: z.number() }),
+    contextSchema: z.object({ context2: z.string() }),
+    execute: async ({ input2 }, { context2 }) => {
+      console.log(input2, context2);
+    },
+  });
+
+  executeTool({
+    tools: {
+      tool1,
+      tool2,
+    },
+    toolName: 'tool1',
+    input: { input1: 'Hello' },
+    context: { context1: 1, context2: 'Hello' },
+  });
+});
