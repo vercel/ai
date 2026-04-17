e484c5ead1b3f6c68b2b861b6be57be01d524c6a
Lars Grammel <lars.grammel@gmail.com>
2026-04-17 15:18:02 +0200

    2

--- packages/ai/src/util/merge-abort-signals.ts
+++ packages/ai/src/util/merge-abort-signals.ts
@@ -22,22 +22,5 @@ export function mergeAbortSignals(
     return validSignals[0];
   }
 
-  const controller = new AbortController();
-
-  for (const signal of validSignals) {
-    if (signal.aborted) {
-      controller.abort(signal.reason);
-      return controller.signal;
-    }
-
-    signal.addEventListener(
-      'abort',
-      () => {
-        controller.abort(signal.reason);
-      },
-      { once: true },
-    );
-  }
-
-  return controller.signal;
+  return AbortSignal.any(validSignals);
 }
