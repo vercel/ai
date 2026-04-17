8b92e4fa3427d0b6e46bd051b731a82731748ec3
Lars Grammel <lars.grammel@gmail.com>
2026-04-17 14:47:13 +0200

    feat(ai): concurrent event notification

--- packages/ai/src/util/notify.ts
+++ packages/ai/src/util/notify.ts
@@ -2,16 +2,18 @@ import { Arrayable, asArray } from '@ai-sdk/provider-utils';
 import type { Callback } from './callback';
 
 /**
- * Notifies all provided callbacks with the given event.
+ * Notifies all provided callbacks with the given event in parallel.
  * Errors in callbacks do not break the generation flow.
  */
 export async function notify<EVENT>(options: {
   event: EVENT;
   callbacks?: Arrayable<Callback<EVENT> | undefined | null>;
 }): Promise<void> {
-  for (const callback of asArray(options.callbacks)) {
-    try {
-      await callback?.(options.event);
-    } catch {}
-  }
+  await Promise.all(
+    asArray(options.callbacks).map(async callback => {
+      try {
+        await callback?.(options.event);
+      } catch {}
+    }),
+  );
 }
