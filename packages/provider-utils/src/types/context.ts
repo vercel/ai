30ffaf893865e45e1dc0cc47d8c2c0b15622d61e
Lars Grammel <lars.grammel@gmail.com>
2026-04-27 16:56:29 +0200

    1

--- packages/provider-utils/src/types/context.ts
+++ packages/provider-utils/src/types/context.ts
@@ -2,3 +2,10 @@
  * A context object that is passed into tool execution.
  */
 export type Context = Record<string, unknown>;
+
+/**
+ * Top-level context properties that contain sensitive data.
+ */
+export type SensitiveContext<CONTEXT extends Context> =
+  | { [KEY in keyof CONTEXT]?: boolean }
+  | undefined;
