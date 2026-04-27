8d05f618b135e919cb7b2ea9584ea0a7e8477973
Lars Grammel <lars.grammel@gmail.com>
2026-04-27 16:58:19 +0200

    2

--- packages/provider-utils/src/types/context.ts
+++ packages/provider-utils/src/types/context.ts
@@ -9,3 +9,26 @@ export type Context = Record<string, unknown>;
 export type SensitiveContext<CONTEXT extends Context> =
   | { [KEY in keyof CONTEXT]?: boolean }
   | undefined;
+
+type SensitiveContextKeys<
+  CONTEXT extends Context,
+  SENSITIVE_CONTEXT extends SensitiveContext<CONTEXT>,
+> = SENSITIVE_CONTEXT extends undefined
+  ? never
+  : {
+      [KEY in keyof CONTEXT]: NonNullable<SENSITIVE_CONTEXT> extends {
+        [K in KEY]?: infer VALUE;
+      }
+        ? VALUE extends true
+          ? KEY
+          : never
+        : never;
+    }[keyof CONTEXT];
+
+/**
+ * A context object with sensitive top-level properties removed.
+ */
+export type RestrictedContext<
+  CONTEXT extends Context,
+  SENSITIVE_CONTEXT extends SensitiveContext<CONTEXT>,
+> = Omit<CONTEXT, SensitiveContextKeys<CONTEXT, SENSITIVE_CONTEXT>>;
