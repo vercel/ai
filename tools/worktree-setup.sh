82c55eefde6b801a15f96c4ea90cc6bb75c804c6
Felix Arntz <felix.arntz@vercel.com>
2026-05-04 12:23:27 -0500

    rename for clarity

--- tools/worktree-setup.sh
+++ tools/worktree-setup.sh
@@ -1,5 +1,5 @@
 # Path to the main worktree root.
-ROOT=$(git worktree list | head -1 | awk '{print $1}')
+MAIN_WORKTREE=$(git worktree list | head -1 | awk '{print $1}')
 
 # Path to this worktree root.
 WORKTREE_ROOT=$(git rev-parse --show-toplevel)
@@ -9,19 +9,19 @@ if [ "$(pwd)" != "$WORKTREE_ROOT" ]; then
   exit 1
 fi
 
-if [ "$(pwd)" = "$ROOT" ]; then
-  echo "Must be run outside the main worktree ($ROOT)" >&2
+if [ "$(pwd)" = "$MAIN_WORKTREE" ]; then
+  echo "Must be run outside the main worktree ($MAIN_WORKTREE)" >&2
   exit 1
 fi
 
 # Link .env files.
-[ -f "$ROOT/.env" ] && ln -sf "$ROOT/.env" ./
+[ -f "$MAIN_WORKTREE/.env" ] && ln -sf "$MAIN_WORKTREE/.env" ./
 if [ ! -d ./examples/ai-functions/ ]; then
   AI_FUNCTIONS_DIR=./examples/ai-core/
 else
   AI_FUNCTIONS_DIR=./examples/ai-functions/
 fi
-[ -f "$ROOT/examples/ai-functions/.env" ] && ln -sf "$ROOT/examples/ai-functions/.env" "$AI_FUNCTIONS_DIR"
-[ -f "$ROOT/examples/ai-e2e-next/.env" ] && ln -sf "$ROOT/examples/ai-e2e-next/.env" ./examples/ai-e2e-next/
+[ -f "$MAIN_WORKTREE/examples/ai-functions/.env" ] && ln -sf "$MAIN_WORKTREE/examples/ai-functions/.env" "$AI_FUNCTIONS_DIR"
+[ -f "$MAIN_WORKTREE/examples/ai-e2e-next/.env" ] && ln -sf "$MAIN_WORKTREE/examples/ai-e2e-next/.env" ./examples/ai-e2e-next/
 
 pnpm install
