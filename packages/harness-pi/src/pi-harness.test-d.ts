19b1993b5e5ece9860791f90d5f47e6bb29fd6ff
Jeff Barg <jeff.barg@clay.com>
2026-07-21 16:01:19 -0400

    feat(harness-pi): support inline extension factories

---
+++ packages/harness-pi/src/pi-harness.test-d.ts
@@ -0,0 +1,16 @@
+import { expectTypeOf, test } from 'vitest';
+import { createPi, type PiHarnessSettings } from './index';
+
+test('PiHarnessSettings accepts readonly extension factory arrays', () => {
+  const extensionFactories = [
+    pi => {
+      pi.on('agent_start', () => {});
+    },
+  ] as const satisfies NonNullable<PiHarnessSettings['extensionFactories']>;
+  const settings: PiHarnessSettings = { extensionFactories };
+
+  expectTypeOf(settings.extensionFactories).toEqualTypeOf<
+    PiHarnessSettings['extensionFactories']
+  >();
+  createPi(settings);
+});
